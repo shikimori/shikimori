8796d9f5f204a79c0e66018484e32554d23db51d
blackchestnut <alexander.kalinichev@gmail.com>
2013-11-17 12:27:43 +0400

    [#30] группировка по эпизодам с пагинацией

--- app/decorators/anime_video_decorator.rb
+++ app/decorators/anime_video_decorator.rb
@@ -1,12 +1,33 @@
 class AnimeVideoDecorator < Draper::Decorator
   delegate_all
 
+  def name
+    if russian
+      "#{object.russian} / #{object.name}"
+    else
+      object.name
+    end
+  end
+
   def description
     if object.description_html.blank?
-      format_html_text object.description_mal
+      h.format_html_text object.description_mal
     else
       object.description_html
     end
+  end
+
+  def current_episode
+    @current_episode ||= [h.params[:episode_id].to_i, 1].max
+  end
+
+  def videos
+    #anime_videos.sort_by {|v| v.episode }
+    @video ||= anime_videos.group_by {|v| v.episode}
+  end
+
+  def current_videos
+    videos[current_episode]
   end
 
   def comments
