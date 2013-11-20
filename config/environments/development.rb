d1a32b82d8d107f50807c490a0fd55582e841fed
blackchestnut <alexander.kalinichev@gmail.com>
2013-11-20 21:10:14 +0400

    восстановил настройки ассетов

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,16 +59,16 @@ Site::Application.configure do
   end
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  #config.serve_static_assets = false
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  #config.assets.compress = true
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  #config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  #config.assets.digest = true
+  config.assets.digest = true
 
 
   #Debugger.start_remote
