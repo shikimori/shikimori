1de50fa6277469e5e23a36143558ee85684d0812
blackchestnut <alexander.kalinichev@gmail.com>
2013-11-26 23:58:23 +0400

    убрал описание к видео если текущий эпизод не 1й

--- app/decorators/anime_video_decorator.rb
+++ app/decorators/anime_video_decorator.rb
@@ -1,6 +1,16 @@
 class AnimeVideoDecorator < AnimeVideoPreviewDecorator
   delegate_all
 
+  def description
+    return if current_episode > 1
+
+    if object.description_html.blank?
+      h.format_html_text object.description_mal
+    else
+      object.description_html
+    end
+  end
+
   def current_episode
     @current_episode ||= [h.params[:episode_id].to_i, 1].max
   end
