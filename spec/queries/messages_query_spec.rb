16b9a4f793604f150ff018053bc649428ca09c95
Andrey Sidorov <takandar@gmail.com>
2013-12-01 20:28:04 +0400

    messages query

---
+++ spec/queries/messages_query_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe MessagesQuery do
+  before { Message.antispam = false }
+  subject { query.fetch 1, 1 }
+  let(:query) { MessagesQuery.new user, type }
+
+  let(:user) { build_stubbed :user }
+  let(:user_2) { build_stubbed :user }
+  let!(:private) { create :message, kind: MessageType::Private, dst: user, src: user_2 }
+  let!(:sent) { create :message, kind: MessageType::Private, dst: user_2, src: user }
+  let!(:news) { create :message, kind: MessageType::Anons, dst: user, src: user_2 }
+  let!(:notification) { create :message, kind: MessageType::FriendRequest, dst: user, src: user_2, read: true }
+
+  describe :fetch do
+    describe :inbox do
+      let!(:private_2) { create :message, kind: MessageType::Private, dst: user, src: user_2, dst_del: true }
+      let(:type) { :inbox }
+
+      it { should have(1).item }
+      its(:first) { should eq private }
+    end
+
+    describe :sent do
+      let!(:sent_2) { create :message, kind: MessageType::Private, dst: user_2, src: user, src_del: true }
+      let(:type) { :sent }
+
+      it { should have(1).item }
+      its(:first) { should eq sent }
+    end
+
+    describe :news do
+      let(:type) { :news }
+      it { should have(1).item }
+      its(:first) { should eq news }
+    end
+
+    describe :notifications do
+      let!(:notification_2) { create :message, kind: MessageType::GroupRequest, dst: user, src: user_2 }
+      let!(:notification_3) { create :message, kind: MessageType::GroupRequest, dst: user, src: user_2 }
+      let(:type) { :notifications }
+
+      it { should have(2).item }
+      its(:first) { should eq notification_2 }
+    end
+  end
+end
