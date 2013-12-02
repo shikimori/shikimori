0a73eb1f9fab38935f57a3028b10c0726f1b800f
Andrey Sidorov <takandar@gmail.com>
2013-12-03 00:45:08 +0400

    картинки с mal теперь будем брать через прокси

--- lib/open_image.rb
+++ lib/open_image.rb
@@ -1,5 +1,5 @@
 module Kernel
-  def open_image(url)
+  def open_image url
     io = open url
     def io.original_filename; base_uri.path.split('/').last; end
     io.original_filename.blank? ? nil : io
