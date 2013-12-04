bed48897e96b2a74ac3ae2d2fc9817518dfe11b3
Andrey Sidorov <takandar@gmail.com>
2013-12-04 22:25:44 +0400

    bugfix

--- app/services/ambiguous_matcher.rb
+++ app/services/ambiguous_matcher.rb
@@ -18,7 +18,7 @@ private
   end
 
   def resolve_by_episodes entries, episodes
-    range = episodes > 5 ? (episodes-episodes/10)..(episodes+episodes/10) : episodes..episdoes
+    range = episodes > 5 ? (episodes-episodes/10)..(episodes+episodes/10) : episodes..episodes
     resolved = entries.select {|v| range.include? v.episodes }
     resolved.any? ? resolved : entries
   end
