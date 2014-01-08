31715b011b8763241f78aad5b495a552b77b021b
Andrey Sidorov <takandar@gmail.com>
2014-01-08 19:46:33 +0400

    багфикс сохранения настроек

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -530,7 +530,8 @@ private
         :anime_in_profile, :manga_in_profile, :manga_first, :clubs_in_profile,
         :statistics_in_profile, :comments_in_profile, :statistics_start_on,
         :page_background, :page_border, :body_background, :about_on_top, :about,
-        :show_hentai_images, :show_social_buttons, :show_smileys, :menu_contest
+        :show_hentai_images, :show_social_buttons, :show_smileys, :menu_contest,
+        :russian_genres, :russian_names, :mylist_in_catalog, :postload_in_catalog
       ]
     )
   end
