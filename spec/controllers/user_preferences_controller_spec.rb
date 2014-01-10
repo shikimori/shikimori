34b92d94d63a5fe1b375cd26978256b0b930deb5
Andrey Sidorov <takandar@gmail.com>
2014-01-11 02:58:00 +0400

    ещё багфикс настроек

--- spec/controllers/user_preferences_controller_spec.rb
+++ spec/controllers/user_preferences_controller_spec.rb
@@ -16,5 +16,10 @@ describe UserPreferencesController do
       specify { assigns(:user).preferences.body_background.should eq 'test2' }
       it { should redirect_to user_settings_url(user, page: :profile) }
     end
+
+    context 'about change' do
+      before { put :update, id: user.to_param, page: :profile, user_preferences: { body_background: 'test2' }, user: { about: 'zxc' } }
+      specify { assigns(:user).about.should eq 'zxc' }
+    end
   end
 end
