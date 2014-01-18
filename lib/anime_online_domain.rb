23df5af75498634d5b4485f6e8fdb863ea69e2a0
Andrey Sidorov <takandar@gmail.com>
2014-01-18 17:51:25 +0400

    пусть пока старый домен тоже работает, чтобы текущие вкладки в браузере не сломались

--- lib/anime_online_domain.rb
+++ lib/anime_online_domain.rb
@@ -1,6 +1,6 @@
 class AnimeOnlineDomain
   HOST = 'play.shikimori.org'
-  HOSTS = ['0.0.0.0', 'animeonline.dev', 'animeonline.production', HOST]
+  HOSTS = ['0.0.0.0', '178.63.23.138', 'animeonline.dev', 'animeonline.production', HOST]
 
   def self.matches? request
     HOSTS.include? request.host
