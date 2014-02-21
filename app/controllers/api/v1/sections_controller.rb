b6bf0d179b5790d68eaa4d37b2ec49a511030fe6
Andrey Sidorov <takandar@gmail.com>
2014-02-21 23:44:20 +0400

    добавил "фиктивные" разделы в апи разделов форума

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SectionsController < Api::V1::ApiController
   # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
   api :GET, "/sections", "List sections"
   def index
-    respond_with Section.all
+    respond_with Section.with_aggregated
   end
 end
 
