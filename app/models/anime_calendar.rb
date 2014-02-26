64a2f767928d8383e6a79b8cb9f6aed9ac71e99c
Andrey Sidorov <takandar@gmail.com>
2014-02-26 07:39:43 +0400

    ещё аниме в календарь

--- app/models/anime_calendar.rb
+++ app/models/anime_calendar.rb
@@ -110,14 +110,15 @@ class AnimeCalendar < ActiveRecord::Base
     query += " or id in (#{calendar.map {|v| v[:anime_id] }.compact.join(',')})"
 
     #animes = Anime.where(:id => 10464).where(query).inject({}) do |data,anime|
-    animes = (Anime.latest + Anime.ongoing.where(query).to_a + Anime.anons.where(query).to_a).select {|v| v.kind == 'TV' || v.kind == 'ONA' || v.id == 15133 }.inject({}) do |data,anime|
-      data[self.hashname(anime.name)] = anime
-      data[anime.id] = anime
-      anime.synonyms.each {|v| data[self.hashname(v)] = anime } if anime.synonyms
-      anime.japanese.each {|v| data[self.hashname(v)] = anime } if anime.japanese
-      anime.english.each {|v| data[self.hashname(v)] = anime } if anime.english
-      data
-    end
+    animes = (Anime.latest + Anime.ongoing.where(query).to_a + Anime.anons.where(query).to_a)
+      .select {|v| v.kind == 'TV' || v.kind == 'ONA' || [15133, 19799].include?(v.id) }.inject({}) do |data,anime|
+        data[self.hashname(anime.name)] = anime
+        data[anime.id] = anime
+        anime.synonyms.each {|v| data[self.hashname(v)] = anime } if anime.synonyms
+        anime.japanese.each {|v| data[self.hashname(v)] = anime } if anime.japanese
+        anime.english.each {|v| data[self.hashname(v)] = anime } if anime.english
+        data
+      end
 
     cache = {}
     imported = Set.new
