33d1b35f6a8800b9dde625dbdefecc35b2a4d518
Andrey Sidorov <takandar@gmail.com>
2014-03-07 00:16:10 +0400

    багфикс верификатора аниме ссылок

--- app/workers/anime_links_verifier.rb
+++ app/workers/anime_links_verifier.rb
@@ -4,14 +4,14 @@ class AnimeLinksVerifier
                   retry: false
 
   def perform
-    raise "Ambiguous anime links found: #{bad_entries.join ', '}" if bad_entries.any?
+    raise "Ambiguous anime links found: #{bad_entries(FindAnimeImporter::SERVICE).join ', '}" if bad_entries(FindAnimeImporter::SERVICE).any?
+    raise "Ambiguous anime links found: #{bad_entries(HentaiAnimeImporter::SERVICE).join ', '}" if bad_entries(HentaiAnimeImporter::SERVICE).any?
   end
 
-  def bad_entries
+  def bad_entries service
     AnimeLink
-      .where(service: [FindAnimeImporter::SERVICE, HentaiAnimeImporter::SERVICE])
-      .all
-      .group_by {|v| v.anime_id }
+      .where(service: service)
+      .group_by(&:anime_id)
       .select {|k,v| v.size > 1 }
       .map {|k,v| "#{k} (#{v.map(&:identifier).join ', '})" }
   end
