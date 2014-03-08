b9f5eb160cba3ab08a9929b27243c0f0e8102f01
Andrey Sidorov <takandar@gmail.com>
2014-03-08 13:39:34 +0400

    thread_id в апи клубов

--- app/serialziers/group_profile_serializer.rb
+++ app/serialziers/group_profile_serializer.rb
@@ -1,5 +1,5 @@
 class GroupProfileSerializer < GroupSerializer
-  attributes :description, :description_html, :image, :mangas, :characters
+  attributes :description, :description_html, :image, :mangas, :characters, :thread_id
   has_many :members, :animes, :mangas, :characters, :images
 
   def image
@@ -9,5 +9,9 @@ class GroupProfileSerializer < GroupSerializer
       x73: object.logo.url(:x73),
       xo64: object.logo.url(:x48)
     }
+  end
+
+  def thread_id
+    object.thread.id
   end
 end
