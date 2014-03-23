78b0350e835e7f3ee59f19c7b8abee7f927a8c01
Andrey Sidorov <takandar@gmail.com>
2014-03-23 13:42:02 +0400

    косметические правки

--- app/workers/import_list_worker.rb
+++ app/workers/import_list_worker.rb
@@ -17,30 +17,30 @@ class ImportListWorker
         Object.const_get("#{klass.name}MalParser").new.fetch_list_pages(limit: pages_limit, url_getter: :all_catalog_url)
 
       when :anons
-        Anime.anons.select(:id).map(&:id)
+        Anime.anons.pluck(:id)
 
       when :ongoing
-        Anime.ongoing.select(:id).map(&:id)
+        Anime.ongoing.pluck(:id)
 
       when :latest
         Anime.latest.map(&:id)
     end
 
-    entries = klass.where("imported_at < date_add(now(), interval -#{hours_limit} hour)")
-                   .where(id: entry_ids)
-                   .select(:id)
-                   .map(&:id)
+    entries = klass
+      .where("imported_at < date_add(now(), interval -#{hours_limit} hour)")
+      .where(id: entry_ids)
+      .pluck(:id)
 
     roles = PersonRole.where("#{klass.name.downcase}_id".to_sym => entries)
-    characters = Character.where("imported_at < date_add(now(), interval -#{hours_limit*10} hour)")
-                          .where(id: roles.map(&:character_id).compact.uniq)
-                          .select(:id)
-                          .map(&:id)
+    characters = Character
+      .where("imported_at < date_add(now(), interval -#{hours_limit*10} hour)")
+      .where(id: roles.map(&:character_id).compact.uniq)
+      .pluck(:id)
 
-    people = Person.where("imported_at < date_add(now(), interval -#{hours_limit*10} hour)")
-                   .where(id: roles.map(&:person_id).compact.uniq)
-                   .select(:id)
-                   .map(&:id)
+    people = Person
+      .where("imported_at < date_add(now(), interval -#{hours_limit*10} hour)")
+      .where(id: roles.map(&:person_id).compact.uniq)
+      .pluck(:id)
 
     unless entries.empty?
       klass.connection.
