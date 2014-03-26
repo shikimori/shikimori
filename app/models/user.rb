8243af2b1822075a82e8843c87bf020ed0ea3ca3
blackchestnut <alexander.kalinichev@gmail.com>
2014-03-26 08:13:17 +0400

    Добавил 2й в доверительных загрузчиков видео
    мне показались адекватными и много загружают: 11883 - Tenno Haruka,
    5064 - Heretic

--- app/models/user.rb
+++ app/models/user.rb
@@ -126,7 +126,7 @@ class User < ActiveRecord::Base
   ContestsModerators = (Admins + [1483]).uniq # 1483 - Zula
   CosplayModerators = (Admins + [2043, 2046]).uniq # 2043 - laitqwerty, 2046 - Котейка
   VideoModerators = (Admins + []).uniq
-  TrustedVideoUploaders = (Admins + [11496, 4099, 12771, 13893]).uniq # 11496 - АлхимиК, 4099 - sttany, 12771 - spinosa, 13893 - const
+  TrustedVideoUploaders = (Admins + [11496, 4099, 12771, 13893, 11883, 5064]).uniq # 11496 - АлхимиК, 4099 - sttany, 12771 - spinosa, 13893 - const, 11883 - Tenno Haruka, 5064 - Heretic
 
   def self.new_with_session(params, session)
     super.tap do |user|
