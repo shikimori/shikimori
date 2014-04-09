7c962b6a7a8cf735563307979879e838b0bd0c6b
Andrey Sidorov <takandar@gmail.com>
2014-04-10 01:21:41 +0400

    очепятки

--- app/models/user.rb
+++ app/models/user.rb
@@ -401,7 +401,7 @@ private
       to_id: self.id,
       kind: MessageType::Notification,
       body: "Добро пожаловать.
-[url=http://shikimori.org/s/85018-FAQ-Chasto-zadavaemye-voprosy]Здесь[/url] находятся овтеты на наиболее часто задаваемые вопросы.
+[url=http://shikimori.org/s/85018-FAQ-Chasto-zadavaemye-voprosy]Здесь[/url] находятся ответы на наиболее часто задаваемые вопросы.
 Импортировать список аниме и манги из [url=http://myanimelist.net]myanimelist.net[/url] или [url=http://anime-planet.com]anime-planet.com[/url] можно в [url=/#{to_param}/settings]настройках профиля[/url]. Там же можно изменить свой никнейм.
 Перед постингом на форуме рекомендуем ознакомиться с [url=http://shikimori.org/s/79042-Pravila-sayta]правилами сайта[/url].
 
