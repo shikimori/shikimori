b3abbc3680f9d9b289345025060ce0db62eb4841
Andrey Sidorov <takandar@gmail.com>
2014-04-13 11:26:21 +0400

    чиню автобан одного пользователя

--- app/workers/prolongate_ban.rb
+++ app/workers/prolongate_ban.rb
@@ -3,6 +3,7 @@ class ProlongateBan
   sidekiq_options queue: :critical
 
   def perform user_id
+    return if user_id == 3581 # Ametrin - у него ip совпадает с одним троллем
     User.find_by_id(user_id).try :prolongate_ban
   end
 end
