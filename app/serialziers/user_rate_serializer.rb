8cd6d15fdeef16ae432aa85c5fb9214778dbdb10
Andrey Sidorov <takandar@gmail.com>
2014-05-06 22:02:44 +0400

    обновление user_Rate сериалайзера

--- app/serialziers/user_rate_serializer.rb
+++ app/serialziers/user_rate_serializer.rb
@@ -2,7 +2,16 @@ class UserRateSerializer < ActiveModel::Serializer
   attributes :id, :score, :status, :status_name, :episodes, :volumes, :chapters, :text, :text_html, :rewatches
 
   has_one :user
-  has_one :target
+  has_one :anime
+  has_one :manga
+
+  def anime
+    object.target.kind_of?(Anime) ? object.target : nil
+  end
+
+  def manga
+    object.target.kind_of?(Manga) ? object.target : nil
+  end
 
   def status_name
     UserRateStatus.get object.status
