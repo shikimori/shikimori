d4c4f27a31c17776d89df463cdce5a56477c3ac9
blackchestnut <alexander.kalinichev@gmail.com>
2014-06-03 22:08:48 +0400

    не пускаем дальше последней страницы

--- app/controllers/manga_online/chapters_controller.rb
+++ app/controllers/manga_online/chapters_controller.rb
@@ -3,7 +3,9 @@ class MangaOnline::ChaptersController < ApplicationController
 
   def show
     @chapter = MangaChapter.includes(:pages).find params[:id]
+    @chapters = MangaChapter.where(manga_id: @chapter.manga.id)
     @page = @chapter.pages.find_by(number: (params[:page_number].try(:to_i) || @chapter.pages.first.number))
+    redirect_to online_manga_chapter_show_url @chapter, @chapter.pages.last.number unless @page
   end
 end
 
