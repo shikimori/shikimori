d728b121e89207c9e87ec1fa7b381244204df41f
Andrey Sidorov <takandar@gmail.com>
2014-06-06 01:18:13 +0400

    чиню тултипы

--- app/assets/javascripts/jquery/jquery.tooltip.js
+++ app/assets/javascripts/jquery/jquery.tooltip.js
@@ -240,7 +240,7 @@
             tip = $(conf.tip).eq(0);
 
           // remote tooltip
-          } else if (trigger.data('href') || trigger.attr('href')) {
+          } else if (!trigger.data('local-tooltip') && (trigger.data('href') || trigger.attr('href'))) {
             tip = $(conf.defaultTemplate).addClass(conf.tipClass)
                                          .css('z-index', parseInt(trigger.parents('.tooltip').css('z-index')) || 1)
                                          .hide()
