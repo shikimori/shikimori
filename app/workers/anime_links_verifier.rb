29bd016930fe92a0a1e4d1d70302564508f3f366
Andrey Sidorov <takandar@gmail.com>
2014-06-14 01:09:55 +0400

    более корректная проверка идентификаторов аниме

--- app/workers/anime_links_verifier.rb
+++ app/workers/anime_links_verifier.rb
@@ -4,16 +4,23 @@ class AnimeLinksVerifier
                   retry: false
 
   def perform
-    raise "Ambiguous anime links found: #{bad_entries(FindAnimeImporter::SERVICE).join ', '}" if bad_entries(FindAnimeImporter::SERVICE).any?
-    raise "Ambiguous anime links found: #{bad_entries(HentaiAnimeImporter::SERVICE).join ', '}" if bad_entries(HentaiAnimeImporter::SERVICE).any?
+    find_anime_errors = bad_entries FindAnimeImporter
+    hentai_anime_errors = bad_entries HentaiAnimeImporter
+
+    raise "Ambiguous anime links found: #{find_anime_errors.join ', '}" if find_anime_errors.any?
+    raise "Ambiguous anime links found: #{hentai_anime_errors.join ', '}" if hentai_anime_errors.any?
   end
 
-  def bad_entries service
+  def bad_entries importer_klass
+    service = importer_klass::SERVICE
+    ignores = importer_klass.new.ignored_in_twice_match.to_a
+
     AnimeLink
       .where(service: service)
       .group_by(&:anime_id)
+      .map {|k,v| [k, v.map(&:identifier) - ignores] }
       .select {|k,v| v.size > 1 }
-      .map {|k,v| "#{k} (#{v.map(&:identifier).join ', '})" }
+      .map {|k,v| "#{k} (#{v.join ', '})" }
   end
 end
 
