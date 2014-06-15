b0428027985be2bae0156a6323c73364af8c52ec
blackchestnut <alexander.kalinichev@gmail.com>
2014-06-15 19:35:36 +0400

    #256 - Лидерам по загруженным видео не показывать рекламу

--- app/services/anime_online/ads_policy.rb
+++ app/services/anime_online/ads_policy.rb
@@ -1,5 +1,9 @@
 class AnimeOnline::AdsPolicy
+  def self.is_viewed? host, user=nil
+    host_allowed?(host) && !User::TrustedVideoUploaders.include?(user.try(:id))
+  end
 
+private
   def self.host_allowed? host
     host == AnimeOnlineDomain::HOST_PLAY
   end
--- spec/services/anime_online/ads_policy_spec.rb
+++ spec/services/anime_online/ads_policy_spec.rb
@@ -3,17 +3,53 @@ require 'spec_helper'
 describe AnimeOnline::AdsPolicy do
   let(:policy) { AnimeOnline::AdsPolicy }
 
-  describe :host_allowed do
-    subject { policy.host_allowed? host }
+  describe :is_viewed? do
+    subject { policy.is_viewed? host, user }
+    let(:guest_user) { build :user, id: User::GuestID }
+    let(:trust_user) { build :user, id: 11496 }
+    let(:simple_user) { build :user, id: 444 }
 
     context :host_play do
       let(:host) { AnimeOnlineDomain::HOST_PLAY }
-      it { should be_true }
+
+      context :guest do
+        let(:user) { guest_user }
+        it { should be_true }
+      end
+
+      context :trust do
+        let(:user) { trust_user }
+        it { should be_false }
+      end
+
+      context :simple do
+        let(:user) { simple_user }
+        it { should be_true }
+      end
+
+      context :nil do
+        let(:user) { nil }
+        it { should be_true }
+      end
     end
 
     context :xhost_play do
       let(:host) { AnimeOnlineDomain::HOST_XPLAY }
-      it { should be_false }
+
+      context :guest do
+        let(:user) { guest_user }
+        it { should be_false }
+      end
+
+      context :trust do
+        let(:user) { trust_user }
+        it { should be_false }
+      end
+
+      context :simple do
+        let(:user) { simple_user }
+        it { should be_false }
+      end
     end
   end
 end
