ceb1dbcd91bd4dad0f06e07b7646e3f23cb05dd4
Andrey Sidorov <takandar@gmail.com>
2014-07-05 02:53:47 +0400

    аниме на перевод

--- app/controllers/translation_controller.rb
+++ app/controllers/translation_controller.rb
@@ -312,7 +312,7 @@ class TranslationController < GroupsController
                      where.not(id: goals_ids).
                      order(:ranked)]
     @goals << ['Подборка 9',
-               Anime.where(id: [1576,5734,5702,6633,1520,690]).
+               Anime.where(id: [1576,5734,5702,6633,1520,690,1372]).
                      where.not(id: goals_ids).
                      where(censored: false).
                      order(:ranked)]
