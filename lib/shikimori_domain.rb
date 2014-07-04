a258118731c3b190a2f36cce8974a33d054379f3
Andrey Sidorov <takandar@gmail.com>
2014-07-05 03:07:20 +0400

    отключил временные домены

--- lib/shikimori_domain.rb
+++ lib/shikimori_domain.rb
@@ -1,6 +1,6 @@
 module ShikimoriDomain
   HOST = "shikimori.#{Rails.env.development? ? :dev : :org}"
-  HOSTS = [HOST, "new.#{HOST}", "vagrant.#{HOST}"]
+  HOSTS = [HOST]
 
   def self.matches? request
     !AnimeOnlineDomain.matches?(request) && !MangaOnlineDomain.matches?(request)
