f44d9cb80677f8a30ab91fc703b0b0aec4b4abdb
Andrey Sidorov <takandar@gmail.com>
2014-07-10 00:18:10 +0400

    багфикс

--- app/parsers/mal_deployer.rb
+++ app/parsers/mal_deployer.rb
@@ -192,7 +192,7 @@ module MalDeployer
     is_two_weeks = if Time.zone.now > Time.zone.parse('2014-07-11')
       (entry.respond_to?(:ongoing?) && entry.ongoing?) || (entry.kind_of?(Character) && entry.animes.ongoing.any?)
     else
-      (entry.respond_to?(:ongoing?) && entry.ongoing?) || (entry.kind_of?(Character) && entry.animes.any? {|v| v.ongoing? || v.released_on > 5.months.ago })
+      (entry.respond_to?(:ongoing?) && entry.ongoing?) || (entry.kind_of?(Character) && entry.animes.any? {|v| v.ongoing? || (v.released_on && v.released_on > 5.months.ago) })
     end
 
     interval = if is_two_weeks
