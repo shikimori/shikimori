b104dea5bd91b6f61f109cf952c17f7b80536247
Andrey Sidorov <takandar@gmail.com>
2014-07-17 18:18:05 +0200

    правка для убивалки конекшенов

--- app/workers/postgres_fix.rb
+++ app/workers/postgres_fix.rb
@@ -2,6 +2,6 @@ class PostgresFix
   include Sidekiq::Worker
 
   def perform
-    ActiveRecord::Base.connection.execute(" SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE query_start < '#{2.hours.ago}'")
+    ActiveRecord::Base.connection.execute(" SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE query_start < '#{2.hours.ago}' and application_name not like '%unicorn%' and application_name not like '%rails%' and application_name not like '%clockwork%' and application_name not like '%bin/sidekiq%'")
   end
 end
