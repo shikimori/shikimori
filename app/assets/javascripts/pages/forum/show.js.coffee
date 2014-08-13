7fe457aa86295538d3d2a5f4bdd010afe1734f22
Andrey Sidorov <takandar@gmail.com>
2014-08-13 23:14:45 +0400

    работа над топиками

--- app/assets/javascripts/pages/forum/show.js.coffee
+++ app/assets/javascripts/pages/forum/show.js.coffee
@@ -1,7 +1,6 @@
 # инициализация топика
 $('.ajax').live 'show:success', (e, data) ->
   _log "show:success"
-  $('.review-block .rate-block').makeRateble()
 
 $(document.body).on 'click', 'p.show-more', ->
   $(@).hide().next().show()
--- app/models/review_comment.rb
+++ app/models/review_comment.rb
@@ -6,7 +6,8 @@ class ReviewComment < AniMangaComment
   # текст топика
   def text
     type = linked.target_type == Anime.name ? 'anime' : 'manga'
-    self[:text] || "Обсуждение обзора [#{type}=#{self.linked.target_id}]#{linked.target_type == Anime.name ? 'аниме' : 'манги'}[/#{type}]."
+    self[:text] ||
+      "Обсуждение обзора [#{type}=#{self.linked.target_id}]#{linked.target_type == Anime.name ? 'аниме' : 'манги'}[/#{type}] от [profile]#{linked.user.nickname}[/profile]."
   end
 
   def generated?
