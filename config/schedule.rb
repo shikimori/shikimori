71d878b9a9b226c7683beec27d2770e3ace5ba58
Andrey Sidorov <takandar@gmail.com>
2014-09-23 06:52:32 +0400

    таск для синхронизации картинок продакшена и беты

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,10 +22,15 @@ set :environment, :production
 set :output, '/home/apps/shikimori/production/shared/log/whenever.log'
 set :job_template, "/usr/bin/zsh -i -c \"source /home/devops/.rvm/scripts/rvm && :job\""
 
-
 # здесь только редкие/долгие таски, которые нельзя на clockwork положить
 every 1.day, at: '4:30 am' do
   command "backup perform --trigger shikimori"
+end
+
+# пока есть бета-стейджинг, будут постоянно синхронизиться картинки
+every 13.minutes do
+  command 'rsync -ahvu /home/apps/shikimori/production/shared/public/images /home/apps/shikimori/beta/shared/public/'
+  command 'rsync -ahvu /home/apps/shikimori/beta/shared/public/images/user_image /home/apps/shikimori/production/shared/public/images/'
 end
 
 #every 1.day, at: '0:45 am' do
