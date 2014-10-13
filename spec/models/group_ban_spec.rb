84ee2afbd2e120abc588877638cb34950e920b14
Andrey Sidorov <takandar@gmail.com>
2014-10-13 22:11:17 +0400

    чиню спеки

--- spec/models/group_ban_spec.rb
+++ spec/models/group_ban_spec.rb
@@ -13,13 +13,21 @@ describe GroupBan do
 
   describe :callbacks do
     let(:user) { create :user }
-    let!(:group) { create :group }
-    let!(:group_role) { create :group_role, user: user, group: group }
-    let!(:incoming_invite) { create :group_invite, dst: user, group: group, src_id: group.owner_id }
-    let!(:outgoing_invite) { create :group_invite, src: user, group: group, dst_id: group.owner_id }
+    let(:group) { create :group }
 
-    before { create :group_ban, group: group, user: user }
-    it { expect(group.joined? user).to be false }
-    it { expect(group.invites).to be_empty }
+    context 'member' do
+      let!(:group_role) { create :group_role, user: user, group: group }
+      let!(:outgoing_invite) { create :group_invite, src: user, group: group, dst_id: group.owner_id }
+      before { create :group_ban, group: group, user: user }
+
+      it { expect(group.reload.joined? user).to be false }
+      it { expect(group.invites).to be_empty }
+    end
+
+    context 'not a member' do
+      let!(:incoming_invite) { create :group_invite, dst: user, group: group, src_id: group.owner_id }
+      before { create :group_ban, group: group, user: user }
+      it { expect(group.invites).to be_empty }
+    end
   end
 end
