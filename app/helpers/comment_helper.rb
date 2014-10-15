698e7e07582cf26ab516108462a993723ed8ca1c
Andrey Sidorov <takandar@gmail.com>
2014-10-16 00:48:17 +0400

    багфикс урлов в ббкодах

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -243,7 +243,7 @@ module CommentHelper
               text.gsub! $~[:match], "<a href=\"#{profile_url user}\" title=\"#{user.nickname}\" class=\"bubbled b-user16\" data-href=\"#{url}\">
 <img src=\"#{user.avatar_url 16}\" alt=\"#{user.nickname}\" /><span>#{user.nickname}</span></a>#{user.sex == 'male' ? 'написал' : 'написала'}:"
             else
-              text.gsub! $~[:match], "<a href=\"#{url_for user}\" title=\"#{user.nickname}\" class=\"bubbled b-mention\" data-href=\"#{url}\"><s>@</s><span>#{$~[:text]}</span></a>"
+              text.gsub! $~[:match], "<a href=\"#{profile_url user}\" title=\"#{user.nickname}\" class=\"bubbled b-mention\" data-href=\"#{url}\"><s>@</s><span>#{$~[:text]}</span></a>"
             end
 
           rescue
