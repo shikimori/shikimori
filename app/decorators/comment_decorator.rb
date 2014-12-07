9a7723c1e3a12ce125a09648aa10942757617fc4
Andrey Sidorov <takandar@gmail.com>
2014-12-07 22:13:32 +0300

    вернул назад сломанное

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,4 +1,7 @@
-class CommentDecorator < BaseDecorator
+# не заменять на "class CommentDecorator < BaseDecorator" - ломает
+class CommentDecorator < Draper::Decorator
+  delegate_all
+
   def can_be_edited?
     can_be_edited_by? h.current_user
   end
