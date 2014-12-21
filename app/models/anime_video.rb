bf303ad97daf3ed63ac1d658c392b9529bbe606a
Andrey Sidorov <takandar@gmail.com>
2014-12-21 19:29:32 +0300

    убрал guilty crown из забаненных видео

--- app/models/anime_video.rb
+++ app/models/anime_video.rb
@@ -38,7 +38,7 @@ class AnimeVideo < ActiveRecord::Base
 
   scope :worked, -> { where state: ['working', 'uploaded'] }
 
-  CopyrightBanAnimeIDs = [10793]
+  CopyrightBanAnimeIDs = [] # 10793
 
   state_machine :state, initial: :working do
     state :working
