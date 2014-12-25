09dc6f5cf00cbda36e92f6300e4d97b2d861f232
Andrey Sidorov <takandar@gmail.com>
2014-12-25 21:18:05 +0300

    постер теперь поддерживает загруженные на шики картинки

--- app/services/bb_codes/poster_tag.rb
+++ app/services/bb_codes/poster_tag.rb
@@ -2,14 +2,21 @@ class BbCodes::PosterTag
   include Singleton
 
   REGEXP = /
-    \[poster\]
-      (?<url>[^\[\]].*?)
-    \[\/poster\]
+      \[poster\]
+        (?<url>[^\[\]].*?)
+      \[\/poster\]
+    |
+      \[poster (?:=(?<id>\d+))\]
   /imx
 
   def format text
-    text.gsub REGEXP do
-      html_for_image $~[:url]
+    text.gsub REGEXP do |matched|
+      if $~[:url]
+        html_for_image $~[:url]
+      else
+        user_image = UserImage.find_by(id: $~[:id])
+        user_image ? html_for_image(user_image.image.url(:original, false)) : matched
+      end
     end
   end
 
--- spec/services/bb_codes/poster_tag_spec.rb
+++ spec/services/bb_codes/poster_tag_spec.rb
@@ -3,9 +3,18 @@ describe BbCodes::PosterTag do
 
   describe 'format' do
     subject { tag.format text }
-    let(:url) { 'http://site.com/site-url' }
-    let(:text) { "[poster]#{url}[/poster]" }
 
-    it { should eq "<img class=\"b-poster\" src=\"#{url}\" />" }
+    context 'external image' do
+      let(:url) { 'http://site.com/site-url' }
+      let(:text) { "[poster]#{url}[/poster]" }
+      it { should eq "<img class=\"b-poster\" src=\"#{url}\" />" }
+    end
+
+    context 'shiki image' do
+      let(:text) { "[poster=#{user_image.id}]" }
+      let(:user_image) { create :user_image, user: build_stubbed(:user), width: 400, height: 500 }
+
+      it { should eq "<img class=\"b-poster\" src=\"#{user_image.image.url :original, false}\" />" }
+    end
   end
 end
