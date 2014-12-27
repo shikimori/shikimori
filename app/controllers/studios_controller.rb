700ea38d0c21b10bcdf17690d4694c4a170cd0e9
Andrey Sidorov <takandar@gmail.com>
2014-12-27 21:32:32 +0300

    адаптирована страница студий

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -5,12 +5,11 @@ class StudiosController < ShikimoriController
     @description = 'Список наиболее крупных студий, занимающихся созданием аниме; отсортировано по объёму работ.'
     set_meta_tags description: @description
 
-    @studios = Studio
+    @collection = Studio
       .joins(:animes)
       .where("animes.kind != 'Special'")
       .group('studios.id')
       .select('studios.*, count(animes.id) as animes_count, max(animes.aired_on) as max_year, min(animes.aired_on) as min_year')
       .order('animes_count desc')
-
   end
 end
