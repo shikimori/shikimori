fac12578ec7be3819a9c17aaecadb76d34222641
Andrey Sidorov <takandar@gmail.com>
2015-01-06 05:36:33 +0300

    named logger

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,7 @@ require Rails.root.join('lib/string')
 require Rails.root.join('lib/i18n_hack')
 require Rails.root.join('lib/open_image')
 require Rails.root.join('lib/responders/json_responder')
+require Rails.root.join('lib/named_logger')
 
 require 'quote_extractor'
 require 'acts_as_voteable'
---
+++ lib/named_logger.rb
@@ -0,0 +1,39 @@
+class NamedLogger
+  def self.method_missing name, *args
+    if args.nil?
+      super
+    else
+      @loggers ||= {}
+      @loggers[name] ||= build_logger name
+    end
+  end
+
+private
+  def self.build_logger name
+    logfile = File.open(Rails.root.join('log', "#{name}.log"), 'a')
+    logfile.sync = true
+
+    logger = ActiveSupport::Logger.new logfile
+    logger.formatter = CommonLogFormatter.new
+    logger
+  end
+end
+
+class CommonLogFormatter < Logger::Formatter
+  def call severity, time, progname, msg
+    if severity == 'INFO'
+      "[#{time.to_s :short}] #{msg2str msg}\n"
+    else
+      "[#{time.to_s :short}] %5s - #{msg2str msg}\n" % [severity]
+    end
+  end
+end
+
+class ColouredLogFormatter < Logger::Formatter
+  SEVERITY_TO_COLOR_MAP   = {'DEBUG'=>'32', 'INFO'=>'0;37', 'WARN'=>'35', 'ERROR'=>'31', 'FATAL'=>'31', 'UNKNOWN'=>'37'}
+
+  def call severity, time, progname, msg
+    color = SEVERITY_TO_COLOR_MAP[severity]
+    "\033[0;37m[%s] \033[#{color}m%5s - %s\033[0m\n" % [time.to_s(:short), severity, msg2str(msg)]
+  end
+end
