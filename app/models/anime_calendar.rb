c3aa86b7eeda2057dd95cc67bd8cc339ba45646d
Andrey Sidorov <takandar@gmail.com>
2015-01-06 03:55:06 +0300

    новые аниме в календарь

--- app/models/anime_calendar.rb
+++ app/models/anime_calendar.rb
@@ -9,6 +9,99 @@ class AnimeCalendar < ActiveRecord::Base
   validates_presence_of :episode
   validates_presence_of :start_at
 
+  # импорт аниме календаря с animecalendar.net
+  def self.parse
+    calendar = self.load_calendar.first.events.map do |v|
+      name = v
+        .categories
+        .first
+          .split(',')
+            .first
+            .downcase
+            .sub(/episodes$/, '')
+            .strip
+            .gsub('ū', 'u')
+            .gsub('ō', 'o')
+            .gsub('×', 'x')
+            .gsub('é', 'e')
+
+      id = Fixes.include?(name) ? Fixes[name] : nil
+
+      {
+        start_at: v.dtstart - 4.hours,
+        episode: v.uid.split('_').last.to_i,
+        anime_name: id ? "FIXES MATCHED #{name}" : name,
+        anime_id: id
+      }
+    end
+    return if calendar.empty?
+    AnimeCalendar.delete_all
+
+    calendar_names = calendar.map {|v| v[:anime_name] }.uniq
+    fixed_calendar_names = calendar_names.map {|v| self.hashname(v) }.uniq
+
+    replaced = "replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(%s, '♪', ''), '☆', ''), 'The ', ''), 'the ', ''), '''', ''), '?', ''), '!', ''), ':', ''), '-', ''), ' ', ''), '.', ''), ',', '')"
+    query = "#{replaced % 'name'} in ('#{fixed_calendar_names.join("','")}')"
+    fixed_calendar_names.each do |v|
+      query += " or #{replaced % 'name'} ilike '%#{v}%'"
+      query += " or #{replaced % 'english'} ilike '%#{v}%'"
+      query += " or #{replaced % 'synonyms'} ilike '%#{v}%'"
+      query += " or #{replaced % 'japanese'} ilike '%#{v}%'"
+    end
+
+    query += " or id in (#{(calendar.map {|v| v[:anime_id] }.compact + [0]).join(',')})"
+
+    #animes = Anime.where(:id => 10464).where(query).inject({}) do |data,anime|
+    animes = (Anime.latest + Anime.ongoing.where(query).to_a + Anime.anons.where(query).to_a)
+      .select {|v| v.kind == 'TV' || v.kind == 'ONA' || [15133, 19799].include?(v.id) }.inject({}) do |data,anime|
+        data[self.hashname(anime.name)] = anime
+        data[anime.id] = anime
+        anime.synonyms.each {|v| data[self.hashname(v)] = anime } if anime.synonyms
+        anime.japanese.each {|v| data[self.hashname(v)] = anime } if anime.japanese
+        anime.english.each {|v| data[self.hashname(v)] = anime } if anime.english
+        data
+      end
+
+    cache = {}
+    imported = Set.new
+
+    batch = []
+
+    calendar.each do |v|
+      key = self.hashname(v[:anime_name])
+      entry = animes[key] || animes[v[:anime_id]]
+
+      next unless entry
+      next if cache.include?(entry.id) && cache[entry.id].include?(v[:episode])
+      v[:episode] -= EpisodesDiff[v[:anime_name]] if EpisodesDiff[v[:anime_name]]
+
+      batch << AnimeCalendar.new({
+          episode: v[:episode],
+          start_at: v[:start_at],
+          anime: entry
+        })
+      imported << v[:anime_name]
+    end
+    AnimeCalendar.import batch.select {|v| v.episode > v.anime.episodes_aired }
+
+    Rails.cache.write 'calendar_unrecognized', (calendar_names - imported.to_a)
+
+    {
+      :imported => imported,
+      :unrecognized => calendar_names - imported.to_a
+    }
+  end
+
+  # получение календаря аниме с animecalendar.net
+  def self.load_calendar
+    content = Proxy.get('http://animecalendar.net/user/ical/8831/e599e8323643658c14eef67e85bdb534', timeout: 30, required_text: 'Calendar for TV from AnimeCalendar', no_proxy: true)
+    Icalendar.parse(content)
+  end
+
+  def self.hashname(name)
+    name.downcase.gsub(/the /, '').gsub(/[ :,.!?'☆♪-]+/, '')
+  end
+
   Fixes = {
     "danganronpa kibo no gakuen to zetsubo no kokosei the animation" => 16592,
     "ginga kikotai majestic prince" => 15863,
@@ -103,6 +196,19 @@ class AnimeCalendar < ActiveRecord::Base
     'madan no o to vanadis' => 24455,
     'yama no susume second season' => 21435,
     'mushishi: the next chapter' => 24701,
+    'aldnoah.zero 2' => 27655,
+    'juo mujin no fafnir' => 24873,
+    'sokyu no fafner exodus' => 17080,
+    'cute high earth defense club love!' => 27727,
+    'kuroko no basuke 3' => 24415,
+    'dog days″' => 16385,
+    'durarara!!x2 sho' => 23199,
+    'shonen hollywood - holly stage for 50' => 27741,
+    'tenkai knights' => 23067,
+    'ansatsu kyoshitsu' => 24833,
+    'jojo no kimyo na boken: stardust crusaders egypt-hen' => 26055,
+    'shinmai mao no testament' => 23233,
+    'kamisama hajimemashita 2' => 25681
   }
 
   EpisodesDiff = {
@@ -113,97 +219,4 @@ class AnimeCalendar < ActiveRecord::Base
     'fairy tail (2014)' => 175,
     'FIXES MATCHED mushishi: the next chapter' => 12
   }
-
-  # импорт аниме календаря с animecalendar.net
-  def self.parse
-    calendar = self.load_calendar.first.events.map do |v|
-      name = v
-        .categories
-        .first
-          .split(',')
-            .first
-            .downcase
-            .sub(/episodes$/, '')
-            .strip
-            .gsub('ū', 'u')
-            .gsub('ō', 'o')
-            .gsub('×', 'x')
-            .gsub('é', 'e')
-
-      id = Fixes.include?(name) ? Fixes[name] : nil
-
-      {
-        start_at: v.dtstart - 4.hours,
-        episode: v.uid.split('_').last.to_i,
-        anime_name: id ? "FIXES MATCHED #{name}" : name,
-        anime_id: id
-      }
-    end
-    return if calendar.empty?
-    AnimeCalendar.delete_all
-
-    calendar_names = calendar.map {|v| v[:anime_name] }.uniq
-    fixed_calendar_names = calendar_names.map {|v| self.hashname(v) }.uniq
-
-    replaced = "replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(%s, '♪', ''), '☆', ''), 'The ', ''), 'the ', ''), '''', ''), '?', ''), '!', ''), ':', ''), '-', ''), ' ', ''), '.', ''), ',', '')"
-    query = "#{replaced % 'name'} in ('#{fixed_calendar_names.join("','")}')"
-    fixed_calendar_names.each do |v|
-      query += " or #{replaced % 'name'} ilike '%#{v}%'"
-      query += " or #{replaced % 'english'} ilike '%#{v}%'"
-      query += " or #{replaced % 'synonyms'} ilike '%#{v}%'"
-      query += " or #{replaced % 'japanese'} ilike '%#{v}%'"
-    end
-
-    query += " or id in (#{(calendar.map {|v| v[:anime_id] }.compact + [0]).join(',')})"
-
-    #animes = Anime.where(:id => 10464).where(query).inject({}) do |data,anime|
-    animes = (Anime.latest + Anime.ongoing.where(query).to_a + Anime.anons.where(query).to_a)
-      .select {|v| v.kind == 'TV' || v.kind == 'ONA' || [15133, 19799].include?(v.id) }.inject({}) do |data,anime|
-        data[self.hashname(anime.name)] = anime
-        data[anime.id] = anime
-        anime.synonyms.each {|v| data[self.hashname(v)] = anime } if anime.synonyms
-        anime.japanese.each {|v| data[self.hashname(v)] = anime } if anime.japanese
-        anime.english.each {|v| data[self.hashname(v)] = anime } if anime.english
-        data
-      end
-
-    cache = {}
-    imported = Set.new
-
-    batch = []
-
-    calendar.each do |v|
-      key = self.hashname(v[:anime_name])
-      entry = animes[key] || animes[v[:anime_id]]
-
-      next unless entry
-      next if cache.include?(entry.id) && cache[entry.id].include?(v[:episode])
-      v[:episode] -= EpisodesDiff[v[:anime_name]] if EpisodesDiff[v[:anime_name]]
-
-      batch << AnimeCalendar.new({
-          episode: v[:episode],
-          start_at: v[:start_at],
-          anime: entry
-        })
-      imported << v[:anime_name]
-    end
-    AnimeCalendar.import batch.select {|v| v.episode > v.anime.episodes_aired }
-
-    Rails.cache.write 'calendar_unrecognized', (calendar_names - imported.to_a)
-
-    {
-      :imported => imported,
-      :unrecognized => calendar_names - imported.to_a
-    }
-  end
-
-  # получение календаря аниме с animecalendar.net
-  def self.load_calendar
-    content = Proxy.get('http://animecalendar.net/user/ical/8831/e599e8323643658c14eef67e85bdb534', timeout: 30, required_text: 'Calendar for TV from AnimeCalendar', no_proxy: true)
-    Icalendar.parse(content)
-  end
-
-  def self.hashname(name)
-    name.downcase.gsub(/the /, '').gsub(/[ :,.!?'☆♪-]+/, '')
-  end
 end
