9c6132c9e3c6c97cd94c89933b0e69260a5a9387
Andrey Sidorov <takandar@gmail.com>
2015-01-09 17:49:31 +0300

    продолжаю чинить спеки

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,21 +3,21 @@ class VideosController < ShikimoriController
 
   def create
     @entry = Anime.find params[:id]
-    @video = @entry.videos.build video_params
-    @video.state = 'uploaded'
+    @resource = @entry.videos.build video_params
+    @resource.state = 'uploaded'
 
-    @video.state = 'confirmed' if params[:apply].present? && current_user.user_changes_moderator?
+    @resource.state = 'confirmed' if params[:apply].present? && current_user.user_changes_moderator?
 
-    if @video.save
+    if @resource.save
       render json: { notice: 'Видео сохранено и будет в ближайшее время рассмотрено модератором. Домо аригато.' }
     else
-      render json: @video.errors, status: :unprocessable_entity
+      render json: @resource.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @video = Video.find params[:id]
-    @video.suggest_deletion current_user
+    @resource = Video.find params[:id]
+    @resource.suggest_deletion current_user
 
     render json: {
       notice: 'Запрос на удаление принят и будет рассмотрен модератором. Домо аригато.'
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,10 +1,5 @@
 describe VideosController do
-  before do
-    allow_any_instance_of(Video).to receive :existence
-    sign_in user
-  end
-
-  let(:user) { create :user, id: 1 }
+  include_context :authenticated, :user_changes_moderator
 
   let(:url) { 'http://youtube.com/watch?v=l1YX30AmYsA' }
   let(:name) { 'test' }
@@ -17,39 +12,24 @@ describe VideosController do
     describe 'response' do
       before { post :create, id: anime_id, video: { url: url, kind: kind, name: name } }
 
-      it { should respond_with 200 }
+      it { should respond_with :success }
+      it { expect(resource).to be_uploaded }
+      it { expect(resource).to have_attributes(url: url, name: name, kind: kind, anime_id: anime_id, uploader_id: user.id) }
+      it { expect(resource).to be_persisted }
       it { expect(response.content_type).to eq 'application/json' }
     end
 
     describe 'apply' do
-      before do
-        request.env["HTTP_REFERER"] = anime_url(anime_id)
-        post :create, id: anime_id, apply: 1, video: { url: url, kind: kind, name: name }
-      end
-      it { should redirect_to request.env['HTTP_REFERER'] }
+      before { post :create, id: anime_id, apply: 1, video: { url: url, kind: kind, name: name } }
+      it { expect(resource).to be_confirmed }
+      it { should respond_with :success }
     end
 
     describe 'apply wo_permissions' do
-      before do
-        sign_in create(:user, id: 9999)
-        post :create, id: anime_id, apply: 1, video: { url: url, kind: kind, name: name }
-      end
-      it { should respond_with 200 }
-    end
-
-    it 'creates video' do
-      expect {
-        post :create, id: anime_id, video: { url: url, kind: kind, name: name }
-      }.to change(Video, :count).by 1
-    end
-
-    describe 'assigns' do
-      before { post :create, id: anime_id, video: { url: url, kind: kind, name: name } }
-      it { expect(assigns(:video).url).to eq url }
-      it { expect(assigns(:video).name).to eq name }
-      it { expect(assigns(:video).kind).to eq kind }
-      it { expect(assigns(:video).anime_id).to eq anime_id }
-      it { expect(assigns(:video).uploader_id).to eq user.id }
+      include_context :authenticated, :user
+      before { post :create, id: anime_id, apply: 1, video: { url: url, kind: kind, name: name } }
+      it { expect(resource).to be_uploaded }
+      it { should respond_with :success }
     end
   end
 
@@ -57,7 +37,7 @@ describe VideosController do
     let(:video) { create :video, state: 'confirmed' }
     before { post :destroy, id: video.id }
 
-    it { should respond_with 200 }
+    it { should respond_with :success }
     it { expect(response.content_type).to eq 'application/json' }
 
     it 'suggest video deletion' do
