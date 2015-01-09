104895a2c66e0034270178bb90cf8eba77cd60ca
Andrey Sidorov <takandar@gmail.com>
2015-01-09 03:09:51 +0300

    спеки

--- spec/controllers/recommendations_controller_spec.rb
+++ spec/controllers/recommendations_controller_spec.rb
@@ -1,15 +1,16 @@
 describe RecommendationsController do
   ['anime', 'manga'].each do |type|
-    describe 'index' do
-      context 'with_params' do
-        before { get :index, klass: type, metric: 'pearson', threshold: RecommendationsController::THRESHOLDS[Anime].first }
-        it { should respond_with :success }
-        it { should respond_with_content_type :html }
-      end
+    describe type do
+      describe '#index' do
+        context 'with_params' do
+          before { get :index, klass: type, metric: 'pearson', threshold: RecommendationsController::THRESHOLDS[Anime].first }
+          it { should respond_with :success }
+        end
 
-      describe 'witout_params' do
-        before { get :index, klass: type }
-        it { should respond_with :redirect }
+        describe 'witout_params' do
+          before { get :index, klass: type }
+          it { should respond_with :redirect }
+        end
       end
     end
   end
