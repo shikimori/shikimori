5bc4cc1a7504405ebb216123e563ebffbad2ef58
Andrey Sidorov <takandar@gmail.com>
2015-02-01 16:49:51 +0300

    допилил сериализатор клубов

--- app/serialziers/group_profile_serializer.rb
+++ app/serialziers/group_profile_serializer.rb
@@ -1,15 +1,6 @@
 class GroupProfileSerializer < GroupSerializer
-  attributes :description, :description_html, :image, :mangas, :characters, :thread_id, :user_role
+  attributes :description, :description_html, :mangas, :characters, :thread_id, :user_role
   has_many :members, :animes, :mangas, :characters, :images
-
-  def image
-    {
-      original: object.logo.url(:original),
-      main: object.logo.url(:main),
-      x73: object.logo.url(:x73),
-      xo64: object.logo.url(:x48)
-    }
-  end
 
   def thread_id
     object.thread.id
--- app/serialziers/group_serializer.rb
+++ app/serialziers/group_serializer.rb
@@ -2,6 +2,12 @@ class GroupSerializer < ActiveModel::Serializer
   attributes :id, :name, :logo
 
   def logo
-    object.logo.url :x48
+    {
+      original: object.logo.url(:original),
+      main: object.logo.url(:main),
+      x96: object.logo.url(:x96),
+      x73: object.logo.url(:x73),
+      x48: object.logo.url(:x48)
+    }
   end
 end
