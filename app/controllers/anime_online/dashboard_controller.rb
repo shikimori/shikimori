1396ddc2e49ed64e9966a7ddff532de4222fa629
Andrey Sidorov <takandar@gmail.com>
2015-02-03 00:27:31 +0300

    отображение хентайных онгоингов сезона на xplay домене

--- app/controllers/anime_online/dashboard_controller.rb
+++ app/controllers/anime_online/dashboard_controller.rb
@@ -3,19 +3,19 @@ class AnimeOnline::DashboardController < ShikimoriController
     @page = [params[:page].to_i, 1].max
     @limit = 8
 
-    @recent_videos, @add_postloader = RecentVideosQuery.new(AnimeOnlineDomain::adult_host?(request)).postload(@page, @limit)
+    is_adult = AnimeOnlineDomain::adult_host? request
+
+    @recent_videos, @add_postloader = RecentVideosQuery.new(is_adult).postload(@page, @limit)
     @recent_videos = @recent_videos.map {|v| AnimeWithEpisode.new v.anime.decorate, v }
 
     unless json?
-      @ongoings = Anime
+      @ongoings = Anime.ongoing
         .includes(:genres)
-        .ongoing
-        .where(kind: 'TV', censored: false)
         .where.not(rating: 'G - All Ages')
         .where('score < 9.9')
+        .where(is_adult ? AnimeVideo::XPLAY_CONDITION : { kind: 'TV', censored: false })
         .order(score: :desc)
-        .limit(15)
-        .decorate
+        .limit(15).decorate
 
       @contributors = AnimeOnline::Uploaders.current_top.map(&:decorate).take(15)
       @seasons = AniMangaSeason.menu_seasons
