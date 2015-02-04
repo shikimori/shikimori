5e148a55dec48c17205f40db0d8096d42856b7e7
Andrey Sidorov <takandar@gmail.com>
2015-02-04 23:43:36 +0300

    доработки для apipie

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,8 @@ Capybara.ignore_hidden_elements = false
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Apipie.record(nil) unless ENV['APIPIE_RECORD']
+
 RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
