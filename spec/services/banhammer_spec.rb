8bae3a90893d156589ff24d02d9c5245a06594df
Andrey Sidorov <takandar@gmail.com>
2015-02-20 09:04:49 +0300

    ещё мат

--- spec/services/banhammer_spec.rb
+++ spec/services/banhammer_spec.rb
@@ -56,6 +56,8 @@ describe Banhammer do
     it { expect(banhammer.abusive? 'сучка').to be_truthy }
     it { expect(banhammer.abusive? 'хер').to be_truthy }
     it { expect(banhammer.abusive? 'херо*о').to be_truthy }
+    it { expect(banhammer.abusive? 'ебать').to be_truthy }
+    it { expect(banhammer.abusive? 'заебись').to be_truthy }
   end
 
   describe '#abusiveness' do
