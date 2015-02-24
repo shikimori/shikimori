9fb8dbdcb7f03edcabd2fb8e16923e7ff632f53e
Andrey Sidorov <takandar@gmail.com>
2015-02-24 21:37:11 +0300

    ещё спека

--- spec/factories/anime_video_report.rb
+++ spec/factories/anime_video_report.rb
@@ -18,8 +18,8 @@ FactoryGirl.define do
 
     after :build do |v|
       v.anime_video = FactoryGirl.build_stubbed(:anime_video) unless v.anime_video_id
-      v.user = FactoryGirl.build_stubbed(:user) unless v.user_id
-      v.approver = FactoryGirl.build_stubbed(:user) unless v.user_id && v.pending?
+      v.user = FactoryGirl.build_stubbed(:user, :user) unless v.user_id
+      v.approver = FactoryGirl.build_stubbed(:user, :user) unless v.user_id && v.pending?
     end
 
     trait :with_video do
