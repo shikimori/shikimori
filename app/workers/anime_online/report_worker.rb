3ad6e7550db18ebe98a0565e71f6d9bd43a3b80e
Andrey Sidorov <takandar@gmail.com>
2015-02-28 16:56:02 +0300

    багфикс не срабатывающей автопроверки нерабочего vk видео

--- app/workers/anime_online/report_worker.rb
+++ app/workers/anime_online/report_worker.rb
@@ -3,12 +3,12 @@ class AnimeOnline::ReportWorker < SiteParserWithCache
   sidekiq_options retry: false
 
   VK_BROKEN_TEXTS = [
-    "\n\nЭто видео изъято из публичного доступа.\n\n",
-    "\n\nThis video has been removed from public access.\n\n",
-    "\n\nДанная видеозапись скрыта настройками приватности и недоступна для просмотра.\n\n",
-    "\n\nThis video is protected by privacy settings.\n\n",
-    "\n\nВидеозапись была помечена модераторами сайта как «Материал для взрослых». Такие видеозаписи запрещено вставлять на внешние сайты.\n\n",
-    "\n\nThis video was marked as Adult.Embedding adult videos is not allowed by VK.\n\n"
+    /\n\nЭто видео изъято из публичного доступа.\n\n/i,
+    /\n\nThis video has been removed from public access.\n\n/i,
+    /\n\nДанная видеозапись скрыта настройками приватности и недоступна для просмотра.\n\n/i,
+    /\n\nThis video is protected by privacy settings.\n\n/i,
+    /\n\nВидеозапись была помечена модераторами сайта как «Материал для взрослых»./i,
+    /\n\nThis video was marked as Adult.Embedding adult videos is not allowed by VK.\n\n/i
   ]
   SIBNET_BROKEN_TEXTS = ["Ошибка обработки видео", "Îøèáêà îáðàáîòêè âèäåî"]
 
@@ -47,7 +47,7 @@ private
   def vk_broken? video
     doc = Nokogiri::HTML get(video.url)
     text = doc.css('#page_wrap div').first.try(:text)
-    VK_BROKEN_TEXTS.any? {|v| text.include? v }
+    VK_BROKEN_TEXTS.any? {|v| text =~ v }
   end
 
   def sibnet_broken? video
