22ed86e3ff0dc77dccd257bb9f76fe4805d19145
Andrey Sidorov <takandar@gmail.com>
2015-03-01 03:23:45 +0300

    спека на защиту от перетирания

--- spec/parsers/read_manga_importer_spec.rb
+++ spec/parsers/read_manga_importer_spec.rb
@@ -2,23 +2,26 @@ describe ReadMangaImporter, vcr: { cassette_name: 'read_manga_parser' } do
   let(:importer) { ReadMangaImporter.new }
 
   describe 'import' do
-    let!(:entry) { create :manga, name: "the magician's bride" }
+    let!(:manga) { create :manga, id: 61189, name: "the magician's bride", description: description }
+    let(:description) { 'test' }
     let(:identifier) { 'the_magician_s_bride' }
-
-    subject(:manga) { entry.reload }
 
     describe 'pages' do
       before { allow_any_instance_of(ReadMangaParser).to receive(:fetch_page_links).and_return [identifier] }
       before { importer.import pages: [0] }
 
-      it { expect(manga.description).to be_present }
+      it { expect(manga.reload.description).to be_present }
     end
 
-    describe 'pages' do
-      before { allow_any_instance_of(ReadMangaParser).to receive(:fetch_page_links).and_return [identifier] }
+    describe 'ids' do
+      let!(:user_change) { }
       before { importer.import ids: [identifier] }
+      it { expect(manga.reload.description).to be_present }
 
-      it { expect(manga.description).to be_present }
+      context 'user changed manga' do
+        let!(:user_change) { create :user_change, item_id: manga.id, model: Manga.name, column: 'description', status: UserChangeStatus::Taken }
+        it { expect(manga.reload.description).to eq description }
+      end
     end
   end
 end
