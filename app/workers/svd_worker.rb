68e3e420fda3bf8b4a157021c19f388c526f80c0
Andrey Sidorov <takandar@gmail.com>
2015-03-01 05:04:48 +0300

    svd worker больше не будет пропадать

--- app/workers/svd_worker.rb
+++ app/workers/svd_worker.rb
@@ -1,6 +1,7 @@
 class SvdWorker
   include Sidekiq::Worker
-  sidekiq_options unique: true
+  sidekiq_options unique: true, dead: false
+  sidekiq_retry_in { 60 * 60 * 24 }
 
   def perform
     Svd.generate! Svd::Partial
