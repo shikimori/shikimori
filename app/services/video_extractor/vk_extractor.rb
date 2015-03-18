cdab5d290b0de998a6f3c884a30de14a9e16bab2
Andrey Sidorov <takandar@gmail.com>
2015-03-18 22:52:00 +0300

    багфиксы vk видео

--- app/services/video_extractor/vk_extractor.rb
+++ app/services/video_extractor/vk_extractor.rb
@@ -1,19 +1,29 @@
+#class VideoExtractor::VkExtractor < VideoExtractor::BaseExtractor
+#class VideoExtractor::VkExtractor < VideoExtractor::OpenGraphExtractor
 class VideoExtractor::VkExtractor < VideoExtractor::BaseExtractor
   URL_REGEX = %r{
     https?://vk.com/video-?(\d+)_(\d+)(?:\?[\w=+%&]+)?
   }xi
 
   def image_url
-    parsed_data['jpg']
+    parsed_data[:image]
   end
 
   def player_url
-    "https://vk.com/video_ext.php?oid=#{parsed_data['oid']}&id=#{parsed_data['vid']}&hash=#{parsed_data['hash2']}&hd=1"
+    "https://vk.com/video_ext.php?oid=#{parsed_data[:oid]}&id=#{parsed_data[:vid]}&hash=#{parsed_data[:hash2]}&hd=1"
   end
 
   def parse_data html
-    File.open('/tmp/test.html', 'wb') {|f| f.write html }
-    data = html.match(/vars = ({.*?});\\nvar/) || raise(EmptyContent, @url)
-    JSON.parse data[1].gsub(/\\/, '')
+    doc = Nokogiri::HTML html
+
+    og_image = doc.css("meta[property='og:image']").first
+    og_video = doc.css("meta[property='og:video'],meta[property='og:video:url']").first
+
+    {
+      image: og_image[:content],
+      oid: og_video[:content][/oid=(\w+)/, 1],
+      vid: og_video[:content][/vid=(\w+)/, 1],
+      hash2: og_video[:content][/embed_hash=(\w+)/, 1],
+    } if og_image && og_video
   end
 end
