cd88832969e0d9509aae4337920efa3eebdb7993
Andrey Sidorov <takandar@gmail.com>
2015-03-27 09:42:56 +0300

    api description

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,18 @@
 Apipie.configure do |config|
-  config.app_name                = "shikimori"
-  config.api_base_url            = "/api"
-  config.doc_base_url            = "/api/doc"
+  config.app_name                = 'shikimori'
+  config.api_base_url            = '/api'
+  config.doc_base_url            = '/api/doc'
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
-  config.default_version = "1"
+  config.default_version         = '1'
+  config.app_info                = <<-DICK
+Do not parse the main site. Use this api instead.
+
+Do not mimic a browser. Put your application name or website url in User-Agent request header.
+
+Do not make more than 3 requests per second by ip address for mobile applications.
+
+Do not make more than 10 requests per second from your webserver.
+
+Message me (http://shikimori.org/morr or takandar@gmail.com) if you have any questions or you need more data in api.
+DICK
 end
