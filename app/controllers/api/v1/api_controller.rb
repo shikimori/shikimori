d324c2f0257a718ce70dae6b2010955b75217303
Andrey Sidorov <takandar@gmail.com>
2015-04-06 14:54:58 +0300

    багфикс апи доступа к сайту по токену

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ApiController < ShikimoriController
   responders :json # для рендеринга контента на patch и put запросы
   before_action :authenticate_user_from_token!
-  skip_before_action :verify_authenticity_token, if: -> { @authenticated_by_token }
+  skip_before_action :verify_authenticity_token, if: -> { request.headers['X-User-Nickname'] && request.headers['X-User-Api-Access-Token'] }
 
   resource_description do
     api_version '1'
@@ -12,14 +12,18 @@ private
     user_nickname = request.headers['X-User-Nickname']
     user_token = request.headers['X-User-Api-Access-Token']
 
-    user = user_nickname && user_token && User.find_by(nickname: user_nickname)
+    if user_nickname && user_token
+      user = User.find_by(nickname: user_nickname)
 
-    # Notice how we use Devise.secure_compare to compare the token
-    # in the database with the token given in the params, mitigating
-    # timing attacks.
-    if user && Devise.secure_compare(user.api_access_token, user_token)
-      sign_in user, store: false
-      @authenticated_by_token = true
+      # Notice how we use Devise.secure_compare to compare the token
+      # in the database with the token given in the params, mitigating
+      # timing attacks.
+      if user && Devise.secure_compare(user.api_access_token, user_token)
+        sign_in user, store: false
+        @authenticated_by_token = true
+      else
+        render json: { error: 'invalid user nickname or api access token' }, status: 403
+      end
     end
   end
 end
