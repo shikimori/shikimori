d74ff9784f82b26e2b51954294829105633cdc48
blackchestnut <alexander.kalinichev@gmail.com>
2015-04-09 13:05:05 +0300

    Немного поменял логику для расчета "доверия" по сломанным жалобам.

--- app/services/anime_online/activists.rb
+++ app/services/anime_online/activists.rb
@@ -1,9 +1,9 @@
 class AnimeOnline::Activists
-  ENOUGH_TO_TRUST_RUTUBE = 10
+  ENOUGH_TO_TRUST_RUTUBE = 30
 
   class << self
     def rutube_responsible
-      @rutube_responsible ||= resposible_users("rutube.ru")
+      @rutube_responsible ||= resposible_users('rutube.ru')
     end
 
     def can_trust? user_id, hosting
@@ -21,15 +21,19 @@ class AnimeOnline::Activists
       active_users = AnimeVideoReport
         .select(:user_id, "count(*) as videos")
         .joins(:anime_video)
-        .where(kind: :broken)
+        .where(kind: :broken, state: :accepted)
         .where("url like 'http://#{hosting}%'")
         .group(:user_id)
         .having("count(*) >= ?", ENOUGH_TO_TRUST_RUTUBE)
         .map(&:user_id)
 
       user_with_rejected = AnimeVideoReport
+        .select(:user_id)
+        .joins(:anime_video)
         .where(kind: :broken, state: :rejected)
+        .where("url like 'http://#{hosting}%'")
         .where(user_id: active_users)
+        .group(:user_id)
         .map(&:user_id)
 
       active_users - user_with_rejected
