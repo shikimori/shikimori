8febad7d59e6ce71716da3b73f82ab94aa6c73fa
Andrey Sidorov <takandar@gmail.com>
2015-04-10 21:42:18 +0300

    [#341] remove name from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410183721) do
+ActiveRecord::Schema.define(version: 20150410184019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -979,7 +979,6 @@ ActiveRecord::Schema.define(version: 20150410183721) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "name",                   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nickname",               limit: 255
