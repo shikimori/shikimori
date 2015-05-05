62c4d718a85d49dcd311388ed363d300ee4cd124
Andrey Sidorov <takandar@gmail.com>
2015-05-05 13:34:01 +0300

    [#490] Доработал отображение клубов. Теперь у клубов показывается число комментариев и создатель

--- app/queries/clubs_query.rb
+++ app/queries/clubs_query.rb
@@ -3,6 +3,7 @@ private
   def query
     Group
       .joins(:member_roles, :thread)
+      .preload(:owner, :thread)
       .group('groups.id, entries.updated_at')
       .having('count(group_roles.id) > 0')
       .order('entries.updated_at desc, id')
