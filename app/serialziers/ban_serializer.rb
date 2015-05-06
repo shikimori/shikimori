7ba444faa3e6ea022720a4f57c4d6aaec33388d8
Andrey Sidorov <takandar@gmail.com>
2015-05-06 13:48:36 +0300

    нфомрация о модераторе и о юзере для bans

--- app/serialziers/ban_serializer.rb
+++ app/serialziers/ban_serializer.rb
@@ -1,6 +1,9 @@
 class BanSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :comment, :moderator_id, :reason, :created_at, :duration_minutes
 
+  has_one :user
+  has_one :moderator
+
   def duration_minutes
     object.duration.value
   end
