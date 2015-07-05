e6671f80aa4985d12a83f391270a2cbe39672860
blackchestnut <alexander.kalinichev@gmail.com>
2015-07-05 14:01:27 +0300

    Remove google ads

--- app/controllers/anime_online/pingmedia_controller.rb
+++ app/controllers/anime_online/pingmedia_controller.rb
@@ -3,15 +3,15 @@ class AnimeOnline::PingmediaController < ShikimoriController
 
   before_action { noindex && nofollow }
 
-  def google
-  end
-
-  def google_leaderboard
-  end
-
   def iframe_240x400
   end
 
   def iframe_728x90
+  end
+
+  def iframe_240x400_advertur
+  end
+
+  def iframe_728x90_advertur
   end
 end
