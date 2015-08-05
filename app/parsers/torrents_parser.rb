f35f38039c8b06f5073c15b3815c97447815776b
Andrey Sidorov <takandar@gmail.com>
2015-08-05 21:49:47 +0300

    подправил регексп

--- app/parsers/torrents_parser.rb
+++ app/parsers/torrents_parser.rb
@@ -64,7 +64,7 @@ class TorrentsParser
 
     if episode_name =~ /cardfight!![ _]vanguard/i && episode_name =~ /link[ _]joker/i
       num.map {|v| v - 104 }
-    elsif episode_name =~ /THE.iDOLM@STER.Cinderella.Girls/i
+    elsif episode_name =~ /THE iDOLM@STER Cinderella Girls/i
       num.map {|v| v - 13 }
     elsif episode_name =~ /Yu-Gi-Oh![ _]Zexal[ _]II/i
       num.map {|v| v - 73 }
