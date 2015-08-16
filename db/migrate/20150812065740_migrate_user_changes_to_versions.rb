256a38ebc63f199f59a778b74f190edeedd796c4
Andrey Sidorov <takandar@gmail.com>
2015-08-16 15:46:28 +0300

    миграция user_changes с учётом кадров

--- db/migrate/20150812065740_migrate_user_changes_to_versions.rb
+++ db/migrate/20150812065740_migrate_user_changes_to_versions.rb
@@ -1,8 +1,8 @@
 class MigrateUserChangesToVersions < ActiveRecord::Migration
   def up
-    UserChange
-      .where(column: %w{description tags russian torrents_name})
-      .each do |user_change|
+    UserChange.
+      where(column: %w{description tags russian torrents_name screenshots}).
+      each do |user_change|
         next if user_change.prior.blank? && user_change.value.blank?
 
         version = Version.create(
@@ -10,12 +10,7 @@ class MigrateUserChangesToVersions < ActiveRecord::Migration
           state: user_change.status.downcase,
           item_id: user_change.item_id,
           item_type: user_change.model,
-          item_diff: {
-            user_change.column => [
-              user_change.prior,
-              user_change.value,
-            ]
-          },
+          item_diff: item_diff(user_change),
           moderator_id: user_change.approver_id,
           reason: user_change.reason,
           created_at: user_change.created_at,
@@ -33,8 +28,44 @@ private
   def pick_type user_change
     if user_change.column == 'description'
       Versions::DescriptionVersion.name
+    elsif user_change.column == 'screenshots'
+      Versions::ScreenshotsVersion.name
     else
       nil
+    end
+  end
+
+  def item_diff user_change
+    if user_change.column != 'screenshots'
+      {
+        user_change.column => [
+          user_change.prior,
+          user_change.value,
+        ]
+      }
+    elsif user_change.action == 'screenshots_upload'
+      {
+        action: Versions::ScreenshotsVersion::ACTIONS[:upload],
+        screenshots: user_change.value.split(',').map(&:to_i)
+      }
+
+    elsif user_change.action == 'screenshots_deletion'
+      {
+        action: Versions::ScreenshotsVersion::ACTIONS[:delete],
+        screenshots: user_change.value.split(',').map(&:to_i)
+      }
+
+    elsif user_change.action == 'screenshots_position'
+      {
+        action: Versions::ScreenshotsVersion::ACTIONS[:reposition],
+        screenshots: [
+          user_change.prior.split(',').map(&:to_i),
+          user_change.value.split(',').map(&:to_i)
+        ]
+      }
+
+    else
+        raise ArgumentError
     end
   end
 end
