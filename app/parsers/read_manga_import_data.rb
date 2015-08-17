ffb65550c850b382ca646afd90077eb5098baad8
Andrey Sidorov <takandar@gmail.com>
2015-08-17 14:16:55 +0300

    починил ссылку на похожих пользователей в профиле

--- app/parsers/read_manga_import_data.rb
+++ app/parsers/read_manga_import_data.rb
@@ -221,6 +221,7 @@ module ReadMangaImportData
       one_more_marionnette: -1,
       marionette: -1,
       marionette_theater_by_the_moonlight_battery: -1,
+      kiss: -1
       #katekyo_hitman_reborn__dj___sanctuary: -1
     }.inject({}) {|rez,v| rez[v[0].to_s] = v[1] and rez }
 end
