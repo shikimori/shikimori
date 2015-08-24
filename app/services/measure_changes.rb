a0c33d416809a2f61d9bb0d195e429f9dff499fd
Andrey Sidorov <takandar@gmail.com>
2015-08-24 21:46:49 +0300

    увеличил лимит для принятия авторства

--- app/services/measure_changes.rb
+++ app/services/measure_changes.rb
@@ -2,16 +2,26 @@ class MeasureChanges
   include HTMLDiff
   pattr_initialize :old_value, :new_value
 
-  MINIMAL_CHANGES_PERCENT = 20
+  MINIMAL_CHANGES_PERCENT = 25
 
   def enough?
     new_text? || added_text? || changed_text? || false
   end
 
+  def text_change
+    if new_text?
+      100
+    elsif old.size.zero?
+      0
+    else
+      insertions.size * 100.0 / old.size
+    end
+  end
+
 private
 
   def changed_text?
-    insertions.size * 100.0 / old.size >= MINIMAL_CHANGES_PERCENT
+    text_change >= MINIMAL_CHANGES_PERCENT
   end
 
   def new_text?
--- spec/services/measure_changes_spec.rb
+++ spec/services/measure_changes_spec.rb
@@ -6,30 +6,33 @@ describe MeasureChanges do
       let(:old) { '' }
       let(:new) { '' }
       it { expect(service).to_not be_enough }
+      it { expect(service.text_change).to eq 0 }
     end
 
     context 'blank old' do
       let(:old) { '' }
       let(:new) { 'zz' }
       it { expect(service).to be_enough }
+      it { expect(service.text_change).to eq 100 }
     end
 
     context 'size decrease' do
       let(:old) { 'aaaaaaa aa' }
       let(:new) { 'aaaaaaa a' }
       it { expect(service).to_not be_enough }
+      it { expect(service.text_change).to eq 10.0 }
     end
 
     context 'size increase' do
       let(:old) { 'aaaaaaaaa ' }
 
-      context '< 20% increase' do
+      context '< 25% increase' do
         let(:new) { 'aaaaaaaaa a' }
         it { expect(service).to_not be_enough }
       end
 
-      context '>= 20% increase' do
-        let(:new) { 'aaaaaaaaa aa' }
+      context '>= 25% increase' do
+        let(:new) { 'aaaaaaaaa aaa' }
         it { expect(service).to be_enough }
       end
     end
@@ -37,14 +40,16 @@ describe MeasureChanges do
     context 'content change' do
       let(:old) { 'aa aa aa aa aa' }
 
-      context '< 20% change' do
+      context '< 25% change' do
         let(:new) { 'aa aa aa aa bb' }
         it { expect(service).to_not be_enough }
+        it { expect(service.text_change).to eq 14.285714285714286 }
       end
 
-      context '>= 20% change' do
+      context '>= 25% change' do
         let(:new) { 'aa aa aa bb bb' }
         it { expect(service).to be_enough }
+        it { expect(service.text_change).to eq 35.714285714285715 }
       end
     end
   end
