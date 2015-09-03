cc8f4dbf17e96632e90807588deda95f7617abc0
Andrey Sidorov <takandar@gmail.com>
2015-09-04 01:19:15 +0300

    fixed spec

--- spec/models/anime_video_spec.rb
+++ spec/models/anime_video_spec.rb
@@ -267,9 +267,17 @@ describe AnimeVideo do
       describe 'persisted video' do
         let(:video) { build_stubbed :anime_video, url: url }
         let(:url) { 'http://rutube.ru/video/ef370e68cd9687a30ea67a68658c6ef8/?ref=logo' }
-        before { video.url = '<iframe width="720" height="405" src="//rutube.ru/play/embed/3599097" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>' }
+        before { video.url = new_url }
 
-        it { expect(video.url).to eq 'http://rutube.ru/play/embed/3599097' }
+        describe 'indirect url' do
+          let(:new_url) { '<iframe width="720" height="405" src="//rutube.ru/play/embed/3599097" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>' }
+          it { expect(video.url).to eq 'http://rutube.ru/play/embed/3599097' }
+        end
+
+        describe 'direct url' do
+          let(:new_url) { 'http://rutube.ru/play/embed/3599097' }
+          it { expect(video.url).to eq 'http://rutube.ru/play/embed/3599097' }
+        end
       end
     end
 
