343fc341daa9410ae9abfa7f765f09cc654d6f4d
Andrey Sidorov <takandar@gmail.com>
2015-09-20 22:37:34 +0300

    rack throlle limits

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -9,12 +9,9 @@ Do not parse the main site. Use this api instead.
 
 Do not mimic a browser. Put your application name or website url in User-Agent request header.
 
-Do not make more than 3 requests per second by ip address for mobile applications.
-
-Do not make more than 10 requests per second from your webserver.
+API access limits: 5 requests per second, 90 requests per minute
 
 Message me (http://shikimori.org/morr or takandar@gmail.com) if you have any questions or you need more data in api.
-
 
 
 Authentication:
--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,5 +1,5 @@
 # Throttle requests to 5 requests per second per ip
-Rack::Attack.throttle('req/ip', limit: 10, period: 1.second) do |req|
+Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
   # If the return value is truthy, the cache key for the return value
   # is incremented and compared with the limit. In this case:
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
@@ -9,6 +9,6 @@ Rack::Attack.throttle('req/ip', limit: 10, period: 1.second) do |req|
   req.ip
 end
 
-Rack::Attack.throttle('req/ip', limit: 180, period: 60.second) do |req|
+Rack::Attack.throttle('req/ip', limit: 90, period: 60.second) do |req|
   req.ip
 end
