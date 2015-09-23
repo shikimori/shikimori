4d5155c79c6932f0a9214d07a5520f63b2161828
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2015-09-24 00:19:11 +0300

    fix migrations

--- db/migrate/20150813101836_fix_description_versions.rb
+++ db/migrate/20150813101836_fix_description_versions.rb
@@ -1,7 +1,7 @@
 class FixDescriptionVersions < ActiveRecord::Migration
   def up
-    #Versions::DescriptionVersion.find_each do |version|
-      #version.fix_state
-    #end
+    Versions::DescriptionVersion.find_each do |version|
+      version.fix_state
+    end
   end
 end
