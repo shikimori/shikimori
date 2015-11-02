ea1cafe6736a6a583585adfa3366307c59e5763c
Andrey Sidorov <takandar@gmail.com>
2015-11-02 19:30:22 +0300

    метка новости

--- app/view_objects/topics/news_view.rb
+++ app/view_objects/topics/news_view.rb
@@ -14,4 +14,8 @@ class Topics::NewsView < Topics::View
   def html_footer
     BbCodeFormatter.instance.format_comment topic.appended_text
   end
+
+  def action_tag
+    i18n_i 'news', :one
+  end
 end
