4476bfddbf1dd2455300c0020340492f12ef317e
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2015-11-05 23:47:43 +0300

    translate pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ShikimoriController
 
   # график онгоингов
   def ongoings
-    @page_title = 'Календарь онгоингов'
+    @page_title = i18n_t 'calendar_of_ongoings'
 
     @ongoings = CalendarsQuery.new.fetch_grouped
     @topic = Topics::Factory.new(false, false).find ONGOINGS_TOPIC_ID
@@ -60,13 +60,13 @@ class PagesController < ShikimoriController
 
   # 404 страница
   def page404
-    @page_title = 'Страница не найдена'
+    @page_title = i18n_t 'page_not_found'
     render 'pages/page404', layout: false, status: 404, formats: :html
   end
 
   # страница с ошибкой
   def page503
-    @page_title = 'Ошибка'
+    @page_title = i18n_t 'error'
     render 'pages/page503', layout: false, status: 503, formats: :html
   end
 
