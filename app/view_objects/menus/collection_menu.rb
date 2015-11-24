e74b0b5e163af7211c2a4e50b9657c8f2a9cb262
Andrey Sidorov <takandar@gmail.com>
2015-11-24 23:56:45 +0300

    hotfix

--- app/view_objects/menus/collection_menu.rb
+++ app/view_objects/menus/collection_menu.rb
@@ -33,18 +33,18 @@ class Menus::CollectionMenu < ViewObjectBase
 
   def seasons
     [
-      SeasonTitle.new(3.months.from_now, :season_year),
-      SeasonTitle.new(Time.zone.now, :season_year),
-      SeasonTitle.new(3.months.ago, :season_year),
-      SeasonTitle.new(6.months.ago, :season_year),
-      SeasonTitle.new(Time.zone.now, :year),
-      SeasonTitle.new(1.year.ago, :year),
-      SeasonTitle.new(2.years.ago, :years_2),
-      SeasonTitle.new(4.years.ago, :years_5),
-      SeasonTitle.new(9.years.ago, :years_7),
-      SeasonTitle.new(Date.parse('1995-01-01'), :decade),
-      SeasonTitle.new(Date.parse('1985-01-01'), :decade),
-      SeasonTitle.new(nil, :ancient)
+      SeasonTitle.new(3.months.from_now, :season_year, klass),
+      SeasonTitle.new(Time.zone.now, :season_year, klass),
+      SeasonTitle.new(3.months.ago, :season_year, klass),
+      SeasonTitle.new(6.months.ago, :season_year, klass),
+      SeasonTitle.new(Time.zone.now, :year, klass),
+      SeasonTitle.new(1.year.ago, :year, klass),
+      SeasonTitle.new(2.years.ago, :years_2, klass),
+      SeasonTitle.new(4.years.ago, :years_5, klass),
+      SeasonTitle.new(9.years.ago, :years_7, klass),
+      SeasonTitle.new(Date.parse('1995-01-01'), :decade, klass),
+      SeasonTitle.new(Date.parse('1985-01-01'), :decade, klass),
+      SeasonTitle.new(nil, :ancient, klass)
     ]
   end
 
