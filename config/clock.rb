85d48cbd6eaa077617c22f664515025988b60819
Andrey Sidorov <takandar@gmail.com>
2015-11-24 08:08:59 +0300

    weekly vacuum

--- config/clock.rb
+++ config/clock.rb
@@ -72,7 +72,7 @@ module Clockwork
     CommentViewsCleaner.perform_async
   end
 
-  every 1.week, 'weekly.vacuum', at: '05:15' do
+  every 1.week, 'weekly.vacuum', at: 'Monday 05:00' do
     VacuumDb.perform_async
   end
 
