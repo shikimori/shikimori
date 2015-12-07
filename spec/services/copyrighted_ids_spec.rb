6e4020087c88ca5e009cf9f7cb49bfcc233b8623
Andrey Sidorov <takandar@gmail.com>
2015-12-07 21:41:19 +0300

    alex minson - moderator

--- spec/services/copyrighted_ids_spec.rb
+++ spec/services/copyrighted_ids_spec.rb
@@ -3,11 +3,13 @@ describe CopyrightedIds do
 
   describe '#change' do
     context 'copyrighted' do
-      it { expect(service.change 145, :anime).to eq "#{CopyrightedIds::MARKER}145" }
+      it { expect(service.change 145, :anime)
+        .to eq "#{CopyrightedIds::MARKER}145" }
     end
 
     context 'twice copyrighted' do
-      it { expect(service.change 9999999, :anime).to eq "#{CopyrightedIds::MARKER*2}9999999" }
+      it { expect(service.change 9999999, :anime)
+        .to eq "#{CopyrightedIds::MARKER*2}9999999" }
     end
 
     context 'not copyrighted' do
@@ -18,17 +20,22 @@ describe CopyrightedIds do
   describe '#restore' do
     context 'copyrighted' do
       context 'changed' do
-        it { expect(service.restore "#{CopyrightedIds::MARKER}145-neo-ranga", :anime).to eq 145 }
+        it do
+          expect(
+            service.restore "#{CopyrightedIds::MARKER}145-neo-ranga", :anime
+          ).to eq 145 }
       end
 
       context 'original' do
         let!(:anime) { create :anime, id: 145 }
-        it { expect{service.restore '145-neo-ranga', :anime}.to raise_error CopyrightedResource }
+        it { expect{service.restore '145-neo-ranga', :anime}
+          .to raise_error CopyrightedResource }
       end
     end
 
     context 'twice copyrighted' do
-      it { expect(service.restore "#{CopyrightedIds::MARKER*2}9999999", :anime).to eq 9999999 }
+      it { expect(service.restore "#{CopyrightedIds::MARKER*2}9999999", :anime)
+        .to eq 9999999 }
     end
 
     context 'not copyrighted' do
