23e6753b827332c0a15ebad45f3bcddd1a0c3170
Andrey Sidorov <takandar@gmail.com>
2015-12-23 09:54:54 +0300

    корректное сохранение выбранных форумов

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -3,12 +3,11 @@ class UserPreferencesController < ProfilesController
     authorize! :edit, @resource
 
     if @resource.preferences.update user_preferences_params
-      if params[:user].present?
-        super
-      else
-        redirect_to edit_profile_path(@resource, params[:page]), notice: t('changes_saved')
-      end
+      return super if params[:user].present?
+      return head 200 if request.xhr?
 
+      redirect_to edit_profile_path(@resource, params[:page]),
+        notice: t('changes_saved')
     else
       flash[:alert] = t 'changes_not_saved'
       edit
