da1c74f78bed63ab253d67c1797173119a0bd8df
Andrey Sidorov <takandar@gmail.com>
2015-12-26 12:54:05 +0300

    remove news from forums list

--- app/view_objects/forums/list.rb
+++ app/view_objects/forums/list.rb
@@ -14,10 +14,10 @@ class Forums::List  < ViewObjectBase
 private
 
   def forums
-    Rails.cache.fetch([:forums, :v2, Entry.last.id], expires_in: 2.weeks) do
+    Rails.cache.fetch([:forums, Entry.last.id], expires_in: 2.weeks) do
       Forum.visible.map { |forum| build forum, false } +
         Array(build Forum.find_by_permalink('reviews'), true) +
-        Array(build Forum::NEWS_FORUM, true) +
+        # Array(build Forum::NEWS_FORUM, true) +
         Array(build Forum::MY_CLUBS_FORUM, true) +
         Array(build Forum.find_by_permalink('clubs'), true)
     end
