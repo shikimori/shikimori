8f8e0595883506a7e8a1cf5e4aeda98528f5b99d
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2015-12-29 19:34:22 +0300

    fix

--- app/view_objects/forums/list.rb
+++ app/view_objects/forums/list.rb
@@ -14,11 +14,11 @@ class Forums::List  < ViewObjectBase
 private
 
   def forums
-    Rails.cache.fetch([:forums, :v9, Entry.last.id], expires_in: 2.weeks) do
+    Rails.cache.fetch([:forums, :v3, Entry.last.id], expires_in: 2.weeks) do
       Forum.visible.map { |forum| decorate forum, false } +
         Array(decorate Forum::NEWS_FORUM, true) +
         Array(decorate Forum.find_by_permalink('reviews'), true) +
-        Array(build Forum.find_by_permalink('contests'), true) +
+        Array(decorate Forum.find_by_permalink('contests'), true) +
         Array(decorate Forum::MY_CLUBS_FORUM, true) +
         Array(decorate Forum.find_by_permalink('clubs'), true)
     end
