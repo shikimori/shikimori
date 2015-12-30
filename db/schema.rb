4d64c4ed88cf15e56a0b06397876c27a4742f28b
Andrey Sidorov <takandar@gmail.com>
2015-12-31 00:55:58 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227160604) do
+ActiveRecord::Schema.define(version: 20151230203542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
   enable_extension "unaccent"
 
   create_table "abuse_requests", force: :cascade do |t|
