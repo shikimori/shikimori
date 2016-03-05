7ca2ebd0689e67421b4a26c31eb37f0986a78ffd
Andrey Sidorov <takandar@gmail.com>
2016-03-05 18:58:08 +0300

    review size

--- app/models/review.rb
+++ app/models/review.rb
@@ -19,7 +19,7 @@ class Review < ActiveRecord::Base
   validates :user, :target, presence: true
   validates :text,
     length: { minimum: MINIMUM_LENGTH, too_short: "слишком короткий (минимум #{MINIMUM_LENGTH} знаков)" },
-    if: -> { text.present? && changes['text'] }
+    if: -> { changes['text'] }
 
   after_create :generate_topic
 
