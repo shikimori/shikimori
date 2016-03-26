3be524ecfebebe7b983aaa838b8aa682d1d7ea1b
Andrey Sidorov <takandar@gmail.com>
2016-03-26 16:23:15 +0300

    fix cleanup smotret-anime

--- app/workers/anime_online/cleanup_smotret_anime.rb
+++ app/workers/anime_online/cleanup_smotret_anime.rb
@@ -2,17 +2,22 @@ class AnimeOnline::CleanupSmotretAnime
   include Sidekiq::Worker
 
   URL_LIKE = "url like '%smotret-anime.ru%'"
-  SELECTS = 'max(id), max(episode) as episode, anime_id'
+  SELECTS = 'max(id), max(episode) as max_episode, anime_id'
 
   def perform
+    smotret_videos.where(anime_id: mismatched_ongoing_ids).each(&:wrong!)
     smotret_videos.where(anime_id: mismatched_episode_ids).each(&:wrong!)
     smotret_videos.where(anime_id: anons_ids).each(&:wrong!)
   end
 
 private
 
+  def mismatched_ongoing_ids
+    smotert_query.select { |v| v.anime.ongoing? && v.anime.episodes_aired > 0 && v.max_episode > v.anime.episodes_aired + 4 }.map(&:anime_id)
+  end
+
   def mismatched_episode_ids
-    smotert_query.select { |v| v.anime.episodes < v.episode }.map(&:anime_id)
+    smotert_query.select { |v| v.anime.episodes > 0 && v.anime.episodes < v.max_episode }.map(&:anime_id)
   end
 
   def anons_ids
@@ -25,5 +30,9 @@ private
 
   def smotert_query
     smotret_videos.group(:anime_id).select(SELECTS).includes(:anime)
+  end
+
+  def max_video_episode anime_id
+    AnimeVideo.where.not(URL_LIKE).where(state: :working).where(anime_id: anime_id)
   end
 end
