c4f3ae60e132e6f40a1094c068ef8b964a0b9397
Andrey Sidorov <takandar@gmail.com>
2016-04-14 00:27:39 +0300

    fix error for incorrect request

--- config/routes.rb
+++ config/routes.rb
@@ -524,7 +524,7 @@ Site::Application.routes.draw do
       get "#{kind}#{ani_manga_format}" => "animes_collection#index", as: kind, klass: kind.singularize, constraints: { page: /\d+/, studio: /[^\/]+/ }
       get "#{kind}/menu(/rating/:rating)" => "animes_collection#menu", klass: kind.singularize, as: "menu_#{kind}"
 
-      resources kind, only: [:show] do
+      resources kind, only: [:show], format: /html/ do
         member do
           get :characters
           get :staff
