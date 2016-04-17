2cc772fe0f6ef431b3f307ee4447d922d86acc37
Andrey Sidorov <takandar@gmail.com>
2016-04-17 13:10:32 +0300

    enable pg_stat_statement

--- db/migrate/20160414165408_remove_db_entry_topics_without_comments.rb
+++ db/migrate/20160414165408_remove_db_entry_topics_without_comments.rb
@@ -1,8 +1,8 @@
 class RemoveDbEntryTopicsWithoutComments < ActiveRecord::Migration
   def change
-    # Topics::EntryTopics::AnimeTopic.where(comments_count: 0).destroy_all
-    # Topics::EntryTopics::MangaTopic.where(comments_count: 0).destroy_all
-    # Topics::EntryTopics::CharacterTopic.where(comments_count: 0).destroy_all
-    # Topics::EntryTopics::PersonTopic.where(comments_count: 0).destroy_all
+    Topics::EntryTopics::AnimeTopic.where(comments_count: 0).destroy_all
+    Topics::EntryTopics::MangaTopic.where(comments_count: 0).destroy_all
+    Topics::EntryTopics::CharacterTopic.where(comments_count: 0).destroy_all
+    Topics::EntryTopics::PersonTopic.where(comments_count: 0).destroy_all
   end
 end
