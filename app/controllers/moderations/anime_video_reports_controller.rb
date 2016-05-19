869a8f83c184fb43d8985b70becba7e94680864c
Andrey Sidorov <takandar@gmail.com>
2016-05-19 21:14:49 +0300

    rubocop fix

--- app/controllers/moderations/anime_video_reports_controller.rb
+++ app/controllers/moderations/anime_video_reports_controller.rb
@@ -4,17 +4,8 @@ class Moderations::AnimeVideoReportsController < ModerationsController
   def index
     @page_title = i18n_t 'page_title'
     @moderators = User.where(id: User::VIDEO_MODERATORS - User::ADMINS)
-    @processed = postload_paginate(params[:page], 20) do
-      AnimeVideoReport.includes(:user, anime_video: :author).processed
-    end
-
-    unless json?
-      @pending = AnimeVideoReport
-        .includes(:user, anime_video: :author)
-        .pending
-        .order(id: :desc)
-        .limit(20)
-    end
+    @processed = processed_reports
+    @pending = pending_reports unless json?
   end
 
   def create
@@ -59,5 +50,19 @@ private
       .require(:anime_video_report)
       .permit(:kind, :anime_video_id, :user_id, :message)
       .merge(user_agent: request.user_agent)
+  end
+
+  def processed_reports
+    postload_paginate(params[:page], 20) do
+      AnimeVideoReport.includes(:user, anime_video: :author).processed
+    end
+  end
+
+  def pending_reports
+    AnimeVideoReport
+      .includes(:user, anime_video: :author)
+      .pending
+      .order(id: :desc)
+      .limit(20)
   end
 end
