d69a07b0976b22a5335b712987017e2910f9e669
Andrey Sidorov <takandar@gmail.com>
2016-05-21 13:34:56 +0300

    tubocop

--- app/view_objects/topics/review_view.rb
+++ app/view_objects/topics/review_view.rb
@@ -19,11 +19,10 @@ class Topics::ReviewView < Topics::View
   end
 
   def offtopic_tag
-    if topic.linked.rejected?
-      I18n.t 'markers.offtopic'
-    end
+    I18n.t 'markers.offtopic' if topic.linked.rejected?
   end
 
+  # rubocop:disable AbcSize
   def topic_title
     if !is_preview
       i18n_t(
@@ -34,6 +33,7 @@ class Topics::ReviewView < Topics::View
       h.localized_name topic.linked.target
     end
   end
+   # rubocop:enable AbcSize
 
   def render_body
     render_results + render_stars + super
@@ -58,8 +58,9 @@ private
   end
 
   def render_stars
-    h.render 'reviews/stars', review: topic.linked,
-      with_music: topic.linked.entry.kind_of?(Anime)
+    h.render 'reviews/stars',
+      review: topic.linked,
+      with_music: topic.linked.entry.is_a?(Anime)
   end
 
   def render_results
