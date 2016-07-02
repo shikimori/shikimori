9c2e1f960fb4bd76582bf5cf9c0d4c834d6ee647
Andrey Sidorov <takandar@gmail.com>
2016-07-03 01:33:15 +0300

    bugfix

--- app/serialziers/video_serializer.rb
+++ app/serialziers/video_serializer.rb
@@ -2,10 +2,10 @@ class VideoSerializer < ActiveModel::Serializer
   attributes :id, :url, :image_url, :player_url, :name, :kind, :hosting
 
   def player_url
-    Url.new(object.player_url).with_http.to_s
+    Url.new(object.player_url).with_http.to_s if object.player_url
   end
 
   def image_url
-    Url.new(object.image_url).with_http.to_s
+    Url.new(object.image_url).with_http.to_s if object.image_url
   end
 end
