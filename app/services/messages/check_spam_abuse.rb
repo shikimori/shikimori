af71dcf6fbd8a7069b5143f30c607154de7c93ce
Andrey Sidorov <takandar@gmail.com>
2016-07-09 09:22:45 +0300

    update antispam

--- app/services/messages/check_spam_abuse.rb
+++ app/services/messages/check_spam_abuse.rb
@@ -5,13 +5,20 @@ class Messages::CheckSpamAbuse < ServiceObjectBase
   SPAM_LINKS = %r{
     cos30.ru/M=5j-N9 |
     aHR0cDovL3ByaW1hcnl4Lm5ldC9ncmVlaz9kbGM9a2ltb3Jp |
-    PrimaryX.NET/greek?dlc=Kimori |
-    goo.gl/KfKxKC
+    PrimaryX.NET/greek\?dlc=Kimori |
+    goo.gl/KfKxKC |
+    primaryx.net/quadro\?dlc=kimori
   }mix
 
-  SPAM_PHRASES = %r{
-    Хорош \s качать \s уже\) \s А \s то \s всё \s качаем,качаем
-  }mx
+  SPAM_PHRASES = [
+    /
+        Хорош \s качать \s уже\) \s А \s то \s всё \s качаем,качаем
+    /mix,
+    /
+      Поднадоело \s читать, \s ищу \s напарника \s со \s мной \s в \s игру. \s
+      Если \s за, \s то \s регайся \s и \s качай \s тут
+    /mix
+  ]
 
   def call
     if spam?
@@ -27,7 +34,10 @@ private
 
   def spam?
     message.kind == MessageType::Private &&
-      (message.body =~ SPAM_LINKS || message.body =~ SPAM_PHRASES)
+      (
+        message.body =~ SPAM_LINKS ||
+        SPAM_PHRASES.any? { |v| v =~ message.body }
+      )
   end
 
   def ban_text
