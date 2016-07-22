44f8bf82e8aebb6d6939bde885881ced8b2e6a5c
Andrey Sidorov <takandar@gmail.com>
2016-07-22 20:12:11 +0300

    smotret-anime preview images

--- app/services/video_extractor/smotret_anime_extractor.rb
+++ app/services/video_extractor/smotret_anime_extractor.rb
@@ -1,5 +1,7 @@
 class VideoExtractor::SmotretAnimeExtractor < VideoExtractor::BaseExtractor
   URL_REGEX = VideoExtractor::UrlExtractor::SMOTRET_ANIME_REGEXP
+  IMAGE_TEMPLATE =
+    'https://smotret-anime.ru/translations/thumbnail/%i.320x180.jpg'
 
   def image_url
     parsed_data[:image_url]
@@ -14,8 +16,9 @@ class VideoExtractor::SmotretAnimeExtractor < VideoExtractor::BaseExtractor
   end
 
   def parsed_data
+    id = url.match(VideoExtractor::UrlExtractor::SMOTRET_ANIME_REGEXP)[:id]
     {
-      image_url: nil,
+      image_url: IMAGE_TEMPLATE % id,
       player_url: Url.new(VideoExtractor::UrlExtractor.call(url)).with_http.to_s,
       hosting: :smotret_anime
     }
--- spec/services/video_extractor/smotret_anime_extractor_spec.rb
+++ spec/services/video_extractor/smotret_anime_extractor_spec.rb
@@ -4,12 +4,13 @@ describe VideoExtractor::SmotretAnimeExtractor do
   describe 'fetch' do
     subject { service.fetch }
     let(:embed_url) { 'http://smotret-anime.ru/translations/embed/939915' }
+    let(:image_url) { 'https://smotret-anime.ru/translations/thumbnail/939915.320x180.jpg' }
 
     context 'full url' do
       let(:url) { 'https://smotret-anime.ru/catalog/anime-krasavica-voin-seylor-mun-kristall-apostoly-smerti-13889/12-seriya-122880/ozvuchka-939915' }
 
       its(:hosting) { is_expected.to eq :smotret_anime }
-      its(:image_url) { is_expected.to be_nil }
+      its(:image_url) { is_expected.to eq image_url }
       its(:player_url) { is_expected.to eq embed_url }
     end
 
@@ -17,7 +18,7 @@ describe VideoExtractor::SmotretAnimeExtractor do
       let(:url) { embed_url }
 
       its(:hosting) { is_expected.to eq :smotret_anime }
-      its(:image_url) { is_expected.to be_nil }
+      its(:image_url) { is_expected.to eq image_url }
       its(:player_url) { is_expected.to eq embed_url }
     end
   end
