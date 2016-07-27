fac9f99790adbe18d6997bb9815358abe4768ed1
Andrey Sidorov <takandar@gmail.com>
2016-07-27 20:31:11 +0300

    fix comment specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,7 @@
 describe CommentsController do
+  include_context :seeds
+
   let(:user) { create :user, :user }
-  let(:topic) { seed :offtopic_topic }
   let(:comment) { create :comment, commentable: offtopic_topic, user: user }
   let(:comment2) { create :comment, commentable: offtopic_topic, user: user }
   before { allow(FayePublisher).to receive(:new).and_return double(FayePublisher, publish: true) }
@@ -26,28 +27,26 @@ describe CommentsController do
   end
 
   describe '#fetch' do
-    let(:user) { build_stubbed :user }
-    let(:topic) { create :entry, user: user }
-
     it do
-      get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: topic.id, skip: 1, limit: 10
+      get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: offtopic_topic.id, skip: 1, limit: 10
       expect(response).to be_success
     end
 
     it 'not_found for wrong comment' do
       expect {
-        get :fetch, comment_id: comment.id+1, topic_type: Entry.name, topic_id: topic.id, skip: 1, limit: 10
+        get :fetch, comment_id: comment.id+1, topic_type: Entry.name, topic_id: offtopic_topic.id, skip: 1, limit: 10
       }.to raise_error ActiveRecord::RecordNotFound
     end
 
     it 'not_found for wrong topic' do
       expect {
-        get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: topic.id+1, skip: 1, limit: 10
+        get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: offtopic_topic.id+1, skip: 1, limit: 10
       }.to raise_error ActiveRecord::RecordNotFound
     end
 
     it 'forbidden for mismatched comment and topic' do
-      get :fetch, comment_id: create(:comment).id, topic_type: Entry.name, topic_id: topic.id, skip: 1, limit: 10
+      comment = create :comment, topic: create(:topic)
+      get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: offtopic_topic.id, skip: 1, limit: 10
       expect(response).to be_forbidden
 
       get :fetch, comment_id: comment.id, topic_type: Entry.name, topic_id: create(:entry).id, skip: 1, limit: 10
@@ -74,7 +73,13 @@ describe CommentsController do
 
   describe '#postload' do
     let(:user) { build_stubbed :user }
-    before { get :postloader, commentable_type: topic.class.name, commentable_id: topic.id, offset: 0, limit: 1 }
+    before do
+      get :postloader,
+        commentable_type: offtopic_topic.class.name,
+        commentable_id: offtopic_topic.id,
+        offset: 0,
+        limit: 1
+    end
     it { expect(response).to have_http_status :success }
   end
 end
