79c721770f758c77222bf88f5017d716b0bc9d32
Andrey Sidorov <takandar@gmail.com>
2016-08-11 08:31:49 +0300

    fix js exports

---
+++ app/helpers/js_exports_helper.rb
@@ -0,0 +1,11 @@
+module JsExportsHelper
+  # не выносить код в JsExports.instance !!!
+  # при переносе туда из-за вызова h.capture из Draper::ViewHelpers контент
+  # дублируется при наличии внутри кода вызова h.controller.render_to_string
+  # (что происходит в BbCodes::EntriesTag)
+  def sweep_js_exports &block
+     html = capture &block
+     JsExports.instance.sweep html
+     html
+   end
+end
