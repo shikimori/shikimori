426c3cc82a269040217bd8e12ea3f5bcaf8f4f36
Andrey Sidorov <takandar@gmail.com>
2016-08-30 21:14:36 +0300

    remove obsolete fields from comment

--- app/serialziers/comment_serializer.rb
+++ app/serialziers/comment_serializer.rb
@@ -1,21 +1,11 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :commentable_id, :commentable_type
   attributes :body, :html_body, :created_at, :updated_at
-  attributes :offtopic, :is_offtopic, :review, :is_summary
+  attributes :is_offtopic, :is_summary
   attributes :can_be_edited?
   # attributes :viewed?
 
   has_one :user
-
-  # TODO remove in half a year (summary 2016)
-  def review
-    object.is_summary
-  end
-
-  # TODO remove in half a year (summary 2016)
-  def offtopic
-    object.is_offtopic
-  end
 
   def html_body
     object.html_body.gsub(%r{(?<!:)//(?=\w)}, 'http://')
