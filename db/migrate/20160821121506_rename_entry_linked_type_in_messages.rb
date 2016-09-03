8e9f222e0b4e2bc9d117f155e0cbab5e53cc3386
Andrey Sidorov <takandar@gmail.com>
2016-09-03 12:15:55 +0300

    add one week ago condition

--- db/migrate/20160821121506_rename_entry_linked_type_in_messages.rb
+++ db/migrate/20160821121506_rename_entry_linked_type_in_messages.rb
@@ -1,9 +1,15 @@
 class RenameEntryLinkedTypeInMessages < ActiveRecord::Migration
   def up
-    Message.where(linked_type: 'Entry').update_all(linked_type: 'Topic')
+    Message
+      .where(linked_type: 'Entry')
+      .where('created_at > ?', 1.week.ago)
+      .update_all(linked_type: 'Topic')
   end
 
   def down
-    Message.where(linked_type: 'Topic').update_all(linked_type: 'Entry')
+    Message
+      .where(linked_type: 'Topic')
+      .where('created_at > ?', 1.week.ago)
+      .update_all(linked_type: 'Entry')
   end
 end
