5baa5469d65f496cf13aadc0b1b186fc13a4054c
Andrey Sidorov <takandar@gmail.com>
2016-09-12 00:10:37 +0300

    fix unstable spec

--- spec/controllers/api/v1/calendars_controller_spec.rb
+++ spec/controllers/api/v1/calendars_controller_spec.rb
@@ -1,4 +1,7 @@
 describe Api::V1::CalendarsController, :show_in_doc do
+  before { Timecop.freeze '2016-09-05 15:00' }
+  after { Timecop.return }
+
   describe '#show' do
     before do
       create :anime
