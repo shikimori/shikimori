fd010ca19cbcada0460c8b764f6ff34020ec91fb
Andrey Sidorov <takandar@gmail.com>
2016-09-13 07:00:35 +0300

    temporarily disable js_compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Site::Application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
