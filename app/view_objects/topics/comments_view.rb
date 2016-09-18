4739da141a39be1ab54901856d3af1b099facc53
Andrey Sidorov <takandar@gmail.com>
2016-09-18 18:17:29 +0300

    fix comments for notopic

--- app/view_objects/topics/comments_view.rb
+++ app/view_objects/topics/comments_view.rb
@@ -101,7 +101,7 @@ class Topics::CommentsView < ViewObjectBase
 
   def cache_key
     [
-      topic.id,
+      topic.is_a?(NoTopic) ? topic.linked : topic.id,
       topic.respond_to?(:commented_at) ? topic.commented_at : topic.updated_at,
       comments_limit,
       h.params[:page],
