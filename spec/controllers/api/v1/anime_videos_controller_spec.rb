f21954c75288e8594550e45f21b6752e6373140f
Andrey Sidorov <takandar@gmail.com>
2016-10-16 19:37:48 +0300

    misc spec changes

--- spec/controllers/api/v1/anime_videos_controller_spec.rb
+++ spec/controllers/api/v1/anime_videos_controller_spec.rb
@@ -10,7 +10,7 @@ describe Api::V1::AnimeVideosController do
 
     context 'video_token' do
       include_context :authenticated, :user
-      before { make_request }
+      subject! { make_request }
       let(:video_token) { Api::V1::AnimeVideosController::RYUTER_TOKEN }
 
       it do
@@ -22,7 +22,7 @@ describe Api::V1::AnimeVideosController do
 
     context 'trusted video uploader' do
       include_context :authenticated, :trusted_video_uploader
-      before { make_request }
+      subject! { make_request }
 
       it do
         expect(collection).to have(1).item
@@ -33,25 +33,32 @@ describe Api::V1::AnimeVideosController do
 
     context 'common user' do
       include_context :authenticated, :user
-      it { expect{make_request}.to raise_error CanCan::AccessDenied }
+      it { expect { make_request }.to raise_error CanCan::AccessDenied }
     end
   end
 
   describe '#create', :show_in_doc do
     include_context :authenticated, :user
-    let(:video_params) {{
-      state: 'uploaded',
-      kind: kind,
-      author_name: 'test',
-      episode: 3,
-      url: 'http://smotret-anime.ru/catalog/anime-princessa-vampir-miyu-1988-2/ova-1-seriya-93732/russkie-subtitry-758598',
-      source: 'http://url-for-page-where-you-got-video',
-      language: 'russian',
-      quality: 'bd',
-      anime_id: anime.id
-    }}
+    let(:video_params) do
+      {
+        state: 'uploaded',
+        kind: kind,
+        author_name: 'test',
+        episode: 3,
+        url: 'http://smotret-anime.ru/catalog/anime-princessa-vampir-miyu-1988-2/ova-1-seriya-93732/russkie-subtitry-758598',
+        source: 'http://url-for-page-where-you-got-video',
+        language: 'russian',
+        quality: 'bd',
+        anime_id: anime.id
+      }
+    end
 
-    before { post :create, anime_id: anime.id, anime_video: video_params, format: :json }
+    subject! do
+      post :create,
+        anime_id: anime.id,
+        anime_video: video_params,
+        format: :json
+    end
 
     let(:kind) { 'fandub' }
 
@@ -71,7 +78,7 @@ describe Api::V1::AnimeVideosController do
     let(:video) { create :anime_video, anime: anime }
     let!(:upload_report) { create :anime_video_report, anime_video: video, kind: 'uploaded', user: user }
 
-    before { delete :destroy, anime_id: anime.id, id: video.id, format: :json }
+    subject! { delete :destroy, anime_id: anime.id, id: video.id, format: :json }
 
     it do
       expect(resource).to be_destroyed
