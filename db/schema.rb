c732ac7da9654bad92cce317e94077c10ee15a4a
Andrey Sidorov <takandar@gmail.com>
2016-11-01 01:09:11 +0300

    create styles for clubs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031103024) do
+ActiveRecord::Schema.define(version: 20161031220746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
