b8788fbb4211c94b64594dfb7451655ae743e9ae
Andrey Sidorov <takandar@gmail.com>
2016-11-01 00:38:48 +0300

    fix spec run on ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,6 @@ RSpec.configure do |config|
 
   config.backtrace_exclusion_patterns << /gems/
   config.order = (ENV['CIRCLE_CI'] ? :defined : :random)
-  config.filter_run :focus
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
