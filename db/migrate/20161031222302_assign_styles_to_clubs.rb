b226e4da2a609dc2dbd50f1c39b9d575461bd687
Andrey Sidorov <takandar@gmail.com>
2016-11-01 01:24:18 +0300

    final styles migration for clubs

---
+++ db/migrate/20161031222302_assign_styles_to_clubs.rb
@@ -0,0 +1,12 @@
+class AssignStylesToClubs < ActiveRecord::Migration
+  def up
+    Club.includes(:styles).find_each do |club|
+      puts "processing club #{club.id}"
+      club.update_column :style_id, club.styles.first.id if club.styles.any?
+    end
+  end
+
+  def down
+    Club.update_all style_id: nil
+  end
+end
