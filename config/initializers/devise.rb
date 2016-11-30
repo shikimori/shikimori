2071f5802347861bb340e6fac269e73fd5c47d3c
Andrey Sidorov <takandar@gmail.com>
2016-11-30 20:03:54 +0300

    disable vk proxy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -156,17 +156,7 @@ Devise.setup do |config|
     :vkontakte,
     Rails.application.secrets.oauth[:vkontakte][:app_id],
     Rails.application.secrets.oauth[:vkontakte][:app_secret],
-    scope: Rails.application.secrets.oauth[:vkontakte][:app_permissions],
-    client_options: {
-      connection_opts: {
-        proxy: {
-          # FLOPS Russia
-          uri: 'http://91.239.26.54:3128',
-          user: 'uptimus',
-          password: 'holy_grail'
-        }
-      }
-    }
+    scope: Rails.application.secrets.oauth[:vkontakte][:app_permissions]
   )
 
   config.omniauth(
