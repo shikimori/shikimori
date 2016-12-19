53570eaf27ff46698893d60718049de63fc622aa
Andrey Sidorov <takandar@gmail.com>
2016-12-19 22:58:46 +0300

    fix spec

--- app/services/import/anime.rb
+++ app/services/import/anime.rb
@@ -1,2 +1,20 @@
 class Import::Anime < Import::Base
+  SPECIAL_FIELDS = %i(
+    genres studios related
+  )
+
+private
+
+  def assign_genres genres
+  end
+
+  def assign_studios studios
+  end
+
+  def assign_related related
+  end
+
+  def klass
+    Anime
+  end
 end
