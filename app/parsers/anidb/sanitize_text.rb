3b986e2f24b739546cbf6c61ce4dd2e11628b8b3
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2016-12-22 21:59:04 +0300

    fix parsing links for anidb description

--- app/parsers/anidb/sanitize_text.rb
+++ app/parsers/anidb/sanitize_text.rb
@@ -1,5 +1,5 @@
 class Anidb::SanitizeText < Mal::SanitizeText
-  SOURCE_REGEXP = %r{
+  ANIDB_SOURCE_REGEXP = %r{
     \n*
     <i>
     Source:\s(?<source>.+?)
@@ -7,9 +7,28 @@ class Anidb::SanitizeText < Mal::SanitizeText
     \n*
   }mix
 
+  ANIDB_CHARACTER_LINK_REGEXP = %r{
+    <a [^>]*? href="(?<url>https://anidb.net/ch[^"]+)" [^>]*? >
+      (?<name>[^<]*)
+    </a>
+  }mix
+
+  ANIDB_CREATOR_LINK_REGEXP = %r{
+    <a [^>]*? href="(?<url>https://anidb.net/cr[^"]+)" [^>]*? >
+      (?<name>[^<]*)
+    </a>
+  }mix
+
   private
 
   def bb_source text
-    super.gsub(SOURCE_REGEXP, '[source]\k<source>[/source]')
+    super.gsub(ANIDB_SOURCE_REGEXP, '[source]\k<source>[/source]')
+  end
+
+  def bb_link text
+    text
+      .gsub(ANIDB_CHARACTER_LINK_REGEXP, '[\k<name>]')
+      .gsub(ANIDB_CREATOR_LINK_REGEXP, '[\k<name>]')
+      .gsub(LINK_REGEXP, '\k<name>')
   end
 end
