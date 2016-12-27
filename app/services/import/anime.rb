458cde0ef32d3579bb85b885cf2d21ad8777d995
Andrey Sidorov <takandar@gmail.com>
2016-12-27 20:45:49 +0300

    import studios

--- app/services/import/anime.rb
+++ app/services/import/anime.rb
@@ -12,16 +12,23 @@ private
         db_entry.mal_id == genre[:id]
       end
       db_genre ||= Genre.create!(
-        mal_id: genre[:id],
-        name: genre[:name],
-        kind: :anime
+        mal_id: genre[:id], name: genre[:name], kind: :anime
       )
       entry.genres << db_genre
     end
   end
-  # rubocop:enable MethodLength
 
   def assign_studios studios
+    studios.each do |studio|
+      db_studio = Repos::Studios.instance.all.find do |db_entry|
+        db_entry.id == studio[:id]
+      end
+      db_studio ||= Studio.create!(
+        id: studio[:id],
+        name: studio[:name]
+      )
+      entry.studios << db_studio
+    end
   end
 
   def assign_related related
--- spec/services/import/anime_spec.rb
+++ spec/services/import/anime_spec.rb
@@ -4,11 +4,13 @@ describe Import::Anime do
     {
       id: id,
       name: 'Test test 2',
-      genres: genres
+      genres: genres,
+      studios: studios
     }
   end
   let(:id) { 987_654_321 }
   let(:genres) { [] }
+  let(:studios) { [] }
 
   subject(:entry) { service.call }
 
@@ -34,6 +36,31 @@ describe Import::Anime do
         expect(entry.genres.first).to have_attributes(
           mal_id: genre.mal_id,
           name: genre.name
+        )
+      end
+    end
+  end
+
+  describe '#assign_studios' do
+    let(:studios) { [{ id: 1, name: 'test' }] }
+
+    context 'new studio' do
+      it do
+        expect(entry.reload.studios).to have(1).item
+        expect(entry.studios.first).to have_attributes(
+          id: studios.first[:id],
+          name: studios.first[:name]
+        )
+      end
+    end
+
+    context 'present studio' do
+      let!(:studio) { create :studio, id: studios.first[:id] }
+      it do
+        expect(entry.studios).to have(1).item
+        expect(entry.studios.first).to have_attributes(
+          id: studio.id,
+          name: studio.name
         )
       end
     end
