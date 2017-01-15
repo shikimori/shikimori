fa216f7ed0316732a7efb91f3d8aeeef1dab2824
Andrey Sidorov <takandar@gmail.com>
2017-01-15 17:22:00 +0300

    Revert "attempt to fix error in comments_helper"
    This reverts commit 6f04a5e9a66693755f5f214a6c7c0bee29ba2ba5.

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -176,24 +176,19 @@ module CommentHelper
           end
 
           begin
-            id = $~[:id]
-            match = $~[:match]
-            text = $~[:text]
-            quote = $~[:quote]
-
-            comment = klass.find id
+            comment = klass.find $~[:id]
             user = comment.respond_to?(:user) ? comment.user : comment.from
-            name = text.present? ? text : user.nickname
+            name = $~[:text].present? ? $~[:text] : user.nickname
 
-            if quote.present?
-              text.gsub! match, "<a href=\"#{profile_url user}\" title=\"#{ERB::Util.h user.nickname}\" class=\"bubbled b-user16\" data-href=\"#{url}\">
+            if $~[:quote].present?
+              text.gsub! $~[:match], "<a href=\"#{profile_url user}\" title=\"#{ERB::Util.h user.nickname}\" class=\"bubbled b-user16\" data-href=\"#{url}\">
 <img src=\"#{user.avatar_url 16}\" srcset=\"#{user.avatar_url 32} 2x\" alt=\"#{ERB::Util.h user.nickname}\" /><span>#{ERB::Util.h user.nickname}</span></a>#{user.sex == 'male' ? 'написал' : 'написала'}:"
             else
-              text.gsub! match, "<a href=\"#{profile_url user}\" title=\"#{ERB::Util.h user.nickname}\" class=\"bubbled b-mention\" data-href=\"#{url}\"><s>@</s><span>#{name}</span></a>"
+              text.gsub! $~[:match], "<a href=\"#{profile_url user}\" title=\"#{ERB::Util.h user.nickname}\" class=\"bubbled b-mention\" data-href=\"#{url}\"><s>@</s><span>#{name}</span></a>"
             end
 
           rescue
-            text.gsub! match, "<span class=\"bubbled b-link\" data-href=\"#{url}\">@#{text}</span>"
+            text.gsub! $~[:match], "<span class=\"bubbled b-link\" data-href=\"#{url}\">@#{$~[:text]}</span>"
           end
 
         elsif klass == Review
