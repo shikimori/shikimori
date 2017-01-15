d95998a857509fcabdd3fa0fe6b773c94808b60a
Andrey Sidorov <takandar@gmail.com>
2017-01-15 04:23:02 +0300

    disable old importers

--- config/clock.rb
+++ config/clock.rb
@@ -73,14 +73,14 @@ module Clockwork
   end
 
   every 1.day, 'daily.mangas', at: '04:00' do
-    MangasImporter.perform_async
+    # MangasImporter.perform_async
     ReadMangaWorker.perform_async
     AdultMangaWorker.perform_async
   end
 
-  every 1.day, 'daily.characters', at: '03:00' do
-    CharactersImporter.perform_async
-  end
+  # every 1.day, 'daily.characters', at: '03:00' do
+    # CharactersImporter.perform_async
+  # end
 
   every 1.day, 'daily.viewings_cleaner', at: '05:00' do
     ViewingsCleaner.perform_async
@@ -97,7 +97,7 @@ module Clockwork
   every 1.week, 'weekly.stuff', at: 'Monday 01:45' do
     FindAnimeWorker.perform_async :two_pages
     HentaiAnimeWorker.perform_async :first_page
-    PeopleImporter.perform_async
+    # PeopleImporter.perform_async
     DanbooruTagsImporter.perform_async
     OldMessagesCleaner.perform_async
     OldNewsCleaner.perform_async
