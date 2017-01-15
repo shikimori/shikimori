75ce4a2ca9aa12af947b1568af307bd77de870e7
Andrey Sidorov <takandar@gmail.com>
2017-01-15 05:01:25 +0300

    add ban texts

--- config/initializers/mal_parser.rb
+++ config/initializers/mal_parser.rb
@@ -7,6 +7,9 @@ BAD_ID_ERRORS = [
   'No manga found, check the manga id and try again',
   'No series found, check the series id and try again'
 ]
+BAN_TEXTS = [
+  'Access has been restricted for this account'
+]
 
 MalParser.configuration.http_get = lambda do |url|
   cookie = %w(
@@ -18,11 +21,12 @@ MalParser.configuration.http_get = lambda do |url|
     'Cookie' => cookie
   }
 
-  Rails.cache.fetch(url, expires_in: 1.day) do
+  Rails.cache.fetch([url, :v2], expires_in: 1.day) do
     content = Proxy.get(
       url,
       timeout: 30,
       required_text: REQUIRED_TEXT,
+      ban_texts: BAN_TEXTS,
       no_proxy: Rails.env.test?,
       log: !Rails.env.test?
     )
