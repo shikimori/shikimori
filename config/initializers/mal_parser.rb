3e40b49a60db4d54ff54f1fa89a41ec79fd2aaa8
Andrey Sidorov <takandar@gmail.com>
2017-01-16 10:36:50 +0300

    фикс для парсера

--- config/initializers/mal_parser.rb
+++ config/initializers/mal_parser.rb
@@ -12,19 +12,17 @@ BAN_TEXTS = [
 ]
 
 MalParser.configuration.http_get = lambda do |url|
-  Rails.cache.fetch([url, :v2], expires_in: 1.day) do
-    content = Proxy.get(
+  html = Rails.cache.fetch([url, :v2], expires_in: 1.day) do
+    Proxy.get(
       url,
       timeout: 30,
       required_text: REQUIRED_TEXT,
       ban_texts: BAN_TEXTS,
       no_proxy: Rails.env.test?,
       log: !Rails.env.test?
-    )
-
-    raise EmptyContentError, url unless content
-    raise InvalidIdError, url if BAD_ID_ERRORS.any? { |v| content.include? v }
-
-    content
+    ) || raise(EmptyContentError)
   end
+
+  raise InvalidIdError, url if BAD_ID_ERRORS.any? { |v| html.include? v }
+  html
 end
