8b5939b8a1a7b9a9056c9fd3526395dfc7a75448
Andrey Sidorov <takandar@gmail.com>
2017-01-18 21:53:15 +0300

    [#1145] fix anime video reports for guests

--- app/controllers/moderations/anime_video_reports_controller.rb
+++ app/controllers/moderations/anime_video_reports_controller.rb
@@ -1,4 +1,6 @@
 class Moderations::AnimeVideoReportsController < ModerationsController
+  skip_before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:create]
   load_and_authorize_resource except: :index
 
   def index
