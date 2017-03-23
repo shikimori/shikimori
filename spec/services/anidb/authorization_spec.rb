1378f676ecbeeae70e6c1b3f8546c944c5f137d2
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2017-03-23 22:01:53 +0300

    add Anidb::Authorization

---
+++ spec/services/anidb/authorization_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+describe Anidb::Authorization, :vcr do
+  let(:service) { Anidb::Authorization.instance }
+
+  describe '#cookie' do
+    subject { service.cookie }
+
+    it do
+      is_expected.to eq %w(
+        adbautopass=vbzjomexrccnxcla;
+        adbautouser=naruto2148;
+        adbsessuser=naruto2148;
+        adbuin=1490295269-RLyR;
+        anidbsettings=%7B%22USEAJAX%22%3A1%7D;
+      )
+    end
+
+    describe 'caching' do
+      let(:cookies) { 'zzz' }
+      before do
+        allow(Rails.cache).to receive :write
+        allow(service).to receive(:authorize).and_return cookies
+      end
+      before { subject }
+
+      it do
+        expect(Rails.cache)
+          .to have_received(:write)
+          .with Anidb::Authorization::CACHE_KEY, cookies, {}
+      end
+    end
+  end
+
+  describe '#refresh' do
+    before do
+      allow(Rails.cache).to receive :delete
+      allow(service).to receive :cookie
+    end
+    before { service.refresh }
+
+    it do
+      expect(Rails.cache)
+        .to have_received(:delete)
+        .with Anidb::Authorization::CACHE_KEY
+      expect(service).to have_received :cookie
+    end
+  end
+end
--- spec/services/anidb/parse_description_spec.rb
+++ spec/services/anidb/parse_description_spec.rb
@@ -43,12 +43,12 @@ describe Anidb::ParseDescription, :vcr do
     it { expect { call }.to raise_error EmptyContentError }
   end
 
-  context 'unkown anime id' do
+  context 'unknown anime id' do
     let(:url) { 'http://anidb.net/perl-bin/animedb.pl?show=anime&aid=33911111' }
     it { expect { call }.to raise_error InvalidIdError }
   end
 
-  context 'unkown character id' do
+  context 'unknown character id' do
     let(:url) { 'http://anidb.net/perl-bin/animedb.pl?show=character&charid=33311111' }
     it { expect { call }.to raise_error InvalidIdError }
   end
