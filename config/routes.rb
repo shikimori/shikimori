ab0e05abc01740faeeaea55195b57a67085b16e6
Andrey Sidorov <takandar@gmail.com>
2017-04-03 10:28:04 +0300

    add seo redirect

--- config/routes.rb
+++ config/routes.rb
@@ -567,6 +567,7 @@ Rails.application.routes.draw do
 
     # seo redirects
     constraints kind: /animes|mangas/, other: /.*/, other2: /.*/ do
+      get ':kind/status/planned:other' => redirect { |params, request| "/#{params[:kind]}/status/anons#{params[:other]}" }
       get ':kind/season/planned:other' => redirect { |params, request| "/#{params[:kind]}/status/anons#{params[:other]}" }
       get ':kind/season/ongoing:other' => redirect { |params, request| "/#{params[:kind]}/status/ongoing#{params[:other]}" }
       get ':kind/season/latest:other' => redirect { |params, request| "/#{params[:kind]}/status/latest#{params[:other]}" }
