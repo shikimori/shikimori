e8e9e913c536636691c2a62dfefae5b56cd7bff0
Andrey Sidorov <takandar@gmail.com>
2017-04-07 15:59:24 +0300

    fix error on circle ci

--- app/controllers/ignores_controller.rb
+++ app/controllers/ignores_controller.rb
@@ -8,10 +8,15 @@ class IgnoresController < ShikimoriController
 
     Ignore.import ignores, on_duplicate_key_ignore: true
 
-    redirect_back(
-      fallback_location: edit_profile_url(
-        current_user, page: 'ignored_topics'
-      )
-    )
+    redirect_back fallback_location: fallback_url
+
+  rescue ActiveRecord::RecordNotUnique
+    redirect_back fallback_location: fallback_url
+  end
+
+private
+
+  def fallback_url
+    edit_profile_url current_user, page: 'ignored_topics'
   end
 end
