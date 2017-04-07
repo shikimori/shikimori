959774f6e97937bec1970a9985c39b6f59cf3a13
Andrey Sidorov <takandar@gmail.com>
2017-04-07 15:59:45 +0300

    rubocop fix

--- app/controllers/ignores_controller.rb
+++ app/controllers/ignores_controller.rb
@@ -9,7 +9,6 @@ class IgnoresController < ShikimoriController
     Ignore.import ignores, on_duplicate_key_ignore: true
 
     redirect_back fallback_location: fallback_url
-
   rescue ActiveRecord::RecordNotUnique
     redirect_back fallback_location: fallback_url
   end
