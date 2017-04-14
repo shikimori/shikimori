ec7104a789306a68b0b0071d7bd56c66b2f45ce2
Andrey Sidorov <takandar@gmail.com>
2017-04-14 14:07:54 +0300

    fix club_page topic poster display

--- app/view_objects/topics/club_page_view.rb
+++ app/view_objects/topics/club_page_view.rb
@@ -3,7 +3,13 @@ class Topics::ClubPageView < Topics::View
     super 'b-club_page-topic'
   end
 
+  def poster is_2x
+    @topic.user.avatar_url is_2x ? 80 : 48
+  end
+
   def html_body
+    return super if preview?
+
     Rails.cache.fetch [topic.linked, :html] do
       BbCodeFormatter.instance.format_comment(topic.linked.text)
     end
