a121e97297051f658e6d626820b322d892cf0bd7
Andrey Sidorov <takandar@gmail.com>
2017-05-03 12:09:56 +0300

    babel debug true

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -4,7 +4,13 @@ module.exports = {
   loader: 'babel-loader',
   options: {
     presets: [
-      ['env', { modules: false }],
+      [
+        'env', {
+          modules: false,
+          // browsers: ['> 1%', 'last 2 versions', 'ie >= 9'],
+          debug: true
+        }
+      ],
     ],
     // plugins: [
       // require('babel-plugin-es6-promise')
