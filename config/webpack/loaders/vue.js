06899e2fd2add5b3f15b5ba03716dbd1f2212ca1
Andrey Sidorov <takandar@gmail.com>
2017-05-08 14:04:27 +0300

    upgrade vue-loader

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,3 +1,27 @@
+// Change it to false if you prefer Vue styles to be inlined by javascript in runtime
+const extractStyles = true
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { env } = require('../configuration.js')
+
+const cssLoader = [
+  { loader: 'css-loader', options: { minimize: env === 'production' } },
+  'postcss-loader',
+]
+const sassLoader = cssLoader.concat(['sass-loader?indentedSyntax'])
+const scssLoader = cssLoader.concat(['sass-loader'])
+
+function vueLoader(loader) {
+  if (extractStyles) {
+    return ExtractTextPlugin.extract({
+      fallback: 'vue-style-loader',
+      use: loader
+    })
+  } else {
+    return ['vue-style-loader'].concat(sassLoader)
+  }
+}
+
 module.exports = {
   test: /.vue$/,
   loader: 'vue-loader',
@@ -5,8 +29,9 @@ module.exports = {
     loaders: {
       js: 'babel-loader',
       file: 'file-loader',
-      scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
-      sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax'
+      css: vueLoader(cssLoader),
+      scss: vueLoader(scssLoader),
+      sass: vueLoader(sassLoader)
     }
   }
 }
