b37b5ccf7b3db6aa026ba8c23230e41b6cbed6b2
Andrey Sidorov <takandar@gmail.com>
2017-05-14 17:31:51 +0300

    change order in anidb import description query

--- app/query_objects/anidb/import_descriptions_query.rb
+++ app/query_objects/anidb/import_descriptions_query.rb
@@ -3,19 +3,32 @@
 class Anidb::ImportDescriptionsQuery
   MAL_SOURCE = '%[source]http://myanimelist.net/anime/%[/source]'
 
+  DESCRIPTION_SQL = <<-SQL.squish
+    description_en LIKE '#{MAL_SOURCE}' OR
+    description_en = '' OR
+    description_en IS NULL
+  SQL
+  ORDER_SQL = <<-SQL.squish
+    (case
+      when popularity=0
+      then 999999
+      else popularity
+    end), (case
+      when censored=true
+      then 2
+      else 1
+    end)
+  SQL
+
   class << self
     def for_import db_entry_relation
       db_entry_relation
         .includes(:anidb_external_link)
         .joins(:external_links)
-        .where("
-          description_en LIKE '#{MAL_SOURCE}' OR
-          description_en = '' OR
-          description_en IS NULL
-        ".squish)
+        .where(DESCRIPTION_SQL)
         .where(external_links: { imported_at: nil })
         .where(external_links: { kind: Types::ExternalLink::Kind[:anime_db] })
-        .order(:ranked)
+        .order(ORDER_SQL)
     end
   end
 end
