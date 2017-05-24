209a3fc6ffe982a508be971179dcfff9af22e2ef
Andrey Sidorov <takandar@gmail.com>
2017-05-24 22:02:39 +0300

    collection_version

---
+++ app/models/versions/collection_version.rb
@@ -0,0 +1,43 @@
+class Versions::CollectionVersion < Version
+  def apply_changes
+    item.class.transaction do
+      item_diff.each do |(association, (_old_collection, new_collection))|
+        item.send(association).delete_all
+        import_collection association, new_collection
+      end
+      item.touch
+    end
+  end
+
+  def rollback_changes
+    item.class.transaction do
+      item_diff.each do |(association, (old_collection, _new_collection))|
+        item.send(association).delete_all
+        import_collection association, old_collection
+      end
+      item.touch
+    end
+  end
+
+private
+
+  def import_collection association, collection
+    klass = association.classify.constantize
+    models = collection.map { |item| klass.new fix(item) }
+
+    klass.import models
+  end
+
+  def fix hash
+    hash.each_with_object({}) do |(key, value), memo|
+      next if key == 'id'
+
+      memo[key] =
+        if key.match?(/^[\w_]+_at$/) && value.present?
+          Time.zone.parse(value)
+        else
+          value
+        end
+    end
+  end
+end
---
+++ spec/models/versions/collection_version_spec.rb
@@ -0,0 +1,64 @@
+describe Versions::CollectionVersion do
+  let(:anime) { create :anime }
+  let(:version) do
+    create :collection_version,
+      item: anime,
+      item_diff: {
+        external_links: [
+          anime.external_links,
+          external_links_data
+        ]
+      }
+  end
+
+  let!(:external_link) do
+    create :external_link,
+      entry: anime,
+      created_at: 2.weeks.ago,
+      updated_at: 1.week.ago
+  end
+  let(:external_links_data) do
+    [{
+      url: 'http://ya.ru',
+      kind: 'wikipedia',
+      source: 'shikimori',
+      entry_type: anime.class.name,
+      entry_id: anime.id
+    }, {
+      url: 'http://google.com',
+      kind: 'anime_db',
+      source: 'shikimori',
+      entry_type: anime.class.name,
+      entry_id: anime.id
+    }]
+  end
+
+  describe 'instance methods' do
+    describe '#apply_changes' do
+      before { version.apply_changes }
+
+      it do
+        expect(anime.reload.external_links).to have(2).items
+        expect(anime.external_links.first)
+          .to have_attributes external_links_data.first
+        expect(anime.external_links.second)
+          .to have_attributes external_links_data.second
+        expect { external_link.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    describe '#rollback_changes' do
+      before { version.apply_changes }
+      before { version.rollback_changes }
+
+      it do
+        expect(anime.reload.external_links).to have(1).item
+        expect(anime.external_links.first)
+          .to have_attributes external_link.attributes.except('id', 'updated_at', 'created_at')
+        expect(anime.external_links.first.created_at)
+          .to be_within(0.1).of(external_link.created_at)
+        expect { external_link.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+end
