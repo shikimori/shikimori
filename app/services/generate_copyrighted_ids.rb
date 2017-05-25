b98ceea1df2f5c4bf3d1a53f103393b1eff12461
Andrey Sidorov <takandar@gmail.com>
2017-05-25 12:05:43 +0300

    service to generate copyrighted_ids

---
+++ app/services/generate_copyrighted_ids.rb
@@ -0,0 +1,72 @@
+class GenerateCopyrightedIds < ServiceObjectBase
+  URL_TEMPLATE = 'https://lumendatabase.org/notices/search?'\
+    'utf8=%E2%9C%93&term=shikimori.org&sort_by=&page='
+
+  VALID_PATH = %r{/(animes|mangas|ranobe|characters|people)/\w+(?=-)}
+  FAIL_TEXT = 'Search Temporarily Unavailable'
+
+  CONFIG_FILE = "#{::Rails.root.to_s}/config/app/copyrighted_ids.yml"
+
+  def call
+    hash = copyrighted_entries
+    File.open(CONFIG_FILE, 'w') { |f| YAML.dump hash, f }
+    hash
+  end
+
+  def copyrighted_entries
+    all_links.each_with_object({}) do |url, memo|
+      path = URI(url).path
+      next unless path.match? VALID_PATH
+      parts = path.split('/')
+
+      type = parts[1].singularize.to_sym
+      id = parts[2].gsub(/-.*/, '')
+
+      (memo[type] ||= []).push id
+      (memo[:ranobe] ||= []).push id if type == :manga
+    end
+  end
+
+  def all_links
+    1.upto(total_pages).flat_map do |page|
+      page_links(page)
+    end
+  end
+
+  def page_links page
+    page_doc(page).css('li.excerpt').text.strip
+      .split("\n")
+      .map(&:strip)
+      .select(&:present?)
+  end
+
+  def total_pages
+    page_doc(1).css('.last a')[0].attr('href').match(/page=(\d+)/)[1].to_i
+  end
+
+private
+
+  def page_doc page
+    Nokogiri::HTML get_page(URL_TEMPLATE + page.to_s)
+  end
+
+  # rubocop:disable AbcSize
+  # rubocop:disable MethodLength
+  def get_page url
+    Rails.cache.fetch(url, expires_in: 1.week) do
+      response = Faraday.get(url) do |req|
+        req.options[:timeout] = 300
+        req.options[:open_timeout] = 300
+      end
+
+      if response.body && !response.body.include?(FAIL_TEXT)
+        sleep 1 unless Rails.env.test?
+        response.body
+      else
+        raise 'search unavailable'
+      end
+    end
+  end
+  # rubocop:enable MethodLength
+  # rubocop:enable AbcSize
+end
---
+++ spec/services/generate_copyrighted_ids_spec.rb
@@ -0,0 +1,43 @@
+describe GenerateCopyrightedIds, vcr: { cassette_name: 'GenerateCopyrightedIds' } do
+  let(:service) { GenerateCopyrightedIds.new }
+
+  describe '#entries', :focus do
+    before { allow(service).to receive(:total_pages).and_return 1 }
+    subject { service.entries }
+
+    it do
+      is_expected.to eq(
+        anime: %w[369 32995 2964 30831 11887 13655 145 1846 11241],
+        manga: %w[14483],
+        ranobe: %w[14483],
+        person: %w[5572],
+      )
+    end
+  end
+
+  describe '#all_links' do
+    before { allow(service).to receive(:total_pages).and_return 2 }
+    subject { service.all_links }
+
+    it do
+      is_expected.to have(31).items
+      expect(subject.first)
+        .to eq 'http://shikimori.org/animes/369-boogiepop-wa-warawanai-boogiepop-phantom.html'
+    end
+  end
+
+  describe '#page_links' do
+    subject { service.page_links 1 }
+
+    it do
+      is_expected.to have(14).items
+      expect(subject.first)
+        .to eq 'http://shikimori.org/animes/369-boogiepop-wa-warawanai-boogiepop-phantom.html'
+    end
+  end
+
+  describe '#total_pages' do
+    subject { service.total_pages }
+    it { is_expected.to eq 51 }
+  end
+end
