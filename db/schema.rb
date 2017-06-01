ed0bbf81653b9e4c5901ce76bc80950eb9a396e6
Andrey Sidorov <takandar@gmail.com>
2017-06-02 00:12:27 +0300

    destroy all is_deleted_by_from=true messages (#1289)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527110338) do
+ActiveRecord::Schema.define(version: 20170601211125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
