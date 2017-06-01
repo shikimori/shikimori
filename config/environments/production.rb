c6734632c2c91da083a9554f22abd818e1c34693
Andrey Sidorov <takandar@gmail.com>
2017-06-01 22:21:29 +0300

    add expires to hsts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,9 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = { hsts: { preload: true, subdomains: true } }
+  config.ssl_options = {
+    hsts: { preload: true, subdomains: true, expires: 10.years }
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
