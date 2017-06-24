01358c9207739e9d599690dc1cd2329465ac09d8
Andrey Sidorov <takandar@gmail.com>
2017-06-24 14:49:32 +0300

    fix translation

--- app/models/topics/news_topics/contest_status_topic.rb
+++ app/models/topics/news_topics/contest_status_topic.rb
@@ -1,9 +1,11 @@
 class Topics::NewsTopics::ContestStatusTopic < Topics::NewsTopic
+  include Translation
+
   enumerize :action,
     in: Types::Topic::NewsTopic::ContestStatusTopic::Action.values,
     predicates: true
 
   def title
-    I18n.t "topics/contest_status_topic.title.#{action}"
+    i18n_t "title.#{action}"
   end
 end
