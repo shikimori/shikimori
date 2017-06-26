71c1cb5bb67a94158f4c7bf88de0449b90fd0aed
Andrey Sidorov <takandar@gmail.com>
2017-06-26 12:12:30 +0300

    fix spec

--- spec/models/topics/news_topics/contest_status_topic_spec.rb
+++ spec/models/topics/news_topics/contest_status_topic_spec.rb
@@ -1,9 +1,17 @@
-describe Topics::NewsTopics::ContestStartedTopic do
+describe Topics::NewsTopics::ContestStatusTopic do
   describe 'instance methods' do
-    let(:topic) { build :contest_started_topic }
+    let(:topic) { build :contest_status_topic, action: action }
 
     describe '#title' do
-      it { expect(topic.title).to eq 'Опрос запущен' }
+      context 'started' do
+        let(:action) { Types::Topic::ContestStatusTopic::Action[:started] }
+        it { expect(topic.title).to eq 'Старт опроса' }
+      end
+
+      context 'finished' do
+        let(:action) { Types::Topic::ContestStatusTopic::Action[:finished] }
+        it { expect(topic.title).to eq 'Завершение опроса' }
+      end
     end
   end
 end
