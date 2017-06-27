99c65164afcb51e35c7d12885d7995b6c9e2a5eb
Andrey Sidorov <takandar@gmail.com>
2017-06-27 17:04:00 +0300

    migration to cleanup contests suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626214810) do
+ActiveRecord::Schema.define(version: 20170627140226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
