c93633933a5fc5dd9e47f62882f06c60759c4971
Andrey Sidorov <takandar@gmail.com>
2017-06-27 17:01:52 +0300

    cleanup suggestions on contest_finish

--- app/operations/contest/finish.rb
+++ app/operations/contest/finish.rb
@@ -6,8 +6,19 @@ class Contest::Finish
       @contest.finish!
       @contest.update! finished_on: Time.zone.today
 
-      User.update_all @contest.user_vote_key => false
+      cleanup_suggestions
+      reset_user_vote_key
       Messages::CreateNotification.new(@contest).contest_finished
     end
+  end
+
+private
+
+  def reset_user_vote_key
+    User.update_all @contest.user_vote_key => false
+  end
+
+  def cleanup_suggestions
+    contest.suggestions.delete_all
   end
 end
