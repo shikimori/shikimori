263e9cab6279b0a46d2ad79f16f47d2cb1bef230
Andrey Sidorov <takandar@gmail.com>
2017-06-27 18:00:49 +0300

    generate winners for finished contests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627140226) do
+ActiveRecord::Schema.define(version: 20170627145841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
