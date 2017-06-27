e5d8f736f36515bc12081d00b0e84d0656018d7d
Andrey Sidorov <takandar@gmail.com>
2017-06-27 17:57:36 +0300

    cosplay_gallery spec

--- spec/models/cosplay_gallery_spec.rb
+++ spec/models/cosplay_gallery_spec.rb
@@ -1,71 +1,17 @@
 # frozen_string_literal: true
 
 describe CosplayGallery do
-  describe 'topics concern' do
-    describe 'associations' do
-      it { is_expected.to have_many :topics }
-    end
-
-    describe 'instance methods' do
-      let(:model) { build_stubbed :cosplay_gallery }
-      let!(:cosplayer) { seed :cosplay_user }
-
-      describe '#generate_topics' do
-        let(:topics) { model.topics }
-        before { model.generate_topics [:en, :ru] }
-
-        it do
-          expect(topics).to have(2).items
-          expect(topics.first.locale).to eq 'ru'
-          expect(topics.second.locale).to eq 'en'
-        end
-      end
-
-      describe '#topic' do
-        let(:topic) { model.topic locale }
-        before { model.generate_topics [:en, :ru] }
-
-        context 'ru topic' do
-          let(:locale) { :ru }
-          it do
-            expect(topic).to be_present
-            expect(topic.locale).to eq locale.to_s
-          end
-        end
-
-        context 'en topic' do
-          let(:locale) { :en }
-          it do
-            expect(topic).to be_present
-            expect(topic.locale).to eq locale.to_s
-          end
-        end
-      end
-
-      describe '#maybe_topic' do
-        let(:topic) { model.maybe_topic locale }
-        before { model.generate_topics [:en, :ru] }
-
-        context 'ru topic' do
-          let(:locale) { :ru }
-          it do
-            expect(topic).to be_present
-            expect(topic.locale).to eq locale
-          end
-        end
-
-        context 'en topic' do
-          let(:locale) { :en }
-          it do
-            expect(topic).to be_present
-            expect(topic.locale).to eq locale.to_s
-          end
-        end
-      end
-
-      describe '#topic_user' do
-        it { expect(model.topic_user).to eq cosplayer }
-      end
-    end
+  describe 'relations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :image }
+    it { is_expected.to have_many(:images).dependent :destroy }
+    it { is_expected.to have_many :deleted_iamges }
+    it { is_expected.to have_many(:links).dependent :destroy }
+    it { is_expected.to have_many :cosplayers }
+    it { is_expected.to have_many :animes }
+    it { is_expected.to have_many :mangas }
+    it { is_expected.to have_many : }
   end
+
+  it_behaves_like :topics_concern, :collection
 end
