6e813b9212b00a06a42d3b32248dadb8a05603e5
Andrey Sidorov <takandar@gmail.com>
2017-07-13 13:19:52 +0300

    log level error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :fatal
+  config.log_level = :error
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
