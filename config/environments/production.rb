be6ffc3f609652a99aa3a03d7eeb102614d5f91f
Andrey Sidorov <takandar@gmail.com>
2017-07-19 18:16:32 +0300

    fatal log level on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :error
+  # config.log_level = :error
+  config.log_level = :fatal
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
