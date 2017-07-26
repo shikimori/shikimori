b742284d79f08e70147d9e991f4090affac90113
Andrey Sidorov <takandar@gmail.com>
2017-07-26 23:01:44 +0300

    updates

--- app/services/user_list_parser.rb
+++ app/services/user_list_parser.rb
@@ -15,7 +15,9 @@ class UserListParser
         # ).parse(params[:login])
 
       when :xml
-        UserListParsers::XmlListParser.new(@klass).parse extract_xml(params[:file])
+        UserListParsers::XmlListParser.new(@klass).parse(
+          extract_xml(params[:file])
+        )
 
       else
         raise UnsupportedListType, params[:list_type]
@@ -23,15 +25,18 @@ class UserListParser
   end
 
 private
+
   def extract_xml file
-    if file.respond_to?(:read)
-      if file.respond_to?(:original_filename) && file.original_filename =~ /\.gz$/
-        Zlib::GzipReader.new(file.tempfile, external_encoding: 'utf-8', internal_encoding: 'utf-8').read
-      else
-        File.open(file.path, encoding: 'utf-8').read
-      end
+    return file unless file.respond_to?(:read)
+
+    if file.respond_to?(:original_filename) && file.original_filename =~ /\.gz$/
+      Zlib::GzipReader.new(
+        file.tempfile,
+        external_encoding: 'utf-8',
+        internal_encoding: 'utf-8'
+      ).read
     else
-      file
+      File.open(file.path, encoding: 'utf-8').read
     end
   end
 end
