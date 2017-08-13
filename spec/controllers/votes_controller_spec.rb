5ce05dc33201ba358854a2520f76d76c62b933af
Andrey Sidorov <takandar@gmail.com>
2017-08-13 12:45:57 +0300

    fix votes controller

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,44 +1,20 @@
 describe VotesController do
-  let(:user) { FactoryGirl.create :user }
-  let(:entry) { FactoryGirl.create :review }
-  let(:params) do
-    {
-      id: entry.to_param,
-      type: entry.class.name,
-      voting: 'yes'
-    }
-  end
+  include_context :authenticated, :user
+  let(:votable) { create :review }
 
   describe '#create' do
-    it 'guest' do
-      post :create, params: params
-      expect(response).to be_redirect
+    before do
+      post :create,
+        params: {
+          id: votable.to_param,
+          type: votable.class.name,
+          voting: 'yes'
+        }
     end
 
-    describe 'sign_in user' do
-      before { sign_in user }
-
-      it 'success' do
-        post :create, params: params
-        expect(response).to be_success
-      end
-
-      it 'only once' do
-        expect {
-          post :create, params: params
-          post :create, params: params.merge(voting: 'no')
-        }.to change(Vote, :count).by(1)
-
-        expect(user.votes.first.voting).to eq false
-
-        expect(response).to be_success
-      end
-
-      # it 'forbidden for own' do
-        # entry2 =  FactoryGirl.create :review, user: user
-        # post :create, params: { id: entry2.to_param, type: entry2.class.name, voting: 'yes' }
-        # expect(response).to be_forbidden
-      # end
+    it do
+      expect(user.liked? votable).to eq true
+      expect(response).to be_success
     end
   end
 end
