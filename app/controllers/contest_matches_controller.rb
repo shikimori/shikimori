620235ec5b9481fa7e6df6a1b7252a28f5bbeb3b
Andrey Sidorov <takandar@gmail.com>
2017-08-31 00:40:14 +0300

    some optimizations

--- app/controllers/contest_matches_controller.rb
+++ app/controllers/contest_matches_controller.rb
@@ -1,28 +1,7 @@
 class ContestMatchesController < ShikimoriController
   before_action { noindex }
 
-  # rubocop:disable AbcSize
-  # rubocop:disable MethodLength
   def show
     @resource = ContestMatch.find(params[:id]).decorate
-
-    # if user_signed_in?
-      # user_vote = ContestMatch::VOTABLE[
-        # current_user.voted_as_when_voted_for(@resource)
-      # ]
-    # end
-
-    # @vote_status =
-      # if @resource.pending?
-        # user_vote
-      # elsif @resource.finished?
-        # if @resource.winner_id == @resource.left_id
-          # ContestMatch::VOTABLE[true]
-        # else
-          # ContestMatch::VOTABLE[false]
-        # end
-      # end
   end
-  # rubocop:enable MethodLength
-  # rubocop:enable AbcSize
 end
