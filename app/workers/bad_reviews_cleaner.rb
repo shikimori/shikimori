025bb755900781a7a6daa52669b56ea3236f9511
Andrey Sidorov <takandar@gmail.com>
2017-09-18 01:16:57 +0300

    fix BadReviewsCleaner

--- app/workers/bad_reviews_cleaner.rb
+++ app/workers/bad_reviews_cleaner.rb
@@ -32,11 +32,9 @@ private
   end
 
   def low_level? review
-    review_votes = votes.select {|v| v.voteable_id == review.id }
+    votes_total = review.cached_votes_up + review.cached_votes_down
 
-    votes_count = review_votes.size * 1.0
-    votes_against = review_votes.count {|v| !v.voting }
-
-    votes_count >= MINIMUM_VOTES && (votes_against / votes_count) >= 0.8
+    votes_total >= MINIMUM_VOTES &&
+      (review.cached_votes_down / votes_total) >= 0.8
   end
 end
