7a181735736e7eaa5b2ebaad35a8ccc19b1968ea
Andrey Sidorov <takandar@gmail.com>
2017-09-17 23:59:43 +0300

    fix spec

--- spec/services/network/faraday_get_spec.rb
+++ spec/services/network/faraday_get_spec.rb
@@ -1,13 +1,5 @@
 describe Network::FaradayGet, :vcr do
   subject(:response) { Network::FaradayGet.call url }
-
-  context 'valid url' do
-    let(:url) { 'http://ya.ru' }
-    it { expect(response.status).to eq 200 }
-  end
-
-  context 'bad url' do
-    let(:url) { 'http://ya.r' }
-    it { expect(response).to be_nil }
-  end
+  let(:url) { 'http://ya.ru' }
+  it { expect(response.status).to eq 200 }
 end
