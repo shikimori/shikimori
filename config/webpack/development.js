5cf78a5fc1e059ecbcb129ab4bc58da0676e6c3e
Andrey Sidorov <takandar@gmail.com>
2017-09-24 23:35:16 +0300

    fix webpacker

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,7 +2,7 @@ const environment = require('./environment')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
-environment.plugins.set('HardSourceWebpackPlugin', new HardSourceWebpackPlugin())
+// environment.plugins.set('HardSourceWebpackPlugin', new HardSourceWebpackPlugin())
 environment.plugins.set('FriendlyErrorsWebpackPlugin', new FriendlyErrorsWebpackPlugin())
 environment.addCacheLoader()
 
