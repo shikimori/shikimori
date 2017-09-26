a52df4a70afb7107f51793030c081d95edd130b6
Andrey Sidorov <takandar@gmail.com>
2017-09-27 02:05:01 +0300

    send user_rate details on neko.put

--- app/services/neko/update.rb
+++ app/services/neko/update.rb
@@ -12,7 +12,29 @@ private
 
   def neko_params user, user_rate_id, action
     params = { user_id: user.id, action: action }
-    params[:id] = user_rate_id if user_rate_id
+
+    if user_rate_id
+      params[:id] = user_rate_id
+
+      if action == Types::Neko::Action[:put]
+        params = params.merge(user_rate_params(user_rate_id))
+      end
+    end
+
     params
+  end
+
+  def user_rate_params user_rate_id
+    user_rate = UserRate.find user_rate_id
+
+    {
+      id: user_rate_id,
+      target_id: user_rate.target_id,
+      score: user_rate.score,
+      status: user_rate.status,
+      episodes: user_rate.episodes
+    }
+  rescue ActiveRecord::RecordNotFound
+    { action: Types::Neko::Action[:delete] }
   end
 end
--- spec/services/neko/update_spec.rb
+++ spec/services/neko/update_spec.rb
@@ -1,7 +1,5 @@
 describe Neko::Update do
   let(:user) { build_stubbed :user }
-  let(:action) { Types::Neko::Action[:noop] }
-  let(:user_rate_id) { 123 }
 
   before do
     allow(Neko::Request)
@@ -18,16 +16,81 @@ describe Neko::Update do
       action: action
   end
 
-  it do
-    expect(Neko::Request)
-      .to have_received(:call)
-      .with(
-        id: user_rate_id,
-        user_id: user.id,
-        action: action
-      )
-    expect(Neko::Apply)
-      .to have_received(:call)
-      .with user, neko_result
+  context 'delete' do
+    let(:action) { Types::Neko::Action[:delete] }
+    let(:user_rate_id) { 123 }
+
+    it do
+      expect(Neko::Request)
+        .to have_received(:call)
+        .with(
+          id: user_rate_id,
+          user_id: user.id,
+          action: action
+        )
+      expect(Neko::Apply)
+        .to have_received(:call)
+        .with user, neko_result
+    end
+  end
+
+  context 'put' do
+    let(:action) { Types::Neko::Action[:put] }
+
+    context 'present user_rate' do
+      let(:user_rate) { create :user_rate }
+      let(:user_rate_id) { user_rate.id }
+
+      it do
+        expect(Neko::Request)
+          .to have_received(:call)
+          .with(
+            id: user_rate_id,
+            target_id: user_rate.target_id,
+            score: user_rate.score,
+            status: user_rate.status,
+            episodes: user_rate.episodes,
+            user_id: user.id,
+            action: action
+          )
+        expect(Neko::Apply)
+          .to have_received(:call)
+          .with user, neko_result
+      end
+    end
+
+    context 'deleted user_rate' do
+      let(:user_rate_id) { 9999988887777 }
+
+      it do
+        expect(Neko::Request)
+          .to have_received(:call)
+          .with(
+            id: user_rate_id,
+            user_id: user.id,
+            action: Types::Neko::Action[:delete]
+          )
+        expect(Neko::Apply)
+          .to have_received(:call)
+          .with user, neko_result
+      end
+    end
+  end
+
+  context 'other actions' do
+    let(:action) { Types::Neko::Action[:noop] }
+    let(:user_rate_id) { nil }
+
+    it do
+      expect(Neko::Request)
+        .to have_received(:call)
+        .with(
+          user_id: user.id,
+          action: action
+        )
+      expect(Neko::Apply)
+        .to have_received(:call)
+        .with user, neko_result
+    end
   end
 end
