9f14d750cc7d8bd60b5b0675dcbe645e0fb6792c
Andrey Sidorov <takandar@gmail.com>
2017-10-14 17:02:15 +0300

    update js packages

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,37 @@
+const { join } = require('path')
+const webpacker_config = require('@rails/webpacker/package/config')
 const environment = require('./environment')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
 environment.plugins.set('FriendlyErrorsWebpackPlugin', new FriendlyErrorsWebpackPlugin())
-environment.addCacheLoader()
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+
+// cache-loader
+function addCacheLoader(rules) {
+  const cacheLoader = {
+    loader: 'cache-loader',
+    options: {
+      cacheDirectory: join(webpacker_config.cache_path, 'cache-loader')
+    }
+  }
+
+  for (let rule of rules) {
+    if (rule.use) {
+      rule.use.unshift(cacheLoader)
+    } else if (rule.loader) {
+      let ruleLoader = null
+      if (rule.options) {
+        ruleLoader = { loader: rule.loader, options: rule.options }
+        delete rule.options
+      }
+      rule.use = [cacheLoader, ruleLoader || rule.loader]
+      delete rule.loader
+    }
+  }
+  return rules
+}
+addCacheLoader(config.module.rules)
+
+
+module.exports = config
