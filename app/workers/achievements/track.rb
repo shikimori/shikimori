979815c4840e08b8ae074a41df45e359e8abaaba
Andrey Sidorov <takandar@gmail.com>
2017-10-15 21:45:31 +0300

    Revert "temporarily disable Achievements::Track"
    This reverts commit 8dc9319bce3fc8c88ddf7002b9a112faeb923c0d.

--- app/workers/achievements/track.rb
+++ app/workers/achievements/track.rb
@@ -12,13 +12,13 @@ class Achievements::Track
   ERRORS = Network::FaradayGet::NET_ERRORS + [RedisMutex::LockError]
 
   def perform user_id, user_rate_id, action
-    # user = User.find user_id
+    user = User.find user_id
 
-    # Retryable.retryable tries: 5, on: ERRORS, sleep: 3 do
-      # RedisMutex.with_lock("neko_#{user_id}", MUTEX_OPTIONS) do
-        # neko_update user, user_rate_id, action
-      # end
-    # end
+    Retryable.retryable tries: 5, on: ERRORS, sleep: 3 do
+      RedisMutex.with_lock("neko_#{user_id}", MUTEX_OPTIONS) do
+        neko_update user, user_rate_id, action
+      end
+    end
   end
 
 private
