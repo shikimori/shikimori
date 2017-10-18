90f2d0a95f06f8613ee672f3d7c778c9a481ac2f
Andrey Sidorov <takandar@gmail.com>
2017-10-19 00:07:28 +0300

    get rid of redis-mutex. neko supports multiple parallel requests per user now

--- app/workers/achievements/track.rb
+++ app/workers/achievements/track.rb
@@ -8,16 +8,17 @@ class Achievements::Track
   )
   sidekiq_retry_in { 60 * 60 * 24 }
 
-  MUTEX_OPTIONS = { block: 60, expire: 60 }
-  ERRORS = Network::FaradayGet::NET_ERRORS + [RedisMutex::LockError]
+  RETRYABLE_OPTIONS = {
+    tries: 5,
+    on: Network::FaradayGet::NET_ERRORS,
+    sleep: 3
+  }
 
   def perform user_id, user_rate_id, action
     user = User.find user_id
 
-    Retryable.retryable tries: 5, on: ERRORS, sleep: 3 do
-      RedisMutex.with_lock("neko_#{user_id}", MUTEX_OPTIONS) do
-        neko_update user, user_rate_id, action
-      end
+    Retryable.retryable RETRYABLE_OPTIONS do
+      neko_update user, user_rate_id, action
     end
   end
 
