3032896023c0befdb5993094cd3404ffe7751db8
Andrey Sidorov <takandar@gmail.com>
2017-10-23 22:47:15 +0300

    rubocop fix

--- lib/mail_logger_interceptor.rb
+++ lib/mail_logger_interceptor.rb
@@ -1,11 +1,22 @@
 class MailLoggerInterceptor
+  # rubocop:disable AbcSize
   def self.delivering_email mail
     return if mail.to.empty?
 
+    body =
+      if mail.body.parts.first
+        mail.body.parts.first.body.to_s
+      else
+        mail.body.to_s
+      end
+
     # http://stackoverflow.com/a/18423438/3632318
-    ::NamedLogger.email.info "TO: #{Array(mail.to).join ', '}
-FROM: #{mail.from.join ', '}
-SUBJECT: #{mail.subject}
-BODY: #{mail.body.parts.first ? mail.body.parts.first.body.to_s : mail.body.to_s}"
+    ::NamedLogger.email.info <<~TEXT
+      TO: #{Array(mail.to).join ', '}
+      FROM: #{mail.from.join ', '}
+      SUBJECT: #{mail.subject}
+      BODY: #{body}
+    TEXT
   end
+  # rubocop:enable AbcSize
 end
