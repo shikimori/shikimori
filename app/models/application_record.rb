4476c06895b4224b4197a5617f2da31a199b76b7
Andrey Sidorov <takandar@gmail.com>
2017-10-26 00:52:09 +0300

    new attempt to fix bigints

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,37 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   class << self
+    # fixes .where(id: 11111111111111111111111111) - bigint
+    # https://github.com/rails/rails/issues/20428
+    def where(*args)
+      id_key = args.size == 1 && args[0].is_a?(Hash) && args[0].key?(:id)
+
+      if id_key && _fixable_ids?(args[0][:id])
+        super(id: _fix_ids(args[0][:id]))
+      else
+        super
+      end
+    end
+
+    def _fixable_ids? ids
+      ids.is_a?(String) || ids.is_a?(Integer) || ids.is_a?(Array)
+    end
+
+    def _fix_ids ids
+      if ids.is_a? Array
+        _fix_id ids
+      else
+        ids.map { |id| _fix_id(id) }.compact
+      end
+    end
+
+    def _fix_id id
+      return id if id.is_a?(String) && !id.match?(/\A\d+/)
+
+      int_id = id.is_a?(String) ? Integer(id) : id
+      (1..2_147_483_647).cover?(int_id) ? int_id : nil
+    end
+
     def boolean_attribute attribute_name
       define_method "#{attribute_name}?" do
         send "is_#{attribute_name}"
