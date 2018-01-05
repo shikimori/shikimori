f0f20ee077466fb91b9e2c2424eb9b8fa0c005b0
Andrey Sidorov <takandar@gmail.com>
2018-01-06 02:17:18 +0300

    upgrade webpacker to 3.2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,18 +2,24 @@
 const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
 
+const coffee = require('./loaders/coffee')
+const vue = require('./loaders/vue')
+
 environment.loaders.get('babel').exclude =
   /node_modules\/(?!delay|p-defer|get-js)/
 
-environment.loaders.get('vue').options.extractCSS = false
+environment.loaders.get('file').exclude =
+  /\.(js|jsx|coffee|ts|tsx|vue|elm|scss|sass|css|html|json|pug)?(\.erb)?$/
 
-environment.loaders.set('jade', {
+environment.loaders.append('vue', vue)
+environment.loaders.append('coffee', coffee)
+
+environment.loaders.append('jade', {
   test: /\.(?:jade|pug)$/,
   loader: 'pug-loader',
   exclude: /node_modules/
 })
-
-environment.plugins.set(
+environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     moment: 'moment',
@@ -26,7 +32,7 @@ environment.plugins.set(
   })
 )
 
-environment.plugins.set(
+environment.plugins.append(
   'ContextReplacement',
   new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /ru/)
 )
