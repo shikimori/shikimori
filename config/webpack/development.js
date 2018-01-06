11a0c11c82c3f68129d3f39d8872df71ec8b149e
Andrey Sidorov <takandar@gmail.com>
2018-01-06 20:49:43 +0300

    trusted self signed certificate for webpack-dev-server

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,6 +2,7 @@ const { join } = require('path')
 const webpacker_config = require('@rails/webpacker/package/config')
 const environment = require('./environment')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const { readFileSync } = require('fs')
 
 environment.plugins.set('FriendlyErrorsWebpackPlugin', new FriendlyErrorsWebpackPlugin())
 
@@ -32,5 +33,29 @@ function addCacheLoader(rules) {
   return rules
 }
 addCacheLoader(config.module.rules)
+
+// generate certifacte with
+// https://www.shellhacks.com/ru/create-csr-openssl-without-prompt-non-interactive/
+/*
+openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout config/webpack/ssl/key.pem \
+    -new \
+    -out config/webpack/ssl/server.pem \
+    -subj "/C=RU/O=shikimori/CN=shikimori.dev" \
+    -reqexts SAN \
+    -config <(cat /System/Library/OpenSSL/openssl.cnf \
+        <(printf '[SAN]\nsubjectAltName=DNS:localhost, DNS:shikimori.dev, DNS:*.shikimori.dev')) \
+    -sha256 \
+    -days 3650
+*/
+// open certificate in keychain and change trust options to "Always trust"
+
+config.devServer.https = {
+  key: readFileSync(join(__dirname, 'ssl/key.pem')),
+  cert: readFileSync(join(__dirname, 'ssl/server.pem'))
+}
 
 module.exports = config
