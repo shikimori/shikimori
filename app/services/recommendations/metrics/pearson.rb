8dfadff2b07ae5731aec75a0b5cf421266ec2af6
Andrey Sidorov <takandar@gmail.com>
2018-01-16 21:55:38 +0300

    fix for recommendations

--- app/services/recommendations/metrics/pearson.rb
+++ app/services/recommendations/metrics/pearson.rb
@@ -1,9 +1,9 @@
 class Recommendations::Metrics::Pearson < Recommendations::Metrics::MetricBase
-  #def ignore_similarity? similarity
-    #super || similarity <= 0
-  #end
+  # def ignore_similarity? similarity
+    # super || similarity <= 0
+  # end
 
-  def compare user_id, user_rates, sampler_id, sampler_rates
+  def compare _user_id, user_rates, _sampler_id, sampler_rates
     return 0 unless user_rates && sampler_rates
     shared_ids = user_rates.keys & sampler_rates.keys
     return 0 if shared_ids.empty? || shared_ids.size < MINIMUM_SHARED
@@ -16,15 +16,18 @@ class Recommendations::Metrics::Pearson < Recommendations::Metrics::MetricBase
 
       sum1   += prefs1_item
       sum2   += prefs2_item
-      sum1Sq += prefs1_item ** 2
-      sum2Sq += prefs2_item ** 2
+      sum1Sq += prefs1_item**2
+      sum2Sq += prefs2_item**2
       pSum   += prefs2_item * prefs1_item
     end
 
-    num = pSum - ( ( sum1 * sum2 ) / shared_ids.size )
-    den = Math.sqrt( ( sum1Sq - ( sum1 ** 2 ) / shared_ids.size ) * ( sum2Sq - ( sum2 ** 2 ) / shared_ids.size ) )
+    num = pSum - ((sum1 * sum2) / shared_ids.size)
 
-    return 0 if den == 0
+    metric = (sum1Sq - (sum1**2) / shared_ids.size) *
+      (sum2Sq - (sum2**2) / shared_ids.size)
+    return 0 if metric <= 0
+
+    den = Math.sqrt(metric)
 
     num / den
   end
