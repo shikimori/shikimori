db6fe9aae2bf4c5cf3d1010560b72e2ee6d99374
Andrey Sidorov <takandar@gmail.com>
2018-02-24 03:55:04 +0300

    do not cleanup oauth tokens

--- app/workers/users/cleanup_doorkeeper_tokens.rb
+++ app/workers/users/cleanup_doorkeeper_tokens.rb
@@ -1,20 +1,23 @@
-class Users::CleanupDoorkeeperTokens
-  include Sidekiq::Worker
+# TODO: make a better cleanup worker
+# do not cleanup by expiration time
+# cleanup all tokens which are put into previous_refresh_token column
+# class Users::CleanupDoorkeeperTokens
+  # include Sidekiq::Worker
 
-  CLEANUP_INTERVAL = 4.months
+  # CLEANUP_INTERVAL = 4.months
 
-  EXPIRE_SQL = <<-SQL
-    (revoked_at is not null and revoked_at < :delete_before)
-    or (
-      expires_in is not null
-      and (created_at + expires_in * interval '1 second') < :delete_before
-    )
-  SQL
+  # EXPIRE_SQL = <<-SQL
+  #   (revoked_at is not null and revoked_at < :delete_before)
+  #   or (
+  #     expires_in is not null
+  #     and (created_at + expires_in * interval '1 second') < :delete_before
+  #   )
+  # SQL
 
-  def perform
-    expire = [EXPIRE_SQL, { delete_before: CLEANUP_INTERVAL.ago }]
+  # def perform
+  #   expire = [EXPIRE_SQL, { delete_before: CLEANUP_INTERVAL.ago }]
 
-    Doorkeeper::AccessGrant.where(expire).delete_all
-    Doorkeeper::AccessToken.where(expire).delete_all
-  end
-end
+  #   Doorkeeper::AccessGrant.where(expire).delete_all
+  #   Doorkeeper::AccessToken.where(expire).delete_all
+  # end
+# end
