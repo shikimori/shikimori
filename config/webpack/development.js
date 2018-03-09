363c7a4357e6380c784dcd61cbf54ba4932f2667
Andrey Sidorov <takandar@gmail.com>
2018-03-09 21:38:23 +0300

    upgrade webpacker to 3.3.0. get rid of cache-loader

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,61 +1,69 @@
-const { join } = require('path')
-const { readFileSync } = require('fs')
-const webpacker_config = require('@rails/webpacker/package/config')
 const environment = require('./environment')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
-environment.plugins.set('FriendlyErrorsWebpackPlugin', new FriendlyErrorsWebpackPlugin())
 
-const config = environment.toWebpackConfig()
+// const { join } = require('path')
+// const { readFileSync } = require('fs')
+// const webpacker_config = require('@rails/webpacker/package/config')
+// const environment = require('./environment')
 
-// cache-loader
-function addCacheLoader(rules) {
-  const cacheLoader = {
-    loader: 'cache-loader',
-    options: {
-      cacheDirectory: join(webpacker_config.cache_path, 'cache-loader')
-    }
-  }
+environment.plugins.add({
+  key: 'FriendlyErrorsWebpackPlugin',
+  value: new FriendlyErrorsWebpackPlugin()
+})
 
-  for (let rule of rules) {
-    if (rule.use) {
-      rule.use.unshift(cacheLoader)
-    } else if (rule.loader) {
-      let ruleLoader = null
-      if (rule.options) {
-        ruleLoader = { loader: rule.loader, options: rule.options }
-        delete rule.options
-      }
-      rule.use = [cacheLoader, ruleLoader || rule.loader]
-      delete rule.loader
-    }
-  }
-  return rules
-}
-addCacheLoader(config.module.rules)
+module.exports = environment.toWebpackConfig()
 
-// generate certifacte with
-// https://www.shellhacks.com/ru/create-csr-openssl-without-prompt-non-interactive/
-/*
-openssl req \
-    -newkey rsa:2048 \
-    -x509 \
-    -nodes \
-    -keyout config/webpack/ssl/key.pem \
-    -new \
-    -out config/webpack/ssl/server.pem \
-    -subj "/C=RU/O=shikimori/CN=shikimori.local" \
-    -reqexts SAN \
-    -config <(cat /System/Library/OpenSSL/openssl.cnf \
-        <(printf '[SAN]\nsubjectAltName=DNS:localhost, DNS:shikimori.local, DNS:*.shikimori.local')) \
-    -sha256 \
-    -days 3650
-*/
-// open certificate in keychain and change trust options to "Always trust"
+// const config = environment.toWebpackConfig()
 
-// config.devServer.https = {
-//   key: readFileSync(join(__dirname, 'ssl/key.pem')),
-//   cert: readFileSync(join(__dirname, 'ssl/server.pem'))
+// // cache-loader
+// function addCacheLoader(rules) {
+//   const cacheLoader = {
+//     loader: 'cache-loader',
+//     options: {
+//       cacheDirectory: join(webpacker_config.cache_path, 'cache-loader')
+//     }
+//   }
+
+//   for (let rule of rules) {
+//     if (rule.use) {
+//       rule.use.unshift(cacheLoader)
+//     } else if (rule.loader) {
+//       let ruleLoader = null
+//       if (rule.options) {
+//         ruleLoader = { loader: rule.loader, options: rule.options }
+//         delete rule.options
+//       }
+//       rule.use = [cacheLoader, ruleLoader || rule.loader]
+//       delete rule.loader
+//     }
+//   }
+//   return rules
 // }
+// addCacheLoader(config.module.rules)
 
-module.exports = config
+// // generate certifacte with
+// // https://www.shellhacks.com/ru/create-csr-openssl-without-prompt-non-interactive/
+// [>
+// openssl req \
+//     -newkey rsa:2048 \
+//     -x509 \
+//     -nodes \
+//     -keyout config/webpack/ssl/key.pem \
+//     -new \
+//     -out config/webpack/ssl/server.pem \
+//     -subj "/C=RU/O=shikimori/CN=shikimori.local" \
+//     -reqexts SAN \
+//     -config <(cat /System/Library/OpenSSL/openssl.cnf \
+//         <(printf '[SAN]\nsubjectAltName=DNS:localhost, DNS:shikimori.local, DNS:*.shikimori.local')) \
+//     -sha256 \
+//     -days 3650
+// */
+// // open certificate in keychain and change trust options to "Always trust"
+
+// // config.devServer.https = {
+// //   key: readFileSync(join(__dirname, 'ssl/key.pem')),
+// //   cert: readFileSync(join(__dirname, 'ssl/server.pem'))
+// // }
+
+// module.exports = config
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,16 +3,16 @@ const environment = require('./environment')
 
 // https://webpack.js.org/plugins/commons-chunk-plugin/
 environment.plugins.get('ExtractText').options.allChunks = true
-environment.plugins.set(
-  'CommonsChunk',
-  new webpack.optimize.CommonsChunkPlugin({
+environment.plugins.add({
+  key: 'CommonsChunk',
+  value: new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
     minChunks: function (module) {
       // this assumes your vendor imports exist in the node_modules directory
       return module.context && module.context.indexOf('node_modules') !== -1;
     }
   })
-)
+})
 
 // environment.plugins.delete('UglifyJs')
 // environment.plugins.delete('Compression')
