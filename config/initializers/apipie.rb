d3949300def15867267b9910d6ad0aace2f23b65
Andrey Sidorov <takandar@gmail.com>
2018-03-29 18:17:29 +0300

    update apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = 'Shikimori API'
   config.api_base_url            = '/api'
   config.doc_base_url            = '/api/doc'
+  config.show_all_examples       = true
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
   config.default_version         = '2.0'
   config.api_routes              = Rails.application.routes
