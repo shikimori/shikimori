408649de391decb72e71f30c1b7c43e3043ee8e8
Andrey Sidorov <takandar@gmail.com>
2018-05-06 01:08:07 +0300

    bugfix

--- app/controllers/users/achievements_controller.rb
+++ app/controllers/users/achievements_controller.rb
@@ -35,8 +35,8 @@ private
 
   def check_access
     return if Rails.env.development?
-    return if current_user&.admin?
-    return if ACHIEVEMENTS_CLUB_USER_IDS.include(@user.id)
+    return if кurrent_user&.admin?
+    return if ACHIEVEMENTS_CLUB_USER_IDS.include?(@user.id)
     return if @user.nickname == 'test2'
 
     raise ActiveRecord::RecordNotFound
