57223d4a8747898eab98134e6fd07bff5817927a
Andrey Sidorov <takandar@gmail.com>
2018-05-11 21:02:31 +0300

    fix franchise

--- db/migrate/20180511175954_fix_franchises_v4.rb
+++ db/migrate/20180511175954_fix_franchises_v4.rb
@@ -1,7 +1,7 @@
 class FixFranchisesV4 < ActiveRecord::Migration[5.1]
   def change
     Animes::UpdateFranchises.new.call(
-      Anime.where(franchise: %w[dr_slump])
+      Anime.where(franchise: %w[dr_slump toaru_majutsu_no_index])
     )
   end
 end
