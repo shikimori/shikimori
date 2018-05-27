734dc3a59bad2067f34c2797c9f1ac1aece62663
Andrey Sidorov <takandar@gmail.com>
2018-05-28 01:57:59 +0300

    bugfix

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
-if Rails.env.production && defined?(Bugsnag)
+if Rails.env.production? && defined?(Bugsnag)
   Bugsnag.configure do |config|
     config.api_key = "eecc8f1fe89fa10263c304de595d26ac"
   end
