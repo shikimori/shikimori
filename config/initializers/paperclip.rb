a66907c39ecd6e50ae868080ce517f91273c28b8
Andrey Sidorov <takandar@gmail.com>
2018-05-28 02:56:42 +0300

    get rid of Cocaine errors. this gem is not longer used for paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -79,22 +79,16 @@ module Paperclip
     private
 
     def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          "%[exif:orientation]" : "1"
-        Paperclip.run(
-          "identify",
-          "-format '%wx%h,#{orientation}' :file", {
-            :file => "#{path}[0]"
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Cocaine::ExitStatusError
-        ""
-      rescue Cocaine::CommandNotFoundError => e
-        raise_because_imagemagick_missing
-      end
+      orientation = Paperclip.options[:use_exif_orientation] ?
+        "%[exif:orientation]" : "1"
+      Paperclip.run(
+        "identify",
+        "-format '%wx%h,#{orientation}' :file", {
+          :file => "#{path}[0]"
+        }, {
+          :swallow_stderr => true
+        }
+      )
     end
 
     def path
@@ -105,10 +99,6 @@ module Paperclip
       if path.blank?
         raise Errors::NotIdentifiedByImageMagickError.new("Cannot find the geometry of a file with a blank name")
       end
-    end
-
-    def raise_because_imagemagick_missing
-      raise Errors::CommandNotFoundError.new("Could not run the `identify` command. Please install ImageMagick.")
     end
   end
 end
