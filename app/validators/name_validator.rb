6344763eb729c38a1cbd5b1a87682a1e6b43d159
Andrey Sidorov <takandar@gmail.com>
2018-05-29 02:08:38 +0300

    new forbiddent names

--- app/validators/name_validator.rb
+++ app/validators/name_validator.rb
@@ -1,8 +1,8 @@
 class NameValidator < ActiveModel::EachValidator
-  BANNED_NICKNAMES = %w(
+  BANNED_NICKNAMES = %w[
     Youtoome
-  )
-  PREDEFINED_PATHS = %i(
+  ]
+  PREDEFINED_PATHS = %i[
     api
     achievements
     animes
@@ -18,44 +18,34 @@ class NameValidator < ActiveModel::EachValidator
     polls
     oauth
     oauth2
-  )
-  FORBIDDEN_NAMES = %r(
-    \A(
+  ]
+  FORBIDDEN_NAMES = /
+    \A(?:
       #{Forum::VARIANTS.join '|'} |
       #{PREDEFINED_PATHS.join '|'} |
       #{BANNED_NICKNAMES.join '|'}
-    )\Z | ((
-      \.css |
-      \.js |
-      \.jpg |
-      \.jpeg |
-      \.png |
-      \.gif |
-      \.css |
-      \.js |
-      \.ttf |
-      \.eot |
-      \.otf |
-      \.svg |
-      \.woff |
-      \.php
-    )\Z)
-  )mix
+    )\Z | (?:
+      \.
+      (?:css|js|jpg|jpeg|png|gif|css|js|ttf|eot|otf|svg|woff|php|rar|zip)
+    \Z)
+  /mix
 
   def validate_each record, attribute, value
-    return unless value.kind_of? String
+    return unless value.is_a? String
 
-    is_taken = value =~ FORBIDDEN_NAMES ||
+    is_taken = value.match?(FORBIDDEN_NAMES) ||
       presence(record, value, Club, :name) ||
       presence(record, value, User, :nickname)
 
     if is_taken
-      message = options[:message] || I18n.t('activerecord.errors.messages.taken')
+      message = options[:message] ||
+        I18n.t('activerecord.errors.messages.taken')
       record.errors[attribute] << message
     end
 
     if Banhammer.instance.abusive? value
-      message = options[:message] || I18n.t('activerecord.errors.messages.abusive')
+      message = options[:message] ||
+        I18n.t('activerecord.errors.messages.abusive')
       record.errors[attribute] << message
     end
   end
@@ -63,7 +53,7 @@ class NameValidator < ActiveModel::EachValidator
 private
 
   def presence record, value, klass, field
-    query = record.kind_of?(klass) ? klass.where.not(id: record.id) : klass
+    query = record.is_a?(klass) ? klass.where.not(id: record.id) : klass
     query
       .where(
         "#{postgres_word_normalizer field} = #{postgres_word_normalizer '?'}",
@@ -73,6 +63,12 @@ private
   end
 
   def postgres_word_normalizer text
-    "translate(lower(unaccent(#{text})), 'абвгдеёзийклмнопрстуфхць', 'abvgdeezijklmnoprstufхc`')"
+    <<~SQL.squish
+      translate(
+        lower(unaccent(#{text})),
+        'абвгдеёзийклмнопрстуфхць',
+        'abvgdeezijklmnoprstufхc`'
+      )
+    SQL
   end
 end
