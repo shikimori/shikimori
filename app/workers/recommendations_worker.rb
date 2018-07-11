e0314868c24fb4b141e8ff2407117a8ae5356d60
Andrey Sidorov <takandar@gmail.com>
2018-07-11 21:27:14 +0300

    refactor a bit

--- app/workers/recommendations_worker.rb
+++ app/workers/recommendations_worker.rb
@@ -1,14 +1,21 @@
-class RecommendationsWorker
+class RecommendationsWorker # rubocop:disable ClassLength
   include Sidekiq::Worker
 
   sidekiq_options(
     unique: :until_executed,
-    unique_args: -> (args) { args.first },
+    unique_args: ->(args) { args.first },
     queue: :cpu_intensive,
     retry: false
   )
 
-  def perform user_id, type, metric, threshold, cache_key, user_list_cache_key
+  def perform( # rubocop:disable ParameterLists
+    user_id,
+    type,
+    metric,
+    threshold,
+    cache_key,
+    user_list_cache_key
+  )
     Rails.cache.fetch cache_key, expires_in: 2.weeks do
       fetch user_id, type.constantize, metric, threshold, user_list_cache_key
     end
@@ -17,95 +24,99 @@ class RecommendationsWorker
 private
 
   def fetch user_id, klass, metric, threshold, user_list_cache_key
-    rates_fetcher = Recommendations::RatesFetcher.new klass
-
-    no_norm = Recommendations::Normalizations::None.new
-    mean_centering = Recommendations::Normalizations::MeanCentering.new
-    z_score = Recommendations::Normalizations::ZScore.new
-
-    sampler = case metric.to_sym
-      when :pearson
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::Pearson.new,
-          rates_fetcher,
-          no_norm,
-          user_list_cache_key
-        )
-
-      # when :pearson_mean
-        # Recommendations::Sampler.new(
-        #   klass,
-        #   Recommendations::Metrics::Pearson.new,
-        #   rates_fetcher,
-        #   mean_centering,
-        #   user_list_cache_key
-        # )
-
-      when :pearson_z
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::Pearson.new,
-          rates_fetcher,
-          z_score,
-          user_list_cache_key
-        )
-
-      when :euclid
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::Euclid.new,
-          rates_fetcher,
-          no_norm,
-          user_list_cache_key
-        )
-
-      when :euclid_z
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::Euclid.new,
-          rates_fetcher,
-          z_score,
-          user_list_cache_key
-        )
-
-      when :svd
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::SvdMetric.new(
-            Svd.where(normalization: :none).last!
-          ),
-          rates_fetcher,
-          no_norm,
-          user_list_cache_key
-        )
-
-      when :svd_mean
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::SvdMetric.new(
-            Svd.where(normalization: :mean_centering).last!
-          ),
-          rates_fetcher,
-          mean_centering,
-          user_list_cache_key
-        )
-
-      when :svd_z
-        Recommendations::Sampler.new(
-          klass,
-          Recommendations::Metrics::SvdMetric.new(
-            Svd.where(normalization: :z_score).last!
-          ),
-          rates_fetcher,
-          z_score,
-          user_list_cache_key
-        )
-
-      else
-        raise ArgumentError, "unknown metric: #{metric}"
-    end
-
+    sampler = send metric.to_sym, klass, user_list_cache_key
     sampler.recommend user_id, threshold
+  end
+
+  def rates_fetcher klass
+    Recommendations::RatesFetcher.new klass
+  end
+
+  def norm_none
+    Recommendations::Normalizations::None.new
+  end
+
+  def norm_mean_centering
+    Recommendations::Normalizations::MeanCentering.new
+  end
+
+  def norm_z_score
+    Recommendations::Normalizations::ZScore.new
+  end
+
+  def perason klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::Pearson.new,
+      rates_fetcher(klass),
+      norm_none,
+      user_list_cache_key
+    )
+  end
+
+  def pearson_z klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::Pearson.new,
+      rates_fetcher(klass),
+      norm_z_score,
+      user_list_cache_key
+    )
+  end
+
+  def euclid klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::Euclid.new,
+      rates_fetcher(klass),
+      norm_none,
+      user_list_cache_key
+    )
+  end
+
+  def euclid_z klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::Euclid.new,
+      rates_fetcher(klass),
+      norm_z_score,
+      user_list_cache_key
+    )
+  end
+
+  def svd klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::SvdMetric.new(
+        Svd.where(normalization: :none).last!
+      ),
+      rates_fetcher(klass),
+      norm_none,
+      user_list_cache_key
+    )
+  end
+
+  def svd_mean klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::SvdMetric.new(
+        Svd.where(normalization: :norm_mean_centering).last!
+      ),
+      rates_fetcher(klass),
+      norm_mean_centering,
+      user_list_cache_key
+    )
+  end
+
+  def svd_z klass, user_list_cache_key
+    Recommendations::Sampler.new(
+      klass,
+      Recommendations::Metrics::SvdMetric.new(
+        Svd.where(normalization: :norm_z_score).last!
+      ),
+      rates_fetcher(klass),
+      norm_z_score,
+      user_list_cache_key
+    )
   end
 end
