288058d5a8370c97f37126810a0773385b8933ca
Andrey Sidorov <takandar@gmail.com>
2018-07-12 00:24:30 +0300

    Animes::Torrents::Get

---
+++ spec/services/animes/torrents/get_spec.rb
@@ -0,0 +1,19 @@
+describe Animes::Torrents::Get do
+  subject { described_class.call anime }
+
+  let(:anime) { build_stubbed :anime }
+
+  context 'has torrents' do
+    let!(:cache_entry) do
+      create :big_data_cache,
+        key: "anime_#{anime.id}_torrents",
+        value: 'zxc'
+    end
+
+    it { is_expected.to eq cache_entry.value }
+  end
+
+  context 'no torrents' do
+    it { is_expected.to eq [] }
+  end
+end
