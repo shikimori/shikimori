3fc843ba2fecefe4271714c2a2a8e8d0b21d19da
Andrey Sidorov <takandar@gmail.com>
2018-07-25 20:51:05 +0300

    fix migration

--- db/migrate/20180725172221_fix_franchises_v6.rb
+++ db/migrate/20180725172221_fix_franchises_v6.rb
@@ -3,5 +3,6 @@ class FixFranchisesV6 < ActiveRecord::Migration[5.1]
     Achievement
       .where(neko_id: 'casshern')
       .update_all neko_id: 'gatchaman'
+    Animes::UpdateFranchises.new.call(Anime.where(franchise: %w[gall_force]))
   end
 end
