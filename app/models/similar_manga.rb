fbd6704e9b0e0f16cbef9283f5090f2a51e997b2
Andrey Sidorov <takandar@gmail.com>
2018-08-11 15:00:45 +0300

    bugfix

--- app/models/similar_manga.rb
+++ app/models/similar_manga.rb
@@ -1,11 +1,11 @@
 class SimilarManga < ApplicationRecord
   belongs_to :src,
-    class_name: Anime.name,
+    class_name: Manga.name,
     foreign_key: :src_id,
     optional: true, # because it can imported from MAL for non existing manga
     touch: true
   belongs_to :dst,
-    class_name: Anime.name,
+    class_name: Manga.name,
     foreign_key: :dst_id,
     optional: true, # because it can imported from MAL for non existing manga
     touch: true
