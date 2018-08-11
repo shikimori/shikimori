bf3fa2f28dedcac702550fb9c3fc4a35ffef8e7d
Andrey Sidorov <takandar@gmail.com>
2018-08-11 22:04:18 +0300

    fix deprecation warning

--- app/query_objects/user_list_query.rb
+++ app/query_objects/user_list_query.rb
@@ -7,29 +7,42 @@ class UserListQuery
   end
 
   def fetch
-    target_ids = AniMangaQuery
-      .new(@klass, @params, @user)
-      .fetch.except(:order).pluck(:id)
-    statuses = UserRate.statuses.keys
-      .each_with_object({}) { |status, memo| memo[status.to_sym] = [] }
-
-    # закоменчено, пока не починят merge в activerecord в хз каком релизе рельс https://github.com/rails/rails/issues/12953
-    user_rates#.merge(AniMangaQuery.new(@klass, @params, @user).fetch.except(:order))
-      .where("#{@klass.table_name}.id in (?)", target_ids)
-      .order("user_rates.status, #{AniMangaQuery.order_sql order, @klass}")
+    user_rates
       .each_with_object(statuses) do |rate, memo|
         memo[rate.status.to_sym] ||= []
         memo[rate.status.to_sym] << rate.decorate
       end
-      .delete_if { |status, rates| rates.none? }
+      .delete_if { |_status, rates| rates.none? }
   end
 
 private
 
   def user_rates
-    @user.send("#{list_type}_rates")
+    # commented until https://github.com/rails/rails/issues/12953 is fixed
+    # user_rates.merge(AniMangaQuery.new(@klass, @params, @user).fetch.except(:order))
+    @user
+      .send("#{list_type}_rates")
       .includes(list_type.to_sym)
       .references(list_type.to_sym)
+      .where("#{@klass.table_name}.id in (?)", target_ids)
+      .order(
+        Arel.sql("user_rates.status, #{AniMangaQuery.order_sql order, @klass}")
+      )
+  end
+
+  def target_ids
+    @target_ids ||= AniMangaQuery
+      .new(@klass, @params, @user)
+      .fetch
+      .except(:order)
+      .pluck(:id)
+  end
+
+  def statuses
+    @statuses ||= UserRate
+      .statuses
+      .keys
+      .each_with_object({}) { |status, memo| memo[status.to_sym] = [] }
   end
 
   def list_type
