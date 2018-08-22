a90c37acfb31b8b5ba1e42fb604b110cf8dfe938
Andrey Sidorov <takandar@gmail.com>
2018-08-22 23:41:23 +0300

    attempt to disable bootsnap on production

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+unless ENV['RAILS_ENV'] == 'production'
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
