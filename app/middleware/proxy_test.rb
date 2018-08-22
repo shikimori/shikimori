922d7a1956f64d17129d76cb1e352bc4dfc48fda
Andrey Sidorov <takandar@gmail.com>
2018-08-23 01:00:44 +0300

    rubocop fix

--- app/middleware/proxy_test.rb
+++ app/middleware/proxy_test.rb
@@ -1,8 +1,8 @@
 class ProxyTest
-  SUCCESS_CONFIRMATION_MESSAGE = "test_passed"
+  SUCCESS_CONFIRMATION_MESSAGE = 'test_passed'
 
-  TEST_PAGE_PATH = "/proxy_test"
-  WHAT_IS_MY_IP_PATH = "/what_is_my_ip"
+  TEST_PAGE_PATH = '/proxy_test'
+  WHAT_IS_MY_IP_PATH = '/what_is_my_ip'
 
   def initialize app
     @app = app
@@ -10,19 +10,25 @@ class ProxyTest
 
   def call env
     if env['PATH_INFO'] == TEST_PAGE_PATH # тест прокси
-      data = env.select {|k,v| k =~ /^[A-Z_]+$/ && k != 'SERVER_ADDR' && k != 'HTTP_COOKIE' }.map {|k,v| v }.join " | "
-      [200, {'Content-Type' => 'text/plain'}, [
-        "#{data} #{SUCCESS_CONFIRMATION_MESSAGE}"
+      [200, { 'Content-Type' => 'text/plain' }, [
+        "#{data(env)} #{SUCCESS_CONFIRMATION_MESSAGE}"
       ]]
     elsif env['PATH_INFO'] == WHAT_IS_MY_IP_PATH # выдача ip
       [200,
-        {'Content-Type' => 'text/plain'},
-        [
-          env['HTTP_X_FORWARDED_FOR'] || env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
-        ]
-      ]
+       { 'Content-Type' => 'text/plain' },
+       [
+         env['HTTP_X_FORWARDED_FOR'] || env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
+       ]]
     else
       @app.call(env)
     end
+  end
+
+private
+
+  def data env
+    env
+      .select { |k, _v| k =~ /^[A-Z_]+$/ && k != 'SERVER_ADDR' && k != 'HTTP_COOKIE' }
+      .map { |_k, v| v }.join ' | '
   end
 end
