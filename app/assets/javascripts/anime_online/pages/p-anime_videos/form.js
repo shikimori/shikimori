d6b0df09dd21679b3479ae814269f5d5d2b85c8a
Andrey Sidorov <takandar@gmail.com>
2018-08-24 04:42:58 +0300

    replace $.ajax with axios request

--- app/assets/javascripts/anime_online/pages/p-anime_videos/form.js
+++ app/assets/javascripts/anime_online/pages/p-anime_videos/form.js
@@ -1,3 +1,5 @@
+import axios from 'helpers/axios';
+
 page_load(
   'anime_videos_new',
   'anime_videos_edit',
@@ -6,35 +8,34 @@ page_load(
   () => {
     const $form = $('form');
 
-    $form.on('submit', (e) => {
+    $form.on('submit', e => {
       if ($form.data('blocked')) {
-        e.preventDefault()
+        e.preventDefault();
       } else {
         $form.data({ blocked: true });
       }
-    })
+    });
 
-
-    const $video_url = $('#anime_video_url');
+    const $videoUrl = $('#anime_video_url');
     const $episode = $('#anime_video_episode');
-    const $video_preview = $('.video-preview');
+    const $videoPreview = $('.video-preview');
 
-    if ($video_preview.data('player_html')) {
-      preview_video($video_preview.data('player_html'));
+    if ($videoPreview.data('player_html')) {
+      previewVideo($videoPreview.data('player_html'));
     }
 
     if ($episode.val() === '') {
       $episode.focus();
     } else {
-      $video_url.focus();
+      $videoUrl.focus();
     }
 
     // клик по "Проверить видео"
-    $('.do-preview').on('click', function() {
-      const video_url = $('#anime_video_url').val();
+    $('.do-preview').on('click', async ({ currentTarget }) => {
+      const videoUrl = $('#anime_video_url').val();
 
       $('.anime_video_url .error').remove();
-      if (!video_url) {
+      if (!videoUrl) {
         $('.anime_video_url').append('<div class="error">не может быть пустым</div>');
         $('#anime_video_url').focus();
         return;
@@ -43,22 +44,12 @@ page_load(
       $form.addClass('b-ajax');
       $('.video-preview').removeClass('hidden');
 
-      $.ajax({
-        url: $(this).data('href'),
-        data: {
-          url: video_url
-        },
-        type: 'POST',
-        dataType: 'json',
-        success(data, status, xhr) {
-          $form.removeClass('b-ajax');
-          preview_video(data.player_html);
-        },
-        error() {
-          $form.removeClass('b-ajax');
-          preview_video(null);
-        }
-      });
+      const { data } = await axios
+        .post($(currentTarget).data('href'), { url: videoUrl })
+        .catch(() => ({ data: null }));
+
+      $form.removeClass('b-ajax');
+      previewVideo(data ? data.player_html : null);
     });
 
     // клик по "Работает и загрузить ещё"
@@ -68,18 +59,18 @@ page_load(
 
     $('#anime_video_author_name')
       .completable()
-      .on('autocomplete:success autocomplete:text', function(e, result) {
+      .on('autocomplete:success autocomplete:text', function (e, result) {
         this.value = (result != null ? result.value : undefined) || result;
-    });
-  });
+      });
+  }
+);
 
-  var preview_video = function(player_html) {
-    $('.video-preview')
-      .show()
-      .html(player_html);
-    $('.create-buttons').show();
+function previewVideo(html) {
+  $('.video-preview')
+    .show()
+    .html(html);
+  $('.create-buttons').show();
 
-    $('.create-buttons .b-errors').toggle(!player_html);
-    return $('.create-buttons .buttons').toggle(!!player_html);
-  };
-});
+  $('.create-buttons .b-errors').toggle(!html);
+  $('.create-buttons .buttons').toggle(!!html);
+}
