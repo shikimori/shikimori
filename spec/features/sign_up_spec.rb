0688b5b423cf3b35c75c8d608a136fa48c13bd4d
Andrey Sidorov <takandar@gmail.com>
2018-08-29 07:19:07 +0300

    fix spec

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,7 +2,10 @@ feature 'sign up' do
   let(:user) { build :user, nickname: 'test+zxc123@gmail.com', password: '123456' }
   let(:created_user) { User.find_by nickname: user.nickname }
 
-  before { allow_any_instance_of(User).to receive :grab_avatar }
+  before do
+    allow_any_instance_of(User).to receive :grab_avatar
+    allow_any_instance_of(User).to receive :add_to_index
+  end
 
   scenario 'when success', :vcr do
     expect { sign_up user }.to change(User, :count).by 1
