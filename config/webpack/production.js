d9f4ffbbfe7ce9ffffefb4503eeb938b3d3540b1
Andrey Sidorov <takandar@gmail.com>
2018-09-07 11:59:45 +0300

    enable scripts compression

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 // const webpack = require('webpack')
 const environment = require('./environment');
 
-environment.plugins.delete('UglifyJs');
-environment.plugins.delete('Compression');
+// environment.plugins.delete('UglifyJs');
+// environment.plugins.delete('Compression');
 
 module.exports = environment.toWebpackConfig();
