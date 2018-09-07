be36a88cfe07a8489402dbc530e2a69c52e77054
Andrey Sidorov <takandar@gmail.com>
2018-09-07 11:56:21 +0300

    debug scripts

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,9 +1,9 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 // const webpack = require('webpack')
-const environment = require('./environment')
+const environment = require('./environment');
 
-// environment.plugins.delete('UglifyJs')
-// environment.plugins.delete('Compression')
-//
-module.exports = environment.toWebpackConfig()
+environment.plugins.delete('UglifyJs');
+environment.plugins.delete('Compression');
+
+module.exports = environment.toWebpackConfig();
