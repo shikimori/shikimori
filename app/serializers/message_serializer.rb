2a3977f8fc1cd027d7a31e25dfb8decd40235718
Andrey Sidorov <takandar@gmail.com>
2018-09-19 00:46:59 +0300

    add linked_id and linked_type to messages serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,10 +1,10 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :kind, :read, :body, :html_body, :created_at, :linked
+  attributes :id, :kind, :read, :body, :html_body, :created_at, :linked_id, :linked_type, :linked
   has_one :from
   has_one :to
 
   def body
-    object.body || (object.linked.to_s if object.linked)
+    object.body || (object.linked&.to_s)
   end
 
   def html_body
@@ -15,8 +15,9 @@ class MessageSerializer < ActiveModel::Serializer
     return nil unless object.linked
     hash = { id: object.linked.id }
 
-    if object.linked && object.linked.kind_of?(Topic)
+    if object.linked&.is_a?(Topic)
       hash[:topic_url] = UrlGenerator.instance.topic_url object.linked
+
       # TODO: deprecated
       hash[:thread_id] = object.linked_id
       hash[:topic_id] = object.linked_id
@@ -25,17 +26,19 @@ class MessageSerializer < ActiveModel::Serializer
       if object.linked.linked
         hash[:type] = object.linked.linked.class.name
 
-        if object.linked.linked.kind_of? Anime
+        if object.linked.linked.is_a? Anime
           hash.merge! AnimeSerializer.new(object.linked.linked).attributes
-        elsif object.linked.linked.kind_of? Manga
+        elsif object.linked.linked.is_a? Manga
           hash.merge! MangaSerializer.new(object.linked.linked).attributes
         end
       end
 
     else
       hash[:type] = object.linked.class.name
-      if object.linked.kind_of? Comment
-        hash[:topic_url] = "#{UrlGenerator.instance.topic_url object.linked.commentable}#comment-#{object.linked.id}"
+      if object.linked.is_a? Comment
+        hash[:topic_url] = UrlGenerator.instance.topic_url(object.linked.commentable) +
+          "#comment-#{object.linked.id}"
+
         # TODO: deprecated
         hash[:thread_id] = object.linked.commentable_id
         hash[:topic_id] = object.linked.commentable_id
