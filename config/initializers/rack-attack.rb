9afe9799ce36ea5881013a4e5ff9a0b5f6bd09e5
Andrey Sidorov <takandar@gmail.com>
2018-09-28 17:40:41 +0300

    allow 3x more requests from smotret-anime

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,3 +1,6 @@
+NEKO_IP = '88.198.7.116'
+SMOTRET_ANIME_USER_AGENT = 'Anime 365 (https://smotret-anime.ru/; info@smotret-anime.ru)'
+
 # Throttle requests to 5 requests per second per ip
 Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
   # If the return value is truthy, the cache key for the return value
@@ -7,14 +10,27 @@ Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
   # If falsy, the cache key is neither incremented nor checked.
 
   # requests from neko are allowed
-  if req.ip != '88.198.7.116'
+  if req.ip != NEKO_IP && req.user_agent != SMOTRET_ANIME_USER_AGENT
+    req.ip
+  end
+end
+
+
+Rack::Attack.throttle('req/ip', limit: 15, period: 1.second) do |req|
+  if req.ip != NEKO_IP && req.user_agent == SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
 
 Rack::Attack.throttle('req/ip', limit: 90, period: 60.second) do |req|
   # requests from neko are allowed
-  if req.ip != '88.198.7.116'
+  if req.ip != NEKO_IP && req.user_agent != SMOTRET_ANIME_USER_AGENT
+    req.ip
+  end
+end
+
+Rack::Attack.throttle('req/ip', limit: 270, period: 60.second) do |req|
+  if req.ip != NEKO_IP && req.user_agent == SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
