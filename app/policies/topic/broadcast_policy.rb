4e6ed9b4dac2c207a4d0db32b39792bdc880a7bd
Andrey Sidorov <takandar@gmail.com>
2018-10-10 00:33:14 +0300

    Topic::BroadcastPolicy

---
+++ app/policies/topic/broadcast_policy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Topic::BroadcastPolicy
+  pattr_initialize :topic
+
+  def required?
+    return false if @topic.processed
+
+    (@topic.broadcast && @topic.saved_change_to_broadcast?) ||
+      (@topic.generated? && @topic.is_a?(Topics::NewsTopic))
+  end
+end
---
+++ spec/policies/topic/broadcast_policy_spec.rb
@@ -0,0 +1,53 @@
+describe Topic::BroadcastPolicy do
+  subject(:policy) { described_class.new topic }
+  let(:topic) do
+    create :topic,
+      broadcast: broadcast,
+      processed: processed,
+      generated: generated
+  end
+
+  let(:broadcast) { false }
+  let(:processed) { false }
+  let(:generated) { false }
+
+  describe '#required?' do
+    it { is_expected.to_not be_required }
+
+    context 'broadcast' do
+      let(:broadcast) { true }
+      it { is_expected.to be_required }
+
+      context 'not broadcast change' do
+        before { topic.update body: 'zxc' }
+        it { is_expected.to_not be_required }
+      end
+
+      context 'already processed topic' do
+        let(:processed) { true }
+        it { is_expected.to_not be_required }
+      end
+    end
+
+    context 'generated news topic' do
+      let(:topic) do
+        create :news_topic,
+          processed: processed,
+          generated: generated
+      end
+      let(:generated) { true }
+
+      it { is_expected.to be_required }
+
+      context 'already processed topic' do
+        let(:processed) { true }
+        it { is_expected.to_not be_required }
+      end
+
+      context 'not generated topic' do
+        let(:generated) { false }
+        it { is_expected.to_not be_required }
+      end
+    end
+  end
+end
