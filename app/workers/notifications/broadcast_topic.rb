898334412ba4fbd6c3031178052181b2caceec9f
Andrey Sidorov <takandar@gmail.com>
2018-10-10 16:36:24 +0300

    working on broadcast topic

--- app/workers/notifications/broadcast_topic.rb
+++ app/workers/notifications/broadcast_topic.rb
@@ -7,6 +7,57 @@ class Notifications::BroadcastTopic
     queue: :history_jobs
   )
 
+  NEWS_EXPIRE_IN = 1.week
+
   def perform topic
+    return
+    return topic.update_column :processed, true if ignored?(topic) || expired?(topic)
+
+    messages = build_messages topic
+
+    ApplicationRecord.transaction do
+      topic.update_column :processed, true
+      messages.each_slice(5000) { |slice| Message.import slice, validate: false }
+    end
+  end
+
+private
+
+  def ignored? topic
+    topic.is_a?(Topics::NewsTopic) &&
+      (!topic.linked || topic.linked.censored? || topic.linked.kind_music?)
+  end
+
+  def expired? topic
+    (topic.created_at || Time.zone.now) + NEWS_EXPIRE_IN < Time.zone.now
+  end
+
+  def build_messages topic
+    subscribed_users(topic).map do |user|
+      build_message topic, user
+    end
+  end
+
+  def subscribed_users topic
+    Topics::SubscribedUsersQuery.call topic
+  end
+
+  def build_message topic, user
+    Message.new(
+      from: topic.user,
+      to: user,
+      body: nil,
+      kind: message_type(topic),
+      linked: topic,
+      created_at: topic.created_at
+    )
+  end
+
+  def message_type topic
+    if topic.class == Topics::NewsTopic && topic.broadcast
+      MessageType::SiteNews
+    else
+      topic.action || raise("unknown message_type for topic #{topic.id}")
+    end
   end
 end
---
+++ spec/workers/notifications/broadcast_topic_spec.rb
@@ -0,0 +1,3 @@
+describe Notifications::BroadcastTopic do
+
+end
