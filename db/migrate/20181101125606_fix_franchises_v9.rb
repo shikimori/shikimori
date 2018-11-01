b332a79b2b2646c01c4e659cc10ab9e54dd22afe
Andrey Sidorov <takandar@gmail.com>
2018-11-01 18:26:23 +0300

    fixing franchises

--- db/migrate/20181101125606_fix_franchises_v9.rb
+++ db/migrate/20181101125606_fix_franchises_v9.rb
@@ -1,5 +1,9 @@
 class FixFranchisesV9 < ActiveRecord::Migration[5.2]
   def change
+    Anime.where(franchise: 'hanamonogatari').update_all franchise: 'bakemonogatari'
     Achievement.where(neko_id: 'hanamonogatari').update_all neko_id: 'bakemonogatari'
+
+    Anime.where(franchise: 'aria_the_ova').update_all franchise: 'aria_the_origination'
+    Achievement.where(neko_id: 'aria_the_ova').update_all neko_id: 'aria_the_origination'
   end
 end
