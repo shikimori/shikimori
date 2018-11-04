fa77fa218fe4efa1465d85791e2d6e10c2d6606f
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-11-04 08:01:09 +0300

    remove ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -145,13 +145,13 @@ class Ad < ViewObjectBase
       Types::Ad::Type[:advrtr_240x400]
     ],
     Types::Ad::Meta[:menu_240x400] => [
-      Types::Ad::Type[:special_x300],
+      # Types::Ad::Type[:special_x300],
       # Types::Ad::Type[:mt_240x400],
       Types::Ad::Type[:yd_240x500],
       Types::Ad::Type[:advrtr_240x400]
     ],
     Types::Ad::Meta[:menu_300x600] => [
-      Types::Ad::Type[:special_x300],
+      # Types::Ad::Type[:special_x300],
       # Types::Ad::Type[:mt_300x600],
       Types::Ad::Type[:yd_300x600],
       Types::Ad::Type[:advrtr_240x400]
