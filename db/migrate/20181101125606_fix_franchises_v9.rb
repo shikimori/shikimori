a9ada4bbac19aec0e21117d8dc8b11dea48ca088
Andrey Sidorov <takandar@gmail.com>
2018-11-11 18:14:59 +0300

    rename franchise

--- db/migrate/20181101125606_fix_franchises_v9.rb
+++ db/migrate/20181101125606_fix_franchises_v9.rb
@@ -21,7 +21,8 @@ class FixFranchisesV9 < ActiveRecord::Migration[5.2]
       'vivid_strike' => 'mahou_shoujo_lyrical_nanoha',
       'umineko_no_naku_koro_ni' => 'when_they_cry',
       'tabidachi' => 'cream_lemon',
-      'puchimas' => 'idolmaster'
+      'puchimas' => 'idolmaster',
+      'chiba_pedal' => 'yowamushi_pedal'
     }.each do |old_name, new_name|
       Anime.where(franchise: old_name).update_all franchise: new_name
       Achievement.where(neko_id: old_name).update_all neko_id: new_name
