28076a87b76fe3f1d62490432097f0bcc6f52002
Andrey Sidorov <takandar@gmail.com>
2018-11-12 22:53:58 +0300

    less delay

--- app/assets/javascripts/pages/p-comments.js
+++ app/assets/javascripts/pages/p-comments.js
@@ -1,7 +1,7 @@
 import delay from 'delay';
 
 page_load('comments_show', async () => {
-  await delay(500);
+  await delay(250);
 
   $('.b-comment').imagesLoaded(() =>
     $('.b-height_shortener .expand').click()
