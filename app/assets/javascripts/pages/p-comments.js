d896e4b9c2da9073a9d3e8e62c416dd12c9e7ab2
Andrey Sidorov <takandar@gmail.com>
2018-11-12 14:28:39 +0300

    small fix

--- app/assets/javascripts/pages/p-comments.js
+++ app/assets/javascripts/pages/p-comments.js
@@ -1,5 +1,8 @@
-page_load('comments_show', () => {
-  $('.b-comment').imagesLoaded(() => {
-    $('.b-height_shortener .expand').click();
-  });
+import delay from 'delay';
+
+page_load('comments_show', async () => {
+  await delay();
+  $('.b-comment').imagesLoaded(() =>
+    $('.b-height_shortener .expand').click()
+  );
 });
