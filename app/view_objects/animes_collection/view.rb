1c7d2e7cd3d433b0e0a7aa87e3c835bb2c771916
Andrey Sidorov <takandar@gmail.com>
2018-11-21 01:58:16 +0300

    reset cache key

--- app/view_objects/animes_collection/view.rb
+++ app/view_objects/animes_collection/view.rb
@@ -9,7 +9,7 @@ class AnimesCollection::View < ViewObjectBase
   SEASON_LIMIT = 1000
 
   DEFAULT_ORDER = 'ranked'
-  CACHE_VERSION = 15
+  CACHE_VERSION = 16
 
   def collection
     if season_page?
