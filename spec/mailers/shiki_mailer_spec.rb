19b4d55b641b06d33dd87322912fe6818e143cd3
Andrey Sidorov <takandar@gmail.com>
2018-12-04 00:28:08 +0300

    fix failed specs after update mail gem

--- spec/mailers/shiki_mailer_spec.rb
+++ spec/mailers/shiki_mailer_spec.rb
@@ -32,13 +32,15 @@ describe ShikiMailer do
       let(:to_user_locale) { :ru }
       it do
         expect(mail.subject).to eq 'Личное сообщение'
-        expect(mail.body.raw_source).to eq "
-          #{to_user.nickname}, у вас 1 новое сообщение на shikimori.test от пользователя #{from_user.nickname}.
-          Прочитать можно тут: https://test.host/#{to_user.nickname}/dialogs
+        expect(mail.body.raw_source).to eq(
+          <<~TEXT.gsub("\n", "\r\n").strip
+            #{to_user.nickname}, у вас 1 новое сообщение на shikimori.test от пользователя #{from_user.nickname}.
+            Прочитать можно тут: https://test.host/#{to_user.nickname}/dialogs
 
-          Отписаться от уведомлений можно по ссылке:
-          https://test.host/messages/#{to_user.nickname}/#{unsubscribe_link_key}/Private/unsubscribe
-        ".gsub(/^ +/, '').strip
+            Отписаться от уведомлений можно по ссылке:
+            https://test.host/messages/#{to_user.nickname}/#{unsubscribe_link_key}/Private/unsubscribe
+          TEXT
+        )
       end
     end
 
@@ -46,13 +48,15 @@ describe ShikiMailer do
       let(:to_user_locale) { :en }
       it do
         expect(mail.subject).to eq 'Private message'
-        expect(mail.body.raw_source).to eq "
-          #{to_user.nickname}, you have 1 new message on shikimori.test from #{from_user.nickname}.
-          Read the message: https://test.host/#{to_user.nickname}/dialogs
+        expect(mail.body.raw_source).to eq(
+          <<~TEXT.gsub("\n", "\r\n").strip
+            #{to_user.nickname}, you have 1 new message on shikimori.test from #{from_user.nickname}.
+            Read the message: https://test.host/#{to_user.nickname}/dialogs
 
-          To unsubscribe from notification emails click here:
-          https://test.host/messages/#{to_user.nickname}/#{unsubscribe_link_key}/Private/unsubscribe
-        ".gsub(/^ +/, '').strip
+            To unsubscribe from notification emails click here:
+            https://test.host/messages/#{to_user.nickname}/#{unsubscribe_link_key}/Private/unsubscribe
+          TEXT
+        )
       end
     end
 
@@ -85,22 +89,24 @@ describe ShikiMailer do
       let(:user_locale) { :ru }
       it do
         expect(mail.subject).to eq 'Инструкция по сбросу пароля'
-        expect(mail.body.raw_source).to eq "
-          Привет!
+        expect(mail.body.raw_source).to eq(
+          <<~TEXT.gsub("\n", "\r\n").strip
+            Привет!
 
-          Кто-то активировал процедуру сброса пароля для вашего аккаунта на shikimori.test.
+            Кто-то активировал процедуру сброса пароля для вашего аккаунта на shikimori.test.
 
-          Ваш логин - #{user.nickname}.
+            Ваш логин - #{user.nickname}.
 
-          Изменить пароль можно, перейдя по данной ссылке: https://test.host/users/password/edit?reset_password_token=#{token}
+            Изменить пароль можно, перейдя по данной ссылке: https://test.host/users/password/edit?reset_password_token=#{token}
 
 
-          Если вам пришло несколько писем о восстановлении пароля, то переходить на страницу сброса пароля нужно обязательно по ссылке из самого последнего письма.
+            Если вам пришло несколько писем о восстановлении пароля, то переходить на страницу сброса пароля нужно обязательно по ссылке из самого последнего письма.
 
-          Если вы не запрашивали сброс пароля, то просто проигнорируйте это письмо.
+            Если вы не запрашивали сброс пароля, то просто проигнорируйте это письмо.
 
-          Ваш пароль не будет изменён до тех пор, пока вы не перейдёте по указанной выше ссылке.
-        ".gsub(/^ +/, '').strip
+            Ваш пароль не будет изменён до тех пор, пока вы не перейдёте по указанной выше ссылке.
+          TEXT
+        )
       end
     end
 
@@ -108,20 +114,22 @@ describe ShikiMailer do
       let(:user_locale) { :en }
       it do
         expect(mail.subject).to eq 'Reset password instructions'
-        expect(mail.body.raw_source).to eq "
-          Hi!
+        expect(mail.body.raw_source).to eq(
+          <<~TEXT.gsub("\n", "\r\n").strip
+            Hi!
 
-          We have received a request to reset your account password on shikimori.test.
+            We have received a request to reset your account password on shikimori.test.
 
-          Your account login is #{user.nickname}.
+            Your account login is #{user.nickname}.
 
-          To reset you password click this link: https://test.host/users/password/edit?reset_password_token=#{token}
+            To reset you password click this link: https://test.host/users/password/edit?reset_password_token=#{token}
 
 
-          If you didn't make a request to reset your password just ignore this message.
+            If you didn't make a request to reset your password just ignore this message.
 
-          Your password will not change until you click the link above.
-        ".gsub(/^ +/, '').strip
+            Your password will not change until you click the link above.
+          TEXT
+        )
       end
     end
 
