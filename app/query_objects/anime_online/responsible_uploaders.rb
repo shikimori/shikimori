2744a20a22dfa8c026bac6d80ae3f06bc0a59c09
Andrey Sidorov <takandar@gmail.com>
2018-12-05 20:55:26 +0300

    better algo in AnimeOnline::ResponsibleUploaders

--- app/query_objects/anime_online/responsible_uploaders.rb
+++ app/query_objects/anime_online/responsible_uploaders.rb
@@ -6,8 +6,10 @@ class AnimeOnline::ResponsibleUploaders
 
   def call
     active_users
-      .select do |user_id, accepts|
-        1 - (user_with_rejects[user_id] || 0) * 1.0 / accepts >= TRUST_THRESHOLD
+      .select do |user_id, uploads_count|
+        errors_count = (rejected_uploads[user_id] || 0) + (complaints_on_user[user_id] || 0)
+
+        1 - errors_count * 1.0 / uploads_count >= TRUST_THRESHOLD
       end
       .map(&:first)
   end
@@ -15,21 +17,52 @@ class AnimeOnline::ResponsibleUploaders
 private
 
   def active_users
-    @active_users ||= AnimeVideoReport
+    @active_users ||= user_uploads_scope
       .select(:user_id, 'count(*) as videos')
-      .where(kind: :uploaded)
       .group(:user_id)
       .having('count(*) >= ?', UPLOADS_TO_TRUST)
       .select('user_id, count(*) as uploads')
       .each_with_object({}) { |v, memo| memo[v.user_id] = v.uploads }
   end
 
-  def user_with_rejects
-    @user_with_rejects ||= AnimeVideoReport
-      .where(kind: :uploaded, state: :rejected)
-      .where(user_id: active_users.map(&:first))
+  def rejected_uploads
+    @user_with_rejects ||= user_uploads_scope
+      .where(state: :rejected)
+      .where(user_id: active_users_ids)
       .group(:user_id)
       .select('user_id, count(*) as rejects')
       .each_with_object({}) { |v, memo| memo[v.user_id] = v.rejects }
+  end
+
+  def complaints_on_user
+    @complaints_on_user ||= AnimeVideoReport
+      .where(kind: %i[wrong other])
+      .where(state: :accepted)
+      .where(anime_video_id: active_users_uploads.select(:anime_video_id))
+      .joins(:anime_video)
+      .joins(
+        <<-SQL
+          left join anime_video_reports av
+            on av.anime_video_id=anime_videos.id
+            and av.kind = 'uploaded'
+        SQL
+      )
+      .group('av.user_id')
+      .select('av.user_id, count(*) as complaints')
+      .each_with_object({}) { |v, memo| memo[v.user_id] = v.complaints }
+  end
+
+  def active_users_ids
+    active_users.map(&:first)
+  end
+
+  def user_uploads_scope
+    AnimeVideoReport.where(kind: :uploaded)
+  end
+
+  def active_users_uploads
+    user_uploads_scope
+      .where(user_id: active_users_ids)
+      .where(state: :accepted)
   end
 end
--- spec/query_objects/anime_online/responsible_uploaders_spec.rb
+++ spec/query_objects/anime_online/responsible_uploaders_spec.rb
@@ -15,14 +15,56 @@ describe AnimeOnline::ResponsibleUploaders do
   let!(:report_3_4) { create :anime_video_report, :uploaded, :rejected, user: user_with_enough_rejected }
 
   let(:user_with_not_enough_rejected) { build_stubbed :user }
-  let!(:report_4_1) { create :anime_video_report, :uploaded, :accepted, user: user_with_not_enough_rejected }
-  let!(:report_4_2) { create :anime_video_report, :uploaded, :accepted, user: user_with_not_enough_rejected }
-  let!(:report_4_3) { create :anime_video_report, :uploaded, :rejected, user: user_with_not_enough_rejected }
+  let(:video_4) { create :anime_video }
+  let!(:report_4_1) do
+    create :anime_video_report, :uploaded, :accepted,
+      user: user_with_not_enough_rejected,
+      anime_video: video_4
+  end
+  let!(:report_4_2) do
+    create :anime_video_report, :uploaded, :accepted,
+      user: user_with_not_enough_rejected,
+      anime_video: video_4
+  end
+  let!(:report_4_3) do
+    create :anime_video_report, :uploaded, :rejected,
+      user: user_with_not_enough_rejected,
+      anime_video: video_4
+  end
+  let!(:report_4_4) do
+    create :anime_video_report, :broken, :accepted, anime_video: video_4
+  end
+
+  let(:user_with_not_enough_rejected_and_enough_reported) { build_stubbed :user }
+  let(:video_5) { create :anime_video }
+  let!(:report_5_1) do
+    create :anime_video_report, :uploaded, :accepted,
+      user: user_with_not_enough_rejected_and_enough_reported,
+      anime_video: video_5
+  end
+  let!(:report_5_2) do
+    create :anime_video_report, :uploaded, :accepted,
+      user: user_with_not_enough_rejected_and_enough_reported,
+      anime_video: video_5
+  end
+  let!(:report_5_3) do
+    create :anime_video_report, :uploaded, :rejected,
+      user: user_with_not_enough_rejected_and_enough_reported,
+      anime_video: video_5
+  end
+  let!(:report_5_4) do
+    create :anime_video_report, %i[wrong other].sample, :accepted, anime_video: video_5
+  end
 
   before do
     stub_const 'AnimeOnline::ResponsibleUploaders::UPLOADS_TO_TRUST', 2
     stub_const 'AnimeOnline::ResponsibleUploaders::TRUST_THRESHOLD', 0.6
   end
 
-  it { is_expected.to eq [user_enough_accepted.id, user_with_not_enough_rejected.id] }
+  it do
+    is_expected.to eq [
+      user_enough_accepted.id,
+      user_with_not_enough_rejected.id
+    ]
+  end
 end
