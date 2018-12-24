97b428d2279354c4e958277dd4782ee879e6e1a8
Andrey Sidorov <takandar@gmail.com>
2018-12-24 17:51:28 +0300

    better redis-mutex logic in Achievements::Track

--- app/workers/achievements/track.rb
+++ app/workers/achievements/track.rb
@@ -7,12 +7,14 @@ class Achievements::Track
   )
 
   def perform user_id, user_rate_id, action
-    RedisMutex.with_lock("track_achievement_#{user_id}_#{user_rate_id}", block: 0) do
+    RedisMutex.with_lock("achievements/track_#{user_id}", block: 0) do
       user = User.find user_id
       neko_update user, user_rate_id, action
     end
-  rescue *(Network::FaradayGet::NET_ERRORS + [Neko::RequestError, RedisMutex::LockError])
+  rescue *(Network::FaradayGet::NET_ERRORS + [Neko::RequestError])
     self.class.perform_in 1.minute, user_id, user_rate_id, action
+  rescue RedisMutex::LockError
+    self.class.perform_in 5.seconds, user_id, user_rate_id, action
   end
 
 private
