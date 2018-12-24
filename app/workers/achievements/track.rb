f00df8ff7d2086446ed629525cb3c50831604d60
Andrey Sidorov <takandar@gmail.com>
2018-12-24 17:46:59 +0300

    wrap Achievements::Track in redis mutex

--- app/workers/achievements/track.rb
+++ app/workers/achievements/track.rb
@@ -7,9 +7,11 @@ class Achievements::Track
   )
 
   def perform user_id, user_rate_id, action
-    user = User.find user_id
-    neko_update user, user_rate_id, action
-  rescue *(Network::FaradayGet::NET_ERRORS + [Neko::RequestError])
+    RedisMutex.with_lock("track_achievement_#{user_id}_#{user_rate_id}", block: 0) do
+      user = User.find user_id
+      neko_update user, user_rate_id, action
+    end
+  rescue *(Network::FaradayGet::NET_ERRORS + [Neko::RequestError, RedisMutex::LockError])
     self.class.perform_in 1.minute, user_id, user_rate_id, action
   end
 
