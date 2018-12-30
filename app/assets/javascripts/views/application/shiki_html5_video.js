b17eedf93edaebba8bfb14eed88beadc67218c41
Andrey Sidorov <takandar@gmail.com>
2018-12-31 00:15:52 +0300

    decaffeinate for shiki_html5_video

---
+++ app/assets/javascripts/views/application/shiki_html5_video.js
@@ -0,0 +1,27 @@
+import { sessionStorage } from 'js-storage';
+import View from 'views/application/view';
+
+const VOLUME_KEY = 'video_volume';
+
+export default class ShikiHtml5Video extends View {
+  initialize() {
+    this.root.volume = sessionStorage.get(VOLUME_KEY) || 1;
+
+    // @on 'error', () => @error()
+    this.on('click', () => this.click());
+    this.on('volumechange', () => this.volumechange());
+  }
+
+  click() {
+    if (this.root.paused) {
+      this.root.play();
+    } else {
+      this.root.pause();
+    }
+    return false;
+  }
+
+  volumechange() {
+    sessionStorage.set(VOLUME_KEY, this.root.volume);
+  }
+}
