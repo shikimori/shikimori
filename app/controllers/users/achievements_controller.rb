9d7a13ae4c6639ed243a6f398c8a5a4cc977d7d6
Andrey Sidorov <takandar@gmail.com>
2018-12-31 16:49:14 +0300

    remove check_access

--- app/controllers/users/achievements_controller.rb
+++ app/controllers/users/achievements_controller.rb
@@ -1,7 +1,6 @@
 class Users::AchievementsController < ProfilesController
   before_action :additional_breadcrumbs, except: [:index]
   before_action { og page_title: i18n_t('achievements') }
-  before_action :check_access
 
   def index
     @view = Profiles::AchievementsView.new(@user)
@@ -14,13 +13,6 @@ class Users::AchievementsController < ProfilesController
   end
 
 private
-
-  def check_access
-    return if Rails.env.development?
-    return if current_user&.admin?
-
-    raise ActiveRecord::RecordNotFound
-  end
 
   def additional_breadcrumbs
     @back_url = profile_achievements_url(@resource)
