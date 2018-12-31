fb5f07abae7fb6cbf8e1b6fb57613141b781c694
Andrey Sidorov <takandar@gmail.com>
2018-12-31 15:26:20 +0300

    fix migration

--- db/migrate/20181231121931_fix_shingeki_no_kyojin_franchise.rb
+++ db/migrate/20181231121931_fix_shingeki_no_kyojin_franchise.rb
@@ -1,5 +1,9 @@
 class FixShingekiNoKyojinFranchise < ActiveRecord::Migration[5.2]
   def change
-    Animes::UpdateFranchises.new.call Anime.where(franchise: 'shingeki_no_kyojin')
+    ids = Anime.where(franchise: 'shingeki_no_kyojin').pluck :id
+
+    Anime.where(id: ids).update_all franchise: nil
+
+    Animes::UpdateFranchises.new.call Anime.where(id: ids)
   end
 end
