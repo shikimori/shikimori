0bd02c202888a8f190ce37cb02c5fe7e805af6cd
Andrey Sidorov <takandar@gmail.com>
2019-01-02 21:26:52 +0300

    fix redirecter for shiki_db

--- app/middleware/redirecter.rb
+++ app/middleware/redirecter.rb
@@ -9,22 +9,27 @@ class Redirecter
     request = Rack::Request.new env
 
     if !VALID_HOSTS.include? request.host
-      [301, {'Location' => request.url.sub(request.host, Shikimori::DOMAIN)}, []]
+      [301, { 'Location' => fixed_url(request).sub(request.host, Shikimori::DOMAIN) }, []]
 
     elsif request.host.starts_with? 'www.'
-      [301, {'Location' => request.url.sub('//www.', '//')}, self]
+      [301, { 'Location' => fixed_url(request).sub('//www.', '//') }, self]
 
     elsif request.url.end_with?('/') && request.path != '/'
-      [301, {'Location' => request.url.sub(/\/$/, '')}, []]
+      [301, { 'Location' => fixed_url(request).sub(%r{/$}, '') }, []]
 
     elsif request.url.end_with?('&') && request.path != '/'
-      [301, {'Location' => request.url.sub(/&$/, '')}, []]
+      [301, { 'Location' => fixed_url(request).sub(/&$/, '') }, []]
 
     elsif request.url.end_with?('.html')
-      [301, {'Location' => request.url.sub(/.html$/, '')}, []]
+      [301, { 'Location' => fixed_url(request).sub(/.html$/, '') }, []]
 
     else
       @app.call(env)
     end
+  end
+
+  def fixed_url request
+    # need to strip port because for some reason requests proxied to shiki_db have port in request.url
+    request.url.sub(%r{:80(?=/|$)}, '\1')
   end
 end
