9be007268999192a5485b8827765ad3f31491018
Andrey Sidorov <takandar@gmail.com>
2019-01-10 21:45:34 +0300

    throttle requests to video_online. better ignore of neko requets

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,4 +1,3 @@
-NEKO_IP = '88.198.7.116'
 SMOTRET_ANIME_USER_AGENT = 'Anime 365 (https://smotretanime.ru/; info@smotretanime.ru)'
 
 # Throttle requests to 5 requests per second per ip
@@ -10,33 +9,53 @@ Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
   # If falsy, the cache key is neither incremented nor checked.
 
   # requests from neko are allowed
-  if req.ip != NEKO_IP && req.user_agent != SMOTRET_ANIME_USER_AGENT
+  if req.user_agent != SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
 
 
 Rack::Attack.throttle('req/ip', limit: 15, period: 1.second) do |req|
-  if req.ip != NEKO_IP && req.user_agent == SMOTRET_ANIME_USER_AGENT
+  if req.user_agent == SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
 
 Rack::Attack.throttle('req/ip', limit: 90, period: 60.second) do |req|
   # requests from neko are allowed
-  if req.ip != NEKO_IP && req.user_agent != SMOTRET_ANIME_USER_AGENT
+  if req.user_agent != SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
 
 Rack::Attack.throttle('req/ip', limit: 270, period: 60.second) do |req|
-  if req.ip != NEKO_IP && req.user_agent == SMOTRET_ANIME_USER_AGENT
+  if req.user_agent == SMOTRET_ANIME_USER_AGENT
     req.ip
   end
 end
 
-Rack::Attack.throttle('req/ip', limit: 3000, period: 1.day) do |req|
-  if req.url =~ %r{/video_online/}
-    req.ip
+if ENV['SHIKI_TYPE'] == 'db'
+  Rack::Attack.throttle('req/ip', limit: 3, period: 1.second) do |req|
+    if req.url =~ %r{/video_online(?:/|$)}
+      req.ip
+    end
+  end
+
+  Rack::Attack.throttle('req/ip', limit: 50, period: 1.minute) do |req|
+    if req.url =~ %r{/video_online(?:/|$)}
+      req.ip
+    end
+  end
+
+  Rack::Attack.throttle('req/ip', limit: 1000, period: 1.hour) do |req|
+    if req.url =~ %r{/video_online(?:/|$)}
+      req.ip
+    end
+  end
+
+  Rack::Attack.throttle('req/ip', limit: 5000, period: 1.day) do |req|
+    if req.url =~ %r{/video_online(?:/|$)}
+      req.ip
+    end
   end
 end
---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,3 @@
+NEKO_IP = '88.198.7.116'
+
+Rack::Attack.safelist_ip NEKO_IP
