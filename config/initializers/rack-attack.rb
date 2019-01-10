4f7e8a48cc5564955705c713ff5792a9c9f03a04
Andrey Sidorov <takandar@gmail.com>
2019-01-10 21:49:59 +0300

    move into one config. stronger throttles

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,4 +1,7 @@
+NEKO_IP = '88.198.7.116'
 SMOTRET_ANIME_USER_AGENT = 'Anime 365 (https://smotretanime.ru/; info@smotretanime.ru)'
+
+Rack::Attack.safelist_ip NEKO_IP
 
 # Throttle requests to 5 requests per second per ip
 Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
@@ -35,25 +38,25 @@ Rack::Attack.throttle('req/ip', limit: 270, period: 60.second) do |req|
 end
 
 if ENV['SHIKI_TYPE'] == 'db'
-  Rack::Attack.throttle('req/ip', limit: 3, period: 1.second) do |req|
+  Rack::Attack.throttle('req/ip', limit: 2, period: 1.second) do |req|
     if req.url =~ %r{/video_online(?:/|$)}
       req.ip
     end
   end
 
-  Rack::Attack.throttle('req/ip', limit: 50, period: 1.minute) do |req|
+  Rack::Attack.throttle('req/ip', limit: 40, period: 1.minute) do |req|
     if req.url =~ %r{/video_online(?:/|$)}
       req.ip
     end
   end
 
-  Rack::Attack.throttle('req/ip', limit: 1000, period: 1.hour) do |req|
+  Rack::Attack.throttle('req/ip', limit: 500, period: 1.hour) do |req|
     if req.url =~ %r{/video_online(?:/|$)}
       req.ip
     end
   end
 
-  Rack::Attack.throttle('req/ip', limit: 5000, period: 1.day) do |req|
+  Rack::Attack.throttle('req/ip', limit: 2000, period: 1.day) do |req|
     if req.url =~ %r{/video_online(?:/|$)}
       req.ip
     end
--- config/initializers/rack_attack.rb
+++
@@ -1,3 +0,0 @@
-NEKO_IP = '88.198.7.116'
-
-Rack::Attack.safelist_ip NEKO_IP
