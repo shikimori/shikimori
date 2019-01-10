deeb81ad77848b058c1473b082b773a97dfce993
Andrey Sidorov <takandar@gmail.com>
2019-01-10 22:06:57 +0300

    update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_120614) do
+ActiveRecord::Schema.define(version: 2019_01_10_175340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
