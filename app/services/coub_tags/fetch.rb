1088b33de5b2a3041cd4b7db6057022ee70bb911
Andrey Sidorov <takandar@gmail.com>
2019-01-17 01:00:30 +0300

    working on fetch coub_tags

--- app/services/coub_tags/fetch.rb
+++ app/services/coub_tags/fetch.rb
@@ -4,29 +4,45 @@ class CoubTags::Fetch
   PER_PAGE = 10
 
   def call
-    fetch @tags.first, 1
+    tag, page, overfetch = (iterator || "#{@tags.first}:1:0").split(':')
+
+    fetch(
+      tag: tag,
+      page: page.to_i,
+      overfetch: overfetch.to_i
+    )
   end
 
 private
 
-  def fetch tag, page, fetched_coubs: []
+  def fetch tag:, page:, overfetch: 0, add_coubs: []
     all_coubs = CoubTags::CoubRequest.call tag, page
-    anime_coubs = fetched_coubs + all_coubs.select(&:anime?)
+    anime_coubs = (add_coubs + all_coubs.select(&:anime?))[overfetch..-1]
+    overloaded_coubs = anime_coubs[PER_PAGE..-1] || []
 
     if finished?(all_coubs) || enough?(anime_coubs)
-      results anime_coubs, next_iterator(all_coubs, tag, page)
+      Coub::Results.new(
+        coubs: anime_coubs - overloaded_coubs,
+        iterator: next_iterator(all_coubs, tag, page, overloaded_coubs)
+      )
 
     else
-      fetch tag, page + 1, fetched_coubs: anime_coubs
+      fetch(
+        tag: tag,
+        page: page + 1,
+        add_coubs: anime_coubs
+      )
     end
   end
 
-  def results coubs, iterator
-    Coub::Results.new coubs: coubs, iterator: iterator
-  end
+  def next_iterator coubs, tag, page, overloaded_coubs
+    return nil if finished? coubs
 
-  def next_iterator coubs, tag, page
-    "#{tag}:#{page + 1}" unless finished? coubs
+    if overloaded_coubs.any?
+      "#{tag}:#{page}:#{overloaded_coubs.size}"
+    else
+      "#{tag}:#{page + 1}:0"
+    end
   end
 
   def finished? coubs
@@ -34,6 +50,6 @@ private
   end
 
   def enough? coubs
-    coubs.size == PER_PAGE
+    coubs.size >= PER_PAGE
   end
 end
--- spec/services/coub_tags/fetch_spec.rb
+++ spec/services/coub_tags/fetch_spec.rb
@@ -12,33 +12,47 @@ describe CoubTags::Fetch do
       .with(tags[0], 2)
       .and_return coubs_2
 
+    allow(CoubTags::CoubRequest)
+      .to receive(:call)
+      .with(tags[0], 3)
+      .and_return coubs_3
+
     stub_const 'CoubTags::Fetch::PER_PAGE', 2
     stub_const 'CoubTags::CoubRequest::PER_PAGE', 2
   end
 
   let(:coubs_1) { [] }
   let(:coubs_2) { [] }
+  let(:coubs_3) { [] }
 
   let(:coub_anime_1) do
     Coub::Entry.new(
-      player_url: 'zxc_1',
-      image_url: 'qwe_1',
+      player_url: 'coub_anime_1',
+      image_url: 'coub_anime_1',
       categories: %w[anime],
       tags: %w[anime]
     )
   end
   let(:coub_anime_2) do
     Coub::Entry.new(
-      player_url: 'zxc_2',
-      image_url: 'qwe_2',
+      player_url: 'coub_anime_2',
+      image_url: 'coub_anime_2',
+      categories: %w[anime],
+      tags: %w[anime]
+    )
+  end
+  let(:coub_anime_3) do
+    Coub::Entry.new(
+      player_url: 'coub_anime_3',
+      image_url: 'coub_anime_3',
       categories: %w[anime],
       tags: %w[anime]
     )
   end
   let(:coub_not_anime_1) do
     Coub::Entry.new(
-      player_url: 'zxc_3',
-      image_url: 'qwe_3',
+      player_url: 'coub_not_anime_1',
+      image_url: 'coub_not_anime_1',
       categories: %w[gaming],
       tags: %w[]
     )
@@ -49,44 +63,107 @@ describe CoubTags::Fetch do
   context 'last tag' do
     let(:tags) { %w[zzz] }
 
-    context 'results from 1st coub request' do
-      context 'no more results' do
-        let(:coubs_1) { [coub_anime_1] }
+    context 'no iterator' do
+      context 'results from 1st coub request' do
+        context 'no more results' do
+          let(:coubs_1) { [coub_anime_1] }
 
-        it do
-          is_expected.to be_kind_of Coub::Results
-          is_expected.to have_attributes(
-            coubs: coubs_1,
-            iterator: nil
-          )
-          expect(CoubTags::CoubRequest).to have_received(:call).once
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: coubs_1,
+              iterator: nil
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).once
+          end
+        end
+
+        context 'has more results' do
+          let(:coubs_1) { [coub_anime_1, coub_anime_2] }
+
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: coubs_1,
+              iterator: 'zzz:2:0'
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).once
+          end
         end
       end
 
-      context 'has more results' do
-        let(:coubs_1) { [coub_anime_1, coub_anime_2] }
+      context 'results from 2nd coub request' do
+        let(:coubs_1) { [coub_anime_1, coub_not_anime_1] }
+        let(:coubs_2) { [coub_anime_2] }
 
-        it do
-          is_expected.to be_kind_of Coub::Results
-          is_expected.to have_attributes(
-            coubs: coubs_1,
-            iterator: 'zzz:2'
-          )
-          expect(CoubTags::CoubRequest).to have_received(:call).once
+        context 'no more results' do
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: [coub_anime_1, coub_anime_2],
+              iterator: nil
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).twice
+          end
+        end
+
+        context 'has more results' do
+          let(:coubs_1) { [coub_anime_1, coub_not_anime_1] }
+          let(:coubs_2) { [coub_anime_2, coub_anime_3] }
+
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: [coub_anime_1, coub_anime_2],
+              iterator: 'zzz:2:1'
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).twice
+          end
         end
       end
     end
 
-    context 'results from 2nd coub request' do
-      let(:coubs_1) { [coub_anime_1, coub_not_anime_1] }
-      let(:coubs_2) { [coub_anime_2] }
+    describe 'has iterator' do
+      context '2nd page w/o overfetch' do
+        let(:iterator) { 'zzz:2:0' }
 
-      context 'no more results' do
+        context 'no more results' do
+          let(:coubs_2) { [coub_anime_1] }
+
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: coubs_2,
+              iterator: nil
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).once
+          end
+        end
+
+        context 'has more results' do
+          let(:coubs_2) { [coub_anime_1, coub_anime_2] }
+
+          it do
+            is_expected.to be_kind_of Coub::Results
+            is_expected.to have_attributes(
+              coubs: coubs_2,
+              iterator: 'zzz:3:0'
+            )
+            expect(CoubTags::CoubRequest).to have_received(:call).once
+          end
+        end
+      end
+
+      context '2nd page with overfetch' do
+        let(:iterator) { 'zzz:2:1' }
+        let(:coubs_2) { [coub_anime_1, coub_anime_2] }
+        let(:coubs_3) { [coub_not_anime_1, coub_anime_3] }
+
         it do
           is_expected.to be_kind_of Coub::Results
           is_expected.to have_attributes(
-            coubs: [coub_anime_1, coub_anime_2],
-            iterator: nil
+            coubs: [coub_anime_2, coub_anime_3],
+            iterator: 'zzz:4:0'
           )
           expect(CoubTags::CoubRequest).to have_received(:call).twice
         end
