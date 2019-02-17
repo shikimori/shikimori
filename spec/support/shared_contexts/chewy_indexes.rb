ebde034cb85d180da24bdd2ac7e77184427350ea
Andrey Sidorov <takandar@gmail.com>
2019-02-18 00:08:25 +0300

    fix spec

--- spec/support/shared_contexts/chewy_indexes.rb
+++ spec/support/shared_contexts/chewy_indexes.rb
@@ -4,16 +4,14 @@ shared_context :chewy_indexes do |indexes|
   include_context :timecop, 'Thu, 10 May 2018 09:00:00 PDT -07:00'
 
   before do
-    Array(indexes).each do |index|
-      ActiveRecord::Base.connection.reset_pk_sequence! index
-      "#{index.to_s.camelize}Index".constantize.purge!
+    if (%i[ranobe] & Array(indexes)).any?
+      ActiveRecord::Base.connection.reset_pk_sequence! :mangas
+      RanobeIndex.purge!
+    else
+      Array(indexes).each do |index|
+        ActiveRecord::Base.connection.reset_pk_sequence! index
+        "#{index.to_s.camelize}Index".constantize.purge!
+      end
     end
   end
-
-  # if (%i[collections] & Array(indexes)).any?
-  #   before do
-  #     ActiveRecord::Base.connection.reset_pk_sequence! :collections
-  #     CollectionsIndex.purge!
-  #   end
-  # end
 end
