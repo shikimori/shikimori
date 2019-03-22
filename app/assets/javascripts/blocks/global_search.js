9faa72dd98e4dcd037ebb02b87894983ce75b1d0
Andrey Sidorov <takandar@gmail.com>
2019-03-22 22:26:43 +0300

    focus on search on '/' press

--- app/assets/javascripts/blocks/global_search.js
+++ app/assets/javascripts/blocks/global_search.js
@@ -1,8 +1,22 @@
 import CollectionSearch from 'views/application/collection_search';
 
+let searchView = null;
+
 $(document).on('turbolinks:load', () => {
-  new CollectionSearch(
+  searchView = new CollectionSearch(
     '.l-top_menu-v2 .global-search',
     $('.b-search-results')
   );
+});
+
+$(document).on('keypress', e => {
+  const target = e.target || e.srcElement;
+  const isIgnored = target.isContentEditable ||
+    target.tagName === 'INPUT' || target.tagName === 'SELECT' || target.tagName === 'TEXTAREA';
+
+  if (e.keyCode === 47 && !isIgnored) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    searchView.$input.focus();
+  }
 });
