998e86e3c88049cf8286c65a5e4a6a03b73254b4
Andrey Sidorov <takandar@gmail.com>
2019-04-08 10:50:40 +0300

    simplier cacher

--- app/assets/javascripts/services/ajax_cacher.js
+++ app/assets/javascripts/services/ajax_cacher.js
@@ -1,5 +1,8 @@
+// import axios from 'helpers/axios';
+
 let store = {};
 let queue = [];
+window.z = store;
 
 const queueLimit = 100;
 const enabled = true;// false;
@@ -16,50 +19,48 @@ function updateQueue(url, noDelete) {
   }
 }
 
-function getUriPart(url) {
-  return url.replace(/https?:\/\/[^/]+/, '');
-}
+// function getUriPart(url) {
+//   return url.replace(/https?:\/\/[^/]+/, '');
+// }
 
 export default {
-  cache(url) {
-    const uri = getUriPart(url);
-    if (!enabled || uri in store || uri === '') {
-      return;
-    }
+  // cache(url) {
+    // const uri = getUriPart(url);
+    // url: `${document.location.protocol}//${document.location.host}${url}`,
 
-    store[uri] = $.ajax({
-      url: `${document.location.protocol}//${document.location.host}${uri}`,
-      data: null,
-      dataType: 'json',
-      success(data, _status, _xhr) {
-        store[uri] = data;
-        updateQueue(uri, true);
-      }
-    });
-  },
+    // if (!enabled || url in store[url] || Object.isEmpty(url)) {
+    //   return;
+    // }
+
+    // store[url] = axios
+    //   .get(url)
+    //   .then(({ data }) => {
+    //     store[url] = data;
+    //     updateQueue(url, true);
+    //   });
+  // },
   push(url, data) {
-    if (!enabled) {
-      return;
-    }
-    if ('next_page' in data && data.next_page) {
-      this.cache(data.next_page);
-    }
-    if ('prev_page' in data && data.prev_page) {
-      this.cache(data.prev_page);
-    }
+    if (!enabled) { return; }
+
+    // if ('next_page_url' in data && data.next_page_url) {
+    //   this.cache(data.next_page_url);
+    // }
+    // if ('prev_page_url' in data && data.prev_page_url) {
+    //   this.cache(data.prev_page_url);
+    // }
     store[url] = data;
     updateQueue(url);
   },
   get(url) {
-    if (enabled && url in store) {
+    if (enabled && store[url]) {
       updateQueue(url);
 
-      if ('next_page' in store[url] && store[url].next_page) {
-        this.cache(store[url].next_page); // eslint-disable-line react/no-this-in-sfc
-      }
-      if ('prev_page' in store[url] && store[url].prev_page) {
-        this.cache(store[url].prev_page); // eslint-disable-line react/no-this-in-sfc
-      }
+      // if ('next_page_url' in store[url] && store[url].next_page_url) {
+      //   this.cache(store[url].next_page_url); // eslint-disable-line react/no-this-in-sfc
+      // }
+      // if ('prev_page_url' in store[url] && store[url].prev_page_url) {
+      //   this.cache(store[url].prev_page_url); // eslint-disable-line react/no-this-in-sfc
+      // }
 
       return store[url];
     }
