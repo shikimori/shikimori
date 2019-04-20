6627a158c428b934ca4cd3bd4f2bf179d5a16540
Andrey Sidorov <takandar@gmail.com>
2019-04-20 13:37:08 +0300

    cleanup code

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,10 +1,5 @@
-puts Rails.application.secrets.to_json
-puts Rails.application.secrets.recaptcha.to_json
-puts Rails.root.join
-puts open(Rails.root.join('config/secrets.yml')).read
-puts Rails.env.to_s
-
 Recaptcha.configure do |config|
+  # weird bug: for some reason recaptcha is nil during deploy js:export task
   if Rails.application.secrets.recaptcha
     config.site_key = Rails.application.secrets.recaptcha[:site_key]
     config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
