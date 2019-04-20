046f99fe51374d4b984162b0f233089ebf54bcd6
Andrey Sidorov <takandar@gmail.com>
2019-04-20 13:35:39 +0300

    debugging

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,9 +2,11 @@ puts Rails.application.secrets.to_json
 puts Rails.application.secrets.recaptcha.to_json
 puts Rails.root.join
 puts open(Rails.root.join('config/secrets.yml')).read
-puts Rails.env
+puts Rails.env.to_s
 
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.secrets.recaptcha[:site_key]
-  config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
+  if Rails.application.secrets.recaptcha
+    config.site_key = Rails.application.secrets.recaptcha[:site_key]
+    config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
+  end
 end
