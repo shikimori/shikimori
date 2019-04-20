452ad977508268f30876061e6a92a1aa59d49b02
Andrey Sidorov <takandar@gmail.com>
2019-04-20 13:26:53 +0300

    debugging

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,6 @@
+puts Rails.application.secrets.to_json
+puts Rails.application.secrets.recaptcha.to_json
+
 Recaptcha.configure do |config|
   config.site_key = Rails.application.secrets.recaptcha[:site_key]
   config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
