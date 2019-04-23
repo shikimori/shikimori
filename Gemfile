80b288db295dfdfd5bb2b74f5a333f9a13019390
Andrey Sidorov <takandar@gmail.com>
2019-04-23 23:03:53 +0300

    add bullet gem into development

--- Gemfile
+++ Gemfile
@@ -139,6 +139,7 @@ group :development do
 
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'bullet'
 
   # gem 'web-console'
   # gem 'listen'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    # Bullet.add_footer = true
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
+  end
 end
