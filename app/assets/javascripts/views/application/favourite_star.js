3cd92839a25371cf2656360d45f852d71b36c7e1
Andrey Sidorov <takandar@gmail.com>
2019-05-07 15:16:09 +0300

    decaffeinate favourite_start

---
+++ app/assets/javascripts/views/application/favourite_star.js
@@ -0,0 +1,46 @@
+import { bind } from 'decko';
+import View from 'views/application/view';
+
+const ADD_CLASS = 'fav-add';
+const REMOVE_CLASS = 'fav-remove';
+
+export default class FavouriteStar extends View {
+  initialize(isFavoured) {
+    this.addText = this.$root.data('add_text');
+    this.removeText = this.$root.data('remove_text');
+    this._update(isFavoured);
+
+    this.on('ajax:success', this._ajaxSuccess);
+  }
+
+  @bind
+  _ajaxSuccess() {
+    this._update(this.root.classList.contains(ADD_CLASS));
+  }
+
+  _update(isFavoured) {
+    if (isFavoured) {
+      this.$root
+        .removeClass(ADD_CLASS)
+        .addClass(REMOVE_CLASS)
+        .attr({
+          title: this.removeText,
+          'original-title': this.removeText,
+          // do not use "data" mthod, because this value is used in css
+          'data-text': this.removeText
+        })
+        .data('method', 'delete');
+    } else {
+      this.$root
+        .removeClass(REMOVE_CLASS)
+        .addClass(ADD_CLASS)
+        .attr({
+          title: this.addText,
+          'original-title': this.addText,
+          // do not use "data" mthod, because this value is used in css
+          'data-text': this.addText
+        })
+        .data('method', 'post');
+    }
+  }
+}
