cdd733a0fe1292744119628543de6f3ee0b8d2b0
Andrey Sidorov <takandar@gmail.com>
2019-05-15 22:35:47 +0300

    new robots

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -9,6 +9,8 @@ class RobotsController < ShikimoriController
   def shikimori
     if ru_host?
       shikimori_ru
+    elsif clean_host?
+      shikimori_clean
     else
       shikimori_en
     end
@@ -38,6 +40,47 @@ class RobotsController < ShikimoriController
       Disallow: /*.html
       Host: https://shikimori.org
       Sitemap: https://shikimori.org/sitemap.xml
+
+      User-agent: AhrefsBot
+      User-agent: moget
+      User-agent: ichiro
+      User-agent: NaverBot
+      User-agent: Yeti
+      User-agent: Baiduspider
+      User-agent: Baiduspider-video
+      User-agent: Baiduspider-image
+      User-agent: sogou spider
+      User-agent: YoudaoBot
+      User-agent: Yahoo Pipes 1.0
+      User-agent: Yahoo Pipes 2.0
+      Disallow: /
+    ROBOTS
+  end
+
+  def shikimori_clean
+    render plain: <<~ROBOTS
+      User-agent: *
+      Disallow: /*?
+      Disallow: /*?rel=nofollow
+      Disallow: /cosplay/*
+      Disallow: /genres
+      Disallow: /animes/*rss
+      Disallow: /animes/order-by/*
+      Disallow: /mangas/*rss
+      Disallow: /mangas/order-by/*
+      Disallow: /animes/search/*
+      Disallow: /mangas/search/*
+      Disallow: /*/comments
+      Disallow: /*/tooltip
+      Disallow: /*/autocomplete
+      Disallow: /*/autocomplete/v2
+      Disallow: /groups/9-Hentai*
+      Disallow: /messages/*
+      Disallow: /*undefined
+      Disallow: /api/*
+      Disallow: /*.html
+      Host: https://shikimori.one
+      Sitemap: https://shikimori.one/sitemap.xml
 
       User-agent: AhrefsBot
       User-agent: moget
