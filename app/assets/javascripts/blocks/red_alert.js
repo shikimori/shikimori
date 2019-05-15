98c7f21f4f88990797ee3c0006eb22200d3ab32a
Andrey Sidorov <takandar@gmail.com>
2019-05-15 21:25:33 +0300

    add red alert

---
+++ app/assets/javascripts/blocks/red_alert.js
@@ -0,0 +1,11 @@
+import delay from 'delay';
+
+$(document).on('turbolinks:load', () => {
+  $('.b-hot_topics-v2.red-alert .b-close').on('click', async e => {
+    const $block = $('.b-hot_topics-v2.red-alert');
+    $.cookie($(e.currentTarget).data('cookie-name'), '1', { expires: 720 });
+    $block.addClass('removing');
+    await delay(1000);
+    $block.remove();
+  });
+});
