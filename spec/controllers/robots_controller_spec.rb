4f814dc617d80fb48962e3b3aba7696f0d3f8a3e
Andrey Sidorov <takandar@gmail.com>
2019-05-16 03:31:34 +0300

    robots specs

--- spec/controllers/robots_controller_spec.rb
+++ spec/controllers/robots_controller_spec.rb
@@ -8,10 +8,24 @@ describe RobotsController do
   end
 
   describe 'shikimori' do
+    before { allow(controller).to receive(:clean_host?).and_return is_clean }
     subject! { get :shikimori }
-    it do
-      expect(response).to have_http_status :success
-      expect(response.content_type).to eq 'text/plain'
+
+    context 'clean' do
+      let(:is_clean) { true }
+      it do
+        expect(response).to have_http_status :success
+        expect(response.content_type).to eq 'text/plain'
+      end
+    end
+
+    context 'not clean' do
+      let(:is_clean) { false }
+      it do
+        is_expected.to redirect_to(
+          "#{Shikimori::PROTOCOL}://#{ShikimoriDomain::CLEAN_HOST}/robots.txt"
+        )
+      end
     end
   end
 end
