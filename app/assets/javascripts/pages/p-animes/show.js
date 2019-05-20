7faa6f2a914978bd327edc078aa75e0208e9c7ac
Andrey Sidorov <takandar@gmail.com>
2019-05-20 23:38:30 +0300

    decaffeinate animes_show + remove unused code

---
+++ app/assets/javascripts/pages/p-animes/show.js
@@ -0,0 +1,33 @@
+import FavoriteStar from 'views/application/favorite_star';
+
+pageLoad('animes_show', 'mangas_show', 'ranobe_show', () => {
+  $('.b-notice').tipsy({ gravity: 's' });
+  $('.c-screenshot').magnificRelGallery();
+
+  $('.text').checkHeight({ max_height: 200 });
+
+  new FavoriteStar($('.b-subposter-actions .fav-add'), gon.is_favoured);
+
+  const $newReview = $('.new_review');
+  if (window.SHIKI_USER.isSignedIn) {
+    const newReviewUrl = $newReview
+      .attr('href')
+      .replace(/%5Buser_id%5D=(\d+|ID)/, `%5Buser_id%5D=${window.SHIKI_USER.id}`);
+    $newReview.attr({ href: newReviewUrl });
+  } else {
+    $newReview.hide();
+  }
+
+  // autoload of resource info for guests
+  $('.l-content')
+    .on('postloaded:success', '.resources-loader', () => $('.c-screenshot').magnificRelGallery());
+
+  $('.other-names.click-loader').on('ajax:success', ({ currentTarget }, data) => {
+    $(currentTarget).closest('.line').replaceWith(data);
+  });
+
+  $('.b-subposter-actions .new_comment').on('click', () => {
+    const $editor = $('.b-form.new_comment textarea');
+    $.scrollTo($editor, () => $editor.focus());
+  });
+});
