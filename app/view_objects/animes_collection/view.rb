234c24034d4acef0a868f675b291e9b996025a33
Andrey Sidorov <takandar@gmail.com>
2019-05-22 11:41:55 +0300

    increment cache key

--- app/view_objects/animes_collection/view.rb
+++ app/view_objects/animes_collection/view.rb
@@ -9,7 +9,7 @@ class AnimesCollection::View < ViewObjectBase
   SEASON_LIMIT = 1000
 
   DEFAULT_ORDER = 'ranked'
-  CACHE_VERSION = 17
+  CACHE_VERSION = 18
 
   def collection
     if season_page?
