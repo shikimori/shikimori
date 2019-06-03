f2ef572e3751d490bc376239e6f26d0373475298
Andrey Sidorov <takandar@gmail.com>
2019-06-04 02:33:57 +0300

    Animes::SmotretAnimeId

---
+++ app/query_objects/animes/smotret_anime_id.rb
@@ -0,0 +1,20 @@
+class Animes::SmotretAnimeId
+  method_object :anime
+
+  def call
+    if anime.association_cached? :all_external_links
+      extract_id anime.all_external_links.find(&:kind_smotret_anime?)
+    else
+      extract_id anime.smotret_anime_external_link
+    end
+  end
+
+private
+
+  def extract_id external_link
+    return unless external_link
+
+    id = external_link.url.split('/').last.to_i
+    id if id != -1
+  end
+end
---
+++ spec/query_objects/animes/smotret_anime_id_spec.rb
@@ -0,0 +1,28 @@
+describe Animes::SmotretAnimeId do
+  subject { described_class.call anime }
+  let(:anime) { create :anime }
+
+  context 'has link' do
+    let!(:external_link) do
+      create :external_link,
+        source: :smotret_anime,
+        kind: :smotret_anime,
+        entry: anime,
+        url: format(Animes::LinkSmotretAnime::SMOTRET_ANIME_URL, smotret_anime_id: smotret_anime_id)
+    end
+
+    context 'has id' do
+      let(:smotret_anime_id) { 123 }
+      it { is_expected.to eq 123 }
+    end
+
+    context 'no id' do
+      let(:smotret_anime_id) { -1 }
+      it { is_expected.to be_nil }
+    end
+  end
+
+  context 'no link' do
+    it { is_expected.to be_nil }
+  end
+end
