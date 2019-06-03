91a5c7fc488087822fb71f3c1cf3b78c1175b59b
Andrey Sidorov <takandar@gmail.com>
2019-06-04 02:15:38 +0300

    add context in spec

--- spec/workers/animes/link_smotret_anime_spec.rb
+++ spec/workers/animes/link_smotret_anime_spec.rb
@@ -26,33 +26,35 @@ describe Animes::LinkSmotretAnime, :vcr do
     it { expect(anime.all_external_links).to have(3).items }
   end
 
-  it do
-    expect(external_link_1.reload).to be_persisted
-    expect { external_link_2.reload }.to raise_error ActiveRecord::RecordNotFound
-    expect(external_link_3.reload).to be_persisted
+  context 'matched mal_id' do
+    it do
+      expect(external_link_1.reload).to be_persisted
+      expect { external_link_2.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect(external_link_3.reload).to be_persisted
 
-    expect(anime.all_external_links).to have(6).items
-    expect(anime.all_external_links[0]).to eq external_link_1
-    expect(anime.all_external_links[1]).to eq external_link_3
-    expect(anime.all_external_links[2]).to have_attributes(
-      kind: 'smotret_anime',
-      url: 'https://smotretanime.ru/catalog/19351',
-      source: 'smotret_anime'
-    )
-    expect(anime.all_external_links[3]).to have_attributes(
-      kind: 'world_art',
-      url: 'http://www.world-art.ru/animation/animation.php?id=9778',
-      source: 'smotret_anime'
-    )
-    expect(anime.all_external_links[4]).to have_attributes(
-      kind: 'wikipedia',
-      url: 'https://en.wikipedia.org/wiki/Kono_Oto_Tomare!',
-      source: 'smotret_anime'
-    )
-    expect(anime.all_external_links[5]).to have_attributes(
-      kind: 'wikipedia',
-      url: 'https://en.wikipedia.org/wiki/Kono_Oto_Tomare!_Sounds_of_Life',
-      source: 'smotret_anime'
-    )
+      expect(anime.all_external_links).to have(6).items
+      expect(anime.all_external_links[0]).to eq external_link_1
+      expect(anime.all_external_links[1]).to eq external_link_3
+      expect(anime.all_external_links[2]).to have_attributes(
+        kind: 'smotret_anime',
+        url: 'https://smotretanime.ru/catalog/19351',
+        source: 'smotret_anime'
+      )
+      expect(anime.all_external_links[3]).to have_attributes(
+        kind: 'world_art',
+        url: 'http://www.world-art.ru/animation/animation.php?id=9778',
+        source: 'smotret_anime'
+      )
+      expect(anime.all_external_links[4]).to have_attributes(
+        kind: 'wikipedia',
+        url: 'https://en.wikipedia.org/wiki/Kono_Oto_Tomare!',
+        source: 'smotret_anime'
+      )
+      expect(anime.all_external_links[5]).to have_attributes(
+        kind: 'wikipedia',
+        url: 'https://en.wikipedia.org/wiki/Kono_Oto_Tomare!_Sounds_of_Life',
+        source: 'smotret_anime'
+      )
+    end
   end
 end
