952b135a1952ec22fa780f016d49097880ca0515
Andrey Sidorov <takandar@gmail.com>
2019-06-04 14:59:44 +0300

    fix migration

--- db/migrate/20190604003750_remove_desynced_external_links_from_all_animes.rb
+++ db/migrate/20190604003750_remove_desynced_external_links_from_all_animes.rb
@@ -3,6 +3,7 @@ class RemoveDesyncedExternalLinksFromAllAnimes < ActiveRecord::Migration[5.2]
     Anime.where("'external_links' = ANY(desynced)").find_each do |anime|
       anime.desynced -= %w[external_links]
       anime.save
+      MalParsers::FetchEntry.perform_async anime.id, 'anime'
     end
   end
 end
