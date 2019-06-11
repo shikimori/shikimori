19925347b528837855d6874b609370772abecddb
Andrey Sidorov <takandar@gmail.com>
2019-06-11 13:49:44 +0300

    also assign created_at

--- app/operations/episode_notification/track.rb
+++ app/operations/episode_notification/track.rb
@@ -1,5 +1,14 @@
-class EpisodeNotifications::Track
-  method_object %i[anime_id! episode! is_raw is_torrent is_unknown is_subtitles is_fandub]
+class EpisodeNotification::Track
+  method_object %i[
+    anime_id!
+    episode!
+    aired_at
+    is_raw
+    is_torrent
+    is_unknown
+    is_subtitles
+    is_fandub
+  ]
 
   def call
     model = find_or_initialize
@@ -21,12 +30,16 @@ private
     )
   end
 
-  def assign model
+  def assign model # rubocop:disable all
     model.is_raw = true if @is_raw
     model.is_torrent = true if @is_torrent
     model.is_unknown = true if @is_unknown
     model.is_subtitles = true if @is_subtitles
     model.is_fandub = true if @is_fandub
+
+    if model.new_record? && @aired_at.present?
+      model.created_at = @aired_at
+    end
   end
 
   def save model
--- spec/operations/episode_notification/track_spec.rb
+++ spec/operations/episode_notification/track_spec.rb
@@ -1,4 +1,6 @@
-describe EpisodeNotifications::Track do
+describe EpisodeNotification::Track do
+  include_context :timecop
+
   let!(:episode_notification) { nil }
   let(:anime) { create :anime, :ongoing, episodes_aired: 2, episodes: 4 }
 
@@ -7,6 +9,7 @@ describe EpisodeNotifications::Track do
     {
       anime_id: anime.id,
       episode: episode,
+      aired_at: aired_at,
       is_raw: is_raw,
       is_torrent: is_torrent,
       is_unknown: is_unknown,
@@ -15,6 +18,7 @@ describe EpisodeNotifications::Track do
     }
   end
   let(:episode) { 3 }
+  let(:aired_at) { 1.week.ago }
   let(:is_raw) { false }
   let(:is_torrent) { true }
   let(:is_unknown) { false }
@@ -32,6 +36,7 @@ describe EpisodeNotifications::Track do
     it do
       is_expected.to eq episode_notification
       expect(episode_notification.reload.is_torrent).to eq true
+      expect(episode_notification.created_at).to be_within(0.1).of Time.zone.now
       expect(anime.episode_notifications).to have(1).item
     end
   end
@@ -54,6 +59,7 @@ describe EpisodeNotifications::Track do
         is_subtitles: is_subtitles,
         is_fandub: is_fandub
       )
+      expect(subject.created_at).to be_within(0.1).of 1.week.ago
       expect(anime.episode_notifications).to have(1).item
       expect(anime.reload.episodes_aired).to eq episode
     end
@@ -69,6 +75,26 @@ describe EpisodeNotifications::Track do
         is_expected.to be_new_record
         expect(anime.episode_notifications).to be_empty
         expect(anime.reload.episodes_aired).to eq 2
+      end
+    end
+
+    context 'no aired_at' do
+      let(:aired_at) { nil }
+
+      it do
+        is_expected.to be_persisted
+        is_expected.to have_attributes(
+          anime_id: anime.id,
+          episode: episode,
+          is_raw: is_raw,
+          is_torrent: is_torrent,
+          is_unknown: is_unknown,
+          is_subtitles: is_subtitles,
+          is_fandub: is_fandub
+        )
+        expect(subject.created_at).to be_within(0.1).of Time.zone.now
+        expect(anime.episode_notifications).to have(1).item
+        expect(anime.reload.episodes_aired).to eq episode
       end
     end
   end
