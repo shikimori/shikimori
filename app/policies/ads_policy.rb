3d57946ff4bc0518751770d21ada11a968d46246
Andrey Sidorov <takandar@gmail.com>
2019-06-11 17:09:26 +0300

    simplify ads policy

--- app/policies/ads_policy.rb
+++ app/policies/ads_policy.rb
@@ -3,45 +3,27 @@ class AdsPolicy
     user!
     ad_provider!
     is_ru_host!
-    is_clean_host!
-    is_shikimori!
     is_disabled!
   ]
 
-  def allowed? # rubocop:disable all
+  def allowed?
     return false if @is_disabled
     return false unless @is_ru_host
-    return false if @is_clean_host
-
-    return false if special?(@ad_provider) && !@is_shikimori
-    return false if mainstream?(@ad_provider) && Rails.env.development?
-    return false if mainstream?(@ad_provider) && !@is_shikimori
 
     return true unless @user
-    return true if istari?(@ad_provider) || special?(@ad_provider)
-    return true if user&.admin?
+    return true if user.admin?
+    return true if special? @ad_provider
 
     !contributor?(@user)
   end
 
 private
 
-  def mainstream? ad_provider
-    [
-      Types::Ad::Provider[:yandex_direct],
-      Types::Ad::Provider[:mytarget]
-    ].include? ad_provider
-  end
-
-  def istari? ad_provider
-    ad_provider == Types::Ad::Provider[:istari]
-  end
-
   def special? ad_provider
     ad_provider == Types::Ad::Provider[:special]
   end
 
-  def contributor? user
+  def contributor? user # rubocop:disable all
     user.forum_moderator? || user.review_moderator? ||
       user.version_moderator? || user.video_moderator? ||
       user.trusted_version_changer? || user.trusted_video_uploader? ||
--- spec/policies/ads_policy_spec.rb
+++ spec/policies/ads_policy_spec.rb
@@ -1,20 +1,16 @@
 describe AdsPolicy do
   subject(:policy) do
     AdsPolicy.new(
-      is_ru_host: is_ru_host,
-      is_clean_host: is_clean_host,
-      is_shikimori: is_shikimori,
       ad_provider: ad_provider,
       user: user,
+      is_ru_host: is_ru_host,
       is_disabled: is_disabled
     )
   end
 
-  let(:is_ru_host) { true }
-  let(:is_clean_host) { false }
-  let(:is_shikimori) { true }
   let(:ad_provider) { Types::Ad::Provider.values.sample }
   let(:user) { nil }
+  let(:is_ru_host) { true }
   let(:is_disabled) { false }
 
   it { is_expected.to be_allowed }
@@ -26,11 +22,6 @@ describe AdsPolicy do
 
   context 'not ru_host' do
     let(:is_ru_host) { false }
-    it { is_expected.to_not be_allowed }
-  end
-
-  context 'clean host' do
-    let(:is_clean_host) { true }
     it { is_expected.to_not be_allowed }
   end
 
@@ -57,48 +48,17 @@ describe AdsPolicy do
         ].sample
       end
 
-      context 'not istari' do
+      context 'not special' do
         let(:ad_provider) do
-          Types::Ad::Provider.values - [Types::Ad::Provider[:istari]]
+          Types::Ad::Provider.values - [Types::Ad::Provider[:special]]
         end
         it { is_expected.to_not be_allowed }
-      end
-
-      context 'istari' do
-        let(:ad_provider) { Types::Ad::Provider[:istari] }
-        it { is_expected.to be_allowed }
       end
 
       context 'special' do
         let(:ad_provider) { Types::Ad::Provider[:special] }
         it { is_expected.to be_allowed }
       end
-    end
-  end
-
-  describe 'is_shikimori & ad_proider' do
-    describe 'not shikimori & mainstream' do
-      let(:is_shikimori) { false }
-      let(:ad_provider) { Types::Ad::Provider[%i[yandex_direct mytarget].sample] }
-      it { is_expected.to_not be_allowed }
-    end
-
-    describe 'shikimori & mainstream' do
-      let(:is_shikimori) { true }
-      let(:ad_provider) { Types::Ad::Provider[%i[yandex_direct mytarget].sample] }
-      it { is_expected.to be_allowed }
-    end
-
-    describe 'not shikimori & not mainstream' do
-      let(:is_shikimori) { false }
-      let(:ad_provider) { Types::Ad::Provider[:advertur] }
-      it { is_expected.to be_allowed }
-    end
-
-    describe 'shikimori & not mainstream' do
-      let(:is_shikimori) { true }
-      let(:ad_provider) { Types::Ad::Provider[:advertur] }
-      it { is_expected.to be_allowed }
     end
   end
 end
