7d0d731650b7f96b3ac2eeef5140a566c2f374bf
Andrey Sidorov <takandar@gmail.com>
2019-06-14 15:35:59 +0300

    temporarily do not ignore exceptions

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,11 +2,11 @@ if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = '829500b442f2f663128489da4ae0ae81'
 
-    Shikimori::IGNORED_EXCEPTIONS
-      .map { |v| v.constantize rescue NameError }
-      .reject { |v| v == NameError }
-      .each do |klass|
-        config.ignore_classes << klass
-      end
+    # Shikimori::IGNORED_EXCEPTIONS
+    #   .map { |v| v.constantize rescue NameError }
+    #   .reject { |v| v == NameError }
+    #   .each do |klass|
+    #     config.ignore_classes << klass
+    #   end
   end
 end
