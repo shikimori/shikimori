f0ab5f5852b715e5ce5a9b75162fd2f3a7291285
Andrey Sidorov <takandar@gmail.com>
2019-06-18 14:15:16 +0300

    fix click loader

---
+++ app/assets/javascripts/blocks/click_loader.js
@@ -0,0 +1,22 @@
+import axios from 'helpers/axios';
+
+$(document).on('click', '.click-loader', async ({ currentTarget }) => {
+  const $loader = $(currentTarget);
+
+  if ($loader.data('locked')) { return; }
+
+  $loader.data({ locked: true });
+  $loader.trigger('ajax:before');
+
+  $loader
+    .data({ html: $loader.html() })
+    .html(`<div class='ajax-loading vk-like' title='${I18n.t('frontend.blocks.click_loader.loading')}' />`);
+
+  const method = $loader.data('format') === 'json' ? 'getJSON' : 'get';
+
+  const { data } = await axios[method]($loader.data('href'));
+
+  $loader
+    .data({ locked: false })
+    .trigger('ajax:success', [data]);
+});
