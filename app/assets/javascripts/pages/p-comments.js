631840d191c7b58bdff7d37539bbfbcf5ba9acd7
Andrey Sidorov <takandar@gmail.com>
2019-07-17 00:18:14 +0300

    fix commend expand logic on comments#show

--- app/assets/javascripts/pages/p-comments.js
+++ app/assets/javascripts/pages/p-comments.js
@@ -3,7 +3,8 @@ import delay from 'delay';
 pageLoad('comments_show', async () => {
   await delay(250);
 
-  $('.b-comment').imagesLoaded(() =>
+  // expand displayed comment and do not expand all replies to that comment
+  $('.b-comment').first().imagesLoaded(() =>
     $('.b-height_shortener .expand').click()
   );
 });
