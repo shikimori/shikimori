154c0cba2acdc5d342be2b709e447052c5e86f86
Andrey Sidorov <takandar@gmail.com>
2019-07-31 00:54:47 +0300

    Topics::DecomposedBody

---
+++ app/value_objects/topics/decomposed_body.rb
@@ -0,0 +1,49 @@
+class Topics::DecomposedBody
+  include ShallowAttributes
+  prepend ActiveCacher.instance
+
+  attribute :text, String, allow_nil: false
+  attribute :source, String, allow_nil: true
+  attribute :wall, String, allow_nil: true
+
+  instance_cache :wall_video, :wall_images
+
+  PARSE_REGEXP = %r{
+    \A
+    (?<text>[\s\S]*?)
+    [\r\n\s]*
+    (?<wall> \[wall\].+\[/wall\])?
+    [\r\n\s]*
+    ( \[source\](?<source>.+)\[/source\])?
+    \Z
+  }mix
+
+  WALL_VIDEO_REGEXP = /\[video = (?<id>\d+) \]/mix
+  WALL_IMAGE_REGEXP = /\[(?:poster|image|wall_image) = (?<id>\d+) \]/mix
+
+  def wall_video
+    return if wall.blank?
+
+    ids = wall.scan(WALL_VIDEO_REGEXP).map { |v| v[0].to_i }
+    Video.find_by(id: ids)
+  end
+
+  def wall_images
+    return [] if wall.blank?
+
+    ids = wall.scan(WALL_IMAGE_REGEXP).map { |v| v[0].to_i }
+    UserImage.where(id: ids).sort_by { |v| ids.index v.id }
+  end
+
+  class << self
+    def from_value value
+      matches = (value || '').match PARSE_REGEXP
+
+      new(
+        text: matches[:text],
+        wall: matches[:wall],
+        source: matches[:source]
+      )
+    end
+  end
+end
---
+++ spec/value_objects/topics/decomposed_body_spec.rb
@@ -0,0 +1,77 @@
+describe Topics::DecomposedBody do
+  subject { Topics::DecomposedBody.from_value body }
+  let(:image_1) { create :user_image }
+
+  context 'text only' do
+    let(:body) do
+      <<~TEXT.squish.strip
+        test[image=#{image_1.id}]
+      TEXT
+    end
+
+    it do
+      is_expected.to have_attributes(
+        text: "test[image=#{image_1.id}]",
+        wall: nil,
+        source: nil
+      )
+      expect(subject.wall_video).to be_nil
+      expect(subject.wall_images).to be_empty
+    end
+  end
+
+  context 'with source' do
+    let(:body) do
+      <<~TEXT.squish.strip
+        test[image=#{image_1.id}]
+        [source]https://zxc.org[/source]
+      TEXT
+    end
+
+    it do
+      is_expected.to have_attributes(
+        text: "test[image=#{image_1.id}]",
+        wall: nil,
+        source: 'https://zxc.org'
+      )
+      expect(subject.wall_video).to be_nil
+      expect(subject.wall_images).to be_empty
+    end
+  end
+
+  context 'with wall & source' do
+    let(:body) do
+      <<~TEXT.squish.strip
+        test[image=#{image_1.id}]
+        [wall]
+        [video=#{video.id}]
+        [image=#{image_2.id}]
+        [image=#{image_3.id}]
+        [/wall]
+        [source]https://zxc.org[/source]
+      TEXT
+    end
+
+    let(:image_2) { create :user_image }
+    let(:image_3) { create :user_image }
+    let(:video) { create :video }
+
+    it do
+      is_expected.to have_attributes(
+        text: "test[image=#{image_1.id}]",
+        wall: (
+          <<~TEXT.squish.strip
+            [wall]
+            [video=#{video.id}]
+            [image=#{image_2.id}]
+            [image=#{image_3.id}]
+            [/wall]
+          TEXT
+        ),
+        source: 'https://zxc.org'
+      )
+      expect(subject.wall_video).to eq video
+      expect(subject.wall_images).to eq [image_2, image_3]
+    end
+  end
+end
