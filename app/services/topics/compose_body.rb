1183d0646b7411fea1323df981aa7a5faa438a56
Andrey Sidorov <takandar@gmail.com>
2019-07-31 01:08:51 +0300

    support source in Topics::ComposeBody

--- app/services/topics/compose_body.rb
+++ app/services/topics/compose_body.rb
@@ -1,9 +1,10 @@
-class Topics::ComposeBody < ServiceObjectBase
-  pattr_initialize :params
+class Topics::ComposeBody
+  method_object :params
 
   def call
     body = params[:body]
-    body += wall params[:video_id], params[:wall_ids] if wall?(params)
+    body += wall params[:video_id], params[:wall_ids] if wall? params
+    body += source params[:source] if source? params
     body
   end
 
@@ -13,12 +14,20 @@ private
     params[:video_id] || params[:wall_ids]
   end
 
+  def source? params
+    params[:source].present?
+  end
+
   def wall video_id, image_ids
     "[wall]#{video video_id}#{images image_ids}[/wall]"
   end
 
   def video video_id
     "[video=#{video_id}]" if video_id
+  end
+
+  def source url
+    "[source]#{url}[/source]"
   end
 
   def images image_ids
--- spec/services/topics/compose_body_spec.rb
+++ spec/services/topics/compose_body_spec.rb
@@ -1,42 +1,51 @@
 describe Topics::ComposeBody do
-  let(:service) { Topics::ComposeBody.new params }
-  let :params do
+  subject { Topics::ComposeBody.call params }
+
+  let(:params) do
     {
       body: body,
       wall_ids: wall_ids,
-      video_id: video_id
+      video_id: video_id,
+      source: source
     }
   end
   let(:body) { 'test' }
   let(:wall_ids) {}
   let(:video_id) {}
+  let(:source) { [nil, ''].sample }
 
-  subject { service.call }
+  it { is_expected.to eq body }
 
-  describe '#call' do
-    it { is_expected.to eq body }
+  context 'images' do
+    let(:user_image) { create :user_image }
+    let(:wall_ids) { [user_image.id.to_s] }
 
-    context 'images' do
-      let(:user_image) { create :user_image }
-      let(:wall_ids) { [user_image.id.to_s] }
+    it { is_expected.to eq "#{body}[wall][wall_image=#{user_image.id}][/wall]" }
+  end
 
-      it { is_expected.to eq "#{body}[wall][wall_image=#{user_image.id}][/wall]" }
-    end
+  context 'video' do
+    let(:video) { create :video }
+    let(:video_id) { video.id.to_s }
+    it { is_expected.to eq "#{body}[wall][video=#{video.id}][/wall]" }
+  end
 
-    context 'video' do
-      let(:video) { create :video }
-      let(:video_id) { video.id.to_s }
-      it { is_expected.to eq "#{body}[wall][video=#{video.id}][/wall]" }
-    end
+  context 'source' do
+    let(:source) { 'https://zxc.org' }
+    it { is_expected.to eq "#{body}[source]https://zxc.org[/source]" }
+  end
 
-    context 'images + video' do
-      let(:user_image) { create :user_image }
-      let(:video) { create :video }
+  context 'all together' do
+    let(:user_image) { create :user_image }
+    let(:video) { create :video }
+    let(:source) { 'https://zxc.org' }
 
-      let(:wall_ids) { [user_image.id.to_s] }
-      let(:video_id) { video.id.to_s }
+    let(:wall_ids) { [user_image.id.to_s] }
+    let(:video_id) { video.id.to_s }
 
-      it { is_expected.to eq "#{body}[wall][video=#{video.id}][wall_image=#{user_image.id}][/wall]" }
+    it do
+      is_expected.to eq(
+        "#{body}[wall][video=#{video.id}][wall_image=#{user_image.id}][/wall][source]https://zxc.org[/source]"
+      )
     end
   end
 end
