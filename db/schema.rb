7140364f2b42abcb0c120556b621563b0cd383e4
Andrey Sidorov <takandar@gmail.com>
2019-08-01 18:22:15 +0300

    udpate forums positions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_110736) do
+ActiveRecord::Schema.define(version: 2019_08_01_151360) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
