5ebcd7cc3f93512f84eceecc9115ebf611ce79bc
Andrey Sidorov <takandar@gmail.com>
2019-08-05 15:34:50 +0300

    get rid of nil values for last_online_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_211631) do
+ActiveRecord::Schema.define(version: 2019_08_05_123142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
