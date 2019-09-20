b38580b3bd44b612b1f8ca9c9c68666c66d56f56
Andrey Sidorov <takandar@gmail.com>
2019-09-21 01:16:00 +0300

    do not allow version_moderator to manage fields of version_texts_moderator and version_fansubbers_moderator (#1757)

--- app/models/abilities/version_moderator.rb
+++ app/models/abilities/version_moderator.rb
@@ -2,6 +2,9 @@ class Abilities::VersionModerator
   include CanCan::Ability
   prepend Draper::CanCanCan
 
+  NOT_MANAGED_FIELDS = Abilities::VersionTextsModerator::MANAGED_FIELDS +
+    Abilities::VersionFansubModerator::MANAGED_FIELDS
+
   def initialize _user
     can :rollback_episode, Anime
     can :upload_episode, Anime
@@ -11,7 +14,8 @@ class Abilities::VersionModerator
     end
 
     can :manage, Version do |version|
-      !version.is_a?(Versions::RoleVersion)
+      !version.is_a?(Versions::RoleVersion) &&
+        (version.item_diff.keys & NOT_MANAGED_FIELDS).none?
     end
   end
 end
