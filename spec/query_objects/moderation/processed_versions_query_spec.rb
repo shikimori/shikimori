c210bceb6706196be08227c572df2bbb79acd776
Andrey Sidorov <takandar@gmail.com>
2019-09-21 18:05:23 +0300

    fix more tests

--- spec/query_objects/moderation/processed_versions_query_spec.rb
+++ spec/query_objects/moderation/processed_versions_query_spec.rb
@@ -1,13 +1,21 @@
 describe Moderation::ProcessedVersionsQuery do
   include_context :timecop, '2016-03-18 15:00:00'
 
-  let(:query) { Moderation::ProcessedVersionsQuery.new 'content', created_on }
+  let(:query) { Moderation::ProcessedVersionsQuery.new 'texts', created_on }
   let(:created_on) { nil }
 
-  let!(:version_1) { create :version, state: 'taken', updated_at: 1.minute.ago, created_at: 28.hours.ago }
-  let!(:version_2) { create :version, state: 'pending', updated_at: 2.minutes.ago, created_at: 2.hours.ago }
-  let!(:version_3) { create :version, state: 'accepted', updated_at: 3.minutes.ago, created_at: 29.hours.ago }
-  let!(:version_4) { create :version, state: 'deleted', updated_at: 4.minutes.ago, created_at: 40.hours.ago }
+  let!(:version_1) do 
+    create :version, state: 'taken', updated_at: 1.minute.ago, created_at: 28.hours.ago
+  end
+  let!(:version_2) do
+    create :version, state: 'pending', updated_at: 2.minutes.ago, created_at: 2.hours.ago
+  end
+  let!(:version_3) do
+    create :version, state: 'accepted', updated_at: 3.minutes.ago, created_at: 29.hours.ago
+  end
+  let!(:version_4) do
+    create :version, state: 'deleted', updated_at: 4.minutes.ago, created_at: 40.hours.ago
+  end
 
   describe '#fetch' do
     subject { query.fetch page, limit }
