b891ec70a7bd8c0f91542847772ec7d47ef0e297
Andrey Sidorov <takandar@gmail.com>
2019-09-21 19:27:26 +0300

    display reviews and collection moderation stats

--- app/controllers/moderations_controller.rb
+++ app/controllers/moderations_controller.rb
@@ -6,7 +6,7 @@ class ModerationsController < ShikimoriController
     og page_title: i18n_t('title')
   end
 
-  def show
+  def show # rubocop:disable All
     @moderation_policy = ModerationPolicy.new(
       current_user,
       locale_from_host,
@@ -20,6 +20,14 @@ class ModerationsController < ShikimoriController
 
     if can? :manage_version_moderator_role, User
       @content_versions_stats = content_versions_stats
+    end
+
+    if can? :manage_review_moderator_role, User
+      @reviews_stats = reviews_stats
+    end
+
+    if can? :manage_collection_moderator_role, User
+      @collections_stats = collections_stats
     end
   end
 
@@ -79,6 +87,32 @@ private
             ROLES
           )
         )
+        .sort_by(&:count)
+        .reverse
+    end
+  end
+
+  def reviews_stats
+    Rails.cache.fetch %i[reviews_stats v4], expires_in: 1.day do
+      Review
+        .where('created_at > ?', 4.month.ago)
+        .where.not(moderation_state: :pending)
+        .where.not(approver_id: nil)
+        .group(:approver_id)
+        .select('approver_id, count(*) as count')
+        .sort_by(&:count)
+        .reverse
+    end
+  end
+
+  def collections_stats
+    Rails.cache.fetch %i[collection_stats v4], expires_in: 1.day do
+      Collection
+        .where('created_at > ?', 4.month.ago)
+        .where.not(moderation_state: :pending)
+        .where.not(approver_id: nil)
+        .group(:approver_id)
+        .select('approver_id, count(*) as count')
         .sort_by(&:count)
         .reverse
     end
