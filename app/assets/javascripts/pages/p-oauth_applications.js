9fa33c149ccabc8db477703503a6a4ff7e1f394b
Andrey Sidorov <takandar@gmail.com>
2019-09-24 09:13:16 +0300

    fix native url sample click

--- app/assets/javascripts/pages/p-oauth_applications.js
+++ app/assets/javascripts/pages/p-oauth_applications.js
@@ -6,8 +6,8 @@ pageLoad(
   'oauth_applications_edit',
   'oauth_applications_update',
   () => {
-    $('.oauth_application_redirect_uri .hint .sample').on('click', (_index, node) => {
-      $('.oauth_application_redirect_uri input').val(node.innerHTML);
+    $('.oauth_application_redirect_uri .hint .sample').on('click', ({ currentTarget }) => {
+      $('.oauth_application_redirect_uri input').val(currentTarget.innerHTML);
     });
 
     $('.b-shiki_editor').each((_editorIndex, editorNode) =>
