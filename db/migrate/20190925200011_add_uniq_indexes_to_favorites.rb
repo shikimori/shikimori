fca21a9a7a680101e95cf47a24a495f99b372adc
Andrey Sidorov <takandar@gmail.com>
2019-09-26 08:35:41 +0300

    fix migration

--- db/migrate/20190925200011_add_uniq_indexes_to_favorites.rb
+++ db/migrate/20190925200011_add_uniq_indexes_to_favorites.rb
@@ -1,5 +1,12 @@
 class AddUniqIndexesToFavorites < ActiveRecord::Migration[5.2]
   def change
+    cleanup do
+      Favourite.where('kind is null').group('user_id, linked_id, linked_type').having('count(*) > 1').select('max(id) as id, count(*) as count')
+    end
+    cleanup do
+      Favourite.where("kind is not null").group('user_id, linked_id, linked_type, kind').having('count(*) > 1').select('max(id) as id, count(*) as count')
+    end
+
     remove_index :favourites, name: 'uniq_favourites'
 
     add_index :favourites, %w[linked_id linked_type kind user_id],
@@ -11,5 +18,13 @@ class AddUniqIndexesToFavorites < ActiveRecord::Migration[5.2]
       name: 'favorites_linked_id_linked_type_user_id',
       where: 'kind is null',
       unique: true
+  end
+
+private
+
+  def cleanup
+    while yield.any?
+      Favourite.where(id: yield.map(&:id)).destroy_all
+    end
   end
 end
