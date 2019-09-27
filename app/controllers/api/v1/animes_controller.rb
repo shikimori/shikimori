c24bda0cc6c04678b642365e8f257e685645a9b9
Andrey Sidorov <takandar@gmail.com>
2019-09-28 01:44:32 +0300

    increment neko cache key

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::AnimesController < Api::V1Controller # rubocop:disable ClassLengt
     NekoRepository.instance.cache_key(
       params[:controller],
       params[:action],
-      :v3
+      Anime.count,
+      :v4
     ).to_json
   }
 
