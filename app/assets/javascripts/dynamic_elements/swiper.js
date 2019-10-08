7a14a9719a25e3cc2cfeec8e5a252c84f464ab76
Andrey Sidorov <takandar@gmail.com>
2019-10-08 22:27:28 +0300

    refactor a bit

--- app/assets/javascripts/dynamic_elements/swiper.js
+++ app/assets/javascripts/dynamic_elements/swiper.js
@@ -4,14 +4,19 @@ import ShikiView from 'views/application/shiki_view';
 import Wall from 'views/wall/view';
 import WallCluster from 'views/wall/cluster';
 
-export default class ShikiSwiper extends ShikiView {
+export default class SwiperView extends ShikiView {
   async initialize() {
-    await this.$root.imagesLoaded();
-    if (this.$('.dynamically-replaced').length) {
-      // when thumbnail of video is broken, then it is replaced to shikimori custom thumbnail image
-      await this.$root.imagesLoaded();
-    }
+    this.setSizes();
+    await this.imagesLoaded();
 
+    const wall = this.buildWall();
+
+    if (wall.images.length > 1) {
+      this.buildSwiper();
+    }
+  }
+
+  setSizes() {
     const width = this.$root.width();
     let height;
 
@@ -21,24 +26,34 @@ export default class ShikiSwiper extends ShikiView {
       height = (width / (16.0 / 9.0)).round();
     }
     this.$root.css('max-height', height);
+  }
 
-    const wall = new Wall(this.$root, {
+  async imagesLoaded() {
+    await this.$root.imagesLoaded();
+    if (this.$('.dynamically-replaced').length) {
+      // when thumbnail of video is broken, then it is replaced to shikimori custom thumbnail image
+      await this.$root.imagesLoaded();
+    }
+  }
+
+  buildWall() {
+    return new Wall(this.$root, {
       isOneCluster: true,
       maxWidth: 9999,
       awaitImagesLoaded: false
     });
+  }
 
-    if (wall.images.length > 1) {
-      this.$root.children()
-        .addClass('swiper-slide')
-        .removeAttr('style')
-        .wrapAll('<div class="swiper-wrapper" />');
+  buildSwiper() {
+    this.$root.children()
+      .addClass('swiper-slide')
+      .removeAttr('style')
+      .wrapAll('<div class="swiper-wrapper" />');
 
-      new Swiper(this.root, {
-        slidesPerView: 'auto',
-        spaceBetween: WallCluster.MARGIN,
-        a11y: false
-      });
-    }
+    new Swiper(this.root, {
+      slidesPerView: 'auto',
+      spaceBetween: WallCluster.MARGIN,
+      a11y: false
+    });
   }
 }
