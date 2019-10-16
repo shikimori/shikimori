9eda3082943c9ecc64cb1aade1e6f1e35659c754
Andrey Sidorov <takandar@gmail.com>
2019-10-16 23:19:06 +0300

    fix favorites limit calculation

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -31,13 +31,10 @@ class Api::V1::FavoritesController < Api::V1Controller
     favorites_limit = Favourite::LIMITS[params[:linked_type]]
     raise CanCan::AccessDenied unless favorites_limit
 
-    added_count = Favourite
-      .where(
-        linked_type: params[:linked_type],
-        user_id: current_user.id,
-        kind: params[:kind]
-      )
-      .size
+    added_scope = Favourite
+      .where(linked_type: params[:linked_type], user_id: current_user.id)
+    added_scope.where! kind: params[:kind] if params[:kind].present?
+    added_count = added_scope.size
 
     if added_count >= favorites_limit
       render(
