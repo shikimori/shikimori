e777a7f2daa6d583ab8873015dcea089c37704a7
Andrey Sidorov <takandar@gmail.com>
2019-10-18 20:24:57 +0300

    better caching

--- app/view_objects/dashboard_view_v2.rb
+++ app/view_objects/dashboard_view_v2.rb
@@ -1,13 +1,9 @@
 class DashboardViewV2 < ViewObjectBase
   instance_cache :collection_topic_views,
     :review_topic_views,
-    :news,
+    :news_topic_views,
     :db_updates,
     :cache_keys
-
-  def contests
-    Contests::CurrentQuery.call
-  end
 
   def collection_topic_views
     collections_scope
@@ -17,16 +13,20 @@ class DashboardViewV2 < ViewObjectBase
       .sort_by { |view| -view.topic.id }
   end
 
-  def review_topic_views
+  def review_topic_views # rubocop:disable AbcSize
     reviews_scope
       .as_views(true, true)
       .shuffle
       .reject { |view| view.topic.linked.target.censored? }
-      .take(5 - contests.size)
+      .take(6 - (contests.size.zero? ? 0 : contests.size + 1))
       .sort_by { |view| -view.topic.id }
   end
 
-  def news
+  def contests
+    contests_scope
+  end
+
+  def news_topic_views
     news_scope
       .paginate(page, page == 1 ? 8 : 15, page == 1 ? 0 : -7)
       .transform do |topic|
@@ -42,7 +42,10 @@ class DashboardViewV2 < ViewObjectBase
 
   def cache_keys
     {
-      reviews: Review.order(id: :desc).first,
+      versions: "v#{rand(5)}",
+      collections: collections_scope.cache_key,
+      reviews: reviews_scope.cache_key,
+      contests: contests_scope.cache_key,
       news: [:news, news_scope.cache_key, page],
       db_updates: [:db_updates, db_updates_scope.cache_key, page]
     }
@@ -60,6 +63,13 @@ class DashboardViewV2 < ViewObjectBase
 
 private
 
+  def collections_scope
+    Topics::Query
+      .fetch(h.locale_from_host)
+      .by_forum(collections_forum, h.current_user, h.censored_forbidden?)
+      .limit(12)
+  end
+
   def reviews_scope
     Topics::Query
       .fetch(h.locale_from_host)
@@ -67,11 +77,8 @@ private
       .limit(8)
   end
 
-  def collections_scope
-    Topics::Query
-      .fetch(h.locale_from_host)
-      .by_forum(collections_forum, h.current_user, h.censored_forbidden?)
-      .limit(6)
+  def contests_scope
+    Contests::CurrentQuery.call
   end
 
   def news_scope
--- app/views/tests/news.json.jbuilder
+++ app/views/tests/news.json.jbuilder
@@ -6,12 +6,12 @@ json.content JsExports::Supervisor.instance.sweep(
   )
 )
 
-if @view.news.next_page
+if @view.news_topic_views.next_page
   json.postloader render 'blocks/postloader',
     filter: 'b-topic',
-    next_url: news_tests_url(page: @view.news.next_page),
-    prev_url: @view.news.prev_page ?
-      news_tests_url(page: @view.news.prev_page) : nil
+    next_url: news_tests_url(page: @view.news_topic_views.next_page),
+    prev_url: @view.news_topic_views.prev_page ?
+      news_tests_url(page: @view.news_topic_views.prev_page) : nil
 end
 
 json.JS_EXPORTS JsExports::Supervisor.instance.export(current_user)
