9c0fbd944f1d334ef9f060cfc65df8e6504e9508
Andrey Sidorov <takandar@gmail.com>
2019-10-31 03:49:01 +0300

    better logic in mobile_detect

--- app/assets/javascripts/helpers/mobile_detect.js
+++ app/assets/javascripts/helpers/mobile_detect.js
@@ -3,10 +3,14 @@ import MobileDetect from 'mobile-detect';
 export const mobileDetect = new MobileDetect(window.navigator.userAgent);
 
 export const isTablet = () =>
-  !!mobileDetect.tablet() || document.documentElement.clientWidth <= 1023;
+  document.documentElement.clientWidth <= 1023 || (
+    !!mobileDetect.tablet() && document.documentElement.clientWidth <= 1023
+  );
 
 export const isPhone = () =>
   !!mobileDetect.phone() || document.documentElement.clientWidth <= 480;
 
 export const isMobile = () =>
-  !!mobileDetect.mobile() || document.documentElement.clientWidth <= 1023;
+  document.documentElement.clientWidth <= 1023 || (
+    !!mobileDetect.mobile() && document.documentElement.clientWidth <= 1023
+  );
