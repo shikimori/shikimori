07ad95bb7edaf5e2450b80d8096946c686100fa5
Andrey Sidorov <takandar@gmail.com>
2019-11-03 03:17:13 +0300

    working on articles moderation

---
+++ app/controllers/moderations/articles_controller.rb
@@ -0,0 +1,54 @@
+class Moderations::ArticlesController < ModerationsController
+  load_and_authorize_resource
+
+  PENDING_PER_PAGE = 15
+  PROCESSED_PER_PAGE = 25
+
+  def index
+    og page_title: i18n_t('page_title')
+
+    @moderators = User
+      .where("roles && '{#{Types::User::Roles[:article_moderator]}}'")
+      .where.not(id: User::MORR_ID)
+      .sort_by { |v| v.nickname.downcase }
+
+    @processed = QueryObjectBase.new(processed_scope).paginate(@page, PROCESSED_PER_PAGE)
+    @pending = pending_scope
+  end
+
+  def accept
+    @resource.accept current_user
+    redirect_back fallback_location: moderations_articles_url
+  end
+
+  def reject
+    @resource.reject current_user
+    redirect_back fallback_location: moderations_articles_url
+  end
+
+  def cancel
+    @resource.cancel
+    redirect_back fallback_location: moderations_articles_url
+  end
+
+private
+
+  def processed_scope
+    Article
+      .where(moderation_state: %i[accepted rejected])
+      .where(state: :published)
+      .where(locale: locale_from_host)
+      .includes(:user, :approver, :topics)
+      .order(created_at: :desc)
+  end
+
+  def pending_scope
+    Article
+      .where(moderation_state: :pending)
+      .where(state: :published)
+      .where(locale: locale_from_host)
+      .includes(:user, :approver, :topics)
+      .order(created_at: :desc)
+      .limit(PENDING_PER_PAGE)
+  end
+end
---
+++ spec/controllers/moderations/articles_controller_spec.rb
@@ -0,0 +1,42 @@
+describe Moderations::ArticlesController do
+  describe '#index' do
+    include_context :authenticated
+    let!(:article) { create :article, :with_topics }
+    subject! { get :index }
+
+    it { expect(response).to have_http_status :success }
+  end
+
+  describe '#accept' do
+    include_context :authenticated, :article_moderator
+    subject! { post :accept, params: { id: article.id } }
+    let(:article) { create :article }
+
+    it do
+      expect(resource).to be_accepted
+      expect(response).to redirect_to moderations_articles_url
+    end
+  end
+
+  describe '#reject' do
+    include_context :authenticated, :article_moderator
+    subject! { post :reject, params: { id: article.id } }
+    let(:article) { create :article, :with_topics }
+
+    it do
+      expect(resource).to be_rejected
+      expect(response).to redirect_to moderations_articles_url
+    end
+  end
+
+  describe '#cancel' do
+    include_context :authenticated, :article_moderator
+    subject! { post :cancel, params: { id: article.id } }
+    let(:article) { create :article, :accepted, approver: user }
+
+    it do
+      expect(resource).to be_pending
+      expect(response).to redirect_to moderations_articles_url
+    end
+  end
+end
