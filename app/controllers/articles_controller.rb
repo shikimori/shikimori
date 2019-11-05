d31074d1d35742417387bef593d3ae0e3368ea2e
Andrey Sidorov <takandar@gmail.com>
2019-11-05 12:01:18 +0300

    working on articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,27 @@
-class CollectionsController < ShikimoriController
-  load_and_authorize_resource :collection, except: %i[index]
+class ArticlesController < ShikimoriController
+  load_and_authorize_resource :article, except: %i[index]
 
-  before_action { og page_title: i18n_i('Collection', :other) }
+  before_action { og page_title: i18n_i('Article', :other) }
   before_action :set_breadcrumbs, except: :index
   before_action :resource_redirect, if: :resource_id
 
-  UPDATE_PARAMS = %i[name text state] + [
-    links: %w[linked_id group text]
-  ]
-  CREATE_PARAMS = %i[user_id kind] + UPDATE_PARAMS
+  UPDATE_PARAMS = %i[name text state]
+  CREATE_PARAMS = %i[user_id] + UPDATE_PARAMS
 
   def index # rubocop:disable AbcSize
     @limit = [[params[:limit].to_i, 4].max, 8].min
 
-    @collection = Collections::Query.fetch(locale_from_host)
+    @article = Articles::Query.fetch(locale_from_host)
       .search(params[:search], locale_from_host)
       .paginate(@page, @limit)
-      .transform do |collection|
+      .transform do |article|
         Topics::TopicViewFactory
           .new(true, true)
-          .build(collection.maybe_topic(locale_from_host))
+          .build(article.maybe_topic(locale_from_host))
       end
 
     if @page == 1 && params[:search].blank? && user_signed_in?
-      @unpublished_collections = current_user.collections.unpublished
+      @unpublished_articles = current_user.articles.unpublished
     end
   end
 
@@ -37,16 +35,16 @@ class CollectionsController < ShikimoriController
   end
 
   def new
-    og page_title: i18n_t('new_collection')
+    og page_title: i18n_t('new_article')
     render :form
   end
 
   def create
-    @resource = Collection::Create.call create_params, locale_from_host
+    @resource = Article::Create.call create_params, locale_from_host
 
     if @resource.errors.blank?
-      redirect_to edit_collection_url(@resource),
-        notice: i18n_t('collection_created')
+      redirect_to edit_article_url(@resource),
+        notice: i18n_t('article_created')
     else
       new
     end
@@ -59,10 +57,10 @@ class CollectionsController < ShikimoriController
   end
 
   def update
-    Collection::Update.call @resource, update_params
+    Article::Update.call @resource, update_params
 
     if @resource.errors.blank?
-      redirect_to edit_collection_url(@resource), notice: t('changes_saved')
+      redirect_to edit_article_url(@resource), notice: t('changes_saved')
     else
       flash[:alert] = t('changes_not_saved')
       edit
@@ -73,32 +71,32 @@ class CollectionsController < ShikimoriController
     @resource.destroy!
 
     if request.xhr?
-      render json: { notice: i18n_t('collection_deleted') }
+      render json: { notice: i18n_t('article_deleted') }
     else
-      redirect_to collections_url, notice: i18n_t('collection_deleted')
+      redirect_to articles_url, notice: i18n_t('article_deleted')
     end
   end
 
 private
 
   def set_breadcrumbs
-    breadcrumb i18n_i('Collection', :other), collections_url
+    breadcrumb i18n_i('Article', :other), articles_url
 
     if %w[edit update].include? params[:action]
       breadcrumb(
         @resource.name,
-        @resource.published? ? collection_url(@resource) :
-          edit_collection_url(@resource)
+        @resource.published? ? article_url(@resource) :
+          edit_article_url(@resource)
       )
     end
   end
 
   def create_params
-    params.require(:collection).permit(*CREATE_PARAMS)
+    params.require(:article).permit(*CREATE_PARAMS)
   end
   alias new_params create_params
 
   def update_params
-    params.require(:collection).permit(*UPDATE_PARAMS)
+    params.require(:article).permit(*UPDATE_PARAMS)
   end
 end
