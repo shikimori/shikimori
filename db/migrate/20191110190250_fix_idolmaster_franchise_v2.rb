d3c53dd85e848a5e9b6b201e789a34d09b37e49b
Andrey Sidorov <takandar@gmail.com>
2019-11-10 16:05:26 +0300

    fix idolmaster franchise v3

---
+++ db/migrate/20191110190250_fix_idolmaster_franchise_v2.rb
@@ -0,0 +1,7 @@
+class FixIdolmasterFranchiseV2 < ActiveRecord::Migration[5.2]
+  def change
+    ids = Anime.where(franchise: 'idolmaster').pluck :id
+    Anime.where(id: ids).update_all franchise: nil
+    Animes::UpdateFranchises.new.call Anime.where(id: ids)
+  end
+end
