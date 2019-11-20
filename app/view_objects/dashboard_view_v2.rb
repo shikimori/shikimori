bfe37ea80d8bd99cf5ef02fccee60addb0c6788c
Andrey Sidorov <takandar@gmail.com>
2019-11-20 10:32:12 +0300

    add ad

--- app/view_objects/dashboard_view_v2.rb
+++ app/view_objects/dashboard_view_v2.rb
@@ -11,7 +11,7 @@ class DashboardViewV2 < ViewObjectBase # rubocop:disable ClassLength
     :collections_views,
     :history
 
-  CACHE_VERSION = :v8
+  CACHE_VERSION = :v9
   NEWS_FIRST_PAGE_LIMIT = 6
   NEWS_OTHER_PAGES_LIMIT = 15
 
