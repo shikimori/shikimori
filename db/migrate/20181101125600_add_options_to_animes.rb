67d9897c2635834733c63d9ed60d77040f65c35f
Andrey Sidorov <takandar@gmail.com>
2019-12-10 18:06:22 +0300

    fix migration

--- db/migrate/20181101125600_add_options_to_animes.rb
+++ db/migrate/20181101125600_add_options_to_animes.rb
@@ -1,6 +1,7 @@
 class AddOptionsToAnimes < ActiveRecord::Migration[5.2]
   def change
     return if Rails.env.production?
+    return if ENV['USER'] == 'morr'
 
     add_column :animes, :options, :string, default: [], null: false, array: true
   end
