3843aac8e0939c4ab16746597976981f420171ff
Andrey Sidorov <takandar@gmail.com>
2019-12-13 17:35:03 +0300

    change colors

--- app/helpers/ani_mangas_collection_helper.rb
+++ app/helpers/ani_mangas_collection_helper.rb
@@ -2,15 +2,15 @@ module AniMangasCollectionHelper
   # разбивка по типам
   def kinds_with_colors
     [
-      { kind: 'tv', color: 'blue' },
+      { kind: 'tv', color: 'red' },
       { kind: 'movie', color: 'orange' },
       { kind: 'OVA/ONA', color: 'green' },
       { kind: 'special', color: 'purple' },
       { kind: 'music', color: 'skyblue' },
 
       { kind: 'manga', color: 'blue' },
-      { kind: 'manhwa', color: 'green' },
-      { kind: 'novel', color: 'purple' },
+      { kind: 'manhwa', color: 'purple' },
+      { kind: 'novel', color: 'green' },
       { kind: 'manhua', color: 'orange' },
       { kind: 'one_shot', color: 'skyblue' },
       { kind: 'doujin', color: 'pink' }
