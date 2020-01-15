577a7953f662f36344d23301f25d0e641da0be2f
Andrey Sidorov <takandar@gmail.com>
2020-01-15 21:00:20 +0300

    new bugsnag api key

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 if defined? Bugsnag
   Bugsnag.configure do |config|
-    config.api_key = 'c32e9b7f70482e678ffc1bf3dbdd6c06'
+    config.api_key = '30578865a35a3c65bca96e9846ca1ab1'
 
     Shikimori::IGNORED_EXCEPTIONS
       .map { |v| v.constantize rescue NameError }
