dc62c5a42c8fe3bd7e7a44ce4973446807e28bb0
Andrey Sidorov <takandar@gmail.com>
2020-01-19 17:27:55 +0300

    upate for major/minor fields

--- app/controllers/db_entries_controller.rb
+++ app/controllers/db_entries_controller.rb
@@ -42,19 +42,14 @@ class DbEntriesController < ShikimoriController
     @field = params[:field]
 
     authorize! :create, temp_verison
-    authorize! :restricted_update, temp_verison if major_fields.include? @field
-    authorize! :minor_change, temp_verison if minor_fields.include? @field
+    authorize! :restricted_update, temp_verison if restricted_fields.include? @field
 
     render template: 'db_entries/edit_field'
   end
 
   def update
-    if (update_params.keys & major_fields).any?
+    if (update_params.keys & restricted_fields).any?
       authorize! :restricted_update, temp_verison
-    end
-
-    if (update_params.keys & minor_fields).any?
-      authorize! :minor_change, temp_verison
     end
 
     Version.transaction do
@@ -107,14 +102,8 @@ private
     og image: ImageUrlGenerator.instance.url(@resource, :original)
   end
 
-  def major_fields
+  def restricted_fields
     @resource.object.class::RESTRICTED_FIELDS.select do |field|
-      field != 'image' || @resource.image.exists?
-    end
-  end
-
-  def minor_fields
-    @resource.object.class::SIGNIFICANT_MINOR_FIELDS.select do |field|
       field != 'image' || @resource.image.exists?
     end
   end
