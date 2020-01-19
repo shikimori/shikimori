78504ae36df187d27bc38cfb739234b4f1bb8191
Andrey Sidorov <takandar@gmail.com>
2020-01-19 17:29:06 +0300

    bugfix in ability check

--- app/models/abilities/trusted_version_changer.rb
+++ app/models/abilities/trusted_version_changer.rb
@@ -2,9 +2,9 @@ class Abilities::TrustedVersionChanger
   include CanCan::Ability
   prepend Draper::CanCanCan
 
-  def initialize _user
+  def initialize user
     can %i[create], Version do |version|
-      version.user_id == @user.id &&
+      version.user_id == user.id &&
         !version.is_a?(Versions::RoleVersion)
     end
 
