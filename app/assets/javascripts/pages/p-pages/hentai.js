272a7c332bd243e87565ec014dd7824cd3bc748b
Andrey Sidorov <takandar@gmail.com>
2020-01-19 22:25:29 +0300

    mass delete hentai screenshots

--- app/assets/javascripts/pages/p-pages/hentai.js
+++ app/assets/javascripts/pages/p-pages/hentai.js
@@ -1,3 +1,7 @@
 pageLoad('pages_hentai', async () => {
   $('.c-screenshot').shikiImage();
+  $('.delete-all').click(({ currentTarget }) => {
+    if (!window.confirm('Точно?')) { return; }
+    $(currentTarget).prev().find('.confirm').click();
+  });
 });
