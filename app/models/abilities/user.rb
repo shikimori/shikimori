5aeebf2b7ff34eed904fdac8c5c80069db308438
Andrey Sidorov <takandar@gmail.com>
2020-01-21 19:51:07 +0300

    fix error with restricted field

--- app/models/abilities/user.rb
+++ app/models/abilities/user.rb
@@ -234,10 +234,10 @@ class Abilities::User
       if version.is_a? Versions::RoleVersion
         false
       else
-        restricted_field = (
-          version.item_diff.keys &
-            "#{version.item_type}::RESTRICTED_FIELDS".constantize
-        ).first
+        restricted_fields = Kernel.const_defined?("#{version.item_type}::RESTRICTED_FIELDS") ?
+          "#{version.item_type}::RESTRICTED_FIELDS".constantize :
+          []
+        matched_restricted_field = (version.item_diff.keys & restricted_fields).first
 
         !@user.banned? && !@user.not_trusted_version_changer? &&
           version.user_id == @user.id && (
@@ -245,8 +245,8 @@ class Abilities::User
             # it will return false in runtime
             # (i.e. during Version creation in DbEntriesController)
             Ability.new(@user).can?(:restricted_update, version) ||
-            restricted_field.nil? ||
-            version.item_diff.dig(restricted_field, 0).nil?  # changing from nil value
+            matched_restricted_field.nil? ||
+            version.item_diff.dig(matched_restricted_field, 0).nil?  # changing from nil value
           )
       end
     end
