d022b72ae5835db2bb10835c6745db9dd83ef4de
Andrey Sidorov <takandar@gmail.com>
2020-02-02 21:54:40 +0300

    touch releated characters and people while importing person roles

--- spec/services/db_import/person_roles_spec.rb
+++ spec/services/db_import/person_roles_spec.rb
@@ -1,9 +1,11 @@
 describe DbImport::PersonRoles do
+  include_context :timecop
+
   let(:service) { DbImport::PersonRoles.new target, characters, staff }
   let(:target) { create :anime, id: 114 }
   let(:characters) do
     [{
-      id: 143628,
+      id: character.id,
       roles: %w[Main]
     }, {
       id: 145176,
@@ -15,12 +17,16 @@ describe DbImport::PersonRoles do
   end
   let(:staff) do
     [{
-      id: 33365,
+      id: person.id,
       roles: %w[Director]
     }]
   end
   let!(:person_role) {}
   let(:person_roles) { target.person_roles.order :id }
+
+  let!(:character) { build_stubbed :character }
+  let!(:person) { build_stubbed :character }
+
   subject! { service.call }
 
   it do
@@ -28,7 +34,7 @@ describe DbImport::PersonRoles do
     expect(person_roles[0]).to have_attributes(
       anime_id: target.id,
       manga_id: nil,
-      character_id: 143_628,
+      character_id: character.id,
       person_id: nil,
       roles: %w[Main]
     )
@@ -43,7 +49,7 @@ describe DbImport::PersonRoles do
       anime_id: target.id,
       manga_id: nil,
       character_id: nil,
-      person_id: 33_365,
+      person_id: person.id,
       roles: %w[Director]
     )
   end
@@ -52,7 +58,7 @@ describe DbImport::PersonRoles do
     let!(:person_role) do
       create :person_role,
         anime_id: target.id,
-        character_id: 28_735
+        character_id: character.id
     end
     it do
       expect { person_role.reload }.to raise_error ActiveRecord::RecordNotFound
@@ -70,6 +76,16 @@ describe DbImport::PersonRoles do
     it do
       expect(person_role.reload).to be_persisted
       expect(person_roles).to have(4).items
+    end
+  end
+
+  describe 'touches related' do
+    let!(:character) { create :character, updated_at: 10.minutes.ago }
+    let!(:person) { create :person, updated_at: 10.minutes.ago }
+
+    it do
+      expect(character.reload.updated_at).to be_within(0.1).of Time.zone.now
+      expect(person.reload.updated_at).to be_within(0.1).of Time.zone.now
     end
   end
 end
