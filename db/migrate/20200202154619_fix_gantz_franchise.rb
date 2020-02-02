e22774ee7083cb837a5ecca496a4d4cd2860912b
Andrey Sidorov <takandar@gmail.com>
2020-02-02 13:11:47 +0300

    fix gantz franchise

---
+++ db/migrate/20200202154619_fix_gantz_franchise.rb
@@ -0,0 +1,7 @@
+class FixGantzFranchise < ActiveRecord::Migration[5.2]
+  def change
+    Animes::UpdateFranchises.new.call(
+      Anime.where(franchise: %w[senran_kagura])
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_190846) do
+ActiveRecord::Schema.define(version: 2020_02_02_154619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
