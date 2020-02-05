337e42cc2c4855d1224e228a8e15a54f4ab0d0d9
Andrey Sidorov <takandar@gmail.com>
2020-02-05 21:52:59 +0300

    fix spec

--- spec/controllers/api/v1/calendars_controller_spec.rb
+++ spec/controllers/api/v1/calendars_controller_spec.rb
@@ -16,7 +16,7 @@ describe Api::V1::CalendarsController, :show_in_doc do
     subject! { get :show, format: :json }
 
     it do
-      expect(collection).to have(3).items
+      expect(collection).to have(4).items
 
       expect(response).to have_http_status :success
       expect(response.content_type).to eq 'application/json'
