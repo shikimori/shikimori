bf7d9a71a35eb21548f8deb2cfc13d6d5d6bcb6b
Andrey Sidorov <takandar@gmail.com>
2020-02-08 15:32:03 +0300

    also delete files of old list imports in ListImports::Cleanup

--- app/workers/list_imports/cleanup.rb
+++ app/workers/list_imports/cleanup.rb
@@ -3,11 +3,37 @@ class ListImports::Cleanup
   sidekiq_options queue: :imports
 
   FAIL_INTERVAL = 7.days
+  FILE_DELETION_INTERVAL = 30.days
 
   def perform
+    fail_expired
+    delete_files
+  end
+
+private
+
+  def fail_expired
     ListImport
       .where(state: :pending)
       .where('created_at < ?', FAIL_INTERVAL.ago)
       .update_all state: :failed
+  end
+
+  def delete_files
+    ListImport
+      .includes(:user)
+      .where('created_at < ?', FILE_DELETION_INTERVAL.ago)
+      .where.not(list_file_size: nil)
+      .each do |list_import|
+        list_file_name = list_import.list_file_name
+
+        list_import.list.destroy
+        list_import.update!(
+          list_file_name: list_file_name,
+          list_content_type: nil,
+          list_file_size: nil,
+          list_updated_at: nil
+        )
+      end
   end
 end
--- spec/workers/list_imports/cleanup_spec.rb
+++ spec/workers/list_imports/cleanup_spec.rb
@@ -1,17 +1,38 @@
 describe ListImports::Cleanup do
-  let!(:list_import_1) do
-    create :list_import, :pending,
-      created_at: described_class::FAIL_INTERVAL.ago + 1.minute
-  end
-  let!(:list_import_2) do
-    create :list_import, :pending,
-      created_at: described_class::FAIL_INTERVAL.ago - 1.minute
-  end
+  let!(:list_import_1) {}
+  let!(:list_import_2) {}
 
   subject! { described_class.new.perform }
 
-  it do
-    expect(list_import_1.reload).to be_pending
-    expect(list_import_2.reload).to be_failed
+  describe 'fails expired imports' do
+    let!(:list_import_1) do
+      create :list_import, :pending,
+        created_at: described_class::FAIL_INTERVAL.ago + 1.minute
+    end
+    let!(:list_import_2) do
+      create :list_import, :pending,
+        created_at: described_class::FAIL_INTERVAL.ago - 1.minute
+    end
+
+    it do
+      expect(list_import_1.reload).to be_pending
+      expect(list_import_2.reload).to be_failed
+    end
+  end
+
+  describe 'deletes files of old imports' do
+    let!(:list_import_1) do
+      create :list_import, :failed, :shiki_json_empty,
+        created_at: described_class::FILE_DELETION_INTERVAL.ago + 1.minute
+    end
+    let!(:list_import_2) do
+      create :list_import, :failed, :shiki_json_empty,
+        created_at: described_class::FILE_DELETION_INTERVAL.ago - 1.minute
+    end
+
+    it do
+      expect(list_import_1.reload.list_content_type).to be_present
+      expect(list_import_2.reload.list_content_type).to be_nil
+    end
   end
 end
