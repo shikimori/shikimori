f58652310f7fe6cd8233fb408954d90be0105942
Andrey Sidorov <takandar@gmail.com>
2020-02-10 21:20:24 +0300

    fix migration

--- db/migrate/20200210114009_fix_gegege_no_kitarou_franchise.rb
+++ db/migrate/20200210114009_fix_gegege_no_kitarou_franchise.rb
@@ -1,4 +1,7 @@
 class FixGegegeNoKitarouFranchise < ActiveRecord::Migration[5.2]
   def change
+    Animes::UpdateFranchises.new.call(
+      Anime.where(franchise: %w[gegege_no_kitarou])
+    )
   end
 end
