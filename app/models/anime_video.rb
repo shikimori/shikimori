c9a072cfffe2b9452fffe80f3ddbc688d3c77a43
Andrey Sidorov <takandar@gmail.com>
2020-02-13 21:23:36 +0300

    remove old animeonline code

--- app/models/anime_video.rb
+++ app/models/anime_video.rb
@@ -44,26 +44,11 @@ class AnimeVideo < ApplicationRecord # rubocop:disable all
   validates :anime, :source, :kind, presence: true
   validates :url,
     presence: true,
-    anime_video_url: true,
     if: -> { new_record? || changes['url'] }
   validates :episode, numericality: { greater_than_or_equal_to: 0 }
 
   # before_save :check_copyrighted_authors,
   #   if: :anime_video_author_id_changed?
-  before_save :check_banned_hostings
-  before_save :check_copyrighted_animes
-
-  after_create :create_episode_notificaiton, unless: -> {
-    anime.anons? || any_videos?
-  }
-  after_destroy :rollback_episode_notification, unless: :any_videos?
-  after_update :check_episode_notification, if: -> {
-    saved_change_to_episode? ||
-      saved_change_to_kind? ||
-      saved_change_to_anime_id?
-  }
-
-  COPYRIGHT_BAN_ANIME_IDS = [-1]
 
   state_machine :state, initial: :working do
     state :working
@@ -92,21 +77,6 @@ class AnimeVideo < ApplicationRecord # rubocop:disable all
     event :uploaded do
       transition %i[working uploaded] => :working
     end
-
-    after_transition(
-      %i[working uploaded] => %i[broken wrong banned_hosting],
-      unless: :any_videos?,
-      do: :rollback_episode_notification
-    )
-    after_transition(
-      %i[working uploaded] => %i[broken wrong banned_hosting],
-      do: :process_reports
-    )
-    after_transition(
-      uploaded: :working,
-      do: :create_episode_notificaiton,
-      if: ->(anime_video) { anime_video.anime.anons? && !anime_video.any_videos? }
-    )
   end
 
   def url= value
@@ -136,101 +106,5 @@ class AnimeVideo < ApplicationRecord # rubocop:disable all
 
   def allowed?
     working? || uploaded?
-  end
-
-  def copyright_ban?
-    COPYRIGHT_BAN_ANIME_IDS.include? anime_id
-  end
-
-  def uploader
-    @uploader ||= AnimeVideoReport.find_by(
-      anime_video_id: id,
-      kind: 'uploaded'
-    )&.user
-  end
-
-  def any_videos?(
-    anime_id: self.anime_id,
-    episode: self.episode,
-    kind: self.kind
-  )
-    AnimeOnline::SameVideos.call(
-      anime_id: anime_id,
-      anime_video_id: id,
-      episode: episode,
-      kind: kind
-    ).any?
-  end
-
-private
-
-  def check_banned_hostings
-    self.state = 'banned_hosting' if BANNED_HOSTINGS.include? hosting
-  end
-
-  def check_copyrighted_animes
-    self.state = 'copyrighted' if copyright_ban?
-  end
-
-  def create_episode_notificaiton
-    EpisodeNotification::Create.call(
-      anime_id: anime_id,
-      episode: episode,
-      kind: kind
-    )
-  end
-
-  def rollback_episode_notification
-    EpisodeNotification::Rollback.call(
-      anime_id: anime_id,
-      episode: episode,
-      kind: kind
-    )
-  end
-
-  def check_episode_notification
-    any_new_videos = any_videos?(
-      anime_id: saved_changes.dig('anime_id', 1) || anime_id,
-      episode: saved_changes.dig('episode', 1) || episode,
-      kind: saved_changes.dig('kind', 1) || kind
-    )
-
-    any_old_videos = any_videos?(
-      anime_id: saved_changes.dig('anime_id', 0) || anime_id,
-      episode: saved_changes.dig('episode', 0) || episode,
-      kind: saved_changes.dig('kind', 0) || kind
-    )
-
-    # must be before EpisodeNotification::Create because in
-    # Anime::RollbackEpisode notifications with episode >= @episode are deleted
-    unless any_old_videos
-      EpisodeNotification::Rollback.call(
-        anime_id: saved_changes.dig('anime_id', 0) || anime_id,
-        episode: saved_changes.dig('episode', 0) || episode,
-        kind: saved_changes.dig('kind', 0) || kind
-      )
-    end
-
-    unless any_new_videos
-      EpisodeNotification::Create.call(
-        anime_id: saved_changes.dig('anime_id', 1) || anime_id,
-        episode: saved_changes.dig('episode', 1) || episode,
-        kind: saved_changes.dig('kind', 1) || kind
-      )
-    end
-  end
-
-  def process_reports
-    reports.each do |report|
-      process_report report
-    end
-  end
-
-  def process_report report
-    if (report.wrong? || report.broken? || report.other?) && report.pending?
-      report.accept_only! BotsService.get_poster
-    elsif report.uploaded? && report.can_post_reject?
-      report.post_reject!
-    end
   end
 end
--- spec/factories/anime_video.rb
+++ spec/factories/anime_video.rb
@@ -7,12 +7,6 @@ FactoryBot.define do
     episode { 1 }
     state { 'working' }
 
-    after :build do |model|
-      stub_method model, :create_episode_notificaiton
-      stub_method model, :rollback_episode_notification
-      stub_method model, :check_episode_notification
-    end
-
     AnimeVideo.kind.values.each do |video_kind|
       trait(video_kind.to_sym) { kind { video_kind } }
     end
@@ -23,14 +17,6 @@ FactoryBot.define do
 
     AnimeVideo.state_machine.states.map(&:value).each do |video_state|
       trait(video_state.to_sym) { state { video_state } }
-    end
-
-    trait :with_episode_notification do
-      after(:build) do |model|
-        unstub_method model, :create_episode_notificaiton
-        unstub_method model, :rollback_episode_notification
-        unstub_method model, :check_episode_notification
-      end
     end
   end
 end
