3ea01d78edf4900bfd18e87c9ba5fe3b2cb33507
Andrey Sidorov <takandar@gmail.com>
2020-02-14 17:13:51 +0300

    use Animes::Filters::Kind in AniMangaQuery

--- app/query_objects/ani_manga_query.rb
+++ app/query_objects/ani_manga_query.rb
@@ -122,53 +122,8 @@ private
       @ids&.any?
   end
 
-  # фильтр по типам
   def kind!
-    return if @kind.blank?
-
-    kinds = @kind
-      .split(',')
-      .each_with_object(complex: [], simple: []) do |kind, memo|
-        memo[kind.match?(/tv_\d+/) ? :complex : :simple] << kind
-      end
-
-    simple_kinds = bang_split kinds[:simple]
-
-    simple_queries = {
-      include: simple_kinds[:include]
-        .delete_if { |v| v == 'tv' && kinds[:complex].any? { |q| q =~ /^tv_/ } }
-        .map { |v| "#{table_name}.kind = #{ApplicationRecord.sanitize v}" },
-      exclude: simple_kinds[:exclude]
-        .map { |v| "#{table_name}.kind = #{ApplicationRecord.sanitize v}" }
-    }
-    complex_queries = { include: [], exclude: [] }
-
-    kinds[:complex].each do |kind|
-      with_bang = kind.starts_with? '!'
-
-      query = case kind
-        when 'tv_13', '!tv_13'
-          "(#{table_name}.kind = 'tv' and episodes != 0 and episodes <= 16) or (#{table_name}.kind = 'tv' and episodes = 0 and episodes_aired <= 16)"
-
-        when 'tv_24', '!tv_24'
-          "(#{table_name}.kind = 'tv' and episodes != 0 and episodes >= 17 and episodes <= 28) or (#{table_name}.kind = 'tv' and episodes = 0 and episodes_aired >= 17 and episodes_aired <= 28)"
-
-        when 'tv_48', '!tv_48'
-          "(#{table_name}.kind = 'tv' and episodes != 0 and episodes >= 29) or (#{table_name}.kind = 'tv' and episodes = 0 and episodes_aired >= 29)"
-      end
-
-      complex_queries[with_bang ? :exclude : :include] << query
-    end
-
-    includes = (simple_queries[:include] + complex_queries[:include]).compact
-    excludes = (simple_queries[:exclude] + complex_queries[:exclude]).compact
-
-    if includes.any?
-      @query = @query.where includes.join(' or ')
-    end
-    if excludes.any?
-      @query = @query.where 'not(' + excludes.join(' or ') + ')'
-    end
+    @query = Animes::Query.new(@query).kind(@kind)
   end
 
   def censored!
--- app/query_objects/animes/filters/helpers.rb
+++ app/query_objects/animes/filters/helpers.rb
@@ -10,25 +10,6 @@ module Animes::Filters::Helpers
     end
   end
 
-  # разбитие на 2 группы по наличию !, плюс возможная обработка элементов
-  def bang_split values, force_integer = false
-    data = values.each_with_object(include: [], exclude: []) do |v, memo|
-      memo[v.starts_with?('!') ? :exclude : :include] << v.sub('!', '')
-    end
-
-    if force_integer
-      data[:include].map!(&:to_i)
-      data[:exclude].map!(&:to_i)
-    end
-
-    if block_given?
-      data[:include].map! { |v| yield v }
-      data[:exclude].map! { |v| yield v }
-    end
-
-    data
-  end
-
   def table_name
     @scope.table_name
   end
