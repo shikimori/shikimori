c80283f7e6ba3293c1e0398476bc5c33fc90f320
Andrey Sidorov <takandar@gmail.com>
2020-02-14 18:58:07 +0300

    more precise tests

--- spec/query_objects/animes/filters/kind_spec.rb
+++ spec/query_objects/animes/filters/kind_spec.rb
@@ -1,5 +1,5 @@
 describe Animes::Filters::Kind do
-  subject { described_class.call Anime.all, term }
+  subject { described_class.call Anime.order(:id), terms }
 
   let!(:anime_1) { create :anime, :tv, episodes: 13 }
   let!(:anime_2) { create :anime, :tv, episodes: 0, episodes_aired: 13 }
@@ -12,67 +12,112 @@ describe Animes::Filters::Kind do
 
   let!(:anime_8) { create :anime, :tv, episodes: 29 }
   let!(:anime_9) { create :anime, :tv, episodes: 0, episodes_aired: 100 }
+
   let!(:anime_10) { create :anime, :movie }
 
   context 'tv' do
-    let(:term) { 'tv' }
-    it { is_expected.to have(9).items }
+    let(:terms) { 'tv' }
+    it do
+      is_expected.to eq [
+        anime_1,
+        anime_2,
+        anime_3,
+        anime_4,
+        anime_5,
+        anime_6,
+        anime_7,
+        anime_8,
+        anime_9
+      ]
+    end
   end
 
   context '!tv' do
-    let(:term) { '!tv' }
-    it { is_expected.to have(1).item }
+    let(:terms) { '!tv' }
+    it { is_expected.to eq [anime_10] }
   end
 
   context 'tv_13' do
-    let(:term) { 'tv_13' }
-    it { is_expected.to have(4).items }
+    let(:terms) { 'tv_13' }
+    it { is_expected.to eq [anime_1, anime_2, anime_3, anime_4] }
   end
 
   context '!tv_13' do
-    let(:term) { '!tv_13' }
-    it { is_expected.to have(6).items }
+    let(:terms) { '!tv_13' }
+    it { is_expected.to eq [anime_5, anime_6, anime_7, anime_8, anime_9, anime_10] }
   end
 
   context 'tv_24' do
-    let(:term) { 'tv_24' }
-    it { is_expected.to have(3).items }
+    let(:terms) { 'tv_24' }
+    it { is_expected.to eq [anime_5, anime_6, anime_7] }
   end
 
   context '!tv_24' do
-    let(:term) { '!tv_24' }
-    it { is_expected.to have(7).items }
+    let(:terms) { '!tv_24' }
+    it { is_expected.to eq [anime_1, anime_2, anime_3, anime_4, anime_8, anime_9, anime_10] }
   end
 
   context 'tv_48' do
-    let(:term) { 'tv_48' }
-    it { is_expected.to have(2).items }
+    let(:terms) { 'tv_48' }
+    it { is_expected.to eq [anime_8, anime_9] }
   end
 
   context '!tv_48' do
-    let(:term) { '!tv_48' }
-    it { is_expected.to have(8).items }
+    let(:terms) { '!tv_48' }
+    it do
+      is_expected.to eq [
+        anime_1,
+        anime_2,
+        anime_3,
+        anime_4,
+        anime_5,
+        anime_6,
+        anime_7,
+        anime_10
+      ]
+    end
   end
 
-  context 'multiple terms' do
+  context 'multiple termss' do
     context 'positive' do
-      let(:term) { 'tv_13,tv_24' }
-      it { is_expected.to have(7).items }
+      let(:terms) { 'tv_13,tv_24' }
+      it do
+        is_expected.to eq [
+          anime_1,
+          anime_2,
+          anime_3,
+          anime_4,
+          anime_5,
+          anime_6,
+          anime_7
+        ]
+      end
     end
 
     context 'negative' do
-      let(:term) { '!tv_13,!tv_24' }
-      it { is_expected.to have(3).items }
+      let(:terms) { '!tv_13,!tv_24' }
+      it { is_expected.to eq [anime_8, anime_9, anime_10] }
     end
 
     context 'mixed' do
-      let(:term) { 'movie,tv_13,tv_24' }
-      it { is_expected.to have(8).items }
+      let(:terms) { 'movie,tv_13,tv_24' }
+      it do
+        is_expected.to eq [
+          anime_1,
+          anime_2,
+          anime_3,
+          anime_4,
+          anime_5,
+          anime_6,
+          anime_7,
+          anime_10
+        ]
+      end
     end
 
     context 'tv + tv_13' do
-      let(:term) { 'tv,tv_13' }
-      it { is_expected.to have(4).items }
+      let(:terms) { 'tv,tv_13' }
+      it { is_expected.to eq [anime_1, anime_2, anime_3, anime_4] }
     end
   end
 end
