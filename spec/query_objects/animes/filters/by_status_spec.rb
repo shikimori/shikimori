2ff03cadcd324e92a99009703dce4e9ddf7b7e77
Andrey Sidorov <takandar@gmail.com>
2020-02-15 10:13:05 +0300

    better specs for Animes::Filters::ByStatus

--- spec/query_objects/animes/filters/by_status_spec.rb
+++ spec/query_objects/animes/filters/by_status_spec.rb
@@ -2,12 +2,15 @@ describe Animes::Filters::ByStatus do
   subject { described_class.call Anime.order(:id), terms }
 
   let!(:anime_1) { create :anime, :ongoing, aired_on: Time.zone.now - 1.month }
+
   let!(:anime_2) { create :anime, :anons }
   let!(:anime_3) { create :anime, :anons }
+
   let!(:anime_4) { create :anime, :released }
   let!(:anime_5) do
     create :anime, :released, released_on: described_class::LATEST_INTERVAL.ago - 1.day
   end
+
   let!(:anime_6) do
     create :anime, :released, released_on: described_class::LATEST_INTERVAL.ago + 1.day
   end
@@ -15,46 +18,46 @@ describe Animes::Filters::ByStatus do
   context 'positive' do
     context 'ongoing' do
       let(:terms) { 'ongoing' }
-      it { is_expected.to have(1).item }
+      it { is_expected.to eq [anime_1] }
     end
 
     context 'latest' do
       let(:terms) { 'latest' }
-      it { is_expected.to have(1).item }
+      it { is_expected.to eq [anime_6] }
     end
 
     context 'anons' do
       let(:terms) { 'anons' }
-      it { is_expected.to have(2).items }
+      it { is_expected.to eq [anime_2, anime_3] }
     end
 
     context 'released' do
       let(:terms) { 'released' }
-      it { is_expected.to have(3).items }
+      it { is_expected.to eq [anime_4, anime_5, anime_6] }
     end
 
     context 'ongoing,anons' do
       let(:terms) { 'ongoing,anons' }
-      it { is_expected.to have(3).items }
+      it { is_expected.to eq [anime_1, anime_2, anime_3] }
     end
   end
 
   context 'negative' do
     context '!ongoing' do
       let(:terms) { '!ongoing' }
-      it { is_expected.to have(5).items }
+      it { is_expected.to eq [anime_2, anime_3, anime_4, anime_5, anime_6] }
     end
 
     context '!anons,!released' do
       let(:terms) { '!anons,!released' }
-      it { is_expected.to have(1).item }
+      it { is_expected.to eq [anime_1] }
     end
   end
 
   context 'both' do
     context 'ongoing' do
       let(:terms) { '!anons,ongoing' }
-      it { is_expected.to have(1).item }
+      it { is_expected.to eq [anime_1] }
     end
   end
 end
