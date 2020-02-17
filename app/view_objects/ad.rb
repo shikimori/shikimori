f8fc83d2daf7881ce2bff5b3660f026446f44df9
Andrey Sidorov <takandar@gmail.com>
2020-02-17 11:44:36 +0300

    disable ads

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -405,12 +405,12 @@ private
         Types::Ad::Type[:mt_300x250]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300], # disable after 2020-02-16
+        # Types::Ad::Type[:special_x300], # disable after 2020-02-16
         Types::Ad::Type[:yd_240x600],
         Types::Ad::Type[:mt_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300], # disable after 2020-02-16
+        # Types::Ad::Type[:special_x300], # disable after 2020-02-16
         Types::Ad::Type[:yd_300x600],
         Types::Ad::Type[:mt_300x600]
       ],
@@ -426,7 +426,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170], # disable after 2020-02-16
+        # Types::Ad::Type[:special_x1170], # disable after 2020-02-16
         Types::Ad::Type[:yd_970x250],
         Types::Ad::Type[:mt_970x250]
       ]
@@ -438,13 +438,13 @@ private
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300], # disable after 2020-02-16
+        # Types::Ad::Type[:special_x300], # disable after 2020-02-16
         # Types::Ad::Type[:mt_240x400],
         # Types::Ad::Type[:yd_240x500],
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300], # disable after 2020-02-16
+        # Types::Ad::Type[:special_x300], # disable after 2020-02-16
         # Types::Ad::Type[:mt_300x600],
         # Types::Ad::Type[:yd_300x600],
         # Types::Ad::Type[:advrtr_240x400],
@@ -461,7 +461,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170] # disable after 2020-02-16
+        # Types::Ad::Type[:special_x1170] # disable after 2020-02-16
       ]
     }
   }
