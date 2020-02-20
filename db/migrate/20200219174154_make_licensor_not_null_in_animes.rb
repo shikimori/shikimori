76b8d69771b33b3f5ea4f3fe704c407016ff13c1
Andrey Sidorov <takandar@gmail.com>
2020-02-20 11:31:57 +0300

    fix migration

--- db/migrate/20200219174154_make_licensor_not_null_in_animes.rb
+++ db/migrate/20200219174154_make_licensor_not_null_in_animes.rb
@@ -1,20 +1,20 @@
 class MakeLicensorNotNullInAnimes < ActiveRecord::Migration[5.2]
   def up
-    change_column_default :animes, :russian, from: nil, to: ''
-    change_column_default :mangas, :russian, from: nil, to: ''
+    change_column_default :animes, :licensor, from: nil, to: ''
+    change_column_default :mangas, :licensor, from: nil, to: ''
 
-    Anime.where(russian: nil).update_all russian: ''
-    Manga.where(russian: nil).update_all russian: ''
+    Anime.where(licensor: nil).update_all licensor: ''
+    Manga.where(licensor: nil).update_all licensor: ''
 
-    change_column :animes, :russian, :string, null: false
-    change_column :mangas, :russian, :string, null: false
+    change_column :animes, :licensor, :string, null: false
+    change_column :mangas, :licensor, :string, null: false
   end
 
   def down
-    change_column_default :animes, :russian, from: '', to: false
-    change_column_default :mangas, :russian, from: '', to: false
+    change_column_default :animes, :licensor, from: '', to: false
+    change_column_default :mangas, :licensor, from: '', to: false
 
-    change_column :animes, :russian, :string, null: true
-    change_column :mangas, :russian, :string, null: true
+    change_column :animes, :licensor, :string, null: true
+    change_column :mangas, :licensor, :string, null: true
   end
 end
