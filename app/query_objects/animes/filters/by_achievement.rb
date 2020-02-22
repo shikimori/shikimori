8d9330098f349eedf2f187b496eef294bc737c1b
Andrey Sidorov <takandar@gmail.com>
2020-02-22 17:12:21 +0300

    add achievement param validation

--- app/query_objects/animes/filters/by_achievement.rb
+++ app/query_objects/animes/filters/by_achievement.rb
@@ -1,9 +1,16 @@
-class Animes::Filters::ByAchievement
+class Animes::Filters::ByAchievement < Animes::Filters::FilterBase
   method_object :scope, :value
 
+  dry_type Types::Achievement::NekoId
+  field :achievement
+
   def call
+    fail_with_scope! unless anime?
+
     @scope.merge(
-      NekoRepository.instance.find(@value, 1).animes_scope.except(:order)
+      NekoRepository.instance.find(dry_type[@value], 1)
+        .animes_scope
+        .except(:order)
     )
   end
 end
--- spec/query_objects/animes/filters/by_achievement_spec.rb
+++ spec/query_objects/animes/filters/by_achievement_spec.rb
@@ -1,6 +1,7 @@
 describe Animes::Filters::ByAchievement do
-  subject { described_class.call Anime.order(:id), terms }
+  subject { described_class.call scope, terms }
 
+  let(:scope) { Anime.order :id }
   let(:terms) { 'otaku' }
 
   let(:hentai) { create :genre, id: Genre::HENTAI_IDS.first }
@@ -10,4 +11,14 @@ describe Animes::Filters::ByAchievement do
   let!(:anime_3) { create :anime }
 
   it { is_expected.to eq [anime_1, anime_2] }
+
+  context 'invalid parameter' do
+    let(:terms) { 'zzz' }
+    it { expect { subject }.to raise_error InvalidParameterError }
+  end
+
+  context 'invalid scope' do
+    let(:scope) { [Manga.all, Ranobe.all].sample }
+    it { expect { subject }.to raise_error InvalidParameterError }
+  end
 end
