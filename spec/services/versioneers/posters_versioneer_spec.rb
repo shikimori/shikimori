20c32f5b740e957e70e106580cdca8f704ce55ee
Andrey Sidorov <takandar@gmail.com>
2020-03-11 21:20:58 +0300

    Versioneers::PostersVersioneer#postmoderate specs

--- spec/services/versioneers/posters_versioneer_spec.rb
+++ spec/services/versioneers/posters_versioneer_spec.rb
@@ -1,8 +1,11 @@
 describe Versioneers::PostersVersioneer do
   let(:service) { described_class.new anime }
   let(:anime) { create :anime }
-  let(:image) { Rack::Test::UploadedFile.new 'spec/files/anime.jpg', 'image/jpg' }
-  let(:author) { build_stubbed :user }
+  let(:image) do
+    Rack::Test::UploadedFile.new 'spec/files/anime.jpg', 'image/jpg'
+  end
+  let(:author) { build_stubbed :user, role }
+  let(:role) { :user }
   let(:reason) { 'change reason' }
 
   describe '#premoderate' do
@@ -26,23 +29,43 @@ describe Versioneers::PostersVersioneer do
   end
 
   describe '#postmoderate' do
-    subject(:version) { service.postmoderate image, author, reason }
+    subject!(:version) { service.postmoderate image, author, reason }
 
-    it do
-      expect { subject }.to raise_error 'not implemented'
-      # expect(anime.image).to be_present
-      # expect(anime).to_not be_changed
-      #
-      # expect(version).to be_persisted
-      # expect(version).to be_auto_accepted
-      # expect(version).to be_instance_of Versions::PosterVersion
-      # expect(version).to have_attributes(
-      #   user: author,
-      #   reason: reason,
-      #   item_diff: { 'image' => [nil, 'anime.jpg'] },
-      #   item: anime,
-      #   moderator: author
-      # )
+    context 'can auto_accept' do
+      let(:role) { :admin }
+      it do
+        expect(anime.image).to be_present
+        expect(anime).to_not be_changed
+
+        expect(version).to be_persisted
+        expect(version).to be_auto_accepted
+        expect(version).to be_instance_of Versions::PosterVersion
+        expect(version).to have_attributes(
+          user: author,
+          reason: reason,
+          item_diff: { 'image' => [nil, 'anime.jpg'] },
+          item: anime,
+          moderator: author
+        )
+      end
+    end
+
+    context 'cannot auto_accept' do
+      it do
+        expect(anime.image).to be_present
+        expect(anime).to_not be_changed
+
+        expect(version).to be_persisted
+        expect(version).to be_pending
+        expect(version).to be_instance_of Versions::PosterVersion
+        expect(version).to have_attributes(
+          user: author,
+          reason: reason,
+          item_diff: { 'image' => [nil, 'anime.jpg'] },
+          item: anime,
+          moderator: nil
+        )
+      end
     end
   end
 end
