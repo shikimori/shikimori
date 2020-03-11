1cdbbfc62c91a256e366c1dab5d02e6186903a06
Andrey Sidorov <takandar@gmail.com>
2020-03-11 19:39:36 +0300

    change sentry account

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,6 +1,6 @@
 if defined? Raven
   Raven.configure do |config|
-    config.dsn = 'https://722c6c94e0304d5f82a7e5213244475d:2616aa4cd01c4c5180b9dd1d6b48dd6f@sentry.io/217873'
+    config.dsn = 'https://49ff8cc56e5c4d0fad6c80a87c977c49:bd503c6e7ba94fd2b1069ffb18c42d86@sentry.io/4384681'
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
     config.environments = %w[production]
     config.ssl_verification = false
