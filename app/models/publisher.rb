e22959abd338b1228047eb9b464fa9022476702d
Andrey Sidorov <takandar@gmail.com>
2020-03-24 20:48:36 +0300

    add one more Shounen Jump merged publisher

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -5,11 +5,12 @@ class Publisher < ApplicationRecord
     206 => 81,
     108 => 72,
     229 => 12,
-    397 => 12
+    397 => 12,
+    129 => 83
   }
 
   def to_param
-    "%d-%s" % [id, name.gsub(/[^\w]+/, '-').gsub(/^-|-$/, '')]
+    format('%<id>d-%<slug>s', id: id, slug: name.gsub(/[^\w]+/, '-').gsub(/^-|-$/, ''))
   end
 
   def name
@@ -17,14 +18,14 @@ class Publisher < ApplicationRecord
   end
 
   # возвращет все id, связанные с текущим
-  def self.related(id, recursive=false)
-    related = Merged.map { |k,v| k == id ? v : (v == id ? k : nil) }.compact
-    related = related.map { |v| self.related(v,true) }.flatten.uniq unless recursive
+  def self.related id, recursive = false
+    related = Merged.map { |k, v| k == id ? v : (v == id ? k : nil) }.compact
+    related = related.map { |v| self.related(v, true) }.flatten.uniq unless recursive
     related << id
   end
 
   # возвращет настоящего издателя, если это был склеенный издатель
   def real
-    Merged.keys.include?(self.id) ? self.class.find(Merged[self.id]) : self
+    Merged.key?(id) ? self.class.find(Merged[id]) : self
   end
 end
