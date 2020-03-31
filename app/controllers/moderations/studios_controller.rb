726c925a8ae0ce301bdf1fa85f98f8fd56733e62
Andrey Sidorov <takandar@gmail.com>
2020-03-31 07:37:18 +0300

    fix is_publisher & is_visible params

--- app/controllers/moderations/studios_controller.rb
+++ app/controllers/moderations/studios_controller.rb
@@ -8,7 +8,12 @@ private
       .require(:studio)
       .permit(:name, :image, :is_publisher, :is_verified)
       .tap do |allowed_params|
-        allowed_params[:is_visible] = allowed_params[:is_visible] != '0'
+        if allowed_params[:is_publisher].present?
+          allowed_params[:is_publisher] = allowed_params[:is_publisher] != '0'
+        end
+        if allowed_params[:is_verified].present?
+          allowed_params[:is_verified] = allowed_params[:is_verified] != '0'
+        end
       end
   end
 end
