96117ccc28949cbbc7a528055a4dd0b98eb00dfe
Andrey Sidorov <takandar@gmail.com>
2020-04-10 22:57:16 +0300

    adding topics form logic to articles

--- app/assets/javascripts/pages/p-articles/_form.js
+++ app/assets/javascripts/pages/p-articles/_form.js
@@ -1,32 +1,12 @@
+import { initTagsApp, initVideo, initWall, initForm } from '../p-topics/_news_form';
+
 pageLoad('articles_new', 'articles_edit', 'articles_create', 'articles_update', () => {
+  const $form = $('.b-form.edit_article, .b-form.new_article');
+
   $('.b-shiki_editor.unprocessed').shikiEditor();
 
-  if ($('#article_tags').length) {
-    initTagsApp();
-  }
+  const $wall = initWall($form);
+  const $video = initVideo('articles', $form, $wall);
+  initTagsApp('article');
+  initForm($form, $wall, $video);
 });
-
-async function initTagsApp() {
-  const { Vue } = await import(/* webpackChunkName: "vue" */ 'vue/instance');
-  const { default: TagsInput } = await import('vue/components/tags_input');
-
-  const $app = $('#vue_tags_input');
-  const $tags = $('.b-input.article_tags');
-  $tags.hide();
-
-  new Vue({
-    el: '#vue_tags_input',
-    render: h => h(TagsInput, {
-      props: {
-        label: $tags.find('label').text(),
-        hint: $tags.find('.hint').html(),
-        input: $tags.find('input')[0],
-        value: $app.data('value'),
-        autocompleteBasic: $app.data('autocomplete_basic'),
-        autocompleteOther: [],
-        tagsLimit: 3,
-        isDowncase: true
-      }
-    })
-  });
-}
