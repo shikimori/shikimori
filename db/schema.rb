80964ac5c37c56efbf4305e5940b875cc1d971f2
Andrey Sidorov <takandar@gmail.com>
2020-04-28 20:30:24 +0300

    migrate affected mangas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_180240) do
+ActiveRecord::Schema.define(version: 2020_04_28_172514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
