8c5f93ca4ec5a128cb9ff062d53bb866bc28daaa
Andrey Sidorov <takandar@gmail.com>
2020-04-30 15:08:43 +0300

    add version_names_moderator to STAFF_ROLES

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
     admin
     super_moderator
     forum_moderator
+    version_names_moderator
     version_texts_moderator
     version_moderator
     version_fansub_moderator
