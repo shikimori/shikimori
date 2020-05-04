dc278d44ddc3f5083d90c0c348b5fa233b7060b0
Andrey Sidorov <takandar@gmail.com>
2020-05-04 22:28:25 +0300

    rubocop fix

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -1,10 +1,10 @@
 module BlocksHelper
   def block_to_partial partial_name, options = {}, &block
-    options.merge! body: capture(&block)
+    options[:body] = capture(&block)
     render partial: partial_name, locals: options, formats: :html
   end
 
-  def spoiler title, options = {display: :block}, &block
+  def spoiler title, options = { display: :block }, &block
     block_to_partial 'blocks/spoiler', options.merge(title: title), &block
   end
 
