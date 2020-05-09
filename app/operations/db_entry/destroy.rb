25be070826b245745f1e4b6acce6a30458f86ed0
Andrey Sidorov <takandar@gmail.com>
2020-05-09 23:38:37 +0300

    faster DbEntry::Destroy logic

--- app/operations/db_entry/destroy.rb
+++ app/operations/db_entry/destroy.rb
@@ -2,6 +2,38 @@ class DbEntry::Destroy
   method_object :entry
 
   def call
-    @entry.destroy!
+    user_ids = related_user_ids
+
+    ActiveRecord::Base.transaction do
+      user_rates.delete_all
+      user_rate_logs.delete_all
+      user_history.delete_all
+
+      User.where(id: user_ids).update_all updated_at: Time.zone.now
+
+      @entry.destroy!
+    end
+  end
+
+private
+
+  def related_user_ids
+    (
+      user_rates.distinct.pluck(:user_id) +
+      user_rate_logs.distinct.pluck(:user_id) +
+      user_history.distinct.pluck(:user_id)
+    ).uniq
+  end
+
+  def user_rates
+    UserRate.where(target: @entry)
+  end
+
+  def user_rate_logs
+    UserRateLog.where(target: @entry)
+  end
+
+  def user_history
+    UserHistory.where(target: @entry)
   end
 end
