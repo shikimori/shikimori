1b9f909545b2b80cd2465b75c6d04c0a83ef39db
Andrey Sidorov <takandar@gmail.com>
2020-05-11 16:55:20 +0300

    news moderation controller

---
+++ app/controllers/moderations/news_controller.rb
@@ -0,0 +1,46 @@
+class Moderations::NewsController < ModerationsController
+  load_resource class: Topics::NewsTopic.name
+
+  PENDING_PER_PAGE = 15
+  PROCESSED_PER_PAGE = 25
+
+  def index
+    og page_title: i18n_t('page_title')
+
+    @moderators = User
+      .where("roles && '{#{Types::User::Roles[:news_moderator]}}'")
+      .where.not(id: User::MORR_ID)
+      .sort_by { |v| v.nickname.downcase }
+
+    @processed = QueryObjectBase.new(processed_scope).paginate(@page, PROCESSED_PER_PAGE)
+    @pending = pending_scope
+  end
+
+  def accept
+    @resource.accept if @resource.can_accept?
+    redirect_back fallback_location: moderations_news_url
+  end
+
+  def reject
+    @resource.reject if @resource.can_reject?
+    redirect_back fallback_location: moderations_news_url
+  end
+
+private
+
+  def processed_scope
+    Topics::NewsTopic
+      .where.not(forum_id: Forum::PREMODERATION_ID)
+      .where(locale: locale_from_host)
+      .includes(:user)
+      .order(created_at: :desc)
+  end
+
+  def pending_scope
+    Topics::NewsTopic
+      .where(forum_id: Forum::PREMODERATION_ID)
+      .where(locale: locale_from_host)
+      .includes(:user)
+      .order(created_at: :desc)
+  end
+end
---
+++ spec/controllers/moderations/news_controller_spec.rb
@@ -0,0 +1,31 @@
+describe Moderations::NewsController do
+  let!(:news_topic) { create :news_topic, forum_id: Forum::PREMODERATION_ID }
+
+  describe '#index', :focus do
+    include_context :authenticated
+    subject! { get :index }
+
+    it { expect(response).to have_http_status :success }
+  end
+
+  describe '#accept' do
+    include_context :authenticated, :news_moderator
+    subject! { post :accept, params: { id: news_topic.id } }
+    let(:news_topic) { create :news_topic }
+
+    it do
+      expect(resource.forum_id).to eq Forum::NEWS_ID
+      expect(response).to redirect_to moderations_news_url
+    end
+  end
+
+  describe '#reject' do
+    include_context :authenticated, :news_moderator
+    subject! { post :reject, params: { id: news_topic.id } }
+
+    it do
+      expect(resource.forum_id).to eq Forum::OFFTOPIC_ID
+      expect(response).to redirect_to moderations_news_url
+    end
+  end
+end
