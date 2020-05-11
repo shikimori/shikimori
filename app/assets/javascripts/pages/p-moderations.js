051b923c67dfc8bce47924b754445c510b99d3f1
Andrey Sidorov <takandar@gmail.com>
2020-05-11 18:39:28 +0300

    remove obsolete animeonline related moderation code

--- app/assets/javascripts/pages/p-moderations.js
+++ app/assets/javascripts/pages/p-moderations.js
@@ -43,19 +43,6 @@ pageLoad('moderations_show', () => {
     .trigger('change');
 });
 
-pageLoad('anime_video_reports_index', 'profiles_videos', () => {
-  datePicker();
-
-  $('.l-page').on('click', '.b-log_entry.video .collapsed', ({ currentTarget }) => {
-    const $player = $(currentTarget).parent().find('.player');
-    if (!$player.data('html')) { return; }
-
-    $player
-      .html($player.data('html'))
-      .data({ html: '' });
-  });
-});
-
 pageLoad('versions_index', 'users_index', datePicker);
 
 pageLoad('versions_show', 'user_rate_logs_show', () => {
@@ -67,7 +54,6 @@ pageLoad(
   'abuse_requests_index',
   'versions_index',
   'review_index',
-  'anime_video_reports_index',
   () => {
     $('.b-brief').checkHeight({ max_height: 150 });
 
@@ -78,23 +64,6 @@ pageLoad(
       $(this).remove();
     });
   });
-
-pageLoad('moderations_missing_videos', () => {
-  $('.missing-video .show-details').one('click', async e => {
-    e.preventDefault();
-
-    const { data } = await axios.get($(e.currentTarget).data('episodes_url'));
-    $(e.currentTarget).parent().find('.details').html(data);
-  });
-
-  $('.missing-video .show-details').on('click', e => {
-    e.preventDefault();
-
-    $(e.currentTarget).parent()
-      .find('.details')
-      .toggleClass('hidden');
-  });
-});
 
 pageLoad('roles_show', () => {
   new CollectionSearch('.b-collection_search');
