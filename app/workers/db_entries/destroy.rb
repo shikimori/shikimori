368310990edb71d747fe37dea8445452149109f1
Andrey Sidorov <takandar@gmail.com>
2020-05-11 19:26:48 +0300

    also no need anymore in sidekiq uniq jobs here

--- app/workers/db_entries/destroy.rb
+++ app/workers/db_entries/destroy.rb
@@ -4,11 +4,7 @@ class DbEntries::Destroy
   Type = Types::Coercible::String
     .enum(Anime.name, Manga.name, Character.name, Person.name)
 
-  sidekiq_options(
-    unique: :until_executed,
-    unique_args: ->(args) { args[0..2].join('-') },
-    queue: :dangerous_actions
-  )
+  sidekiq_options queue: :dangerous_actions
 
   def perform type, id, user_id
     NamedLogger.destroy.info "#{type}##{id} User##{user_id}"
--- app/workers/db_entries/merge_into_other.rb
+++ app/workers/db_entries/merge_into_other.rb
@@ -1,14 +1,9 @@
 class DbEntries::MergeIntoOther
   include Sidekiq::Worker
+  sidekiq_options queue: :dangerous_actions
 
   Type = Types::Coercible::String
     .enum(Anime.name, Manga.name, Character.name, Person.name)
-
-  sidekiq_options(
-    unique: :until_executed,
-    unique_args: ->(args) { args[0..2].join('-') },
-    queue: :dangerous_actions
-  )
 
   def perform type, from_id, to_id, user_id
     NamedLogger.merge_into_other.info "#{type}##{from_id} -> #{type}#{to_id} User##{user_id}"
