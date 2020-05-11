d34cea820ee03d323e76ee7e94eb2c8840671f2a
Andrey Sidorov <takandar@gmail.com>
2020-05-11 14:29:36 +0300

    add club_link uniqueness validation to avoid runtime errors

--- app/models/club_link.rb
+++ app/models/club_link.rb
@@ -1,4 +1,6 @@
 class ClubLink < ApplicationRecord
   belongs_to :club, touch: true
   belongs_to :linked, polymorphic: true, touch: true
+
+  validates :club_id, uniqueness: { scope: %i[linked_id linked_type] }
 end
--- spec/models/club_link_spec.rb
+++ spec/models/club_link_spec.rb
@@ -3,4 +3,8 @@ describe ClubLink do
     it { is_expected.to belong_to :club }
     it { is_expected.to belong_to :linked }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:club_id).scoped_to(:linked_id, :linked_type) }
+  end
 end
