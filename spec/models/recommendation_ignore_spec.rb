efb0b4d01795add4fee217daa69654d2d799316e
Andrey Sidorov <takandar@gmail.com>
2020-05-11 15:21:56 +0300

    refactor spec

--- spec/models/recommendation_ignore_spec.rb
+++ spec/models/recommendation_ignore_spec.rb
@@ -1,44 +1,47 @@
 describe RecommendationIgnore do
   describe 'relations' do
-    it { should belong_to :user }
-    it { should belong_to :target }
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :target }
   end
 
   context 'class_methods' do
     let(:anime1) { create :anime, kind: :special }
-    let(:anime2) { create :anime }
-    let(:anime3) { create :anime }
+    let(:anime_2) { create :anime }
+    let(:anime_3) { create :anime }
 
     after { Animes::BannedRelations.instance.clear_cache! }
 
     describe '.block' do
-      before do
-        create :related_anime, source_id: anime2.id, anime_id: anime3.id
-        create :related_anime, source_id: anime3.id, anime_id: anime2.id
+      let!(:related_1) { create :related_anime, source_id: anime_2.id, anime_id: anime_3.id }
+      let!(:related_2) { create :related_anime, source_id: anime_3.id, anime_id: anime_2.id }
+
+      subject { RecommendationIgnore.block anime_3, user }
+
+      it do
+        expect { subject }.to change(RecommendationIgnore, :count).by 2
+        is_expected.to eq [anime_3.id, anime_2.id]
       end
 
-      subject { RecommendationIgnore.block anime3, user }
-
-      it { should eq [anime3.id, anime2.id] }
-      it { expect { subject }.to change(RecommendationIgnore, :count).by 2 }
-
       describe 'second run' do
-        before { RecommendationIgnore.block anime3, user }
-        it { should eq [anime3.id] }
-        it { expect { subject }.to_not change(RecommendationIgnore, :count) }
+        before { RecommendationIgnore.block anime_3, user }
+
+        it do
+          expect { subject }.to_not change(RecommendationIgnore, :count)
+          is_expected.to eq [anime_3.id]
+        end
       end
 
       describe 'block of new entry' do
-        let(:anime4) { create :anime }
-        before do
-          RecommendationIgnore.block anime3, user
+        let(:anime_4) { create :anime }
+        before { RecommendationIgnore.block anime_3, user }
 
-          create :related_anime, source_id: anime2.id, anime_id: anime4.id
-          create :related_anime, source_id: anime4.id, anime_id: anime2.id
+        let!(:related_3) { create :related_anime, source_id: anime_2.id, anime_id: anime_4.id }
+        let!(:related_4) { create :related_anime, source_id: anime_4.id, anime_id: anime_2.id }
+
+        it do
+          expect { subject }.to change(RecommendationIgnore, :count).by 1
+          is_expected.to eq [anime_4.id, anime_3.id]
         end
-
-        it { should eq [anime4.id, anime3.id] }
-        it { expect { subject }.to change(RecommendationIgnore, :count).by 1 }
       end
     end
   end
