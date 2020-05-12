59e39d9c59daffa698145dc4954269fa8cd3e293
Andrey Sidorov <takandar@gmail.com>
2020-05-12 16:27:18 +0300

    transitionToCollapse

--- app/assets/javascripts/helpers/animated.js
+++ app/assets/javascripts/helpers/animated.js
@@ -1,44 +1,56 @@
-// https://css-tricks.com/using-css-transitions-auto-dimensions/
+// based on https://css-tricks.com/using-css-transitions-auto-dimensions/
 import delay from 'delay';
 
-const ANIMATED_DELAY = 350;
+const ANIMATED_DELAY = 3500;
 
 let uniqId = 0;
 const newId = () => uniqId += 1;
 const animations = {};
 
 export function animatedCollapse(element) {
-  if (animations[element]) { cleanup(element); }
+  const isTransitioning = !!animations[element];
+
+  if (isTransitioning) {
+    cleanup(element);
+  }
 
   const animationId = newId();
   animations[element] = animationId;
 
-  const sectionHeight = element.scrollHeight;
   element.classList.add('animated-collapse');
 
-  element.style.height = sectionHeight + 'px';
-
-  requestAnimationFrame(async () => {
-    if (animations[element] !== animationId) { return; }
-
-    element.style.height = '0px';
-    element.style.paddingTop = '0px';
-    element.style.paddingBottom = '0px';
-    element.style.marginTop = '0px';
-    element.style.marginBottom = '0px';
-
-    await delay(ANIMATED_DELAY);
-
-    if (animations[element] !== animationId) { return; }
-    cleanup(element);
-    element.classList.add('hidden');
-  });
+  if (isTransitioning) {
+    transitionToCollapse(element, animationId);
+  } else {
+    element.style.height = `${element.scrollHeight}px`;
+    requestAnimationFrame(() => transitionToCollapse(element, animationId));
+  }
 
   return delay(ANIMATED_DELAY);
 }
 
+async function transitionToCollapse(element, animationId) {
+  if (animations[element] !== animationId) { return; }
+
+  element.style.height = '0px';
+  element.style.paddingTop = '0px';
+  element.style.paddingBottom = '0px';
+  element.style.marginTop = '0px';
+  element.style.marginBottom = '0px';
+
+  await delay(ANIMATED_DELAY);
+
+  if (animations[element] !== animationId) { return; }
+  cleanup(element);
+  element.classList.add('hidden');
+}
+
 export function animatedExpand(element) {
-  if (animations[element]) { cleanup(element); }
+  const isTransitioning = !!animations[element];
+
+  if (isTransitioning) {
+    cleanup(element);
+  }
 
   const animationId = newId();
   animations[element] = animationId;
@@ -80,6 +92,9 @@ export function animatedExpand(element) {
 
   return delay(ANIMATED_DELAY);
 }
+
+// async function transitionExpand(element, animationId) {
+// }
 
 function cleanup(element) {
   console.log(element.style.height);
