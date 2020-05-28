0035f5aef72515ccdd4dbb04b735ab5b2784e3ba
Andrey Sidorov <takandar@gmail.com>
2020-05-28 20:04:50 +0300

    blockquote input rule

--- app/assets/javascripts/views/shiki_editor/plugins.js
+++ app/assets/javascripts/views/shiki_editor/plugins.js
@@ -4,10 +4,12 @@ import { baseKeymap } from 'prosemirror-commands';
 import { menuBar } from 'prosemirror-menu';
 import { menu } from './menu';
 import { keys } from './keys';
+import inputRules from './inputrules';
 
 export const plugins = [
   history(),
   keymap(keys),
   keymap(baseKeymap),
-  menuBar(menu)
+  menuBar(menu),
+  inputRules
 ];
