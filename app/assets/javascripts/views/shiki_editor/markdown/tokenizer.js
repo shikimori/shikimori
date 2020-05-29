5ae509f9f6f09baa22ea611be1df10a1703c86ef
Andrey Sidorov <takandar@gmail.com>
2020-05-29 16:15:33 +0300

    working on inline content parsing

--- app/assets/javascripts/views/shiki_editor/markdown/tokenizer.js
+++ app/assets/javascripts/views/shiki_editor/markdown/tokenizer.js
@@ -3,79 +3,82 @@ import { Token } from './token';
 export class Tokenizer {
   constructor(text) {
     this.text = text;
+
     this.charIndex = -1;
+    this.char1 = null;
+    this.char2 = null;
+    this.char3 = null;
 
     this.tokens = [];
+    this.inlineTokens = [];
   }
 
   parse() {
     while (this.charIndex < this.text.length - 1) {
-      this.charIndex += 1;
+      this.next(1);
       this.parseLine(this.charIndex);
     }
 
     return this.tokens.flatten();
   }
 
+  next(steps) {
+    this.charIndex += steps;
+    this.char1 = this.text[this.charIndex];
+    this.char2 = this.text[this.charIndex + 1];
+    this.char3 = this.text[this.charIndex + 2];
+  }
+
   parseLine(startIndex) {
     while (this.charIndex <= this.text.length) {
-      const char = this.text[this.charIndex];
-
+      const { char1, char2 } = this;
       const isStart = startIndex === this.charIndex;
-      const isEnd = char === '\n' || char === undefined;
-
-      const nextChar = this.text[this.charIndex + 1];
-      const nextNextChar = this.text[this.charIndex + 2];
-
-      if (isStart) {
-        if (char === '>' && nextChar === ' ') {
-          this.parseBlockQuote();
-          break;
-        }
-      }
+      const isEnd = char1 === '\n' || char1 === undefined;
 
       if (isEnd) {
-        this.paragraph(this.text.slice(startIndex, this.charIndex));
+        this.processParagraph(startIndex);
         break;
+      } else if (isStart && char1 === '>' && char2 === ' ') {
+        this.processBlockQuote();
+        break;
+      } else {
+        this.processInline();
       }
-
-      this.charIndex += 1;
     }
   }
 
-  parseBlockQuote() {
+  processInline() {
+    if (!this.inlineTokens.length) {
+      this.inlineTokens.push(new Token('text', '', '', 0));
+    }
+    const token = this.inlineTokens.last();
+
+    token.content += this.char1;
+    this.next(1);
+  }
+
+  processParagraph(startIndex) {
+    const text = this.text.slice(startIndex, this.charIndex);
+
+    this.push(this.tagOpen('paragraph', 'p'));
+    this.push(new Token('inline', '', text, 0, this.inlineTokens));
+    this.push(this.tagClose('paragraph', 'p'));
+
+    this.inlineTokens = [];
+  }
+
+  processBlockQuote() {
     this.push(this.tagOpen('blockquote', 'blockquote'));
 
-    this.charIndex += 2;
+    this.next(2);
     this.parseLine(this.charIndex);
 
-    const nextChar = this.text[this.charIndex + 1];
-    const nextNextChar = this.text[this.charIndex + 2];
-    if (nextChar === '>' && nextNextChar === ' ') {
-      this.charIndex += 3;
+    if (this.char2 === '>' && this.char3 === ' ') {
+      this.next(3);
       this.parseLine(this.charIndex);
     }
 
     this.push(this.tagClose('blockquote', 'blockquote'));
-  }
-
-  paragraph(text) {
-    const inlineTokens = this.parseInline(text);
-    const innerToken = new Token('inline', '', text, 0, inlineTokens);
-
-    this.wrap('paragraph', 'p', innerToken);
-  }
-
-  parseInline(text) {
-    return [
-      new Token('text', '', text, 0)
-    ];
-  }
-
-  wrap(type, tag, token) {
-    this.push(this.tagOpen('paragraph', 'p'));
-    this.push(token);
-    this.push(this.tagClose('paragraph', 'p'));
   }
 
   tagOpen(type, tag) {
