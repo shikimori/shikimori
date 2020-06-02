1e80f898e8d3b2f52a16d50eaa01d9100c40fec1
Andrey Sidorov <takandar@gmail.com>
2020-06-02 21:47:16 +0300

    decaffeinate mytarget_ad

---
+++ app/assets/javascripts/views/ads/mytarget_ad.js
@@ -0,0 +1,47 @@
+import { bind } from 'decko';
+import getjs from 'get-js';
+
+import { AdView } from './ad_view';
+
+let state = null;
+const pendingAds = [];
+
+const STATE = {
+  LOADED: 'loaded',
+  LOADING: 'loading'
+};
+
+export class MytargetAd extends AdView {
+  initialize(html, cssClass) {
+    this.html = html;
+    this.cssClass = cssClass;
+
+    if (state === STATE.LOADED) {
+      this._render();
+    } else if (state === STATE.LOADING) {
+      this._schedule();
+    } else if (state === null) {
+      this._loadJs();
+    }
+  }
+
+  _loadJs() {
+    state = STATE.LOADING;
+
+    getjs('//ad.mail.ru/static/ads-async.js').then(() => {
+      state = STATE.LOADED;
+      pendingAds.forEach(render => render());
+      this._render();
+    });
+  }
+
+  _schedule() {
+    pendingAds.push(this._render);
+  }
+
+  @bind
+  _render() {
+    this._replace_node();
+    (window.MRGtag = window.MRGtag || []).push({});
+  }
+}
