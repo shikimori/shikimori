6e5e18b5b07294eeba73c38dc1257926559e5c17
Andrey Sidorov <takandar@gmail.com>
2020-06-09 19:56:17 +0300

    add proxy to open_image

--- lib/open_image.rb
+++ lib/open_image.rb
@@ -1,7 +1,15 @@
 module Kernel
-  def open_image url, *args
-    io = open URI.encode(url), *args
-    def io.original_filename; base_uri.path.split('/').last; end
+  def open_image url, options = {}
+    io = OpenURI.open_uri(
+      URI.encode(url),
+      {
+        **options,
+        **Proxy.paid_proxy
+      }
+    )
+    def io.original_filename
+      base_uri.path.split('/').last
+    end
     io.original_filename.blank? ? nil : io
   end
 end
