c84a1a52e512846b50317ab531ac9c563dafc7e1
Andrey Sidorov <takandar@gmail.com>
2020-06-10 15:15:24 +0300

    fix image load

--- lib/open_image.rb
+++ lib/open_image.rb
@@ -2,7 +2,10 @@ module Kernel
   def open_image url, options = {}
     io = OpenURI.open_uri(
       URI.encode(url),
-      { **Proxy.prepaid_proxy }.merge(options)
+      (options[:proxy] ?
+        options :
+        { **Proxy.prepaid_proxy }.merge(options)
+      )
     )
     def io.original_filename
       base_uri.path.split('/').last
