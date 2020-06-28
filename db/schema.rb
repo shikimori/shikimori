b038cb1b05eb9fe8d8fd121bfde4c9e3db4bb6e7
Andrey Sidorov <takandar@gmail.com>
2020-06-28 21:03:06 +0300

    fix broken versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_094007) do
+ActiveRecord::Schema.define(version: 2020_06_28_175712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
