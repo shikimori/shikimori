20077907d3e8f1090681bad9e3975e953a51109f
Andrey Sidorov <takandar@gmail.com>
2020-07-06 21:52:44 +0300

    load shiki-file-uploader from npm package

--- app/assets/javascripts/views/file_uploader.js
+++ app/assets/javascripts/views/file_uploader.js
@@ -1,8 +1,17 @@
-import { ShikiFileUploader } from './shiki_file_uploader';
+import ShikiFileUploader from 'shiki-file-uploader';
+import flash from 'services/flash';
+
+import csrf from 'helpers/csrf';
+import uppyLocaleRu from 'vendor/uppy_locale_ru';
 
 export class FileUploader extends ShikiFileUploader {
   constructor(node) {
-    super({ node });
+    super({
+      node,
+      flash,
+      uppyLocale: window.LOCALE === 'ru' ? uppyLocaleRu : undefined,
+      xhrHeaders: () => csrf().headers
+    });
 
     this.node.classList.remove('b-ajax');
     this._scheduleUnbind();
