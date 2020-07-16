8cb8434e7a7dc9c9a2cd5821080d2b82e09292e8
Andrey Sidorov <takandar@gmail.com>
2020-07-17 02:04:51 +0300

    bundle analyzer

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,14 @@
+// run to get bundle analyzer result
+// BUNDLE_ANALYZER=true NODE_ENV=production yarn webpack --config config/webpack/production.js
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig();
+const config = environment.toWebpackConfig();
+
+if (process.env.BUNDLE_ANALYZER) {
+  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // eslint-disable-line
+  config.plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = config;
