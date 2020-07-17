7c88a375788813273a2c83283fe47cce953a308c
Andrey Sidorov <takandar@gmail.com>
2020-07-17 23:34:25 +0300

    split chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -38,8 +38,6 @@ environment.plugins.append(
   new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru/)
 );
 
-
-
 // // fix issue with SASS not working in vue
 // const getStyleRule = require('@rails/webpacker/package/utils/get_style_rule');
 //
@@ -99,62 +97,62 @@ environment.plugins.append(
 //
 //
 //
-// if (process.env.NODE_ENV !== 'test') {
-//   // https://webpack.js.org/migrate/4/#commonschunkplugin
-//   environment.splitChunks(config => (
-//     Object.assign({}, config, {
-//       optimization: {
-//         splitChunks: {
-//           cacheGroups: {
-//             vendors: {
-//               test: /[\\/]node_modules[\\/]/,
-//               chunks: 'initial',
-//               priority: -10,
-//               name: 'vendors'
-//             },
-//             vendors_async: {
-//               test: /[\\/]node_modules[\\/]/,
-//               minChunks: 1,
-//               chunks: 'async',
-//               priority: -1,
-//               name(module, chunks, cacheGroupKey) {
-//                 const moduleFileName = module.identifier().split('/').reduceRight(item => item);
-//                 const allChunksNames = chunks.map(item => item.name).join('~');
-//                 // return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
-//                 // return allChunksNames || `${cacheGroupKey}-${moduleFileName}`;
-//                 // return allChunksNames || moduleFileName;
-//                 return `${cacheGroupKey}-${allChunksNames || moduleFileName}`;
-//               }
-//             },
-//             app_sync: {
-//               chunks: 'async',
-//               priority: -5,
-//               name(module, chunks, cacheGroupKey) {
-//                 const moduleFileName = module.identifier().split('/').reduceRight(item => item);
-//                 const allChunksNames = chunks.map(item => item.name).join('~');
-//                 // return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
-//                 // return allChunksNames || `${cacheGroupKey}-${moduleFileName}`;
-//                 // return allChunksNames || moduleFileName;
-//                 return `${cacheGroupKey}-${allChunksNames || moduleFileName}`;
-//               }
-//             },
-//             vendors_styles: {
-//               name: 'vendors',
-//               test: /\.s?(?:c|a)ss$/,
-//               chunks: 'all',
-//               minChunks: 1,
-//               reuseExistingChunk: true,
-//               enforce: true,
-//               priority: 1
-//             }
-//           }
-//         },
-//         runtimeChunk: false,
-//         namedModules: true,
-//         namedChunks: true
-//       }
-//     })
-//   ));
-// }
-//
+if (process.env.NODE_ENV !== 'test') {
+  // https://webpack.js.org/migrate/4/#commonschunkplugin
+  environment.splitChunks(config => (
+    Object.assign({}, config, {
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              chunks: 'initial',
+              priority: -10,
+              name: 'vendors'
+            },
+            vendors_async: {
+              test: /[\\/]node_modules[\\/]/,
+              minChunks: 1,
+              chunks: 'async',
+              priority: -1,
+              name(module, chunks, cacheGroupKey) {
+                const moduleFileName = module.identifier().split('/').reduceRight(item => item);
+                const allChunksNames = chunks.map(item => item.name).join('~');
+                // return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+                // return allChunksNames || `${cacheGroupKey}-${moduleFileName}`;
+                // return allChunksNames || moduleFileName;
+                return `${cacheGroupKey}-${allChunksNames || moduleFileName}`;
+              }
+            },
+            app_sync: {
+              chunks: 'async',
+              priority: -5,
+              name(module, chunks, cacheGroupKey) {
+                const moduleFileName = module.identifier().split('/').reduceRight(item => item);
+                const allChunksNames = chunks.map(item => item.name).join('~');
+                // return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+                // return allChunksNames || `${cacheGroupKey}-${moduleFileName}`;
+                // return allChunksNames || moduleFileName;
+                return `${cacheGroupKey}-${allChunksNames || moduleFileName}`;
+              }
+            },
+            vendors_styles: {
+              name: 'vendors',
+              test: /\.s?(?:c|a)ss$/,
+              chunks: 'all',
+              minChunks: 1,
+              reuseExistingChunk: true,
+              enforce: true,
+              priority: 1
+            }
+          }
+        },
+        runtimeChunk: false,
+        namedModules: true,
+        namedChunks: true
+      }
+    })
+  ));
+}
+
 module.exports = environment;
