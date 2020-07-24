3a6b06174f279ad16e9444a32150ff4dcef5e747
Andrey Sidorov <takandar@gmail.com>
2020-07-24 21:45:51 +0300

    fix options-floated positioning

--- app/assets/javascripts/pages/p-tests/editor.js
+++ app/assets/javascripts/pages/p-tests/editor.js
@@ -5,8 +5,96 @@ import autosize from 'autosize';
 const IS_RAW = false && !IS_LOCAL_SHIKI_PACKAGES;
 const IS_VUE = true || !IS_LOCAL_SHIKI_PACKAGES;
 
-const TEST_DEMO_CONTENT = `
+const TEST_DEMO_CONTENT = `# Shiki BbCodes
+[anime=1] test
+[anime=1]test[/anime]
+[anime=16049]
+[anime=3456789]
+[ranobe=9115]
+[image=1124146]
+
+# Headings
+[hr]
+# Heading level 1: \`# Heading level 1\`
+## Heading level 2: \`## Heading level 2\`
+### Heading level 3: \`### Heading level 3\`
+#### Heading level 4: \`#### Heading level 4\`
+##### Heading level 5: \`##### Heading level 5\`
+
+# Basic styles
+[hr]
+B[b]old tex[/b]t
+I[i]talic tex[/i]t
+U[u]nderlined tex[/u]t
+S[s]triked tex[/s]t
+Inline c\`ode tex\`t
+Inline s||poiler tex||t    \`||spoiler content||\`
+C[color=red]olored tex[/color]t   \`[color=red]...[/color]\`
+s[size=18]ized tex[/size]t   \`[size=18]...[/size]\`
+L[url=https://github.com/shikimori/shiki-editor]ink tex[/url]t
+
+# Spoilers
+[hr]
+
+[spoiler=spoiler block with label]
+spoiler \`content\`
+[/spoiler]
+[spoiler]
+spoiler content
+[/spoiler]
+
+[hr]
+
+:) :shock:
+
+Custom DIV
+\`[div=fc-2][div=f-column][/div][div=f-column][/div][/div]\`
+
+[div=fc-2]
+[div=f-column]
+\`[div=f-column]\`
+[/div]
+[div=f-column]
+\`[div=f-column]\`
+[/div]
+[/div]
+
+[hr]
+
+[right]\`[right]...[/right]\`[/right]
+[center]\`[center]...[/center]\`[/center]
+
+[hr]
+
+\`\`\`
+code block
+\`\`\`
+\`\`\`css
+css code block
+\`\`\`
+- Bulet List
+- def
+> - \`quoted\` list
+- > list \`quoted\`
+
+> Quote
+> > nope
+> yes
+
+Image
+[img no-zoom 225x317]https://kawai.shikimori.one/system/animes/original/38481.jpg?1592053805[/img]     [img no-zoom width=200]https://kawai.shikimori.one/system/animes/original/38481.jpg?1592053805[/img]     [img]https://kawai.shikimori.one/system/animes/original/38481.jpg?1592053805[/img] [img]https://kawai.shikimori.one/system/users/x160/1.png?1591612283[/img]
+Poster
+[poster]https://www.ljmu.ac.uk/~/media/ljmu/news/starsedit.jpg[/poster]
+
+[div=b-link_button]
+\`[div=b-link_button]...[/div]\`
+[/div]
+
+div [div=b-link_button]inside line is not parsed[/div]
+
 [quote]Old style quote support[/quote]
+[quote=zxc]Old style quote with nickname[/quote]
+[quote=c1246;1945;Silentium°]Old style quote with user[/quote]
 `.trim();
 
 pageLoad('tests_editor', async () => {
