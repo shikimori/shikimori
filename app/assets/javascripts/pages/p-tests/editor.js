90a956b60ac5af4c5c7cb5e3910e5c3ecfcc2df9
Andrey Sidorov <takandar@gmail.com>
2020-07-31 17:56:45 +0300

    buffixes in shiki editor

--- app/assets/javascripts/pages/p-tests/editor.js
+++ app/assets/javascripts/pages/p-tests/editor.js
@@ -8,16 +8,9 @@ const IS_VUE = !IS_RAW || !IS_LOCAL_SHIKI_PACKAGES;
 let TEST_DEMO_CONTENT;
 
 // TEST_DEMO_CONTENT = `
-// [manga=1]
-// [manga=2]
-// [manga=3]
-// [manga=4]
-// [manga=5]
-// [manga=6]
-// [manga=7]
-// [manga=8]
-// [manga=9]
-// [manga=10]
+// [spoiler=spoiler block with label]
+// spoiler \`content\`
+// [/spoiler]
 // `.trim();
 
 // TEST_DEMO_CONTENT = `
