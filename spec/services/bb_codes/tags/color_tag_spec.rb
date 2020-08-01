ce73c319feba192321c9b188ee2300b9a15a00ce
Andrey Sidorov <takandar@gmail.com>
2020-08-01 14:08:29 +0300

    misc

--- spec/services/bb_codes/tags/color_tag_spec.rb
+++ spec/services/bb_codes/tags/color_tag_spec.rb
@@ -3,16 +3,16 @@ describe BbCodes::Tags::ColorTag do
 
   context 'name' do
     let(:text) { '[color=red]test[/color]' }
-    it { should eq '<span style="color: red;">test</span>' }
+    it { is_expected.to eq '<span style="color: red;">test</span>' }
   end
 
   context 'code' do
     let(:text) { '[color=#00ff00]test[/color]' }
-    it { should eq '<span style="color: #00ff00;">test</span>' }
+    it { is_expected.to eq '<span style="color: #00ff00;">test</span>' }
   end
 
   context 'xss' do
     let(:text) { '[color=#00ff00</span><script>]test[/color]' }
-    it { should eq '[color=#00ff00</span><script>]test[/color]' }
+    it { is_expected.to eq '[color=#00ff00</span><script>]test[/color]' }
   end
 end
