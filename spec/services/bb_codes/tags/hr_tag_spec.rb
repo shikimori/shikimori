4f7e54373f0ec9cd3e3f6174861030ae123ec2df
Andrey Sidorov <takandar@gmail.com>
2020-08-06 00:56:08 +0300

    support markdown hr (#1942)

--- spec/services/bb_codes/tags/hr_tag_spec.rb
+++ spec/services/bb_codes/tags/hr_tag_spec.rb
@@ -1,5 +1,28 @@
 describe BbCodes::Tags::HrTag do
   subject { described_class.instance.format text }
-  let(:text) { "[hr][hr]\n" }
-  it { is_expected.to eq '<hr><hr>' }
+
+  context 'sample' do
+    let(:text) { '[hr]' }
+    it { is_expected.to eq '<hr>' }
+  end
+
+  context 'sample' do
+    let(:text) { "[hr][hr]\n" }
+    it { is_expected.to eq '<hr><hr>' }
+  end
+
+  context 'sample' do
+    let(:text) { ['---', '___', '***'].sample + "\n" }
+    it { is_expected.to eq '<hr>' }
+  end
+
+  context 'sample' do
+    let(:text) { " ---\n" }
+    it { is_expected.to eq text }
+  end
+
+  context 'sample' do
+    let(:text) { "---z\n" }
+    it { is_expected.to eq text }
+  end
 end
