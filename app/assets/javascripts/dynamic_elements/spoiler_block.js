e800f888ae9f5f6e62c0197d09c8f2ea5a0c1938
Andrey Sidorov <takandar@gmail.com>
2020-08-17 20:12:34 +0300

    animated block spoiler expand/collapse

--- app/assets/javascripts/dynamic_elements/spoiler_block.js
+++ app/assets/javascripts/dynamic_elements/spoiler_block.js
@@ -1,19 +1,29 @@
 import View from 'views/application/view';
 import { bind } from 'shiki-decorators';
+import { animatedCollapse, animatedExpand } from 'helpers/animated';
 
 export default class SpoilerBlock extends View {
   initialize() {
     this.button = this.node.children[0];
+    this.content = this.node.children[1];
+
     this.button.addEventListener('click', this._toggle);
   }
 
   @bind
-  _toggle({ x, y }) {
-    this.node.classList.toggle('is-opened');
+  async _toggle({ x, y }) {
+    // remove :focus on mouse click (event has x=0,y=0 when pressed space on focused node)
+    if (x || y) { this.button.blur(); }
 
-    // is is really mouse click
-    if (x || y) {
-      this.button.blur();
+    const wasOpened = this.node.classList.contains('is-opened');
+
+    if (wasOpened) {
+      await animatedCollapse(this.content);
+      requestAnimationFrame(() => this.node.classList.remove('is-opened'));
+    } else {
+      this.node.classList.add('is-opened');
+      animatedExpand(this.content);
     }
+
   }
 }
