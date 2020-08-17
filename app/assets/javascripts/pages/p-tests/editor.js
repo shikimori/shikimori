e9a0564e674aa6e4109e3cd8486dcbf787120104
Andrey Sidorov <takandar@gmail.com>
2020-08-17 23:48:20 +0300

    testing editor

--- app/assets/javascripts/pages/p-tests/editor.js
+++ app/assets/javascripts/pages/p-tests/editor.js
@@ -34,6 +34,116 @@ TEST_DEMO_CONTENT = `
 [spoiler=Спойлер]1\n2\n3[/spoiler]
 `.trim()
 
+TEST_DEMO_CONTENT = `
+[b]Результаты прошлых опросов:[/b]
+[spoiler=[b]01[/b]][b]Результаты опроса #1 - "Любимая аниме-тян":[/b]
+1 место -  [character=7373] и [Asuna Yuuki] (21 голос)
+2 место - [Kurisu Makise] (19 голосов)
+3 место - [Hitagi Senjougahara] (17 голосов)
+4 место - [Mikasa Ackerman] (14 голосов)
+5 место - [Saeko Busujima] и [Yuno Gasai] (11 голосов)
+6 место - [Misaki Ayuzawa], [character=738], [Mashiro Shiina] и [Shiki Ryougi] (8 голосов)
+7 место - [Aika Fuwa], [character=4158], [Rias Gremory], [Urumi Kanzaki], [C.C.]  (6 голосов)
+8 место - [character=674], [character=64595], [Sophie Hatter], [Yoruichi Shihouin], [Kurousagi], [Mei Misaki], [Mikoto Misaka] (5 голосов)
+9 место - [character=17364], [Morgiana], [character=723], [Rikka Takanashi], [Saber], [Sena Kashiwazaki], [Tsukiko Tsutsukakushi], [Victorique de Blois], [Arcueid Brunestud], [Asuka Langley Souryuu], [Chihaya Ayase], [Emi Yusa] и [Erza Scarlet] (4 голоса)
+10 место - [Himeko Inaba], [character=272], [character=87], [Kuroyukihime], [Koko Hekmatyar], [Konata Izumi], [Lucy Heartfilia], [Masami Iwasawa], [Momiji Binboda], [Nana Osaki], [Ruri Gokou], [Taiga Aisaka], [Yuuko Ichihara] и [Yuuko Kanoe] (3 голоса)
+[/spoiler]
+, [spoiler=[b]02[/b]][b]
+Результаты опроса #2 - "Любимый аниме-кун":[/b]
+1 место - [Kazuto Kirigaya] и [Lelouch Lamperouge] (17 голосов)
+2 место - [Koyomi Araragi] и [Rintarou Okabe] (14 голосов)
+3 место - [Gintoki Sakata] и [Hei] (13 голосов)
+4 место - [Eikichi Onizuka] (11 голосов)
+5 место - [Kamina] (9 голосов)
+6 место - [Hachiman Hikigaya] и [Houtarou Oreki] (8 голосов)
+7 место - [Izaya Orihara], [L Lawliet] и [Shizuo Heiwajima] (7 голосов)
+8 место - [Accelerator], [Edward Elric], [Howl], [Keima Katsuragi], [Kyon], [Mugen], [Natsu Dragneel], [Rin Okumura], [Shougo Makishima], [Takashi Natsume], [Takumi Usui] и [Toua Tokuchi] (6 голосов)
+9 место -  [Eren Yeager], [Guts], [Izayoi Sakamaki], [Killua Zoldyck], [Kimihiro Watanuki], [Kraft Lawrence], [Tetsuya Kuroko], [character=18344], [Vash the Stampede] и [Yuzuru Otonashi] (5 голосов)
+10 место - [character=31], [Light Yagami], [Sebastian Michaelis], [Sougo Okita], [Spike Spiegel] и [Toushirou Hijikata] (4 голоса)
+11 место - [Akatsuki Ousawa], [Death the Kid], [character=391], [Kyouya Hibari], [character=45627], [Luffy Monkey D.], [Ryouta Sakamoto], [Taiga Kagami] и [Tomoya Okazaki] (3 голоса)
+[/spoiler], [spoiler=[b]03[/b]]
+[b]Результаты опроса #3 - "Лучший антагонист":[/b]
+1 место - [Shougo Makishima] (15 голосов)
+2 место - [Izaya Orihara] (12 голосов)
+3 место - [character=96223]Beatrice Ushiromiya[/character] (9 голосов)
+4 место - [Kurumi Tokisaki] (8 голосов) 
+5 место - [Accelerator] (7 голосов)
+6 место - [Akihiko Kayaba], [character=2514] и [Sousuke Aizen] (6 голосов)
+7 место - [character=53641], [King Bradley], [Makoto Shishio], [character=18210] и [Tyki Mikk] (5 голосов)
+8 место - [Shinsuke Takasugi], [Kirei Kotomine], [Ulquiorra Cifer], [Byakuran], [character=19714] и [Suigintou] (4 голоса)
+9 место - [Griffith], [Dio Brando], [Medusa Gorgon], [character=351], [character=12732], [Johan Liebert], [Sai], [Hao Asakura], [character=31] и [Riko Mine] (3 голоса)
+[/spoiler], [spoiler=[b]04[/b]]
+[b]Результаты опроса #4 - "Лучший питомец":[/b]
+1 место - [character=13784]Madara[/character] (25 голосов)
+2 место - [character=43187]Kuro[/character] (15 голосов)
+3 место - [character=5188]Happy[/character] (13 голосов)
+4 место - [character=2165]Mao[/character] и [Sadaharu] (10 голосов)
+5 место - [Pikachu] (8 голосов)
+6 место - [character=64595]Neko[/character] (6 голосов)
+7 место - [Mokona Modoki] (5 голосов)
+8 место - [character=6887]Mokona Modoki[/character], [Kyuubey] и [character=3133]Jiji[/character] (4 голоса)
+9 место - [Chopper Tony Tony], [Dera Mochimazzi], [Rinon], [Cerberos] и [Saito Hiraga] (3 голоса)
+[/spoiler], [spoiler=[b]05[/b]]
+[b]Результаты опроса #5 - "Лишний персонаж":[/b]
+1 место - [Chiho Sasaki], [Hinata Shintani] и [Nate River] (5 голосов)
+2 место - [Kyouko Sasagawa] и [Haru Miura] (4 голоса)
+3 место - [Suguha Kirigaya], [Ranko Saouji], [Momoe Okonogi], [Haruka Nanami] и [Yukiteru Amano] (3 голоса) 
+[/spoiler], [spoiler=[b]06[/b]]
+[b]Результаты опроса #6 - "Лучший опенинг":[/b]
+1. [Mirai Nikki] OP1
+2. [Deadman Wonderland]
+3. [Death Note] OP1
+4. [Shingeki no Kyojin] OP1
+5. [Steins;Gate] 
+6. [Higurashi no Naku Koro ni] 
+7. [Angel Beats!] 
+8. [Ergo Proxy]
+9. [Samurai Champloo] 
+10. [Soul Eater] OP1
+11. [Ao no Exorcist] OP1
+12. [Baccano!]
+13. [Bakemonogatari] OP1
+14. [Cowboy Bebop]	
+15. [Durarara!!] OP1
+16. [Elfen Lied] 
+17. [Guilty Crown] OP1
+18. [Sayonara Zetsubou Sensei]
+19. [Toradora!] OP2
+[/spoiler], [spoiler=[b]07[/b]]
+[b]Результаты опроса #7 - "Лучшая яндере/янгире":[/b]
+1 место - [Yuno Gasai] (25 голосов)
+2 место - [character=738]Lucy[/character] (14 голосов)
+3 место - [Rena Ryuuguu] (11 голосов)
+4 место - [Shion Sonozaki] (9 голосов)
+5 место - [character=8631]Shiro[/character] (7 голосов)
+6 место - [Ayase Aragaki], [Kotonoha Katsura] и [Misa Amane] (5 голосов)
+7 место - [Minatsuki Takami] и [Megumi Shimizu]  (4 голоса)
+8 место - [Ryouko Asakura], [Tsubasa Hanekawa], [Shouko Kirishima], [Kurumi Tokisaki] и [Miya Satsuki] (3 голоса)
+[/spoiler], [spoiler=[b]08[/b]]
+[b]Результаты опроса #8 - "Лучший эндинг":[/b]
+1. [Free!]
+2. [Ookami to Koushinryou]
+3. [Kill Me Baby]
+4. [Bakemonogatari]
+5. [FLCL]
+6. [Steins;Gate]
+7. [Angel Beats!]
+8. [anime=9756]Mahou Shoujo Madoka★Magica[/anime]
+9. [Sayonara Zetsubou Sensei]
+10. [K-On!!]
+11. [Cowboy Bebop]
+12. [Devil Survivor 2 The Animation] 
+13. [Cuticle Tantei Inaba]
+14. [Gosick]
+15. [Joshiraku]
+16. [K-On!]
+17. [Monster]
+18. [Psycho-Pass]
+19. [Shinsekai yori]
+20. [Umineko no Naku Koro ni]
+[/spoiler]
+`.trim()
+
 pageLoad('tests_editor', async () => {
   const $shikiEditor = $('.b-shiki_editor').shikiEditor();
   const $textarea = $shikiEditor.find('textarea');
