0552b2cdb85177221994fb6fdc810a6994e02c51
Andrey Sidorov <takandar@gmail.com>
2020-08-20 11:41:39 +0300

    forbid mailru

--- app/mailers/shiki_mailer.rb
+++ app/mailers/shiki_mailer.rb
@@ -25,6 +25,7 @@ class ShikiMailer < ActionMailer::Base
     return unless message
     return if message.read?
     return if generated? message.to.email
+    return if mailru? user.emial
 
     subject = i18n_t(
       'private_message_email.subject',
@@ -49,6 +50,7 @@ class ShikiMailer < ActionMailer::Base
 
   def reset_password_instructions user, token, options
     return if generated? user.email
+    return if mailru? user.emial
 
     subject = i18n_t(
       'reset_password_instructions.subject',
@@ -86,6 +88,10 @@ private
   end
 
   def generated? email
-    !!(email.blank? || email =~ /^generated_/)
+    !!(email.blank? || email.match?(/^generated_/))
+  end
+
+  def mailru? email
+    !!(email.blank? || email.match?(/@(mail|inbox|list|bk)\.ru$/))
   end
 end
