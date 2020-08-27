52a262fbdc98762486589cd08071fdd45bba29c6
Andrey Sidorov <takandar@gmail.com>
2020-08-27 19:55:22 +0300

    udpate inline spoiler logic

--- app/assets/javascripts/dynamic_elements/spoiler_inline.js
+++ app/assets/javascripts/dynamic_elements/spoiler_inline.js
@@ -4,16 +4,42 @@ import { bind } from 'shiki-decorators';
 export default class SpoilerInline extends View {
   initialize() {
     this.node.addEventListener('click', this._toggle);
+    this.node.addEventListener('keypress', this._keypress);
+    this.node.addEventListener('keydown', this._keydown);
   }
 
   @bind
   async _toggle(e) {
     // prevent form submition
     e.preventDefault();
+    e.stopImmediatePropagation();
 
     // remove :focus on mouse click (event has x=0,y=0 when pressed space on focused node)
     if (e.x || e.y) { this.node.blur(); }
 
     this.node.classList.toggle('is-opened');
+  }
+
+  @bind
+  _keypress(e) {
+    switch (e.keyCode) {
+      case 32: // space
+      case 13: //enter
+        this._toggle(e);
+        return;
+    }
+  }
+
+  @bind
+  _keydown(e) {
+    // esc
+    switch (e.keyCode) {
+      case 27: // esc
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        this.node.blur();
+        return;
+    }
   }
 }
