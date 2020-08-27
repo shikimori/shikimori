97bc460216145386eadf15154fbfd599620a3f53
Andrey Sidorov <takandar@gmail.com>
2020-08-27 18:15:12 +0300

    prevent generation of wall with too many images

--- app/services/bb_codes/tags/wall_tag.rb
+++ app/services/bb_codes/tags/wall_tag.rb
@@ -3,11 +3,28 @@ class BbCodes::Tags::WallTag
 
   REGEXP = %r{
     \[wall\]
-      (.*?)
+      (?<content>.*?)
     \[/wall\]
   }mix
+  IMAGES_COUNTER_REGEXP = %r{
+    \[poster= |
+    \[image= |
+    \[wall_image= |
+    \[/img\]
+  }x
+
+  MAXIMUM_IMAGES = 12
 
   def format text
-    text.gsub(REGEXP, '<div class="b-shiki_wall to-process" data-dynamic="wall">\1</div>')
+    text.gsub(REGEXP) do |match|
+      content = $LAST_MATCH_INFO[:content]
+
+      if content.scan(IMAGES_COUNTER_REGEXP).size > MAXIMUM_IMAGES
+        match
+      else
+        "<div class='b-shiki_wall to-process' data-dynamic='wall'>" \
+          "#{content}</div>"
+      end
+    end
   end
 end
--- spec/services/bb_codes/tags/wall_tag_spec.rb
+++ spec/services/bb_codes/tags/wall_tag_spec.rb
@@ -1,5 +1,35 @@
 describe BbCodes::Tags::WallTag do
   subject { described_class.instance.format text }
   let(:text) { '[wall]test[/wall]' }
-  it { is_expected.to eq '<div class="b-shiki_wall to-process" data-dynamic="wall">test</div>' }
+  it do
+    is_expected.to eq(
+      "<div class='b-shiki_wall to-process' data-dynamic='wall'>test</div>"
+    )
+  end
+
+  describe 'images count restriction' do
+    let(:text) { "[wall]#{content}[/wall]" }
+    let(:content) do
+      [
+        '[image=fsd',
+        '[wall_image=fsd',
+        '[poster=fsd',
+        '[/img]'
+      ].sample * count
+    end
+
+    context 'allowed' do
+      let(:count) { described_class::MAXIMUM_IMAGES }
+      it do
+        is_expected.to eq(
+          "<div class='b-shiki_wall to-process' data-dynamic='wall'>#{content}</div>"
+        )
+      end
+    end
+
+    context 'not allowed', :focus do
+      let(:count) { described_class::MAXIMUM_IMAGES + 1 }
+      it { is_expected.to eq text }
+    end
+  end
 end
