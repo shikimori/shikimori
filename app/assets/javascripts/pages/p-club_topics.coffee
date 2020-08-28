ce356b24e0c12415fe6aec29f19a526b6ab9bbad
Andrey Sidorov <takandar@gmail.com>
2020-08-28 21:31:25 +0300

    decaffeintate p-club_topics

--- app/assets/javascripts/pages/p-club_topics.coffee
+++
@@ -1,3 +0,0 @@
-pageLoad 'club_topics_new', 'club_topics_edit', 'club_topics_create', 'club_topics_update', ->
-  $form = $ '.b-form.edit_topic, .b-form.new_topic'
-  $('.b-shiki_editor', $form).shikiEditor()
---
+++ app/assets/javascripts/pages/p-club_topics.js
@@ -0,0 +1,9 @@
+pageLoad(
+  'club_topics_new',
+  'club_topics_edit',
+  'club_topics_create',
+  'club_topics_update',
+  () => {
+    const $form = $('.b-form.edit_topic, .b-form.new_topic');
+    $('.b-shiki_editor', $form).shikiEditor();
+  });
