c2a8a7dd9ba4111e8a1047daaf056eb0f04f86ca
Andrey Sidorov <takandar@gmail.com>
2020-08-29 22:00:57 +0300

    decaffeinate rateable

---
+++ app/assets/javascripts/jquery.plugins/jquery.rateable.js
@@ -0,0 +1,60 @@
+$.fn.extend({
+  rateable() {
+    return this.each(function() {
+      const $stars = $('.stars-container', this);
+      const $hover = $('.hover', this);
+      const $hoverableTrigger = $('.hoverable-trigger', this);
+
+      const $score = $('.score', this);
+      const $scoreValue = $('.score-value', this);
+      const $scoreNotice = $('.score-notice', this);
+
+      const notices = $(this).data('notices');
+      const inputSelector = $(this).data('input_selector');
+      const isWithInput = !!inputSelector;
+
+      let scoreInitial = parseInt($scoreValue.text()) || 0;
+      let scoreNew = null;
+
+      $hoverableTrigger.on('mousemove', e => {
+        const offset = $(e.target).offset().left;
+        const scoreRaw = ((e.clientX - offset) * 10.0) / $stars.width();
+        scoreNew = scoreRaw > 0.5 ?
+          [Math.floor(scoreRaw) + 1, 10].min() :
+          0;
+
+        $scoreNotice.html(notices[scoreNew] || '&nbsp;');
+        $hover.attr({ class: `${withoutScore($hover)} score-${scoreNew}` });
+        return $scoreValue
+          .html(scoreNew)
+          .attr({ class: `${withoutScore($scoreValue)} score-${scoreNew}` });
+      });
+
+      $hoverableTrigger.on('mouseover', e => $score.addClass('hovered'));
+
+      $hoverableTrigger.on('mouseout', e => {
+        $score.removeClass('hovered');
+        $scoreNotice.html(notices[scoreInitial] || '&nbsp;');
+        $hover.attr({ class: withoutScore($hover) });
+        $score.attr({ class: `${withoutScore($score)} score-${scoreInitial}` });
+        return $scoreValue
+          .attr({ class: `${withoutScore($scoreValue)} score-${scoreInitial}` })
+          .html(scoreInitial);
+      });
+
+      return $hoverableTrigger.on('click', function(e) {
+        if (isWithInput) {
+          scoreInitial = scoreNew;
+          $(this).trigger('mouseout');
+          $(this).closest('form').find(inputSelector).val(scoreNew);
+        }
+
+        return $(this).trigger('rate:change', scoreNew);
+      });
+    });
+  }
+});
+
+function withoutScore($node) {
+  return $node.attr('class').replace(/\s?score-\d+/, '');
+}
