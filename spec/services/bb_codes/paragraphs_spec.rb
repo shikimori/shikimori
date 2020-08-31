15eb4a30a0e2ca2c01e85cade0b6b14142df5c5a
Andrey Sidorov <takandar@gmail.com>
2020-08-31 15:48:35 +0300

    improving paragraphs logic

--- spec/services/bb_codes/paragraphs_spec.rb
+++ spec/services/bb_codes/paragraphs_spec.rb
@@ -2,20 +2,25 @@ describe BbCodes::Paragraphs do
   subject { described_class.call text }
   let(:long_line) { 'x' * described_class::LINE_SIZE }
 
-  describe '\n' do
+  describe 'sample' do
+    let(:text) { long_line }
+    it { is_expected.to eq "[p]#{long_line}[/p]" }
+  end
+
+  describe 'sample' do
     let(:text) { "#{long_line}1\n#{long_line}2\n333" }
     it { is_expected.to eq "[p]#{long_line}1[/p][p]#{long_line}2[/p]333" }
   end
 
-  describe '<br>' do
-    let(:text) { "#{long_line}1<br>#{long_line}2<br />333" }
-    it { is_expected.to eq "[p]#{long_line}1[/p][p]#{long_line}2[/p]333" }
-  end
+  # describe '<br>' do
+  #   let(:text) { "#{long_line}1<br>#{long_line}2<br />333" }
+  #   it { is_expected.to eq "[p]#{long_line}1[/p][p]#{long_line}2[/p]333" }
+  # end
 
-  describe '&lt;br&gt;' do
-    let(:text) { "#{long_line}1&lt;br&gt;#{long_line}2&lt;br/&gt;333" }
-    it { is_expected.to eq "[p]#{long_line}1[/p][p]#{long_line}2[/p]333" }
-  end
+  # describe '&lt;br&gt;' do
+  #   let(:text) { "#{long_line}1&lt;br&gt;#{long_line}2&lt;br/&gt;333" }
+  #   it { is_expected.to eq "[p]#{long_line}1[/p][p]#{long_line}2[/p]333" }
+  # end
 
   describe '[*]' do
     let(:text) { "[list]\n [*]z\r\n[/list]" }
@@ -30,6 +35,12 @@ describe BbCodes::Paragraphs do
       let(:text) { "test\n\n[*]z" }
       it { is_expected.to eq "test\n\n[*]z" }
     end
+  end
+
+  context 'markdown list or quote' do
+    let(:prefix) { %w[- + * >].sample }
+    let(:text) { " #{prefix} #{long_line}" }
+    it { is_expected.to eq text }
   end
 
   describe '[quote]' do
