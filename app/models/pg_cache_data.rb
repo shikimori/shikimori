07ebead6f0a18d5ea5cf814726b1e3273724d6fb
Andrey Sidorov <takandar@gmail.com>
2020-09-06 08:48:12 +0300

    misc rubocop changes

--- app/models/pg_cache_data.rb
+++ app/models/pg_cache_data.rb
@@ -1,3 +1,19 @@
 class PgCacheData < ApplicationRecord
   validates :key, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.fetch_raw_data sql, batch_size, &block
+    offset = 0
+
+    begin
+      batch = ActiveRecord::Base.connection.select_all <<-SQL
+        #{sql}
+        LIMIT #{batch_size}
+        OFFSET #{offset}
+      SQL
+
+      batch.each(&block)
+
+      offset += batch_size
+    end until batch.empty? # rubocop:disable Lint/Loop
+  end
 end
