7fb34dad88d80abe4835a52d994ba7b0d2e1247f
Andrey Sidorov <takandar@gmail.com>
2020-09-07 20:19:37 +0300

    fix dayjs dates parsing in constructor

--- app/assets/javascripts/helpers/dayjs.js
+++ app/assets/javascripts/helpers/dayjs.js
@@ -2,10 +2,12 @@ import dayjs from 'dayjs';
 
 import 'dayjs/locale/ru';
 
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import localeData from 'dayjs/plugin/localeData';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+dayjs.extend(customParseFormat);
 dayjs.extend(localeData);
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
