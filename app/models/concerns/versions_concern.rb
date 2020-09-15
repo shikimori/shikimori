db78c40c4bf5d5a87029bd41fd373081edbb5184
Andrey Sidorov <takandar@gmail.com>
2020-09-16 01:52:14 +0300

    use class.base_class in verisons

--- app/models/concerns/versions_concern.rb
+++ app/models/concerns/versions_concern.rb
@@ -3,7 +3,7 @@ module VersionsConcern
 
   included do
     has_many :versions,
-      ->(entry) { where item_type: entry.class.name },
+      ->(entry) { where item_type: entry.class.base_class.name },
       foreign_key: :item_id,
       dependent: :destroy
   end
