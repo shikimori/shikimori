47ca4644d5bad0e87496e67416805e8ebdccc4f4
Andrey Sidorov <takandar@gmail.com>
2020-09-19 17:46:37 +0300

    add quote to BbCodes::BLOCK_TAG_EDGE_PREFIX_REGEXP

--- app/services/bb_codes.rb
+++ app/services/bb_codes.rb
@@ -3,6 +3,7 @@ module BbCodes
   BLOCK_TAG_EDGE_PREFIX_REGEXP = %r{
     (?:
       (?: </?div[^>]*+> | \[/?div[^\]]*+\] ) \n? |
+      (?: \[/?quote[^\]]*+\] ) \n? |
       </ul> \n? |
       <li> \n? |
       (?: </?p> | \[/?p\] ) \n? |
