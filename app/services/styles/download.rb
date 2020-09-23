665546eaa06a1676bb3e1dbd671b823924ec4cd6
Andrey Sidorov <takandar@gmail.com>
2020-09-23 19:45:50 +0300

    Styles::Download only downloads now

--- app/services/styles/download.rb
+++ app/services/styles/download.rb
@@ -1,28 +1,22 @@
 class Styles::Download
   method_object :url
 
-  CACHE_KEY = 'style_%<url>s'
   EXPIRES_IN = 8.hours
+  CACHE_VERSION = :v1
 
   ALLOWED_EXCEPTIONS = Network::FaradayGet::NET_ERRORS
 
   def call
-    "/* #{@url} */\n" + sanitize(cached_download)
-  end
-
-private
-
-  def sanitize css
-    Misc::SanitizeEvilCss.call css
-  end
-
-  def cached_download
-    Rails.cache.fetch format(CACHE_KEY, url: @url), expires_in: EXPIRES_IN do
+    Rails.cache.fetch [:style, @url, CACHE_VERSION], url: @url, expires_in: EXPIRES_IN do
       Retryable.retryable tries: 2, on: ALLOWED_EXCEPTIONS, sleep: 1 do
         download
       end
     end
+  rescue StandardError
+    ''
   end
+
+private
 
   def download
     NamedLogger.download_style.info "#{@url} start"
--- spec/services/styles/download_spec.rb
+++ spec/services/styles/download_spec.rb
@@ -2,14 +2,10 @@ describe Styles::Download, :vcr do
   subject { described_class.call url }
   let(:url) { 'https://thiaya.github.io/1/shi.Modern.css' }
 
-  it do
-    is_expected.to eq(
-      "/* https://thiaya.github.io/1/shi.Modern.css */\nx"
-    )
-  end
+  it { is_expected.to eq ' x eval ' }
 
   context 'bad content' do
     let(:url) { 'https://i.imgur.com/ywBxdCN.png' }
-    it { is_expected.to eq "/* #{url} */\n" }
+    it { is_expected.to eq '' }
   end
 end
