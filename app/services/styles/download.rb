6bebe34edba3c760e31a1730e1e0c77f4c13074f
Andrey Sidorov <takandar@gmail.com>
2020-09-24 08:27:12 +0300

    fix styles cache key

--- app/services/styles/download.rb
+++ app/services/styles/download.rb
@@ -2,12 +2,14 @@ class Styles::Download
   method_object :url
 
   EXPIRES_IN = 8.hours
+
   CACHE_VERSION = :v1
+  CACHE_KEY = "style_%<url>s_#{CACHE_VERSION}"
 
   ALLOWED_EXCEPTIONS = Network::FaradayGet::NET_ERRORS
 
   def call
-    Rails.cache.fetch [:style, @url, CACHE_VERSION], url: @url, expires_in: EXPIRES_IN do
+    Rails.cache.fetch format(CACHE_KEY, url: @url), url: @url, expires_in: EXPIRES_IN do
       Retryable.retryable tries: 2, on: ALLOWED_EXCEPTIONS, sleep: 1 do
         download
       end
