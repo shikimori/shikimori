1ed99c793b5a5c29ecb77fb8492787208378583f
Andrey Sidorov <takandar@gmail.com>
2020-10-08 19:06:23 +0300

    replace quote double quotes to single quotes

--- app/services/bb_codes/tags/quote_tag.rb
+++ app/services/bb_codes/tags/quote_tag.rb
@@ -4,25 +4,31 @@ class BbCodes::Tags::QuoteTag
 
   COMMENT_QUOTE_START_REGEXP = /
     \[quote=
-      c?(?<comment_id>\d+);
-      \d+;
-      (?<nickname>[^\]]+)
+      (?<attrs>
+        c?(?<comment_id>\d+);
+        \d+;
+        (?<nickname>[^\]]+)
+      )
     \] \n?
   /mix
 
   MESSAGE_QUOTE_START_REGEXP = /
     \[quote=
-      m(?<message_id>\d+);
-      \d+;
-      (?<nickname>[^\]]+)
+      (?<attrs>
+        m(?<message_id>\d+);
+        \d+;
+        (?<nickname>[^\]]+)
+      )
     \] \n?
   /mix
 
   TOPIC_QUOTE_START_REGEXP = /
     \[quote=
-      t(?<topic_id>\d+);
-      \d+;
-      (?<nickname>[^\]]+)
+      (?<attrs>
+        t(?<topic_id>\d+);
+        \d+;
+        (?<nickname>[^\]]+)
+      )
     \] \n?
   /mix
 
@@ -31,7 +37,7 @@ class BbCodes::Tags::QuoteTag
   /mix
 
   SIMPLE_QUOTE_2_START_REGEXP = /
-    \[quote=(?<nickname>[^\]]+)\] \n?
+    \[quote=(?<attrs>(?<nickname>[^\]]+))\] \n?
   /mix
 
   QUOTE_END_REGEXP = %r{
@@ -58,10 +64,10 @@ private
     result = text.gsub COMMENT_QUOTE_START_REGEXP do
       replacements += 1
 
-      '<div class="b-quote"><div class="quoteable">'\
+      "<div class='b-quote'><div class='quoteable'>"\
         "[comment=#{$LAST_MATCH_INFO[:comment_id]} quote]"\
         "#{$LAST_MATCH_INFO[:nickname]}[/comment]"\
-        '</div><div class="quote-content">'
+        "</div><div class='quote-content'>"
     end
 
     [result, replacements, original_text]
@@ -70,10 +76,10 @@ private
   def message_quote text, replacements, original_text
     result = text.gsub MESSAGE_QUOTE_START_REGEXP do
       replacements += 1
-      '<div class="b-quote"><div class="quoteable">'\
+      "<div class='b-quote'><div class='quoteable'>"\
         "[message=#{$LAST_MATCH_INFO[:message_id]} quote]"\
         "#{$LAST_MATCH_INFO[:nickname]}[/message]"\
-        '</div><div class="quote-content">'
+        "</div><div class='quote-content'>"
     end
 
     [result, replacements, original_text]
@@ -82,10 +88,10 @@ private
   def topic_quote text, replacements, original_text
     result = text.gsub TOPIC_QUOTE_START_REGEXP do
       replacements += 1
-      '<div class="b-quote"><div class="quoteable">'\
+      "<div class='b-quote'><div class='quoteable'>"\
         "[topic=#{$LAST_MATCH_INFO[:topic_id]} quote]"\
         "#{$LAST_MATCH_INFO[:nickname]}[/topic]"\
-        '</div><div class="quote-content">'
+        "</div><div class='quote-content'>"
     end
 
     [result, replacements, original_text]
@@ -94,9 +100,9 @@ private
   def simple_quote_2 text, replacements, original_text
     result = text.gsub SIMPLE_QUOTE_2_START_REGEXP do
       replacements += 1
-      '<div class="b-quote"><div class="quoteable">'\
+      "<div class='b-quote'><div class='quoteable'>"\
         "[user]#{$LAST_MATCH_INFO[:nickname]}[/user]"\
-        '</div><div class="quote-content">'
+        "</div><div class='quote-content'>"
     end
 
     [result, replacements, original_text]
@@ -105,7 +111,7 @@ private
   def simple_quote_1 text, replacements, original_text
     result = text.gsub SIMPLE_QUOTE_1_START_REGEXP do
       replacements += 1
-      '<div class="b-quote"><div class="quote-content">'
+      "<div class='b-quote'><div class='quote-content'>"
     end
 
     [result, replacements, original_text]
