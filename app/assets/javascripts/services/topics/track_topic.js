4ae0139524dc2236d857f1ac50ddebda3f374daa
Andrey Sidorov <takandar@gmail.com>
2020-10-09 18:56:04 +0300

    decaffeinate topics tracker

---
+++ app/assets/javascripts/services/topics/track_topic.js
@@ -0,0 +1,11 @@
+import $with from 'helpers/with';
+
+export default class TrackTopic {
+  constructor(topic, $root) {
+    $with(this._selector(topic), $root).data('model', topic);
+  }
+
+  _selector(topic) {
+    return `.b-topic#${topic.id}`;
+  }
+}
