6abfba6e14246bb1a9f0e8325fb2718bdcc7129c
Andrey Sidorov <takandar@gmail.com>
2020-10-18 17:25:30 +0300

    fix deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :deploy_to, "/home/apps/#{fetch :application}/#{fetch :stage}"
 
 server '88.198.7.123', user: fetch(:user), roles: %w{app web}
-# server '88.198.7.116', user: fetch(:user), roles: %w{app web db}
+server '88.198.7.116', user: fetch(:user), roles: %w{app web db}
