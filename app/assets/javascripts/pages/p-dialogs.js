c5ac3087b9c71a61251030f8b142a155fabad958
Andrey Sidorov <takandar@gmail.com>
2020-10-19 20:41:36 +0300

    fix messages preview #2035

--- app/assets/javascripts/pages/p-dialogs.js
+++ app/assets/javascripts/pages/p-dialogs.js
@@ -1,3 +1,13 @@
 pageLoad('dialogs_index', 'dialogs_show', () => {
   $('textarea:appeared').focus();
+
+  $('.b-topic').on('preview:params', '.b-shiki_editor', ({ currentTarget }) => {
+    const $form = $(currentTarget).closest('form');
+
+    return {
+      kind: $form.find('input[name="message[kind]"]').val(),
+      from_id: $form.find('input[name="message[from_id]"]').val(),
+      to_id: $form.find('input[name="message[to_id]"]').val()
+    };
+  });
 });
