c1b5abeb728e75395dd42e27ef03cf293dd4deed
Andrey Sidorov <takandar@gmail.com>
2020-10-25 21:08:11 +0300

    add change_allowed method to VersionsPolicy

--- app/policies/versions_policy.rb
+++ app/policies/versions_policy.rb
@@ -1,17 +1,18 @@
 class VersionsPolicy
-  pattr_initialize :user, %i[version item field]
+  pattr_initialize :user, %i[version db_entry field]
 
   def self.version_allowed? user, version
     new(user, version: version).call
   end
 
-  def call
-    return false if @user.banned?
-    return false if @user.not_trusted_version_changer?
-    return false if @user.not_trusted_names_changer? && name_changed?
-    return false if not_matched_author?
+  def self.change_allowed? user, db_entry, field
+    new(user, db_entry: db_entry, field: field).call
+  end
 
-    if changed_restricted_fields.any?
+  def call
+    return false unless allowed?
+
+    if changing_restricted_fields.any?
       restriction_change_allowed?
     else
       true
@@ -20,38 +21,63 @@ class VersionsPolicy
 
 private
 
+  def allowed?
+    return false if @user.banned?
+    return false if @user.not_trusted_version_changer?
+    return false if @user.not_trusted_names_changer? && name_changing?
+    return false if @version && not_matched_author?
+
+    true
+  end
+
   def not_matched_author?
     @version.user_id != @user.id
   end
 
-  def changed_restricted_fields
-    @changed_restricted_fields ||= @version.item_diff.keys & restricted_fields
+  def change_fields
+    if @version
+      @version.item_diff.keys
+    else
+      [@field.to_s]
+    end
+  end
+
+  def changing_restricted_fields
+    @changing_restricted_fields ||= change_fields & restricted_fields
   end
 
   def restricted_fields
     @restricted_fields ||=
-      Kernel.const_defined?("#{@version.item_type}::RESTRICTED_FIELDS") ?
-        "#{@version.item_type}::RESTRICTED_FIELDS".constantize :
+      Kernel.const_defined?("#{item_type}::RESTRICTED_FIELDS") ?
+        "#{item_type}::RESTRICTED_FIELDS".constantize :
         []
+  end
+
+  def item_type
+    @version ? @version.item_type : @db_entry.class.name
   end
 
   def restriction_change_allowed?
     # must be new ability object here otherwise
     # it will return false in runtime
     # (i.e. during Version creation in DbEntriesController)
-    return true if Ability.new(@user).can?(:restricted_update, @version)
+    return true if Ability.new(@user).can?(:restricted_update, Version)
 
     # allow changes from nil
-    changed_restricted_fields.all? do |field|
-      @version.item_diff.dig(field, 0).nil?
+    changing_restricted_fields.all? do |field|
+      if @version
+        @version.item_diff.dig(field, 0).nil?
+      else
+        @db_entry.send(field).nil?
+      end
     end
   end
 
-  def name_changed?
+  def name_changing?
     (
-      @version.item_diff.keys & Abilities::VersionNamesModerator::MANAGED_FIELDS
+      change_fields & Abilities::VersionNamesModerator::MANAGED_FIELDS
     ).any? && Abilities::VersionNamesModerator::MANAGED_MODELS.include?(
-      version.item_type
+      item_type
     )
   end
 end
--- spec/policies/versions_policy_spec.rb
+++ spec/policies/versions_policy_spec.rb
@@ -1,69 +1,78 @@
 describe VersionsPolicy do
-  context '.version_allowed?' do
-    subject { described_class.version_allowed? user, version }
+  let(:version_allowed) { described_class.version_allowed? user, version }
+  let(:change_allowed) { described_class.change_allowed? user, item, field }
 
-    let(:version) { build :version, item: item, item_diff: item_diff, user: author }
-    let(:author) { user }
-    let(:user) { seed :user }
+  let(:version) do
+    build :version, item: item, item_diff: item_diff, user: author
+  end
+  let(:author) { user }
+  let(:user) { seed :user }
 
-    let(:item) { build_stubbed :anime }
-    let(:item_diff) do
-      {
-        field => [change_from, change_to]
-      }
+  let(:item) { build_stubbed :anime, field => change_from }
+  let(:item_diff) do
+    {
+      field => [change_from, change_to]
+    }
+  end
+  let(:field) { :russian }
+  let(:change_from) { 'a' }
+  let(:change_to) { 'b' }
+
+  it { expect(version_allowed).to eq true }
+  it { expect(change_allowed).to eq true }
+
+  context 'user banned' do
+    before { user.read_only_at = 1.hour.from_now }
+    it { expect(version_allowed).to eq false }
+    it { expect(change_allowed).to eq false }
+  end
+
+  context 'not_trusted_version_changer' do
+    before { user.roles = %i[not_trusted_version_changer] }
+    it { expect(version_allowed).to eq false }
+    it { expect(change_allowed).to eq false }
+  end
+
+  context 'not_trusted_names_changer' do
+    before { user.roles = %i[not_trusted_names_changer] }
+
+    context 'not name field' do
+      let(:field) { 'description_ru' }
+      it { expect(version_allowed).to eq true }
+      it { expect(change_allowed).to eq true }
     end
-    let(:field) { :russian }
-    let(:change_from) { 'a' }
-    let(:change_to) { 'b' }
 
-    it { is_expected.to eq true }
-
-    context 'user banned' do
-      before { user.read_only_at = 1.hour.from_now }
-      it { is_expected.to eq false }
-    end
-
-    context 'not_trusted_version_changer' do
-      before { user.roles = %i[not_trusted_version_changer] }
-      it { is_expected.to eq false }
-    end
-
-    context 'not_trusted_names_changer' do
-      before { user.roles = %i[not_trusted_names_changer] }
-
-      context 'not name field' do
-        let(:field) { 'description_ru' }
-        it { is_expected.to eq true }
+    context 'name field' do
+      context 'not DbEntry model' do
+        let(:item) { build_stubbed :video }
+        it { expect(version_allowed).to eq true }
+        it { expect(change_allowed).to eq true }
       end
 
-      context 'name field' do
-        context 'not DbEntry model' do
-          let(:item) { build_stubbed :video }
-          it { is_expected.to eq true }
-        end
-
-        context 'DbEntry model' do
-          it { is_expected.to eq false }
-        end
+      context 'DbEntry model' do
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
       end
     end
+  end
 
-    context 'not matched author' do
-      let(:author) { user_2 }
-      it { is_expected.to eq false }
+  context 'not matched author' do
+    let(:author) { user_2 }
+    it { expect(version_allowed).to eq false }
+  end
+
+  context 'changed restricted field' do
+    let(:field) { :name }
+
+    context 'from nil to value' do
+      let(:change_from) { nil }
+      it { expect(version_allowed).to eq true }
+      it { expect(change_allowed).to eq true }
     end
 
-    context 'changed restricted field' do
-      let(:field) { :name }
-
-      context 'from nil to value' do
-        let(:change_from) { nil }
-        it { is_expected.to eq true }
-      end
-
-      context 'from value to value' do
-        it { is_expected.to eq false }
-      end
+    context 'from value to value' do
+      it { expect(version_allowed).to eq false }
+      it { expect(change_allowed).to eq false }
     end
   end
 end
