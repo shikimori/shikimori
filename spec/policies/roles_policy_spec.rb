a01c6cc960a019f69d7099c8cdd7b9ae5d44ad34
Andrey Sidorov <takandar@gmail.com>
2020-10-26 20:57:44 +0300

    staff can see restricted roles

--- spec/policies/roles_policy_spec.rb
+++ spec/policies/roles_policy_spec.rb
@@ -13,12 +13,20 @@ describe RolesPolicy do
         .to receive(:h)
         .and_return view_contex
     end
-    let(:view_contex) { double "can?": can_manage }
+    let(:view_contex) { double "can?": can_manage, "current_user": (double staff?: is_staff) }
     let(:can_manage) { [true, false].sample }
+    let(:is_staff) { false }
 
     it do
       is_expected.to eq can_manage
       expect(view_contex).to have_received(:"can?").with(:"manage_#{role}_role", User)
+    end
+
+    context 'staff has access' do
+      let(:can_manage) { false }
+      let(:is_staff) { [true, false].sample }
+
+      it { is_expected.to eq is_staff }
     end
   end
 end
