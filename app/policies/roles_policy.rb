6b1502a69c947ada367635ab30ca92849b29845b
Andrey Sidorov <takandar@gmail.com>
2020-10-26 20:50:28 +0300

    add new roles to roles policy

--- app/policies/roles_policy.rb
+++ app/policies/roles_policy.rb
@@ -4,6 +4,8 @@ class RolesPolicy
   RESTRICTED_ROLES = %i[
     not_trusted_version_changer
     not_trusted_names_changer
+    not_trusted_texts_changer
+    not_trusted_fansub_changer
     not_trusted_abuse_reporter
     censored_avatar
     censored_profile
