b4774254b270cb745c3d21851f05053db8bed874
Andrey Sidorov <takandar@gmail.com>
2020-10-30 20:28:46 +0300

    special treatment for licensors, animes, mangas and ranobe in chewy_indexes

--- spec/support/shared_contexts/chewy_indexes.rb
+++ spec/support/shared_contexts/chewy_indexes.rb
@@ -4,9 +4,20 @@ shared_context :chewy_indexes do |indexes|
   include_context :timecop, 'Thu, 10 May 2018 09:00:00 PDT -07:00'
 
   before do
-    if (%i[ranobe] & Array(indexes)).any?
+    if (%i[anime] & Array(indexes)).any?
+      ActiveRecord::Base.connection.reset_pk_sequence! :animes
+      AnimesIndex.purge!
+      LicensorsIndex.purge!
+    elsif (%i[manga] & Array(indexes)).any?
+      ActiveRecord::Base.connection.reset_pk_sequence! :mangas
+      MangasIndex.purge!
+      LicensorsIndex.purge!
+    elsif (%i[ranobe] & Array(indexes)).any?
       ActiveRecord::Base.connection.reset_pk_sequence! :mangas
       RanobeIndex.purge!
+      LicensorsIndex.purge!
+    elsif (%i[licensors] & Array(indexes)).any?
+      LicensorsIndex.purge!
     else
       Array(indexes).each do |index|
         ActiveRecord::Base.connection.reset_pk_sequence! index
