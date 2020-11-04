484d4f1458c29bcb0ccfab9eea46a4ec11c6f62c
Andrey Sidorov <takandar@gmail.com>
2020-11-04 21:35:25 +0300

    fix editor focus in dialogs

--- app/assets/javascripts/pages/p-dialogs.js
+++ app/assets/javascripts/pages/p-dialogs.js
@@ -1,3 +1,8 @@
-pageLoad('dialogs_index', 'dialogs_show', () => {
-  $('textarea:appeared').focus();
+import delay from 'delay';
+
+pageLoad('dialogs_index', 'dialogs_show', async () => {
+  await delay(25);
+  const editor = $('.b-shiki_editor, .b-shiki_editor-v2').view();
+  await editor.initialization;
+  editor.focus();
 });
