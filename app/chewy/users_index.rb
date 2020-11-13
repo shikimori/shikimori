d834e041cadf74309c499dbd465119c2057bc84e
Andrey Sidorov <takandar@gmail.com>
2020-11-13 20:30:56 +0300

    consider length weight in users query (#2088)

--- app/chewy/users_index.rb
+++ app/chewy/users_index.rb
@@ -4,16 +4,20 @@ class UsersIndex < ApplicationIndex
   settings DEFAULT_SETTINGS
 
   define_type User do
-    NAME_FIELDS.each do |name_field|
-      field name_field,
-        type: 'keyword',
-        index: false,
-        fields: {
-          original: ORIGINAL_FIELD,
-          edge_phrase: EDGE_PHRASE_FIELD,
-          edge_word: EDGE_WORD_FIELD,
-          ngram: NGRAM_FIELD
-        }
-    end
+    field :nickname,
+      type: 'keyword',
+      index: false,
+      fields: {
+        original: ORIGINAL_FIELD,
+        edge_phrase: EDGE_PHRASE_FIELD,
+        edge_word: EDGE_WORD_FIELD,
+        ngram: NGRAM_FIELD
+      }
+
+    field :weight,
+      type: 'half_float',
+      index: false,
+      # https://www.wolframalpha.com/input/?i=plot+%281.1+%2F+%281+%2B+%281.1+-+1%29+*+%28min%281%2C+max%28%28%28x+-+1.0%29+%2F+%2820.0+-+1.0%29%29%2C+0%29%29%29%29%29+from+x%3D0+to+30
+      value: -> (entry, _) { Relevance::LengthWeight.call entry.nickname.length }
   end
 end
--- app/services/elasticsearch/query/user.rb
+++ app/services/elasticsearch/query/user.rb
@@ -1,2 +1,16 @@
 class Elasticsearch::Query::User < Elasticsearch::Query::QueryBase
+  private
+
+  def query
+    {
+      function_score: {
+        query: super,
+        field_value_factor: {
+          field: 'weight',
+          modifier: 'none',
+          factor: 1
+        }
+      }
+    }
+  end
 end
