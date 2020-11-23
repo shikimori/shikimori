d2838d883679bfd2020f21f505bf8fe3e67e63ee
Andrey Sidorov <takandar@gmail.com>
2020-11-23 22:38:12 +0300

    users exclude logic in Achievements::UsersQuery

--- spec/query_objects/achievements/users_query_spec.rb
+++ spec/query_objects/achievements/users_query_spec.rb
@@ -1,6 +1,4 @@
 describe Achievements::UsersQuery do
-  subject { described_class.new(scope).call rule.neko_id, rule.level }
-
   let(:rule) do
     Neko::Rule.new(
       neko_id: neko_id,
@@ -45,11 +43,61 @@ describe Achievements::UsersQuery do
       user: user_3
   end
 
-  let(:scope) { User.all }
-  it { is_expected.to eq [user, user_2] }
+  context '.new' do
+    subject do
+      described_class.new(scope).filter(
+        neko_id: rule.neko_id,
+        level: rule.level
+      )
+    end
 
-  context 'sample' do
-    let(:scope) { User.where(id: user_2.id) }
-    it { is_expected.to eq [user_2] }
+    describe '#filter' do
+      let(:scope) { User.all }
+      it { is_expected.to eq [user, user_2] }
+
+      context 'sample' do
+        let(:scope) { User.where(id: user_2.id) }
+        it { is_expected.to eq [user_2] }
+      end
+    end
+  end
+
+  context '.fetch' do
+    subject do
+      described_class.fetch(fetched_user).filter(
+        neko_id: rule.neko_id,
+        level: rule.level
+      )
+    end
+
+    describe '#filter' do
+      let(:fetched_user) { nil }
+      it { is_expected.to eq [user, user_2] }
+
+      context 'banned user' do
+        before do
+          user.update! roles: [Types::User::ROLES_EXCLUDED_FROM_STATISTICS.sample]
+        end
+
+        it { is_expected.to eq [user_2] }
+
+        context 'user not set' do
+          let(:fetched_user) { nil }
+          it { is_expected.to eq [user_2] }
+        end
+
+        context 'user is set' do
+          context 'set banned user' do
+            let(:fetched_user) { user }
+            it { is_expected.to eq [user, user_2] }
+          end
+
+          context 'set not banned user' do
+            let(:fetched_user) { user_2 }
+            it { is_expected.to eq [user_2] }
+          end
+        end
+      end
+    end
   end
 end
