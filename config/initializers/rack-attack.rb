4a2cdba800af5209822efdf15b66672ca087bbc8
Andrey Sidorov <takandar@gmail.com>
2020-12-09 17:34:20 +0300

    better rack-attack rules

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -19,6 +19,14 @@ Rack::Attack.safelist('screenshots_upload') do |req|
   req.post? && req.path.ends_with?('/screenshots')
 end
 
+Rack::Attack.safelist('autocomplete_and_search_and_shiki_editor') do |req|
+  req.get? && (
+    req.path.ends_with?('/autocomplete') ||
+    req.env['REQUEST_URI'].starts_with?('/api/users?search=') ||
+    req.path.starts_with?('/api/shiki_editor')
+  )
+end
+
 if Rails.env.development?
   Rack::Attack.safelist('localhost') do |req|
     req.real_ip == '127.0.0.1'
@@ -32,20 +40,19 @@ Rack::Attack.throttle('req/ip', limit: 5, period: 1.second) do |req|
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
-  if req.user_agent != SMOTRET_ANIME_USER_AGENT && !req.url.include?('/autocomplete')
+  if req.user_agent != SMOTRET_ANIME_USER_AGENT
     req.real_ip
   end
 end
 
-
 Rack::Attack.throttle('per second', limit: 15, period: 1.second) do |req|
-  if req.user_agent == SMOTRET_ANIME_USER_AGENT && !req.url.include?('/autocomplete')
+  if req.user_agent == SMOTRET_ANIME_USER_AGENT
     req.real_ip
   end
 end
 
 Rack::Attack.throttle('per minute', limit: 90, period: 60.second) do |req|
-  if req.user_agent != SMOTRET_ANIME_USER_AGENT && !req.url.include?('/autocomplete')
+  if req.user_agent != SMOTRET_ANIME_USER_AGENT
     req.real_ip
   end
 end
