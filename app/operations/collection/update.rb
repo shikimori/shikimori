8b14d4b3e628eb1d45bf58286450b633ff4d3b74
Andrey Sidorov <takandar@gmail.com>
2021-01-05 17:05:46 +0300

    introduce UserContent::UpdateBase

--- app/operations/collection/update.rb
+++ app/operations/collection/update.rb
@@ -1,24 +1,15 @@
 # frozen_string_literal: true
 
-class Collection::Update < ServiceObjectBase
-  pattr_initialize :model, :params
+class Collection::Update < UserContent::UpdateBase
+  klass Collection
   MAX_LINKS = 500
-
-  def call
-    Collection.transaction do
-      update_collection
-      publish if @model.published? && hidden_topic?
-    end
-    @model
-  end
 
 private
 
-  def update_collection
+  def update
     if @model.update update_params
       @model.links.delete_all
       CollectionLink.import collection_links
-      @model.touch
     end
   end
 
@@ -28,28 +19,10 @@ private
     touch_creation_date
   end
 
-  def publish_topic
-    @model.topics.each do |topic|
-      Topic::Update.call(
-        topic: topic,
-        params: {
-          forum_id: Topic::FORUM_IDS[model.class.name],
-          created_at: Time.zone.now,
-          updated_at: Time.zone.now
-        },
-        faye: faye_service
-      )
-    end
-  end
-
   def touch_all_linked
     @model.kind.capitalize.constantize
       .where(id: links.pluck(:linked_id))
       .update_all updated_at: Time.zone.now
-  end
-
-  def touch_creation_date
-    @model.update created_at: Time.zone.now, updated_at: Time.zone.now
   end
 
   def collection_links
@@ -67,13 +40,5 @@ private
 
   def update_params
     params.except(:links)
-  end
-
-  def faye_service
-    FayeService.new @model.user, nil
-  end
-
-  def hidden_topic?
-    @model.topics.first.forum_id == Forum::HIDDEN_ID
   end
 end
---
+++ app/operations/user_content/update_base.rb
@@ -0,0 +1,50 @@
+class UserContent::UpdateBase < ServiceObjectBase
+  extend DslAttribute
+  dsl_attribute :klass
+
+  pattr_initialize :model, :params
+
+  def call
+    klass.transaction do
+      update
+      publish if @model.published? && hidden_topic?
+    end
+    @model
+  end
+
+private
+
+  def update
+    @model.update @params
+  end
+
+  def publish
+    publish_topic
+  end
+
+  def publish_topic
+    @model.topics.each do |topic|
+      Topic::Update.call(
+        topic: topic,
+        params: {
+          forum_id: Topic::FORUM_IDS[model.class.name],
+          created_at: Time.zone.now,
+          updated_at: Time.zone.now
+        },
+        faye: faye_service
+      )
+    end
+  end
+
+  def touch_creation_date
+    @model.update created_at: Time.zone.now, updated_at: Time.zone.now
+  end
+
+  def hidden_topic?
+    @model.topics.first.forum_id == Forum::HIDDEN_ID
+  end
+
+  def faye_service
+    FayeService.new @model.user, nil
+  end
+end
