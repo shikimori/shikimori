b3a913a05eca3067f4c79592298e7c3cb4020753
Andrey Sidorov <takandar@gmail.com>
2021-01-05 17:13:59 +0300

    generate hidden topics for all unpublished articles

--- db/migrate/20210105121319_generate_hidden_topics_for_all_unpublished_collections.rb
+++ db/migrate/20210105121319_generate_hidden_topics_for_all_unpublished_collections.rb
@@ -1,13 +1,13 @@
 class GenerateHiddenTopicsForAllUnpublishedCollections < ActiveRecord::Migration[5.2]
   def up
-    Collection.where(state: :unpublished).each do |collection|
-      collection.generate_topics collection.locale, forum_id: Forum::HIDDEN_ID
+    Collection.where(state: :unpublished).each do |model|
+      model.generate_topics model.locale, forum_id: Forum::HIDDEN_ID
     end
   end
 
   def down
-    Collection.where(state: :unpublished).each do |collection|
-      collection.topics.destroy_all
+    Collection.where(state: :unpublished).each do |model|
+      model.topics.destroy_all
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_121319) do
+ActiveRecord::Schema.define(version: 2021_01_05_140924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
