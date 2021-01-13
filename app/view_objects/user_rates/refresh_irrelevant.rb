b354788fbd7e7a9ebc9f2904db64c9fcf83435a0
Andrey Sidorov <takandar@gmail.com>
2021-01-14 01:27:50 +0300

    update UserRates::RefreshIrrelevant logic

--- app/view_objects/user_rates/refresh_irrelevant.rb
+++ app/view_objects/user_rates/refresh_irrelevant.rb
@@ -1,10 +1,10 @@
-# class reload 'anonses' and 'ongoings'
+# class reloads 'anonses' and 'ongoings'
 # in user_rate assication of provided libary
-class UserRates::RefreshIrrelevant < ServiceObjectBase
+class UserRates::RefreshIrrelevant
   # library is a hash with
   # key = user_rate.status
   # value = user_rates of this status
-  pattr_initialize :library, :klass
+  method_object :library, :klass
 
   def call
     reload irrelevants
@@ -15,7 +15,8 @@ private
   def irrelevants
     @library.each_with_object({}) do |(status, list), memo|
       list.user_rates.each_with_index do |user_rate, index|
-        next unless user_rate.target.anons? || user_rate.target.ongoing?
+        next unless user_rate.target_is_anons || user_rate.target_is_ongoing
+
         memo[user_rate.target_id] = [status, index]
       end
     end
@@ -24,7 +25,6 @@ private
   def reload irrelevants
     @klass
       .where(id: irrelevants.keys)
-      .decorate
       .each do |entry|
         status, index = irrelevants[entry.id]
         assign_entry status, index, entry
@@ -32,11 +32,11 @@ private
   end
 
   def assign_entry status, index, entry
-    association_name = @klass.name.downcase.to_sym
     user_rate = @library[status].user_rates[index]
-    association_cache = (user_rate.decorated? ? user_rate.object : user_rate)
-      .instance_variable_get('@association_cache')
 
-    association_cache[association_name].target = entry
+    user_rate.target_episodes = entry.episodes
+    user_rate.target_episodes_aired = entry.episodes_aired
+    user_rate.target_is_anons = entry.anons?
+    user_rate.target_is_ongoing = entry.ongoing?
   end
 end
--- spec/view_objects/user_rates/refresh_irrelevant_spec.rb
+++ spec/view_objects/user_rates/refresh_irrelevant_spec.rb
@@ -1,33 +1,37 @@
 describe UserRates::RefreshIrrelevant do
-  let(:service) { UserRates::RefreshIrrelevant.new library, Anime }
+  let!(:library) do
+    {
+      planned: OpenStruct.new(
+        user_rates: [
+          UserRates::StructEntry.create(anons_rate),
+          UserRates::StructEntry.create(released_rate)
+        ]
+      ),
+      watching: OpenStruct.new(
+        user_rates: [
+          UserRates::StructEntry.create(ongoing_rate)
+        ]
+      )
+    }
+  end
+  let(:anons_rate) { create :user_rate, anime: anons }
+  let(:released_rate) { create :user_rate, anime: released }
+  let(:ongoing_rate) { create :user_rate, anime: ongoing }
 
-  describe '#perform' do
-    let!(:library) do
-      {
-        planned: OpenStruct.new(user_rates: [anons_rate, released_rate]),
-        watching: OpenStruct.new(user_rates: [ongoing_rate.decorate])
-      }
-    end
-    let(:anons_rate) { create :user_rate, anime: anons }
-    let(:released_rate) { create :user_rate, anime: released }
-    let(:ongoing_rate) { create :user_rate, anime: ongoing }
+  let(:anons) { create :anime, :anons, episodes: 20 }
+  let(:released) { create :anime, :released, episodes: 20 }
+  let(:ongoing) { create :anime, :ongoing, episodes: 20 }
 
-    let(:anons) { create :anime, :anons, episodes: 20 }
-    let(:released) { create :anime, :released, episodes: 20 }
-    let(:ongoing) { create :anime, :ongoing, episodes: 20 }
+  before do
+    Anime.find(anons.id).update episodes_aired: 10
+    Anime.find(released.id).update episodes_aired: 11
+    Anime.find(ongoing.id).update episodes_aired: 12
+  end
+  subject! { described_class.call library, Anime }
 
-    before do
-      Anime.find(anons.id).update episodes_aired: 10
-      Anime.find(released.id).update episodes_aired: 11
-      Anime.find(ongoing.id).update episodes_aired: 12
-    end
-    subject! { service.call }
-
-    it do
-      expect(library[:planned].user_rates.first.anime).to be_decorated
-      expect(library[:planned].user_rates.first.anime.episodes_aired).to eq 10
-      expect(library[:planned].user_rates.second.anime.episodes_aired).to eq 0
-      expect(library[:watching].user_rates.first.anime.episodes_aired).to eq 12
-    end
+  it do
+    expect(library[:planned].user_rates.first.target_episodes_aired).to eq 10
+    expect(library[:planned].user_rates.second.target_episodes_aired).to eq 0
+    expect(library[:watching].user_rates.first.target_episodes_aired).to eq 12
   end
 end
