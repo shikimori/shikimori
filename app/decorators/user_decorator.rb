cc053720aa7fe29b777aa60f46d07b3d5424824a
Andrey Sidorov <takandar@gmail.com>
2021-01-18 16:06:51 +0300

    additional cache for activity stats

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,6 +1,6 @@
 class UserDecorator < BaseDecorator
   instance_cache :clubs_for_domain, :exact_last_online_at,
-    :is_friended?, :mutual_friended?, :list_stats
+    :is_friended?, :mutual_friended?, :list_stats, :activity_stats
 
   CACHE_VERSION = :v1
 
