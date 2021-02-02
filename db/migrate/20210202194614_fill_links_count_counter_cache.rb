b55ecc962095309e0a53da8b6d70a79ec926761b
Andrey Sidorov <takandar@gmail.com>
2021-02-02 23:13:09 +0300

    fix migration

--- db/migrate/20210202194614_fill_links_count_counter_cache.rb
+++ db/migrate/20210202194614_fill_links_count_counter_cache.rb
@@ -1,12 +1,13 @@
 class FillLinksCountCounterCache < ActiveRecord::Migration[5.2]
   def change
     commit_db_transaction
-    scope = Collection.includes(:links)
+    scope = Collection.all
     size = scope.size
 
     scope.each_with_index do |collection, index|
       puts "#{index + 1} / #{size}"
-      collection.update_column :links_count, collection.links.size
+
+      Collection.reset_counters collection.id, :links_count
     end
   end
 end
