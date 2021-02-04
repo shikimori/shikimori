e4e7e9df38aaf4b75c28be214a8608bac3f84784
Andrey Sidorov <takandar@gmail.com>
2021-02-04 21:29:07 +0300

    Animes::RefreshStats scores_stats logic

--- app/services/animes/refresh_stats.rb
+++ app/services/animes/refresh_stats.rb
@@ -2,6 +2,17 @@ class Animes::RefreshStats
   method_object :scope
 
   SELECT_SQL = <<~SQL.squish
+    %<table_name>s.id as id,
+      sum(case when user_rates.score = 10 then 1 else 0 end) as score_10,
+      sum(case when user_rates.score = 9 then 1 else 0 end) as score_9,
+      sum(case when user_rates.score = 8 then 1 else 0 end) as score_8,
+      sum(case when user_rates.score = 7 then 1 else 0 end) as score_7,
+      sum(case when user_rates.score = 6 then 1 else 0 end) as score_6,
+      sum(case when user_rates.score = 5 then 1 else 0 end) as score_5,
+      sum(case when user_rates.score = 4 then 1 else 0 end) as score_4,
+      sum(case when user_rates.score = 3 then 1 else 0 end) as score_3,
+      sum(case when user_rates.score = 2 then 1 else 0 end) as score_2,
+      sum(case when user_rates.score = 1 then 1 else 0 end) as score_1
   SQL
 
   def call
@@ -18,10 +29,24 @@ private
   def build_stats
     @scope
       .joins(:rates)
+      .where.not(user_rates: { user_id: User.cheat_bot })
       .group(:id)
-      .select(SELECT_SQL)
-      .each do |entry|
-        ap entry
+      .select(format(SELECT_SQL, table_name: @scope.table_name))
+      .map do |entry|
+        AnimeStat.new(
+          entry: entry,
+          scores_stats: scores_stats(entry),
+          list_stats: []
+        )
       end
+  end
+
+  def scores_stats entry
+    10.downto(1)
+      .map do |i|
+        key = :"score_#{i}"
+        { key: key, value: entry.send(key) } if entry.send(key).positive?
+      end
+      .compact
   end
 end
--- spec/services/animes/refresh_stats_spec.rb
+++ spec/services/animes/refresh_stats_spec.rb
@@ -2,8 +2,10 @@ describe Animes::RefreshStats do
   subject { described_class.call scope }
   let(:scope) { Anime.all }
 
-  let!(:anime_1) { create :anime }
-  let!(:anime_2) { create :anime }
+  let(:anime_1) { create :anime }
+  let(:anime_2) { create :anime }
+
+  let(:user_4) { create :user }
 
   before { user_3.update roles: %i[cheat_bot] }
   let!(:anime_1_rate_1) do
@@ -27,6 +29,13 @@ describe Animes::RefreshStats do
       status: :dropped,
       score: 1
   end
+  let!(:anime_1_rate_4) do
+    create :user_rate,
+      target: anime_1,
+      user: user_4,
+      status: :watching,
+      score: 10
+  end
   let!(:anime_2_rate_1) do
     create :user_rate,
       target: anime_2,
@@ -35,9 +44,26 @@ describe Animes::RefreshStats do
       score: 10
   end
 
-  context 'no anime stat' do
+  context 'no anime stat', :focus do
     it do
       expect { subject }.to change(AnimeStat, :count).by 2
+      expect(anime_1.stat).to have_attributes(
+        scores_stats: [{
+          'key' => 'score_10',
+          'value' => 2
+        }, {
+          'key' => 'score_8',
+          'value' => 1
+        }],
+        list_stats: []
+      )
+      expect(anime_2.stat).to have_attributes(
+        scores_stats: [{
+          'key' => 'score_10',
+          'value' => 1
+        }],
+        list_stats: []
+      )
     end
   end
 
