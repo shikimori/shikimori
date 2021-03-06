6fcfd8305ff8e6c0228ee8fdd06c37b8af3d3b65
Andrey Sidorov <takandar@gmail.com>
2021-03-06 18:55:08 +0300

    fix BbCodes::Tags::SizeTag spec

--- spec/services/bb_codes/tags/size_tag_spec.rb
+++ spec/services/bb_codes/tags/size_tag_spec.rb
@@ -1,5 +1,6 @@
 describe BbCodes::Tags::SizeTag do
   subject { described_class.instance.format text }
+  let(:max_size) { described_class::MAXIMUM_FONT_SIZE }
 
   context 'small size' do
     let(:text) { '[size=13]test[/size]' }
@@ -7,18 +8,18 @@ describe BbCodes::Tags::SizeTag do
   end
 
   context 'large size' do
-    let(:text) { '[size=36]test[/size]' }
-    it { is_expected.to eq '<span style="font-size: 35px;">test</span>' }
+    let(:text) { "[size=#{max_size}]test[/size]" }
+    it { is_expected.to eq "<span style=\"font-size: #{max_size}px;\">test</span>" }
+  end
+
+  context 'too large size' do
+    let(:text) { "[size=#{max_size + 1}]test[/size]" }
+    it { is_expected.to eq "<span style=\"font-size: #{max_size}px;\">test</span>" }
   end
 
   context 'newline' do
     let(:text) { '[size=16]\n\rtest\n[/size]' }
     it { is_expected.to eq '<span style="font-size: 16px;">\\n\\rtest\\n</span>' }
-  end
-
-  context 'too large size' do
-    let(:text) { '[size=27]test[/size]' }
-    it { is_expected.to eq '<span style="font-size: 26px;">test</span>' }
   end
 
   context 'multiple tags' do
