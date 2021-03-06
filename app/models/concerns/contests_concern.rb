e42e64eff9948f2a8b9a69e1da603956d3b3b19b
Andrey Sidorov <takandar@gmail.com>
2021-03-06 14:49:07 +0300

    add missing contest_suggestions association

--- app/models/concerns/contests_concern.rb
+++ app/models/concerns/contests_concern.rb
@@ -5,13 +5,20 @@ module ContestsConcern
     has_many :contest_links,
       ->(entry) { where linked_type: entry.class.base_class.name },
       foreign_key: :linked_id,
+      inverse_of: :item,
       dependent: :destroy
 
     has_many :contest_winners,
       ->(entry) { where item_type: entry.class.base_class.name },
       foreign_key: :item_id,
+      inverse_of: :item,
       dependent: :destroy
 
     has_many :contests, through: :contest_links
+    has_many :contest_suggestions,
+      ->(entry) { where item_type: entry.class.base_class.name },
+      foreign_key: :item_id,
+      inverse_of: :item,
+      dependent: :destroy
   end
 end
--- spec/support/shared_examples/contests_concern.rb
+++ spec/support/shared_examples/contests_concern.rb
@@ -4,6 +4,7 @@ shared_examples :contests_concern do
       it { is_expected.to have_many(:contest_links).dependent :destroy }
       it { is_expected.to have_many(:contest_winners).dependent :destroy }
       it { is_expected.to have_many :contests }
+      it { is_expected.to have_many(:contest_suggestions).dependent :destroy }
     end
   end
 end
