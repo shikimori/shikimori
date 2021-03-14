c39eb5064505b9b2f6be80d9090d1efd515dd29f
Andrey Sidorov <takandar@gmail.com>
2021-03-14 17:34:16 +0300

    imageboards art does not work on ranobe#art page #2183

--- app/assets/javascripts/pages/p-animes/art.js
+++ app/assets/javascripts/pages/p-animes/art.js
@@ -1,4 +1,4 @@
-pageLoad('animes_art', 'mangas_art', async () => {
+pageLoad('animes_art', 'mangas_art', 'ranobe_art', async () => {
   const { ImageboardsGallery } =
     await import(/* webpackChunkName: "galleries" */ 'views/images/imageboards_gallery');
   new ImageboardsGallery('.b-gallery');
