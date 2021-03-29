1e3219c0ed51b7445790d7f0a7ebd2b2171dc7be
Andrey Sidorov <takandar@gmail.com>
2021-03-29 10:00:51 +0300

    fix occasionally ActiveRecord::RecordNotUnique

--- app/controllers/api/v1/ignores_controller.rb
+++ app/controllers/api/v1/ignores_controller.rb
@@ -8,16 +8,20 @@ class Api::V1::IgnoresController < Api::V1Controller
   api :POST, '/ignores/:id', 'Create an ignore', deprecated: true
   description 'Requires `ignores` oauth scope'
   def create
-    @target_user = User.find(params[:id])
-    current_user.ignores.create!(target: @target_user) unless current_user.ignores?(@target_user)
+    @target_user = User.find params[:id]
+
+    unless current_user.ignores? @target_user
+      current_user.ignores.create! target: @target_user rescue ActiveRecord::RecordNotUnique
+    end
+
     render json: { notice: i18n_t('ignored', nickname: @target_user.nickname) }
   end
 
   api :DELETE, '/ignores/:id', 'Destroy an ignore', deprecated: true
   description 'Requires `ignores` oauth scope'
   def destroy
-    @user = User.find(params[:id])
-    current_user.ignored_users.delete(@user)
+    @user = User.find params[:id]
+    current_user.ignored_users.delete @user
     render json: { notice: i18n_t('not_ignored', nickname: @user.nickname) }
   end
 end
