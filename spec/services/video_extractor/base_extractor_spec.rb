f717cf333b3575babf5ac297ea5625987a72c646
Andrey Sidorov <takandar@gmail.com>
2021-04-14 22:18:11 +0300

    remove VideoExtractor::PlayerUrlExtractor

--- spec/services/video_extractor/base_extractor_spec.rb
+++
@@ -1,8 +0,0 @@
-describe VideoExtractor::BaseExtractor do
-  let(:service) { VideoExtractor::BaseExtractor.new 'test' }
-
-  describe 'hosting' do
-    subject { service.hosting }
-    it { is_expected.to eq :base }
-  end
-end
