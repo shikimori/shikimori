806b35d6a61f1a0ce1ab98f0e5590226fe406a69
Andrey Sidorov <takandar@gmail.com>
2021-04-27 18:18:50 +0300

    decaffeinate shiki_view

---
+++ app/assets/javascripts/views/application/shiki_view.js
@@ -0,0 +1,60 @@
+import { bind } from 'shiki-decorators';
+
+import View from 'views/application/view';
+import axios from 'helpers/axios';
+
+const MAX_PREVIEW_HEIGHT = 450;
+const COLLAPSED_HEIGHT = 150;
+
+// общий класс для комментария, топика, редактора
+export default class ShikiView extends View {
+  _initialize(...args) {
+    super._initialize(...args);
+
+    this.$node.removeClass('unprocessed');
+    this.$inner = this.$('>.inner');
+
+    // if (!this.$inner.exists()) { return; }
+  }
+
+  @bind
+  _checkHeight() {
+    if (!window.SHIKI_USER.isCommentsAutoCollapsed) { return; }
+
+    this.$inner.checkHeight({
+      maxHeight: this.MAX_PREVIEW_HEIGHT,
+      collapsedHeight: this.COLLAPSED_HEIGHT
+    });
+  }
+
+  @bind
+  _shade() {
+    return this.$node.addClass('b-ajax');
+  }
+
+  @bind
+  _unshade() {
+    return this.$node.removeClass('b-ajax');
+  }
+
+  @bind
+  async _reload() {
+    this._shade();
+    const { data } = await axios.get(this._reloadUrl());
+    this._replace(data.content, data.JS_EXPORTS);
+  }
+
+  // урл для перезагрузки элемента
+  _reloadUrl() {
+    return this.$node.data('url');
+  }
+
+  _replace(html, JS_EXPORTS) {
+    const $replaced = $(html);
+    this.$node.replaceWith($replaced);
+
+    $replaced
+      .process(JS_EXPORTS)
+      .yellowFade();
+  }
+}
