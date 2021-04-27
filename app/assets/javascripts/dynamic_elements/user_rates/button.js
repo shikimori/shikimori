2557eefdb3ec4df967259433fc33e9379258b015
Andrey Sidorov <takandar@gmail.com>
2021-04-27 18:04:00 +0300

    decaffeinate user_rates/button

---
+++ app/assets/javascripts/dynamic_elements/user_rates/button.js
@@ -0,0 +1,127 @@
+import delay from 'delay';
+import { flash } from 'shiki-utils';
+import { bind } from 'shiki-decorators';
+
+import UserRatesTracker from 'services/user_rates/tracker';
+import View from 'views/application/view';
+import JST from 'helpers/jst';
+
+import * as AuthorizedAction from '../authorized_action';
+
+const TEMPLATE = 'user_rates/button';
+const I18N_KEY = 'activerecord.attributes.user_rate.statuses';
+
+export default class UserRateButton extends View {
+  initialize() {
+    // data attribute is set in UserRatesTracker
+    this.model = this.$root.data('model');
+    this._render();
+
+    // delegated handlers because @_render can be called multiple times
+    this.on('click', '.trigger-arrow', this._toggleList);
+    this.on('click', '.edit-trigger', this._toggleList);
+
+
+    this.on('click', '.add-trigger', this._submitStatus);
+
+    this.on('ajax:before', this._ajaxBefore);
+    this.on('ajax:error', this._ajaxComplete);
+    this.on('ajax:success', this._ajaxSuccess);
+  }
+
+  get isPersisted() {
+    return !!this.model.id;
+  }
+
+  // handlers
+  @bind
+  async _toggleList() {
+    this.$('.b-add_to_list').toggleClass('expanded');
+
+    if (!this.$('.expanded-options').data('height')) {
+      this.$('.expanded-options')
+        .data('height', this.$('.expanded-options').height())
+        .css('height', 0)
+        .show();
+    }
+
+    await delay();
+
+    const height = this.$('.b-add_to_list').hasClass('expanded') ?
+      this.$('.expanded-options').data('height') :
+      0;
+
+    this.$('.expanded-options').css('height', height);
+  }
+
+  @bind
+  _submitStatus(e) {
+    const $form = $(e.target).closest('form');
+
+    $form
+      .find('input[name="user_rate[status]"]')
+      .val($(e.currentTarget).data('status'));
+
+    $form.submit();
+  }
+
+  @bind
+  _ajaxBefore() {
+    if (!window.SHIKI_USER.isSignedIn) {
+      flash.info(I18n.t(`${AuthorizedAction.I18N_KEY}.register_to_complete_action`));
+      return false;
+    }
+
+    this.$root.addClass('b-ajax');
+  }
+
+  @bind
+  _ajaxComplete() {
+    this.$root.removeClass('b-ajax');
+  }
+
+  @bind
+  _ajaxSuccess(e, model) {
+    UserRatesTracker.update(model || this._newUserRate());
+    this._ajaxComplete();
+  }
+
+  // functions
+  update(model) {
+    this.model = model;
+    this._render();
+  }
+
+  _render() {
+    this
+      .html(JST[TEMPLATE](this._renderParams()))
+      .process();
+  }
+
+  _renderParams() {
+    const submit_url = this.isPersisted ?
+      `/api/v2/user_rates/${this.model.id}` :
+      '/api/v2/user_rates';
+
+    return {
+      model: this.model,
+      user_id: window.SHIKI_USER.id,
+      statuses: I18n.t(`${I18N_KEY}.${this.model.target_type.toLowerCase()}`),
+      form_url: submit_url,
+      form_method: this.isPersisted ? 'PATCH' : 'POST',
+      destroy_url: (this.isPersisted ? `/api/v2/user_rates/${this.model.id}` : undefined),
+      extended_html: this._extendedHtml()
+    };
+  }
+
+  _newUserRate() {
+    return {
+      status: 'planned',
+      target_id: this.model.target_id,
+      target_type: this.model.target_type
+    };
+  }
+
+  // must be redefined in inherited class
+  _extendedHtml() {}
+}
---
+++ app/assets/javascripts/dynamic_elements/user_rates/extended.js
@@ -0,0 +1,100 @@
+import { bind } from 'shiki-decorators';
+
+import JST from 'helpers/jst';
+import axios from 'helpers/axios';
+import UserRateButton from './button';
+
+const EXTENDED_TEMPLATE = 'user_rates/extended';
+const SCORE_TEMPLATE = 'user_rates/score';
+
+export default class UserRateExtended extends UserRateButton {
+  formHtml = null
+
+  initialize() {
+    this.entry = this.$root.data('entry');
+
+    this.on('click', '.cancel', this._hideForm);
+
+    this.on('ajax:success', '.remove', this._hideForm);
+    this.on('ajax:success', '.rate-edit', this._hideForm);
+    this.on('rate:change', this._changeScore);
+
+    super.initialize();
+  }
+
+  // handlers
+  @bind
+  _toggleList({ currentTarget }) {
+    if (this.isPersisted && currentTarget.classList.contains('edit-trigger')) {
+      if (this.formHtml) {
+        this._hideForm();
+      } else {
+        this._fetchForm();
+      }
+    } else {
+      super._toggleList();
+    }
+  }
+
+  @bind
+  async _fetchForm() {
+    this._ajaxBefore();
+    const { data } = await axios.get(`/user_rates/${this.model.id}/edit`);
+
+    this._ajaxComplete();
+    this._showForm(data);
+  }
+
+  @bind
+  _showForm(html) {
+    this.formHtml = html;
+    this._render();
+    this.$('.remove.bottom').addClass('hidden');
+    this.$('.delete-button.top').removeClass('hidden');
+  }
+
+  @bind
+  _hideForm() {
+    this.formHtml = null;
+    this._render();
+  }
+
+  @bind
+  _changeScore(e, score) {
+    this.$('input[name="user_rate[score]"]').val(score);
+    this.$('form').submit();
+  }
+
+  // functions
+  _extendedHtml() {
+    if (this.isPersisted) {
+      return this.formHtml || this._renderExtended();
+    }
+  }
+
+  _renderExtended() {
+    return JST[EXTENDED_TEMPLATE]({
+      entry: this.entry,
+      model: this.model,
+      increment_url: this._incrementUrl(),
+      rate_html: JST[SCORE_TEMPLATE]({ score: this.model.score })
+    });
+  }
+
+  _render() {
+    super._render();
+    this.$('.b-rate').rateable();
+  }
+
+  _incrementUrl() {
+    if (this.isPersisted) {
+      let suffix;
+
+      if (this.model.volumes !== 0) {
+        suffix = '?volumes';
+      }
+
+      return `/api/v2/user_rates/${this.model.id}/increment${suffix || ''}`;
+    }
+  }
+}
