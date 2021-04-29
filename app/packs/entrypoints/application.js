494fe0cdf5bf2bc453e88fba89a3865caf39e5ba
Andrey Sidorov <takandar@gmail.com>
2021-04-29 21:15:32 +0300

    fix jquery issue

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -19,7 +19,11 @@ $(() => (
   ), 50)
 ));
 
-import 'jquery';
+const key1 = '$';
+window[key1] = require('jquery'); // eslint-disable-line import/newline-after-import
+
+const key2 = 'jQuery';
+window[key2] = require('jquery'); // eslint-disable-line import/newline-after-import
 
 import 'magnific-popup';
 import 'magnific-popup/dist/magnific-popup.css';
--- app/packs/entrypoints/standalone.js
+++ app/packs/entrypoints/standalone.js
@@ -1,2 +1,5 @@
-window.$ = require('jquery'); // eslint-disable-line import/newline-after-import
-window.jQuery = window.$;
+const key1 = '$';
+window[key1] = require('jquery'); // eslint-disable-line import/newline-after-import
+
+const key2 = 'jQuery';
+window[key2] = require('jquery'); // eslint-disable-line import/newline-after-import
