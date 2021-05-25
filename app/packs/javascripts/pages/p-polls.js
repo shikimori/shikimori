8573da9ff690542f07caa055441478fc9d9aefe7
Andrey Sidorov <takandar@gmail.com>
2021-05-25 17:51:22 +0300

    convert polls to vue 3

--- app/packs/javascripts/pages/p-polls.js
+++ app/packs/javascripts/pages/p-polls.js
@@ -1,30 +1,29 @@
-// pageLoad('.polls', async () => {
-//   if (!$('#vue_poll_variants').exists()) { return; }
-//
-//   const { Vue, Vuex } = await import(/* webpackChunkName: "vue" */ '@/vue/instance');
-//   const { default: ArrayField } = await import('@/vue/components/array_field');
-//   const { default: storeSchema } = await import('@/vue/stores/collection');
-//
-//   const pollVariants = $('#poll_form').data('poll').variants;
-//
-//   const store = new Vuex.Store(storeSchema);
-//   store.state.collection = pollVariants.map((pollVariant, index) =>
-//     ({
-//       key: index,
-//       value: pollVariant.label
-//     })
-//   );
-//
-//   new Vue({
-//     el: '#vue_poll_variants',
-//     store,
-//     render: h => h(ArrayField, {
-//       props: {
-//         resourceType: 'Poll',
-//         field: 'poll_variants',
-//         inputName: 'poll[variants_attributes][][label]',
-//         emptyInputName: 'poll[variants_attributes][]'
-//       }
-//     })
-//   });
-// });
+pageLoad('.polls', async () => {
+  if (!$('#vue_poll_variants').exists()) { return; }
+
+  const { createApp } = await import(/* webpackChunkName: "vue" */ 'vue');
+  const { createStore } = await import(/* webpackChunkName: "vuex" */ 'vuex');
+
+  const { default: ArrayField } = await import('@/vue/components/array_field');
+  const { default: storeSchema } = await import('@/vue/stores/collection');
+
+  const pollVariants = $('#poll_form').data('poll').variants;
+
+  const store = createStore(storeSchema);
+  store.state.collection = pollVariants.map((pollVariant, index) =>
+    ({
+      key: index,
+      value: pollVariant.label
+    })
+  );
+
+  const app = createApp(ArrayField, {
+    resourceType: 'Poll',
+    field: 'poll_variants',
+    inputName: 'poll[variants_attributes][][label]',
+    emptyInputName: 'poll[variants_attributes][]'
+  });
+  app.use(store);
+  app.config.globalProperties.I18n = I18n;
+  app.mount('#vue_poll_variants');
+});
