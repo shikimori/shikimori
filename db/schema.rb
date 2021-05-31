8e7f8cbb71d6e376bf60dd40b0682d212c10e58f
Andrey Sidorov <takandar@gmail.com>
2021-05-31 23:36:19 +0300

    update smotretanime external links to new domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_171910) do
+ActiveRecord::Schema.define(version: 2021_05_31_202935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
