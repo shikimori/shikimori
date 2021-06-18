b6c3c5eace82f5207f296ced3bcaf392c1c4bbea
Andrey Sidorov <takandar@gmail.com>
2021-06-18 20:31:46 +0300

    make rollback possible in latest migrations

--- db/migrate/20210618115743_fix_comment_viewings_schema.rb
+++ db/migrate/20210618115743_fix_comment_viewings_schema.rb
@@ -1,8 +1,12 @@
 class FixCommentViewingsSchema < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :comment_viewings, :user_id, :integer, null: false
     change_column :comment_viewings, :viewed_id, :integer, null: false
 
     add_foreign_key :comment_viewings, :users
+  end
+
+  def down
+    remove_foreign_key :comment_viewings, :users
   end
 end
--- db/migrate/20210618120204_fix_topic_viewings_schema.rb
+++ db/migrate/20210618120204_fix_topic_viewings_schema.rb
@@ -1,8 +1,12 @@
 class FixTopicViewingsSchema < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :topic_viewings, :user_id, :integer, null: false
     change_column :topic_viewings, :viewed_id, :integer, null: false
 
     add_foreign_key :topic_viewings, :users
+  end
+
+  def down
+    remove_foreign_key :topic_viewings, :users
   end
 end
