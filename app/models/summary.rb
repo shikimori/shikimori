2acdbd1e6f2b876540f7393342d0521e752377dd
Andrey Sidorov <takandar@gmail.com>
2021-06-18 15:49:32 +0300

    summary antispam options

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -11,6 +11,12 @@ class Summary < ApplicationRecord
   # validates :anime, presence: true, if: -> { manga_id.nil? }
   # validates :manga, presence: true, if: -> { anime_id.nil? }
 
+  antispam(
+    interval: 1.minute,
+    disable_if: -> { user.admin? && Rails.env.development? },
+    user_id_key: :user_id
+  )
+
   def html_body
     BbCodes::Text.call body
   end
