d767af6b547d69fe13b2e7d3fcf8feb3b3d78864
Andrey Sidorov <takandar@gmail.com>
2021-08-21 19:57:29 +0300

    working on summaries

--- bin/generate_summaries.rb
+++ bin/generate_summaries.rb
@@ -23,32 +23,33 @@ end
     includes(:user, commentable: :linked).
     where(is_summary: true).
     order(id: :desc).
-    # limit(10000).where(commentable_id: Anime.find(3358).decorate.main_topic_view.id).
+    limit(3).where(commentable_id: Anime.find(31240).decorate.main_topic_view.id).
     find_each do |comment|
       db_entry = comment.commentable.linked
       next if db_entry.class.base_class != klass
 
       user = comment.user
 
-      rates_fetcher.user_ids = user.id
+      rates_fetcher.user_ids = [user.id]
       rates_fetcher.user_cache_key = user.cache_key_with_version
       rates = rates_fetcher.fetch(normalization)
 
       normalized_score = rates.dig(user.id, db_entry.id)
+      binding.pry
 
-      tone =
+      opinion =
         if normalized_score
           if normalized_score >= 0.095
-            Types::Summary::Tone[:positive]
+            Types::Summary::Opinion[:positive]
           elsif normalized_score <= -0.095
-            Types::Summary::Tone[:negative]
+            Types::Summary::Opinion[:negative]
           else
-            Types::Summary::Tone[:neutral]
+            Types::Summary::Opinion[:neutral]
           end
         else
-          Types::Summary::Tone[:unknown]
+          Types::Summary::Opinion[:unknown]
         end
-      ap "#{normalized_score} #{tone}"
+      ap "#{normalized_score} #{opinion}"
 
       is_written_before_release =
         if (db_entry.released? && !db_entry.released_on) || (db_entry.is_a?(Manga) && db_entry.discontinued?)
@@ -67,7 +68,7 @@ end
         body: comment.body,
         anime: (db_entry if db_entry.anime?),
         manga: (db_entry if db_entry.manga? || db_entry.ranobe?),
-        tone: tone,
+        opinion: opinion,
         is_written_before_release: is_written_before_release
       )
       summary.instance_variable_set :@is_migration, true
