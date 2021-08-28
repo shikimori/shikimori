577de55485c65bc194d7e7a5302c060adbbfe212
Andrey Sidorov <takandar@gmail.com>
2021-08-28 19:25:49 +0300

    use invisible recaptcha v3 with v2 fallback for sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
-  prepend_before_action :check_captcha, only: [:create]
-
-  def edit
-    super
-  end
+  prepend_before_action :check_captcha, only: %i[create] # rubocop:disable LexicallyScopedActionFilter
 
   def update
-    if resource.update_attributes(params[resource_name])
+    if resource.update params[resource_name]
       set_flash_message :notice, :updated
-      redirect_to profile_path(resource)
+      redirect_to profile_path resource
     else
-      clean_up_passwords(resource)
+      clean_up_passwords resource
       render 'edit'
     end
   end
@@ -27,8 +23,13 @@ private
   end
 
   def check_captcha
-    unless verify_recaptcha
+    auto_success = verify_recaptcha action: 'sign_up', minimum_score: 0.5,
+      secret_key: Rails.application.secrets.recaptcha[:v3][:secret_key]
+    checkbox_success = verify_recaptcha unless auto_success
+
+    unless auto_success || checkbox_success
       self.resource = resource_class.new sign_up_params
+      @show_checkbox_recaptcha = true
       # disabled because of expensive email validation
       # resource.validate # Look for any other validation errors besides Recaptcha
       set_minimum_password_length
