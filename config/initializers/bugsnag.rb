b7f4b78557eaa54ef66ee3b76d4cd2eb5b00edcb
Andrey Sidorov <takandar@gmail.com>
2021-08-30 18:35:02 +0300

    temporarily enable all exceptions

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,11 +2,11 @@ if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = 'f916cca439d63640b542a6116b5e2aad'
 
-    Shikimori::IGNORED_EXCEPTIONS
-      .map { |v| v.constantize rescue NameError }
-      .reject { |v| v == NameError }
-      .each do |klass|
-        config.ignore_classes << klass
-      end
+    # Shikimori::IGNORED_EXCEPTIONS
+    #   .map { |v| v.constantize rescue NameError }
+    #   .reject { |v| v == NameError }
+    #   .each do |klass|
+    #     config.ignore_classes << klass
+    #   end
   end
 end
