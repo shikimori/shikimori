e49eda02ea85dc3f044a7bb783dd1ccfd5781e1f
Andrey Sidorov <takandar@gmail.com>
2021-09-04 16:45:38 +0300

    misc

--- app/decorators/ani_manga_decorator.rb
+++ app/decorators/ani_manga_decorator.rb
@@ -31,7 +31,7 @@ class AniMangaDecorator < DbEntryDecorator
   end
 
   def summary_views
-    object.summaries.includes(:user).take(20)
+    object.summaries.includes(:user).take(4)
   end
 
   def reviews_count
