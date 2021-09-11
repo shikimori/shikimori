912ba4e8a492356921bf008a975ec84bbaf9f320
Andrey Sidorov <takandar@gmail.com>
2021-09-11 20:54:26 +0300

    do not cleanup too fresh comments in Topic::Cleanup

--- app/operations/topic/cleanup.rb
+++ app/operations/topic/cleanup.rb
@@ -2,11 +2,16 @@ class Topic::Cleanup
   method_object :topic
 
   COMMENTS_OFFSET = 1_000
+  COMMENT_LIVE_TIME = 6.months
 
   def call
     return if @topic.comments_count < COMMENTS_OFFSET
 
-    comments(@topic).find_each { |comment| Comment::Cleanup.call comment }
+    comments(@topic).find_each do |comment|
+      next if comment.created_at > COMMENT_LIVE_TIME.ago
+
+      Comment::Cleanup.call comment
+    end
   end
 
 private
@@ -21,6 +26,7 @@ private
   def offset_comment topic
     topic
       .comments
+      .except(:order)
       .order(id: :desc)
       .offset(COMMENTS_OFFSET - 1)
       .limit(1)
