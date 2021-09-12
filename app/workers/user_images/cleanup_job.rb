4cba84534c4787c0e9620b71ea9322dceae32eaa
Andrey Sidorov <takandar@gmail.com>
2021-09-12 19:50:09 +0300

    UserImages::CleanupJob full logic

--- app/workers/user_images/cleanup_job.rb
+++ app/workers/user_images/cleanup_job.rb
@@ -2,7 +2,36 @@ class UserImages::CleanupJob
   include Sidekiq::Worker
   sidekiq_options queue: :cleanup_jobs
 
+  TEXT_FIELDS_MAPPING = {
+    Club => :description,
+    ClubPage => :text,
+    Review => :text,
+    Article => :body,
+    Poll => :text,
+    User => :about,
+    Collection => :text,
+    Contest => :description_ru
+  }
+
   def perform user_image_id
+    used_image_ids =
+      Rails.cache.fetch(:used_image_ids, expires_in: 5.minutes) { fetch_used_image_ids }
+    return if used_image_ids.include? user_image_id.to_i
+
     UserImage.find_by(id: user_image_id)&.destroy
+  end
+
+private
+
+  def fetch_used_image_ids
+    TEXT_FIELDS_MAPPING
+      .flat_map do |klass, field|
+        klass
+          .where("#{field} like '%image=%' or #{field} like '%poster=%'")
+          .map { |v| v.send field }
+          .compact
+          .flat_map { |text| Comment::Cleanup.scan_user_image_ids text }
+      end
+      .uniq
   end
 end
--- spec/workers/user_images/cleanup_job_spec.rb
+++ spec/workers/user_images/cleanup_job_spec.rb
@@ -1,9 +1,20 @@
 describe UserImages::CleanupJob do
-  subject! { described_class.new.perform user_image_id }
-  let(:user_image_id) { user_image.id }
   let(:user_image) { create :user_image }
+  let!(:some_model) { nil }
 
-  it do
-    expect { user_image.reload }.to raise_error ActiveRecord::RecordNotFound
+  subject! { described_class.new.perform user_image.id }
+
+  context 'not used anywhere' do
+    it do
+      expect { user_image.reload }.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
+
+  context 'used somewhere' do
+    let!(:some_model) { create :poll, text: "[image=#{user_image.id}]" }
+
+    it do
+      expect(user_image.reload).to be_persisted
+    end
   end
 end
