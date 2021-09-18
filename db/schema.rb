22fe265182c7359878eea97c34425d6c0229feb1
Andrey Sidorov <takandar@gmail.com>
2021-09-18 13:53:33 +0300

    rename review_moderator role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_140310) do
+ActiveRecord::Schema.define(version: 2021_09_18_105159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
