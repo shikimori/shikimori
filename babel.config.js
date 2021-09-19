40650c973abe0ed31f18880977a05d1086790537
Andrey Sidorov <takandar@gmail.com>
2021-09-19 13:15:46 +0300

    fix nasty webpack warning

--- babel.config.js
+++ babel.config.js
@@ -67,6 +67,7 @@ module.exports = function(api) {
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-proposal-private-methods', { loose: true }],
       ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
       [
         '@babel/plugin-transform-runtime',
         {
