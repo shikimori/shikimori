c6beaca0162f519e938e76479c3d3c83e4a23a3a
Andrey Sidorov <takandar@gmail.com>
2021-09-24 16:09:37 +0300

    wrap review content in inner block, fix check-height logic

--- app/packs/javascripts/dynamic_elements/review.js
+++ app/packs/javascripts/dynamic_elements/review.js
@@ -5,7 +5,21 @@ export default class Review extends Topic {
   _typeLabel() { return I18n.t('frontend.dynamic_elements.review.type_label'); } // eslint-disable-line camelcase
 
   initialize() {
+    this.MAX_PREVIEW_HEIGHT = 200;
+    this.COLLAPSED_HEIGHT = 150;
+
+    this.$body = this.$inner.children('.body');
+
     this._checkHeight();
     this.on('appear', this._appear);
+  }
+
+  _checkHeight() {
+    if (!this.$body.hasClass('check_height')) { return; }
+
+    this.$body.checkHeight({
+      maxHeight: this.MAX_PREVIEW_HEIGHT,
+      collapsedHeight: this.COLLAPSED_HEIGHT
+    });
   }
 }
