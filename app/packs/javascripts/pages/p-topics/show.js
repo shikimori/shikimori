c7f9808aec517abcb08af43dd10b1134b781482a
Andrey Sidorov <takandar@gmail.com>
2021-09-25 16:47:37 +0300

    bugfix

--- app/packs/javascripts/pages/p-topics/show.js
+++ app/packs/javascripts/pages/p-topics/show.js
@@ -4,7 +4,7 @@ pageLoad('topics_show', async () => {
   const $stars = $('.body-inner .critique-stars');
 
   if ($stars.length) {
-    const $firstImage = $().first();
+    const $firstImage = $('.body-inner img.b-poster').first();
 
     await loadImagesFinally('.body-inner');
     const imageOffset = $firstImage.offset();
