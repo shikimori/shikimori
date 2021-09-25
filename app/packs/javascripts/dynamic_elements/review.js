b68e85f5d62d5b8d029e042be8e27f35f9e32410
Andrey Sidorov <takandar@gmail.com>
2021-09-25 18:27:23 +0300

    misc styles update

--- app/packs/javascripts/dynamic_elements/review.js
+++ app/packs/javascripts/dynamic_elements/review.js
@@ -6,8 +6,8 @@ export default class Review extends Topic {
   _typeLabel() { return I18n.t('frontend.dynamic_elements.review.type_label'); } // eslint-disable-line camelcase
 
   initialize() {
-    this.MAX_PREVIEW_HEIGHT = 200;
-    this.COLLAPSED_HEIGHT = 150;
+    this.MAX_PREVIEW_HEIGHT = 220;
+    this.COLLAPSED_HEIGHT = 170;
 
     // data attribute is set in Topics.Tracker
     this.model = this.$node.data('model') || this._defaultModel();
