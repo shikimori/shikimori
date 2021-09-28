04044b4f55b2c2fed919b67c8a231473fa5a3df6
Andrey Sidorov <takandar@gmail.com>
2021-09-28 09:58:27 +0300

    handle mention in review thread properly

--- app/services/messages/mention_source.rb
+++ app/services/messages/mention_source.rb
@@ -1,8 +1,7 @@
-class Messages::MentionSource < ServiceObjectBase
+class Messages::MentionSource
   include Translation
 
-  pattr_initialize :linked, :comment_id
-  instance_cache :url, :text
+  method_object :linked, :comment_id
 
   def call
     i18n_t(
@@ -10,47 +9,59 @@ class Messages::MentionSource < ServiceObjectBase
       name: ERB::Util.h(linked_name),
       url: "#{linked_url}#{comment_hash}",
       bubble: link_bubble
-    ).html_safe # rubocop:disable OutputSafety
+    ).html_safe
   end
 
 private
 
   def i18n_key
-    case linked
+    case @linked
       when NilClass then :nil
       when Topic then :topic
       when User then :profile
-      else raise ArgumentError, "#{linked.class} #{linked.to_param}"
+      when Review then :review
+      else raise ArgumentError, "#{@linked.class} #{@linked.to_param}"
     end
   end
 
   def linked_name
-    case linked
+    case @linked
       when Topic
-        linked.respond_to?(:full_title) ? linked.full_title : linked.title
+        @linked.respond_to?(:full_title) ?
+          @linked.full_title :
+          @linked.title
 
       when User
         linked.nickname
+
+      when Review
+        linked.db_entry.name
     end
   end
 
   def linked_url
-    case linked
+    case @linked
       when NilClass then nil
-      when Topic then UrlGenerator.instance.topic_url linked
-      when User then UrlGenerator.instance.profile_url linked
-      when ClubPage then UrlGenerator.instance.topic_url linked
+      when Topic, ClubPage then UrlGenerator.instance.topic_url @linked
+      when User then UrlGenerator.instance.profile_url @linked
+      when Review
+        UrlGenerator.instance.send(
+          "#{@linked.db_entry.class.name.downcase}_review_url",
+          @linked.db_entry,
+          @linked
+        )
+      else raise ArgumentError, "#{@linked.class} #{@linked.to_param}"
     end
   end
 
   def comment_hash
-    "#comment-#{comment_id}" if comment_id
+    "#comment-#{@comment_id}" if @comment_id
   end
 
   def link_bubble
-    return unless comment_id
+    return unless @comment_id
 
     " class=\"bubbled b-link\" \
-data-href=\"#{UrlGenerator.instance.comment_url id: comment_id}\""
+data-href=\"#{UrlGenerator.instance.comment_url id: @comment_id}\""
   end
 end
