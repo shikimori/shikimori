a3e3cca3bf21e5b47519aee984378a8d46012b1c
Andrey Sidorov <takandar@gmail.com>
2021-09-28 19:39:42 +0300

    add review support to BbCodes::Tags::QuoteTag

--- app/services/bb_codes/tags/quote_tag.rb
+++ app/services/bb_codes/tags/quote_tag.rb
@@ -2,30 +2,15 @@
 class BbCodes::Tags::QuoteTag
   include Singleton
 
-  COMMENT_QUOTE_START_REGEXP = /
+  FORUM_ENTRY_QUOTE_START_REGEXP = /
     \[quote=
       (?<attrs>
-        c?(?<comment_id>\d+);
-        (?<user_id>\d+);
-        (?<nickname>[^\]]+)
-      )
-    \] \n?
-  /mix
-
-  MESSAGE_QUOTE_START_REGEXP = /
-    \[quote=
-      (?<attrs>
-        m(?<message_id>\d+);
-        (?<user_id>\d+);
-        (?<nickname>[^\]]+)
-      )
-    \] \n?
-  /mix
-
-  TOPIC_QUOTE_START_REGEXP = /
-    \[quote=
-      (?<attrs>
-        t(?<topic_id>\d+);
+        (:?
+          m(?<message_id>\d+) |
+          t(?<topic_id>\d+) |
+          r(?<review_id>\d+) |
+          c?(?<comment_id>\d+)
+        );
         (?<user_id>\d+);
         (?<nickname>[^\]]+)
       )
@@ -50,58 +35,25 @@ class BbCodes::Tags::QuoteTag
     quote_end(*
       simple_quote_1(*
       simple_quote_2(*
-      topic_quote(*
-      message_quote(*
-      comment_quote(*
+      forum_entry_quote(*
         text,
         0,
-        text))))))
+        text))))
   end
 
 private
 
-  def comment_quote text, replacements, original_text
-    result = text.gsub COMMENT_QUOTE_START_REGEXP do
+  def forum_entry_quote text, replacements, original_text
+    result = text.gsub FORUM_ENTRY_QUOTE_START_REGEXP do
       replacements += 1
       attrs = $LAST_MATCH_INFO[:attrs]
 
       "<div class='b-quote' data-attrs='#{ERB::Util.h attrs}'><div class='quoteable'>" +
         BbCodes::Quotes::QuoteableToBbcode.instance.call(
           comment_id: $LAST_MATCH_INFO[:comment_id],
-          user_id: $LAST_MATCH_INFO[:user_id],
-          nickname: $LAST_MATCH_INFO[:nickname]
-        ) +
-        "</div><div class='quote-content'>"
-    end
-
-    [result, replacements, original_text]
-  end
-
-  def message_quote text, replacements, original_text
-    result = text.gsub MESSAGE_QUOTE_START_REGEXP do
-      replacements += 1
-      attrs = $LAST_MATCH_INFO[:attrs]
-
-      "<div class='b-quote' data-attrs='#{ERB::Util.h attrs}'><div class='quoteable'>" +
-        BbCodes::Quotes::QuoteableToBbcode.instance.call(
           message_id: $LAST_MATCH_INFO[:message_id],
-          user_id: $LAST_MATCH_INFO[:user_id],
-          nickname: $LAST_MATCH_INFO[:nickname]
-        ) +
-        "</div><div class='quote-content'>"
-    end
-
-    [result, replacements, original_text]
-  end
-
-  def topic_quote text, replacements, original_text
-    result = text.gsub TOPIC_QUOTE_START_REGEXP do
-      replacements += 1
-      attrs = $LAST_MATCH_INFO[:attrs]
-
-      "<div class='b-quote' data-attrs='#{ERB::Util.h attrs}'><div class='quoteable'>" +
-        BbCodes::Quotes::QuoteableToBbcode.instance.call(
           topic_id: $LAST_MATCH_INFO[:topic_id],
+          review_id: $LAST_MATCH_INFO[:review_id],
           user_id: $LAST_MATCH_INFO[:user_id],
           nickname: $LAST_MATCH_INFO[:nickname]
         ) +
