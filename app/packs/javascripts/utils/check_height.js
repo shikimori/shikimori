ec8ae5176f02a086b6b7196d8dc98e0873992916
Andrey Sidorov <takandar@gmail.com>
2021-09-28 14:06:13 +0300

    b-height_shortened

--- app/packs/javascripts/utils/check_height.js
+++ app/packs/javascripts/utils/check_height.js
@@ -9,39 +9,39 @@ export default function checkHeight(
     placeholderHeight: 0
   }
 ) {
+  const $node = $(node);
+  if ($node.hasClass('b-height_shortened') || $node.height() <= options.maxHeight) { return; }
+
   const collapsedHeight = options.collapsedHeight ||
     Math.round((options.maxHeight * 2.0) / 3);
 
   const shadeHtml = options.isNoShade ? '' : '<div class=\'shade\'></div>';
   const placeholderHtml = options.placeholderHeight > 0 ?
-    `<div class='placeholder' style='height: ${options.placeholderHeight}px;'></div>` :
+    `<div class="placeholder" style="height: ${options.placeholderHeight}px;"></div>` :
     '';
   const expandHtml = (options.expandHtml != null) ?
     options.expandHtml :
-    `<div class='expand'>${placeholderHtml}<span>` +
+    `<div class="expand">${placeholderHtml}<span>` +
       `${I18n.t('frontend.dynamic_elements.check_height.expand')}</span></div>`;
 
-  const $node = $(node);
+  const marginBottom = parseInt($node.css('margin-bottom'));
+  const marginBottomHtml = marginBottom ? ` style="margin-bottom: ${marginBottom}px"` : '';
 
-  if (($node.height() > options.maxHeight) && !$node.hasClass('shortened')) {
-    const marginBottom = parseInt($node.css('margin-bottom'));
-    const html = '<div class=\'b-height_shortener\' ' +
-      `style='margin-bottom: ${marginBottom}px'>${shadeHtml}${expandHtml}</div>`;
+  const html = '<div class="b-height_shortener"' +
+    `${marginBottomHtml}>${shadeHtml}${expandHtml}</div>`;
 
-    $node
-      .addClass('shortened')
-      .css({ height: collapsedHeight });
+  $node
+    .addClass('b-height_shortened')
+    .css({ height: collapsedHeight });
 
-    $(html)
-      .insertAfter($node)
-      .on('click', e => {
-        if ((expandHtml != null) && !expandHtml) { return; }
+  $(html)
+    .insertAfter($node)
+    .on('click', e => {
+      if ((expandHtml != null) && !expandHtml) { return; }
 
-        $node.removeClass('shortened');
-        animatedExpand($node[0]);
+      $node.removeClass('b-height_shortened');
+      animatedExpand($node[0]);
 
-        $(e.currentTarget).remove();
-      });
-  }
+      $(e.currentTarget).remove();
+    });
 }
-
