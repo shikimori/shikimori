6702698ce6631e446538ede4a4abd33a813b92c5
Andrey Sidorov <takandar@gmail.com>
2021-10-01 21:17:51 +0300

    add event machine ssl silencer

---
+++ config/initializers/event_machine_ssl_silencer.rb
@@ -0,0 +1,13 @@
+# patch to silence Faye EventMachine ssl errors
+# until LetsEncrypt SSL 2020-09-30 problem is fixed
+class HttpStubConnectionFix
+  def ssl_verify_peer cert_string
+    true
+  end
+
+  def ssl_handshake_completed
+    true
+  end
+end
+
+EventMachine::HttpStubConnection.send :prepend, HttpStubConnectionFix
