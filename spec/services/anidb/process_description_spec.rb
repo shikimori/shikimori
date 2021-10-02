ed8d82bfd73bdcf5e89336f2b4be5162db291d96
Andrey Sidorov <takandar@gmail.com>
2021-10-02 14:32:00 +0300

    prevent filling empty anidb descriptions

--- spec/services/anidb/process_description_spec.rb
+++ spec/services/anidb/process_description_spec.rb
@@ -2,27 +2,32 @@ describe Anidb::ProcessDescription do
   subject { described_class.call value, anidb_url }
   let(:anidb_url) { 'http://anidb.net/perl-bin/animedb.pl?show=anime&aid=3395' }
 
+  context 'empty text' do
+    let(:value) { '' }
+    it { is_expected.to eq '' }
+  end
+
   context 'without source' do
     let(:value) { 'foo' }
-    it { is_expected.to eq("foo[source]#{anidb_url}[/source]") }
+    it { is_expected.to eq "foo[source]#{anidb_url}[/source]" }
   end
 
   context 'with empty source' do
     let(:value) { 'foo[source][/source]' }
-    it { is_expected.to eq("foo[source]#{anidb_url}[/source]") }
+    it { is_expected.to eq "foo[source]#{anidb_url}[/source]" }
   end
 
   context 'with source' do
     let(:value) { 'foo[source]bar[/source]' }
     it do
-      is_expected.to eq('foo[source]bar[/source]')
+      is_expected.to eq 'foo[source]bar[/source]'
     end
   end
 
   context 'with source ANN' do
     let(:value) { 'foo[source]ANN[/source]' }
     it do
-      is_expected.to eq('foo[source]animenewsnetwork.com[/source]')
+      is_expected.to eq 'foo[source]animenewsnetwork.com[/source]'
     end
   end
 end
