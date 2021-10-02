e8fbb3b83a10b3d32a52db492af139fa2f696ef6
Andrey Sidorov <takandar@gmail.com>
2021-10-02 17:34:06 +0300

    fix broken migrations

--- db/migrate/20210618115743_fix_comment_viewings_schema.rb
+++ db/migrate/20210618115743_fix_comment_viewings_schema.rb
@@ -1,5 +1,15 @@
 class FixCommentViewingsSchema < ActiveRecord::Migration[5.2]
   def up
+    execute %q[
+      delete from comment_viewings where user_id in (
+        select distinct(comment_viewings.user_id)
+          from comment_viewings
+          left join users
+            on users.id = comment_viewings.user_id
+          where users.id is null
+      )
+    ]
+
     change_column :comment_viewings, :user_id, :integer, null: false
     change_column :comment_viewings, :viewed_id, :integer, null: false
 
--- db/migrate/20210618120204_fix_topic_viewings_schema.rb
+++ db/migrate/20210618120204_fix_topic_viewings_schema.rb
@@ -1,5 +1,15 @@
 class FixTopicViewingsSchema < ActiveRecord::Migration[5.2]
   def up
+    execute %q[
+      delete from topic_viewings where user_id in (
+        select distinct(topic_viewings.user_id)
+          from topic_viewings
+          left join users
+            on users.id = topic_viewings.user_id
+          where users.id is null
+      )
+    ]
+
     change_column :topic_viewings, :user_id, :integer, null: false
     change_column :topic_viewings, :viewed_id, :integer, null: false
 
