483ad13e759ef83eedec3cb48ac700d262911f93
Andrey Sidorov <takandar@gmail.com>
2021-10-08 18:13:25 +0300

    refactoring AbuseRequestsService specs

--- spec/services/abuse_requests_service_spec.rb
+++ spec/services/abuse_requests_service_spec.rb
@@ -4,12 +4,24 @@ describe AbuseRequestsService do
       comment: comment,
       topic: topic,
       review: review,
-      reporter: user
+      reporter: reporter
     )
   end
-  let!(:user) { create :user, id: 99 }
+  # let!(:reporter) { create :user, id: 99 }
 
-  let(:comment) { create :comment }
+  let(:comment) do
+    create :comment, :skip_cancel_summary,
+      user: comment_user,
+      is_offtopic: is_offtopic,
+      is_summary: is_summary,
+      created_at: created_at
+  end
+  let(:is_offtopic) { false }
+  let(:is_summary) { false }
+  let(:created_at) { Time.zone.now }
+  let(:comment_user) { seed :user }
+  let(:reporter) { comment_user }
+
   let(:topic) { nil }
   let(:review) { nil }
 
@@ -17,10 +29,11 @@ describe AbuseRequestsService do
 
   describe '#offtopic' do
     subject(:act) { service.offtopic faye_token }
-    let(:comment) { create :comment, user: user }
 
-    it { expect { act }.to_not change AbuseRequest, :count }
-    it { is_expected.to eq [comment.id] }
+    it do
+      expect { act }.to_not change AbuseRequest, :count
+      is_expected.to eq [comment.id]
+    end
 
     describe 'offtopic?' do
       before { act }
@@ -29,21 +42,22 @@ describe AbuseRequestsService do
     end
 
     describe 'cancel' do
+      let(:is_offtopic) { true }
+
       context 'user' do
         context 'old comment' do
-          let(:comment) { create :comment, :offtopic, user: user, created_at: 1.month.ago }
+          let(:created_at) { 1.month.ago }
           it { expect { act }.to change(AbuseRequest, :count).by 1 }
         end
 
         context 'new comment' do
-          let(:comment) { create :comment, :offtopic, user: user }
           it { expect { act }.to_not change AbuseRequest, :count }
         end
       end
 
       context 'moderator' do
-        let(:user) { create :user, :forum_moderator }
-        let(:comment) { create :comment, :offtopic, user: user, created_at: 1.month.ago }
+        let(:created_at) { 1.month.ago }
+        let(:reporter) { create :user, :forum_moderator }
         it { expect { act }.to change(AbuseRequest, :count).by 0 }
       end
     end
@@ -51,7 +65,6 @@ describe AbuseRequestsService do
 
   describe '#summary' do
     subject(:act) { service.summary faye_token }
-    let(:comment) { create :comment, user: user, created_at: created_at }
 
     context 'new comment' do
       let(:created_at) { 4.minutes.ago }
@@ -63,7 +76,7 @@ describe AbuseRequestsService do
       end
 
       describe 'cancel' do
-        let(:comment) { create :comment, :summary, :skip_cancel_summary, user: user }
+        let(:is_summary) { true }
 
         it do
           expect { act }.to_not change AbuseRequest, :count
@@ -75,6 +88,7 @@ describe AbuseRequestsService do
 
     context 'old comment' do
       let(:created_at) { 6.minutes.ago }
+
       it do
         expect { act }.to change AbuseRequest, :count
         is_expected.to eq []
@@ -83,32 +97,32 @@ describe AbuseRequestsService do
     end
   end
 
-  %i[summary offtopic abuse spoiler].each do |method|
-    describe method.to_s do
-      if %i[summary offtopic].include? method # rubocop:disable CollectionLiteralInLoop
-        let(:reason) { nil }
-        subject(:act) { service.send method, faye_token }
-      else
-        let(:reason) { 'zxcvbn' }
-        subject(:act) { service.send method, reason }
-      end
-
-      let(:user) { create :user, id: 99 }
-
-      it { expect { act }.to change(AbuseRequest, :count).by 1 }
-      it { is_expected.to eq [] }
-
-      describe 'abuse_request' do
-        before { act }
-        subject { user.abuse_requests.last }
-
-        it { expect(subject).to have_attributes kind: method.to_s, value: true, comment_id: comment.id, reason: reason }
-      end
-
-      context 'already acted' do
-        before { act }
-        it { expect { act }.to change(AbuseRequest, :count).by 0 }
-      end
-    end
-  end
+  # %i[summary offtopic abuse spoiler].each do |method|
+  #   describe method.to_s do
+  #     if %i[summary offtopic].include? method # rubocop:disable CollectionLiteralInLoop
+  #       let(:reason) { nil }
+  #       subject(:act) { service.send method, faye_token }
+  #     else
+  #       let(:reason) { 'zxcvbn' }
+  #       subject(:act) { service.send method, reason }
+  #     end
+  # 
+  #     let(:user) { create :user, id: 99 }
+  # 
+  #     it { expect { act }.to change(AbuseRequest, :count).by 1 }
+  #     it { is_expected.to eq [] }
+  # 
+  #     describe 'abuse_request' do
+  #       before { act }
+  #       subject { user.abuse_requests.last }
+  # 
+  #       it { expect(subject).to have_attributes kind: method.to_s, value: true, comment_id: comment.id, reason: reason }
+  #     end
+  # 
+  #     context 'already acted' do
+  #       before { act }
+  #       it { expect { act }.to change(AbuseRequest, :count).by 0 }
+  #     end
+  #   end
+  # end
 end
