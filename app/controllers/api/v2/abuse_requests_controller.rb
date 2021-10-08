204345245438f83b0e4c83d14e2cd935afd78a95
Andrey Sidorov <takandar@gmail.com>
2021-10-08 22:25:11 +0300

    add support of topics and reviews to Api::V2::AbuseRequestsController

--- app/controllers/api/v2/abuse_requests_controller.rb
+++ app/controllers/api/v2/abuse_requests_controller.rb
@@ -1,15 +1,17 @@
 # TODO: remove `unless params[:user_id]` after 01-09-2017
 class Api::V2::AbuseRequestsController < Api::V2Controller
   before_action :authenticate_user!, only: %i[offtopic summary spoiler abuse]
+  before_action :fetch_entries
 
   api :POST, '/v2/abuse_requests/offtopic', 'Mark comment as offtopic'
   param :comment_id, :number, required: true
   description 'Request will be sent to moderators.'
   def offtopic
-    comment = Comment.find params[:comment_id]
-    ids = AbuseRequestsService.new(comment, current_user).offtopic(faye_token)
+    ids = AbuseRequestsService
+      .new(comment: @comment, reporter: current_user)
+      .offtopic(faye_token)
 
-    respond_with result(comment, ids)
+    respond_with result(@comment, ids)
   rescue ActiveRecord::RecordNotSaved => e
     render json: e.record.errors.full_messages, status: :unprocessable_entity
   end
@@ -18,46 +20,59 @@ class Api::V2::AbuseRequestsController < Api::V2Controller
   param :comment_id, :number, required: true
   description 'Request will be sent to moderators.'
   def summary
-    comment = Comment.find params[:comment_id]
-    ids = AbuseRequestsService.new(comment, current_user).summary(faye_token)
+    ids = AbuseRequestsService
+      .new(comment: @comment, reporter: current_user)
+      .summary(faye_token)
 
-    respond_with result(comment, ids)
+    respond_with result(@comment, ids)
   rescue ActiveRecord::RecordNotSaved => e
     render json: e.record.errors.full_messages, status: :unprocessable_entity
   end
 
   api :POST, '/v2/abuse_requests/abuse', 'Create abuse about violation of site rules'
-  param :comment_id, :number, required: true
+  param :comment_id, :number, required: false
+  param :topic_id, :number, required: false
+  param :review_id, :number, required: false
   param :reason, String, required: false
   description 'Request will be sent to moderators.'
   def abuse
-    comment = Comment.find params[:comment_id]
-    ids = AbuseRequestsService.new(comment, current_user).abuse params[:reason]
+    ids = AbuseRequestsService
+      .new(comment: @comment, review: @review, topic: @topic, reporter: current_user)
+      .abuse(params[:reason])
 
-    respond_with result(comment, ids)
+    respond_with result(@comment || @review || @topic, ids)
   rescue ActiveRecord::RecordNotSaved => e
     render json: e.record.errors.full_messages, status: :unprocessable_entity
   end
 
-  api :POST, '/v2/abuse_requests/spoiler', 'Create abuse spoiler content in comment'
-  param :comment_id, :number, required: true
+  api :POST, '/v2/abuse_requests/spoiler', 'Create abuse about spoiler in content'
+  param :comment_id, :number, required: false
+  param :topic_id, :number, required: false
+  param :review_id, :number, required: false
   param :reason, String, required: false
   description 'Request will be sent to moderators.'
   def spoiler
-    comment = Comment.find params[:comment_id]
-    ids = AbuseRequestsService.new(comment, current_user).spoiler params[:reason]
+    ids = AbuseRequestsService
+      .new(comment: @comment, review: @review, topic: @topic, reporter: current_user)
+      .spoiler(params[:reason])
 
-    respond_with result(comment, ids)
+    respond_with result(@comment || @review || @topic, ids)
   rescue ActiveRecord::RecordNotSaved => e
     render json: e.record.errors.full_messages, status: :unprocessable_entity
   end
 
 private
 
-  def result comment, ids
+  def fetch_entries
+    @comment = Comment.find params[:comment_id] if params[:comment_id].present?
+    @review = Review.find params[:review_id] if params[:revie_id].present?
+    @topic = Topic.find params[:topic_id] if params[:topic_id].present?
+  end
+
+  def result model, ids
     {
       kind: params[:action],
-      value: comment.try(:"#{params[:action]}?") || false,
+      value: model.try(:"#{params[:action]}?") || false,
       affected_ids: ids
     }
   end
