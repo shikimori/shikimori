5b891bc5941d885506b22bb7bb4069f8f1b1bdce
Andrey Sidorov <takandar@gmail.com>
2021-10-10 14:27:09 +0300

    create Work of Life genre

---
+++ db/migrate/20211010100154_add_work_genre.rb
@@ -0,0 +1,20 @@
+class AddWorkGenre < ActiveRecord::Migration[5.2]
+  def up
+    return if Rails.env.test?
+
+    %i[anime manga].each_with_index do |kind, index|
+      Genre.create!(
+        name: 'Work Life',
+        russian: 'Работа',
+        position: Genre.find_by(name: 'Slice of Life', kind: kind),
+        kind: kind,
+        seo: 99,
+        mal_id: 48
+      )
+    end
+  end
+
+  def down
+    Genre.where(mal_id: 48).destroy_all
+  end
+end
