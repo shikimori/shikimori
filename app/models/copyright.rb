66d35e03c7949c73779c692f7dac220118fccc83
Andrey Sidorov <takandar@gmail.com>
2021-10-13 09:22:59 +0300

    allow to upload screenshots for previously for animes in Copyright::ANIME_SCREENSHOTS

--- app/models/copyright.rb
+++ app/models/copyright.rb
@@ -2,7 +2,8 @@
 # модель для сбора id аниме и манги, заблокированных требованиями копирайта
 module Copyright
   # by Hetzner abuse team
-  ANIME_SCREENSHOTS = [28215, 23587]
+  # ANIME_SCREENSHOTS = [28215, 23587]
+  ANIME_SCREENSHOTS = []
   ANIME_VIDEOS = ANIME_SCREENSHOTS
 
   # by Hetzner abuse team
