18014f23857ca0a4118c5f78f84dcd625d624eaf
Andrey Sidorov <takandar@gmail.com>
2022-01-05 21:41:14 +0300

    split specs

--- spec/services/bb_codes/quotes/replace_spec.rb
+++ spec/services/bb_codes/quotes/replace_spec.rb
@@ -9,50 +9,41 @@ describe BbCodes::Quotes::Replace do
   let(:from_reply) { build_stubbed :comment, id: 99999 }
   let(:to_reply) { build_stubbed :topic, id: 88888 }
 
-  [
-    # invalid replacements
+  describe 'invalid replacements' do
     [
       '[quote=99999]',
-      '[quote=99999]'
-    ],
-    [
       '[quote=100000;11111;test]',
-      '[quote=100000;11111;test]'
-    ],
-    [
       '[comment=100000]',
-      '[comment=100000]'
-    ],
-    [
-      '>?c100000;11111;test',
       '>?c100000;11111;test'
-    ],
+    ].each do |original_text|
+      context "\"#{original_text}\"" do
+        let(:text) { original_text }
+        it { is_expected.to eq text }
+      end
+    end
+  end
 
-    # valid replacements
-    [
+  describe 'valid replacements' do
+    [[
       '[quote=99999;11111;test]',
       '[quote=t88888;11111;test]'
-    ],
-    [
+    ], [
       '[quote=c99999;11111;test]',
       '[quote=t88888;11111;test]'
-    ],
-    [
+    ], [
       '[comment=99999]',
       '[topic=88888]'
-    ],
-    [
+    ], [
       '[comment=99999;1',
       '[topic=88888;1'
-    ],
-    [
+    ], [
       '>?c99999;11111;test',
       '>?t88888;11111;test'
-    ]
-  ].each do |(original_text, replaced_text)|
-    context "\"#{original_text}\" => \"#{replaced_text}\"" do
-      let(:text) { original_text }
-      it { is_expected.to eq replaced_text }
+    ]].each do |(original_text, replaced_text)|
+      context "\"#{original_text}\" => \"#{replaced_text}\"" do
+        let(:text) { original_text }
+        it { is_expected.to eq replaced_text }
+      end
     end
   end
 end
