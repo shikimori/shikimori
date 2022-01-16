595a5281ef56080e0ac85e5781e12043c90f6834
Andrey Sidorov <takandar@gmail.com>
2022-01-16 16:11:58 +0300

    fixing review topic generation issues

--- app/models/topics/entry_topics/review_topic.rb
+++ app/models/topics/entry_topics/review_topic.rb
@@ -1,2 +1,16 @@
-class Topics::EntryTopics::ReviewTopic < Topics::EntryTopic
+class Topics::EntryTopics::ReviewTopic < Topics::EntryTopics::CritiqueTopic
+  def title
+    linked.db_entry.name
+  end
+
+  def full_title
+    first_key = linked_type.underscore
+    second_key = linked.db_entry.class.name.underscore
+
+    I18n.t(
+      "topics/entry_topic.full_title.#{first_key}.#{second_key}",
+      target_name: title,
+      author: linked.user.nickname
+    )
+  end
 end
