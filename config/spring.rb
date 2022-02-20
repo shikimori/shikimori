2f3a1bccfa2e6c338544309e5d7c007844097937
Andrey Sidorov <takandar@gmail.com>
2022-02-20 20:48:12 +0300

    fix spring watcher

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,22 @@
-%w[
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-].each { |path| Spring.watch(path) }
+BLACKLIST = %w[
+  .git
+  log
+  node_modules
+  public
+  tmp
+]
+
+module SpringWatcherListenIgnorer
+  def start
+    super
+    listener.ignore Regexp.new(BLACKLIST.map { |v| "^#{v}" }.join('|'))
+  end
+end
+Spring::Watcher::Listen.prepend SpringWatcherListenIgnorer
+
+Spring.watch(
+  '.ruby-version',
+  '.rbenv-vars',
+  'tmp/restart.txt',
+  'tmp/caching-dev.txt'
+)
