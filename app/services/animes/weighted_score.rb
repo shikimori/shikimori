cc409a64d6cb5fe21da990f56554b275d9b019f8
Andrey Sidorov <takandar@gmail.com>
2022-03-13 22:44:13 +0300

    Animes::WeightedScore specs

--- app/services/animes/weighted_score.rb
+++ app/services/animes/weighted_score.rb
@@ -4,11 +4,11 @@ class Animes::WeightedScore
   MIN_SCORES = 30
 
   def call
-    return 0 if number_of_scores < MIN_SCORES
+    return 0 if @number_of_scores < MIN_SCORES
 
     (
-      ((number_of_scores.to_f / (number_of_scores + MIN_SCORES)) * average_user_score) +
-        ((MIN_SCORES.to_f / (number_of_scores + MIN_SCORES)) * @global_average)
+      ((@number_of_scores.to_f / (@number_of_scores + MIN_SCORES)) * @average_user_score) +
+        ((MIN_SCORES.to_f / (@number_of_scores + MIN_SCORES)) * @global_average)
     ).round 2
   end
 end
