76b52096ca703e4e3fa5ea61ed91b28785dae4d1
Andrey Sidorov <takandar@gmail.com>
2022-03-23 23:39:10 +0300

    additional test for Proxies::Check

--- spec/services/proxies/check_spec.rb
+++ spec/services/proxies/check_spec.rb
@@ -5,4 +5,9 @@ describe Proxies::Check, :vcr do
     let(:proxy) { Proxy.new ip: '51.158.169.52', port: 29976 }
     it { is_expected.to eq true }
   end
+
+  context 'not transparent proxy' do
+    let(:proxy) { Proxy.new ip: '51.158.169.52', port: 29976 }
+    it { is_expected.to eq false }
+  end
 end
