bbc4344b53247348ba4edb90566884cbff3e52b6
Andrey Sidorov <takandar@gmail.com>
2022-04-01 00:16:13 +0300

    add howto update note

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,2 +1,3 @@
+# NOTE: howto upgrade to 0.10x https://github.com/rails-api/active_model_serializers/issues/2028
 # https://www.rubydoc.info/gems/active_model_serializers
 ActiveModelSerializers.config.adapter = :attributes
