a3a65eec492de4cb2583cf8971994bbb347f3457
Andrey Sidorov <takandar@gmail.com>
2022-04-07 14:34:43 +0300

    remove is_summary from comments

---
+++ db/migrate/20220407113355_remove_is_summary_from_comments.rb
@@ -0,0 +1,7 @@
+class RemoveIsSummaryFromComments < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :comments, :is_summary, :boolean,
+      default: false,
+      null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_163320) do
+ActiveRecord::Schema.define(version: 2022_04_07_113355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -373,7 +373,6 @@ ActiveRecord::Schema.define(version: 2022_03_31_163320) do
     t.integer "user_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean "is_summary", default: false, null: false
     t.boolean "is_offtopic", default: false, null: false
     t.index ["commentable_id"], name: "index_comments_on_commentable_id"
     t.index ["created_at"], name: "index_comments_on_created_at"
