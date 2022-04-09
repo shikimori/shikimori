1024ac468adb9c9022739919cc873d41f0a7ec93
Andrey Sidorov <takandar@gmail.com>
2022-04-09 22:41:33 +0300

    bring back open struct formatter

--- config/initializers/amazing_print.rb
+++ config/initializers/amazing_print.rb
@@ -1,8 +1,8 @@
-# class AwesomePrint::Formatter
-#   send :remove_const, :CORE_FORMATTERS
-#   CORE_FORMATTERS = [:array, :bigdecimal, :class, :dir, :file, :hash, :method, :rational, :set, :struct, :unboundmethod, :openstruct]
-#
-#   def awesome_openstruct(s)
-#     awesome_hash(s.marshal_dump)
-#   end
-# end
+class AmazingPrint::Formatter
+  send :remove_const, :CORE_FORMATTERS
+  CORE_FORMATTERS = [:array, :bigdecimal, :class, :dir, :file, :hash, :method, :rational, :set, :struct, :unboundmethod, :openstruct]
+
+  def amazing_openstruct(s)
+    amazing_hash(s.marshal_dump)
+  end
+end
