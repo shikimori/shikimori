7c313758f17d28cfd6dacf8015101292c66a2c04
Andrey Sidorov <takandar@gmail.com>
2022-04-19 23:23:38 +0300

    fixed last broken spec

--- app/controllers/moderations/versions_controller.rb
+++ app/controllers/moderations/versions_controller.rb
@@ -29,7 +29,8 @@ class Moderations::VersionsController < ModerationsController
 
   def create
     if @resource.save
-      @resource.accept current_user if can? :accept, @resource
+      @resource.accept! moderator: current_user if can? :accept, @resource
+
       redirect_back(
         fallback_location: @resource.item.decorate.url,
         notice: i18n_t("version_#{@resource.state}")
