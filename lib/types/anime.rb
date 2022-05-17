74b4b0894b25d41207411b92f13a271f31bfdb3d
Yokitori <andrew.undream@gmail.com>
2022-05-17 23:16:44 +0300

    add filter option to animes

--- lib/types/anime.rb
+++ lib/types/anime.rb
@@ -15,14 +15,22 @@ module Types
       .constructor(&:to_sym)
       .enum(:none, :g, :pg, :pg_13, :r, :r_plus, :rx)
 
-    OPTIONS = %i[
+    options = %i[
       strict_torrent_name_match
       disabled_torrents_sync
       disabled_anime365_sync
     ]
 
+    # entry rates from 1 to 10
+    (1..10).each do |score|
+      # possible percentages to filter
+      (1..100).each do |percent|
+        options << "score_filter_#{score}_#{percent}"
+      end
+    end
+
     Options = Types::Strict::Symbol
       .constructor(&:to_sym)
-      .enum(*OPTIONS)
+      .enum(*options)
   end
 end
