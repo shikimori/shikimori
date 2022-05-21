9cb3657f1666d318e2aa8a09385adb7382351d94
Yokitori <andrew.undream@gmail.com>
2022-05-22 00:05:23 +0300

    fix margins and js code, mobile view

--- app/packs/javascripts/pages/p-topics/show.js
+++ app/packs/javascripts/pages/p-topics/show.js
@@ -1,15 +1,15 @@
 import { loadImagesFinally } from '@/utils/load_image';
 
-pageLoad('topics_show', async () => {
+pageLoad('critiques_show', async () => {
   const $stars = $('.body-inner .critique-stars');
 
   if ($stars.length) {
-    const $firstImage = $('.body-inner img.b-poster').first();
+    const $firstImage = $('.body-inner .b-poster').first();
 
     await loadImagesFinally('.body-inner');
     const imageOffset = $firstImage.offset();
 
-    if (imageOffset && imageOffset.top === ($stars.offset().top + $stars.outerHeight())) {
+    if (imageOffset && (imageOffset.top - ($stars.offset().top + $stars.outerHeight())) < 15) {
       $firstImage.addClass('critique-poster');
     }
   }
