d13293368fbb45549f63a1a06a7f8497811e6309
Yokitori <andrew.undream@gmail.com>
2022-05-21 19:16:20 +0300

    fix refresh score spec

--- spec/operations/anime/refresh_score_spec.rb
+++ spec/operations/anime/refresh_score_spec.rb
@@ -11,17 +11,20 @@ describe Anime::RefreshScore do
   let(:current_score) { 5.0 }
   let(:global_average) { 8.0 }
 
-  Animes::WeightedScore::MIN_SCORES.times do |i|
+  scores_count = Animes::WeightedScore::MIN_SCORES * 2
+  let(:scores_count) { Animes::WeightedScore::MIN_SCORES * 2 }
+
+  scores_count.times do |i|
     let(:"user_#{i + 1}") { create :user }
   end
 
-  Animes::WeightedScore::MIN_SCORES.times do |i|
+  scores_count.times do |i|
     let!(:"anime_rate_#{i + 1}".to_sym) do
       create :user_rate,
         target: anime,
         status: :completed,
         score: 9,
-        user: send(:"user_#{i + 1}") # user_1
+        user: send(:"user_#{i + 1}")
     end
   end
 
@@ -40,7 +43,7 @@ describe Anime::RefreshScore do
       expect(Animes::WeightedScore)
         .to have_received(:call)
         .with(
-          number_of_scores: Animes::WeightedScore::MIN_SCORES,
+          number_of_scores: scores_count,
           average_user_score: 9,
           global_average: global_average
         )
@@ -63,10 +66,53 @@ describe Anime::RefreshScore do
       expect(Animes::WeightedScore)
         .to have_received(:call)
         .with(
-          number_of_scores: Animes::WeightedScore::MIN_SCORES,
+          number_of_scores: scores_count,
           average_user_score: 9,
           global_average: global_average
         )
+    end
+  end
+
+  context 'filtered score 9 by 50%' do
+    before { anime.update(options: ['score_filter_9_50']) }
+    let(:new_score) { 1.3 }
+
+    it do
+      expect { subject }.to change(anime, :score_2).to new_score
+      expect(anime.reload.updated_at).to be_within(0.1).of Time.zone.now
+      expect(Animes::WeightedScore)
+        .to have_received(:call)
+        .with(
+          number_of_scores: scores_count / 2,
+          average_user_score: 9,
+          global_average: global_average
+        )
+    end
+  end
+
+  context 'filtered score 9 by 75%' do
+    before { anime.update(options: ['score_filter_9_75']) }
+    let(:new_score) { 1.3 }
+
+    it do
+      expect { subject }.to change(anime, :score_2).to new_score
+      expect(anime.reload.updated_at).to be_within(0.1).of Time.zone.now
+      expect(Animes::WeightedScore)
+        .to have_received(:call)
+        .with(
+          number_of_scores: scores_count / 4,
+          average_user_score: 9,
+          global_average: global_average
+        )
+    end
+  end
+
+  context 'filtered score 9 by 100%' do
+    before { anime.update(options: ['score_filter_9_100']) }
+    let(:new_score) { current_score }
+
+    it do
+      expect { subject }.to_not change(anime, :score_2)
     end
   end
 end
