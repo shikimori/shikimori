29354b9523107d73a17e32a51f449196d6719ee9
Yokitori <andrew.undream@gmail.com>
2022-05-25 14:22:05 +0300

    Revert "add filter option to animes"
    This reverts commit 74b4b0894b25d41207411b92f13a271f31bfdb3d.

--- app/operations/anime/refresh_score.rb
+++ app/operations/anime/refresh_score.rb
@@ -5,7 +5,7 @@ class Anime::RefreshScore
     new_score = @entry.anons? ?
       0 :
       Animes::WeightedScore.call(
-        number_of_scores: user_rates_scope.count,
+        number_of_scores: user_rates_scope.size,
         average_user_score: user_rates_scope.average(:score),
         global_average: @global_average
       )
@@ -16,37 +16,10 @@ class Anime::RefreshScore
 private
 
   def user_rates_scope
-    if filter_options.any?
-      filtered_user_rates_scope
-    else
-      all_user_rates_scope
-    end
-  end
-
-  def all_user_rates_scope
     UserRate
       .joins('JOIN users ON users.id = user_rates.user_id')
       .where(target_id: @entry.id, target_type: @entry.class.base_class.name)
       .where('score > 0')
       .where.not(user_id: User.excluded_from_statistics)
-  end
-
-  def filtered_user_rates_scope
-    @rates = all_user_rates_scope
-    filter_options.each { |option| filter_rates_by(option) }
-    @rates
-  end
-
-  def filter_options
-    @entry.options.filter { |option| option.include? 'score_filter_' }
-  end
-
-  def filter_rates_by(option)
-    score_to_filter = option.gsub('score_filter_', '').split('_').first.to_i
-    filter_percentage = option.gsub('score_filter_', '').split('_').second.to_i
-    rates_to_filter = @rates.where(score: score_to_filter)
-    filtered_rates_count = (rates_to_filter.count * filter_percentage / 100).to_i
-    filtered_rates = rates_to_filter.limit(filtered_rates_count)
-    @rates = @rates.where.not(id: filtered_rates.select(:id)) if filtered_rates.any?
   end
 end
--- lib/types/anime.rb
+++ lib/types/anime.rb
@@ -15,22 +15,14 @@ module Types
       .constructor(&:to_sym)
       .enum(:none, :g, :pg, :pg_13, :r, :r_plus, :rx)
 
-    options_list = %i[
+    OPTIONS = %i[
       strict_torrent_name_match
       disabled_torrents_sync
       disabled_anime365_sync
     ]
 
-    # entry rates from 1 to 10
-    (1..10).each do |score|
-      # possible percentages to filter
-      (1..100).each do |percent|
-        options_list << "score_filter_#{score}_#{percent}"
-      end
-    end
-
     Options = Types::Strict::Symbol
       .constructor(&:to_sym)
-      .enum(*options_list)
+      .enum(*OPTIONS)
   end
 end
