d45a50d32de00ab6774b62908a0d2b2485f0ac0c
Andrey Sidorov <takandar@gmail.com>
2022-05-25 22:40:22 +0300

    refactor spec

--- spec/operations/anime/refresh_score_spec.rb
+++ spec/operations/anime/refresh_score_spec.rb
@@ -5,37 +5,32 @@ describe Anime::RefreshScore do
   let(:anime) do
     create :anime, status,
       score_2: current_score,
-      updated_at: 10.minutes.ago
+      updated_at: 10.minutes.ago,
+      options: options
   end
   let(:status) { %i[released ongoing].sample }
   let(:current_score) { 5.0 }
   let(:global_average) { 8.0 }
+  let(:options) { [] }
 
-  scores_count = Animes::WeightedScore::MIN_SCORES * 2
-  let(:scores_count) { Animes::WeightedScore::MIN_SCORES * 2 }
+  let(:scores_count) { 2 }
 
-  scores_count.times do |i|
-    let(:"user_#{i + 1}") { create :user }
-  end
-
-  scores_count.times do |i|
-    let!(:"anime_rate_#{i + 1}".to_sym) do
+  before do
+    scores_count.times do
       create :user_rate,
         target: anime,
         status: :completed,
         score: 9,
-        user: send(:"user_#{i + 1}")
+        user: create(:user)
     end
-  end
 
-  before do
     allow(Animes::WeightedScore)
       .to receive(:call)
       .and_return new_score
   end
+  let(:new_score) { 1.3 }
 
   context 'score has changed' do
-    let(:new_score) { 1.3 }
     it do
       expect { subject }.to change(anime, :score_2).to new_score
 
@@ -73,46 +68,46 @@ describe Anime::RefreshScore do
     end
   end
 
-  context 'filtered score 9 by 50%' do
-    before { anime.update(options: ['score_filter_9_50']) }
-    let(:new_score) { 1.3 }
+  describe 'anicheat filter' do
+    let(:scores_count) { 4 }
 
-    it do
-      expect { subject }.to change(anime, :score_2).to new_score
-      expect(anime.reload.updated_at).to be_within(0.1).of Time.zone.now
-      expect(Animes::WeightedScore)
-        .to have_received(:call)
-        .with(
-          number_of_scores: scores_count / 2,
-          average_user_score: 9,
-          global_average: global_average
-        )
+    context 'filtered score 9 by 50%' do
+      let(:options) { ['score_filter_9_50'] }
+
+      it do
+        expect { subject }.to change(anime, :score_2).to new_score
+        expect(Animes::WeightedScore)
+          .to have_received(:call)
+          .with(
+            number_of_scores: scores_count / 2,
+            average_user_score: 9,
+            global_average: global_average
+          )
+      end
     end
-  end
 
-  context 'filtered score 9 by 75%' do
-    before { anime.update(options: ['score_filter_9_75']) }
-    let(:new_score) { 1.3 }
+    context 'filtered score 9 by 75%' do
+      let(:options) { ['score_filter_9_75'] }
 
-    it do
-      expect { subject }.to change(anime, :score_2).to new_score
-      expect(anime.reload.updated_at).to be_within(0.1).of Time.zone.now
-      expect(Animes::WeightedScore)
-        .to have_received(:call)
-        .with(
-          number_of_scores: scores_count / 4,
-          average_user_score: 9,
-          global_average: global_average
-        )
+      it do
+        expect { subject }.to change(anime, :score_2).to new_score
+        expect(Animes::WeightedScore)
+          .to have_received(:call)
+          .with(
+            number_of_scores: scores_count / 4,
+            average_user_score: 9,
+            global_average: global_average
+          )
+      end
     end
-  end
 
-  context 'filtered score 9 by 100%' do
-    before { anime.update(options: ['score_filter_9_100']) }
-    let(:new_score) { current_score }
+    context 'filter out all scores' do
+      before { anime.update(options: ['score_filter_9_100']) }
+      let(:new_score) { current_score }
 
-    it do
-      expect { subject }.to_not change(anime, :score_2)
+      it do
+        expect { subject }.to_not change(anime, :score_2)
+      end
     end
   end
 end
