b1d43c8f68a0bbf2da50783a4dd1d5031a198e5e
Yokitori <andrew.undream@gmail.com>
2022-06-05 23:07:08 +0300

    remove boolean logic

--- app/services/comments/has_long_content.rb
+++ app/services/comments/has_long_content.rb
@@ -2,10 +2,10 @@ class Comments::HasLongContent
   method_object :comment
 
   def call
-    (comment.body.size > 500 ||
-      comment.body.include?('[poster') ||
-      comment.body =~ /\[ima?g/ ||
-      comment.body.count("\n") > 5
-    ) ? true : false # important to not return size causing bugs in height shortener
+    (comment.body.size > 500) ||
+    comment.body.include?('[img') ||
+    comment.body.include?('[imag') ||
+    comment.body.include?('[poster') ||
+    (comment.body.count("\n") > 5)
   end
 end
