c7e588d81cbb7ea289e2f4a1c1439182098bdab0
Yokitori <andrew.undream@gmail.com>
2022-06-05 22:17:08 +0300

    Revert "add migration to update collections wuth [SPOILERS] mark"
    This reverts commit 14a0db9d9ee2354d9b97837d05f8b42a4bb4ef56.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_155125) do
+ActiveRecord::Schema.define(version: 2022_06_01_082939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
