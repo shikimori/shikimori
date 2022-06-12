df8894ca82ff48175b99e22be84eb8fae145288e
Andrey Sidorov <takandar@gmail.com>
2022-06-12 14:34:21 +0300

    fix formatting

--- app/services/comments/has_long_content.rb
+++ app/services/comments/has_long_content.rb
@@ -2,10 +2,10 @@ class Comments::HasLongContent
   method_object :comment
 
   def call
-    (comment.body.size > 500) ||
-    comment.body.include?('[img') ||
-    comment.body.include?('[imag') ||
-    comment.body.include?('[poster') ||
-    (comment.body.count("\n") > 5)
+    @comment.body.size > 500 ||
+      @comment.body.include?('[img') ||
+      @comment.body.include?('[imag') ||
+      @comment.body.include?('[poster') ||
+      @comment.body.count("\n") > 5
   end
 end
