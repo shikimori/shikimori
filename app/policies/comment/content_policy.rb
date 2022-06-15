84fdf2c1b154dbd3154aa2a6f3b72d2c93652374
Andrey Sidorov <takandar@gmail.com>
2022-06-15 16:54:26 +0300

    Comment::ContentPolicy

--- app/policies/comment/content_policy.rb
+++ app/policies/comment/content_policy.rb
@@ -1,11 +1,13 @@
-class Comments::HasLongContent
-  method_object :comment
+class Comment::ContentPolicy
+  static_facade :check_height?, :comment
 
-  def call
-    @comment.body.size > 500 ||
+  def check_height?
+    @comment.persisted? && (
+      @comment.body.size > 500 ||
       @comment.body.include?('[img') ||
       @comment.body.include?('[imag') ||
       @comment.body.include?('[poster') ||
       @comment.body.count("\n") > 5
+    )
   end
 end
