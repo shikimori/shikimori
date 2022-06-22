bc56cfbafcf516154467840584eb31b973a81ff3
Yokitori <andrew.undream@gmail.com>
2022-06-22 20:31:54 +0300

    remove old migrations

--- db/migrate/20220525153844_add_has_spoilers_to_collections.rb
+++
@@ -1,5 +0,0 @@
-class AddHasSpoilersToCollections < ActiveRecord::Migration[6.1]
-  def change
-    add_column :collections, :has_spoilers, :boolean
-  end
-end
--- db/migrate/20220605155125_set_has_spoilers_to_collections.rb
+++
@@ -1,9 +0,0 @@
-class SetHasSpoilersToCollections < ActiveRecord::Migration[6.1]
-  def up
-    Collection.where("name ILIKE '%[СПОЙЛЕРЫ]%'").update_all(has_spoilers: true)
-  end
-
-  def down
-    Collection.where("name ILIKE '%[СПОЙЛЕРЫ]%'").update_all(has_spoilers: false)
-  end
-end
