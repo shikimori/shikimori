7fa6e99f9422466599998007595d51332606dfdc
Andrey Sidorov <takandar@gmail.com>
2022-06-22 21:21:56 +0300

    changes in Animes::TrackStatusChanges logic: not it does not rollback release when all anime episodes are aired

--- spec/services/animes/track_status_changes_spec.rb
+++ spec/services/animes/track_status_changes_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Animes::TrackStatusChanges do
-  let!(:news_topic) {}
+  let!(:news_topic) { nil }
   before { anime.assign_attributes status: new_status }
   subject! { described_class.call anime }
 
@@ -43,13 +43,22 @@ describe Animes::TrackStatusChanges do
   end
 
   describe 'rollback released status' do
-    let(:anime) { create :anime, status: old_status, released_on: released_on }
+    let(:anime) do
+      create :anime,
+        status: old_status,
+        released_on: released_on,
+        episodes: episodes,
+        episodes_aired: episodes_aired
+    end
 
     let(:old_status) { :ongoing }
     let(:new_status) { :released }
+    let(:episodes) { 0 }
+    let(:episodes_aired) { 0 }
 
     context 'without released_on' do
       let(:released_on) { nil }
+
       it 'does not rollback released status' do
         expect(anime).to be_released
         expect(anime.released_on).to eq released_on
@@ -58,16 +67,39 @@ describe Animes::TrackStatusChanges do
 
     context 'released_on in the past' do
       let(:released_on) { Time.zone.yesterday }
+      let(:episodes) { [0, 10].sample }
+
       it 'does not rollback released status' do
         expect(anime).to be_released
         expect(anime.released_on).to eq released_on
+      end
+    end
+
+    context 'released_on in the future' do
+      let(:released_on) { Time.zone.tomorrow }
+
+      it 'does rollback release status' do
+        expect(anime).to be_ongoing
+        expect(anime.released_on).to eq released_on
+      end
+
+      context 'all episodes aired' do
+        let(:episodes) { 10 }
+        let(:episodes_aired) { 10 }
+
+        it 'does not rollback released status' do
+          expect(anime).to be_released
+          expect(anime.released_on).to eq released_on
+        end
       end
     end
   end
 
   describe 'rollback released status' do
     let(:anime) { create :anime, status: old_status }
-    let!(:news_topic) { create :news_topic, linked: anime, action: AnimeHistoryAction::Released }
+    let!(:news_topic) do
+      create :news_topic, linked: anime, action: AnimeHistoryAction::Released
+    end
     let(:old_status) { :released }
     let(:new_status) { :ongoing }
 
