f11f80a2ceb223e2b97c2d8d49040c3b5cb6bb7a
Andrey Sidorov <takandar@gmail.com>
2022-06-23 16:56:03 +0300

    add cache to proxy check

--- app/services/proxies/check.rb
+++ app/services/proxies/check.rb
@@ -6,16 +6,20 @@ class Proxies::Check
   TEST_URL = "https://shikimori.org#{ProxyTest::TEST_PAGE_PATH}"
 
   def call
-    content = Proxy.get(TEST_URL, timeout: 10, proxy: @proxy)
-    content&.include?(ProxyTest::SUCCESS_CONFIRMATION_MESSAGE) &&
-      ips.none? { |ip| content.include? ip }
-  rescue *::Network::FaradayGet::NET_ERRORS
-    false
+    Rails.cache.fetch(@proxy.to_s, expires_in: 2.hours) { do_check }
   end
 
 private
 
   def ips
     @ips ||= Proxies::WhatIsMyIps.call
+  end
+
+  def do_check
+    content = Proxy.get(TEST_URL, timeout: 10, proxy: @proxy)
+    content&.include?(ProxyTest::SUCCESS_CONFIRMATION_MESSAGE) &&
+      ips.none? { |ip| content.include? ip }
+  rescue *::Network::FaradayGet::NET_ERRORS
+    false
   end
 end
