0a4859e96cfb88fae9353ce241ec23c2ce0ddaf7
Andrey Sidorov <takandar@gmail.com>
2022-07-04 21:12:46 +0300

    sync critiques and reviews topics in Animes::SyncTopicsIsCensored

--- app/services/animes/sync_topics_is_censored.rb
+++ app/services/animes/sync_topics_is_censored.rb
@@ -3,5 +3,24 @@ class Animes::SyncTopicsIsCensored
 
   def call
     @entry.all_topics.update_all is_censored: @entry.is_censored
+    critiques_scope
+      .or(reviews_scope)
+      .update_all is_censored: @entry.is_censored
+  end
+
+private
+
+  def critiques_scope
+    Topic.where(
+      linked_type: 'Critique',
+      linked_id: @entry.critiques.select(:id)
+    )
+  end
+
+  def reviews_scope
+    Topic.where(
+      linked_type: 'Review',
+      linked_id: @entry.reviews.select(:id)
+    )
   end
 end
--- spec/services/animes/sync_topics_is_censored_spec.rb
+++ spec/services/animes/sync_topics_is_censored_spec.rb
@@ -1,13 +1,27 @@
 describe Animes::SyncTopicsIsCensored do
-  let(:anime) { build_stubbed :anime, is_censored: is_censored }
-  let(:is_censored) { false }
-  let!(:anime_topic) do
-    create :anime_topic, linked: anime, is_censored: !is_censored
-  end
+  %i[anime manga].each do |kind|
+    context kind do
+      let(:entry) { create kind, is_censored: is_censored }
+      let(:is_censored) { false }
+      let!(:linked_topic) do
+        create :"#{kind}_topic", linked: entry, is_censored: !is_censored
+      end
+      let(:critique) { create :critique, target: entry }
+      let!(:critique_topic) do
+        create :critique_topic, linked: critique, is_censored: !is_censored
+      end
+      let(:review) { create :review, kind => entry }
+      let!(:review_topic) do
+        create :review_topic, linked: review, is_censored: !is_censored
+      end
 
-  subject! { described_class.call anime }
+      subject! { Animes::SyncTopicsIsCensored.call entry }
 
-  it do
-    expect(anime_topic.reload.is_censored).to eq is_censored
+      it do
+        expect(linked_topic.reload.is_censored).to eq is_censored
+        expect(critique_topic.reload.is_censored).to eq is_censored
+        expect(review_topic.reload.is_censored).to eq is_censored
+      end
+    end
   end
 end
