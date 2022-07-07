0f3e56f7e7f419e9c36b20245621fd80e89eefc7
Yokitori <andrew.undream@gmail.com>
2022-07-07 23:49:51 +0300

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -174,7 +174,6 @@ ActiveRecord::Schema.define(version: 2022_07_07_131139) do
     t.date "russia_released_on"
     t.decimal "score_2", default: "0.0", null: false
     t.text "russia_released_on_hint", default: "", null: false
-    t.integer "ranked_random"
     t.index ["kind"], name: "index_animes_on_kind"
     t.index ["name"], name: "index_animes_on_name"
     t.index ["rating"], name: "index_animes_on_rating"
@@ -689,7 +688,6 @@ ActiveRecord::Schema.define(version: 2022_07_07_131139) do
     t.string "licensors", default: [], null: false, array: true
     t.decimal "score_2", default: "0.0", null: false
     t.string "options", default: [], null: false, array: true
-    t.integer "ranked_random"
     t.index ["kind"], name: "index_mangas_on_kind"
     t.index ["name"], name: "index_mangas_on_name"
     t.index ["russian"], name: "index_mangas_on_russian"
