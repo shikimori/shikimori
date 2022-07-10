2dc7e91bb9ea48f9afd4413e54bad67e4c1744ae
Yokitori <andrew.undream@gmail.com>
2022-07-11 01:27:12 +0300

    add specs and misc change

--- app/models/abilities/article_moderator.rb
+++ app/models/abilities/article_moderator.rb
@@ -2,10 +2,10 @@ class Abilities::ArticleModerator
   include CanCan::Ability
   prepend Draper::CanCanCan
 
-  ARTICLES_MODERATOR_ACTIONS = %i[edit update destroy]
+  ARTICLE_MODERATOR_ACTIONS = %i[edit update destroy]
 
   def initialize _user
-    can :manage, Article
-    can ARTICLES_MODERATOR_ACTIONS, Topics::EntryTopics::ArticleTopic
+    can :manage, Article # needed to accept or reject
+    can ARTICLE_MODERATOR_ACTIONS, Topics::EntryTopics::ArticleTopic
   end
 end
