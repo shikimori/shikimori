aa047f05e88b804d6c1c20e0361035d15813a7d6
Yokitori <andrew.undream@gmail.com>
2022-07-11 01:03:42 +0300

    allow article moderator to edit and destroy article topics

--- app/models/abilities/article_moderator.rb
+++ app/models/abilities/article_moderator.rb
@@ -2,7 +2,10 @@ class Abilities::ArticleModerator
   include CanCan::Ability
   prepend Draper::CanCanCan
 
+  ARTICLES_MODERATOR_ACTIONS = %i[edit update destroy]
+
   def initialize _user
     can :manage, Article
+    can ARTICLES_MODERATOR_ACTIONS, Topics::EntryTopics::ArticleTopic
   end
 end
