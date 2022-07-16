1e68a9975d5c0fa00684da727d8fbc5ddfac8604
Andrey Sidorov <takandar@gmail.com>
2022-07-16 10:24:43 +0300

    add time to note

--- app/query_objects/animes/filters/order_by.rb
+++ app/query_objects/animes/filters/order_by.rb
@@ -110,7 +110,7 @@ class Animes::Filters::OrderBy < Animes::Filters::FilterBase # rubocop:disable C
     Field[:kind] => '%<table_name>s.kind',
     Field[:licensor] => '%<table_name>s.licensor',
     Field[:ranked_random] => '%<table_name>s.ranked_random',
-    # TODO: switch 'random()' to '%<table_name>s.ranked_random'
+    # TODO: after 2023-01-01 switch 'random()' to '%<table_name>s.ranked_random'
     Field[:random] => 'random()'
   }
 
