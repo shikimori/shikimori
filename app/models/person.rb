e32ac61db0b4d0db512fd06c6fa412fcd55ddde8
Andrey Sidorov <takandar@gmail.com>
2022-08-17 20:06:09 +0300

    remove Types::IncompleteDateType since Types::JsonbActiveModel included to IncompleteDate replaces it

--- app/models/person.rb
+++ app/models/person.rb
@@ -45,7 +45,7 @@ class Person < DbEntry
   validates :image, attachment_content_type: { content_type: /\Aimage/ }
   validates :name, :japanese, length: { maximum: 255 }
 
-  attribute :birth_on_v2, Types::IncompleteDateType.new
+  attribute :birth_on_v2, IncompleteDate
 
   SEYU_ROLES = %w[
     English Italian Hungarian Japanese German Hebrew Brazilian French
