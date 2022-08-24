323da136c1a765d631b505beb3b9cba494b2ee2c
Andrey Sidorov <takandar@gmail.com>
2022-08-24 19:25:50 +0300

    fix ad image

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -201,12 +201,12 @@ private
     :"@is_#{placement}_ad_shown"
   end
 
-  # convert -resize 50% app/assets/images/globals/events/2022-06-18/menu_1@2x.jpg app/assets/images/globals/events/2022-06-18/menu_1.jpg
   SPECIAL_X300 = {
     provider: Types::Ad::Provider[:special],
     # AD START: IMBA - remove 2022-08-29 23:59
     url: 'https://bit.ly/3zYB034',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
+    # convert -resize 50% app/assets/images/globals/events/2022-06-18/menu_1@2x.jpg app/assets/images/globals/events/2022-06-18/menu_1.jpg
     images: (1..1).map do |i|
       {
         src: "/assets/globals/events/2022-06-18/menu_#{i}.jpg",
@@ -231,10 +231,11 @@ private
     # AD START: IMBA - remove 2022-08-29 23:59
     url: 'https://bit.ly/3zYB034',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
+    # convert -resize 50% app/assets/images/globals/events/2022-07-16/wide_1@2x.jpg app/assets/images/globals/events/2022-07-16/wide_1.jpg
     images: (1..1).map do |i|
       {
-        src: "/assets/globals/events/2022-07-16/wide_#{i}.jpg"
-        # src_2x: "/assets/globals/events/2022-05-24/wide_#{i}@2x.jpg"
+        src: "/assets/globals/events/2022-07-16/wide_#{i}.jpg",
+        src_2x: "/assets/globals/events/2022-07-16/wide_#{i}@2x.jpg"
       }
     end,
     # AD END
