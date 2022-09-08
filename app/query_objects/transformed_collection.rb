20e1f997b9cf029c168b42f92bac7f9227093d31
Andrey Sidorov <takandar@gmail.com>
2022-09-08 20:02:44 +0300

    support different transformations besides of map in TransformedCollection

--- app/query_objects/transformed_collection.rb
+++ app/query_objects/transformed_collection.rb
@@ -1,34 +1,35 @@
 class TransformedCollection
   include Enumerable
-  delegate :==, :eql?, :equal?, to: :mapped
+  delegate :==, :eql?, :equal?, to: :transformed
 
-  def initialize collection, mapper = nil, &block
+  def initialize collection, transformation, action = nil, &block
     @collection = collection
-    @mapper = mapper || block
+    @transformation = transformation
+    @action = action || block
   end
 
   def each
-    mapped.each { |map| yield map }
+    transformed.each { |map| yield map }
   end
 
-  def mapped
-    @mapped ||= @collection.map do |item|
-      @mapper.call item
+  def transformed
+    @transformed ||= @collection.send(@transformation) do |item|
+      @action.call item
     end
   end
-  alias to_a mapped
+  alias to_a transformed
 
   def respond_to? method, *args
     # rails 6.1 fix to prevent usage of PreloadCollectionIterator in actionview/lib/action_view/renderer/collection_renderer.rb:107
-    return false if method == :preload_associations && mapped.present?
+    return false if method == :preload_associations && transformed.present?
 
     super(method, *args) ||
-      mapped.respond_to?(method, *args) ||
+      transformed.respond_to?(method, *args) ||
       @collection.respond_to?(method, *args)
   end
 
   def method_missing method, *args, &block
-    target = mapped.respond_to?(method) ? mapped : @collection
+    target = transformed.respond_to?(method) ? transformed : @collection
     target.send method, *args, &block
   end
 
@@ -36,62 +37,10 @@ private
 
   def respond_to_missing? method, *args
     # rails 6.1 fix to prevent usage of PreloadCollectionIterator in actionview/lib/action_view/renderer/collection_renderer.rb:107
-    return false if method == :preload_associations && mapped.present?
+    return false if method == :preload_associations && transformed.present?
 
     super(method, *args) ||
-      mapped.send(:respond_to_missing?, method, *args) ||
+      transformed.send(:respond_to_missing?, method, *args) ||
       @collection.send(:respond_to_missing?, method, *args)
   end
 end
-
-# class TransformedCollection < SimpleDelegator
-#   include Enumerable
-#
-#   def initialize collection, transformation
-#     @original = collection
-#     super @original
-#     @transformation = transformation
-#   end
-#
-#   def each
-#     to_a.each { |item| yield item }
-#   end
-#
-#   def to_a
-#     1/0
-#     @transformed ||= begin
-#       collection = __getobj__.map { |item| @transformation.call(item) }
-#       __setobj__(collection)
-#       collection
-#     end
-#   end
-#
-#   def respond_to? *args
-#     super(*args) || @original.respond_to?(*args)
-#   end
-#
-#   def respond_to_missing? *args
-#     super(*args) || @original.send(:respond_to_missing?, *args)
-#   end
-#
-#   def method_missing method, *args, &block
-#     @original.send method, *args, &block
-#   end
-#
-#   # def method_missing method, *args, &block
-#   #   raise @transformed.class.to_json
-#   #   if @transformed.present? && !@transformed.send(:respond_to_missing?, method) &&
-#   #       @original.send(:respond_to_missing?, method)
-#   #     @original.send method, *args, &block
-#   #   else
-#   #     super method, *args, &block
-#   #   end
-#   # end
-#
-# # private
-# #
-# #   def respond_to_missing? method, include_private = false
-# #     super(method, include_private)# ||
-# #       # (@transformed.present? && @original.send(:respond_to_missing?, method, include_private))
-# #   end
-# end
--- spec/query_objects/transformed_collection_spec.rb
+++ spec/query_objects/transformed_collection_spec.rb
@@ -1,13 +1,21 @@
 describe TransformedCollection do
   subject(:mapping) do
-    TransformedCollection.new collection do |value|
-      value * 2
-    end
+    described_class.new collection, transformation_method, action
   end
   let(:collection) { PaginatedCollection.new([1, 2], 1, 2) }
+  let(:transformation_method) { :map }
+  let(:action) { ->(value) { value * 2 } }
 
   describe 'collection' do
-    it { is_expected.to eq [2, 4] }
+    context 'map' do
+      it { is_expected.to eq [2, 4] }
+    end
+
+    context 'filter' do
+      let(:transformation_method) { :filter }
+      let(:action) { ->(value) { value == 1 } }
+      it { is_expected.to eq [1] }
+    end
   end
 
   describe 'respond_to original collection methods' do
