b08baca0095ccf2e5410163e51eeb17736a868a1
Andrey Sidorov <takandar@gmail.com>
2022-09-15 19:52:20 +0300

    also authorize read access when got AgeRestricted

--- app/controllers/concerns/can_can_get_404_concern.rb
+++ app/controllers/concerns/can_can_get_404_concern.rb
@@ -1,8 +1,21 @@
 module CanCanGet404Concern
   extend ActiveSupport::Concern
 
+  included do
+    prepend FetchResouceOverride
+  end
+
   # this method is checked in error handler of errors concern
   def cancan_get_404? error
     request.get? && error.is_a?(CanCan::AccessDenied)
+  end
+
+  module FetchResouceOverride
+    def fetch_resource
+      super
+    rescue AgeRestricted
+      authorize! :read, @resource
+      raise
+    end
   end
 end
