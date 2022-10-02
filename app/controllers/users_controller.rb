e58743c8ce1bb963276a312b478ad47854433338
Andrey Sidorov <takandar@gmail.com>
2022-10-03 00:24:12 +0300

    temporarily disable recommendations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ShikimoriController
   end
 
   def similar # rubocop:disable all
+    return render plain: 'рекомендации временно не работают по техническим причинам'
     og noindex: true
     @threshold = params[:threshold].to_i
     @klass = params[:klass] == Manga.name.downcase ? Manga : Anime
