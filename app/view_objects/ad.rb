9350da0eabd2700cfdb9d193b7e6f9f6a0ddb599
Andrey Sidorov <takandar@gmail.com>
2022-10-05 10:44:54 +0300

    new ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -203,14 +203,14 @@ private
 
   SPECIAL_X300 = {
     provider: Types::Ad::Provider[:special],
-    # AD START: IMBA - remove 2022-08-29 23:59
-    url: 'https://bit.ly/3zYB034',
+    # AD START: HZ - remove 2022-10-19 23:59
+    url: 'https://bit.ly/3fFvZWL',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
     # convert -resize 50% app/assets/images/globals/events/2022-06-18/menu_1@2x.jpg app/assets/images/globals/events/2022-06-18/menu_1.jpg
     images: (1..1).map do |i|
       {
-        src: "/assets/globals/events/2022-06-18/menu_#{i}.jpg",
-        src_2x: "/assets/globals/events/2022-06-18/menu_#{i}@2x.jpg"
+        src: "/assets/globals/events/2022-10-05/menu_#{i}.jpg",
+        src_2x: "/assets/globals/events/2022-10-05/menu_#{i}@2x.jpg"
       }
     end,
     # images: [{
@@ -229,40 +229,19 @@ private
   SPECIAL_X1170 = {
     provider: Types::Ad::Provider[:special],
     # AD START: IMBA - remove 2022-09-30 23:59
-    # url: 'https://bit.ly/3RFxUZy',
+    url: 'https://bit.ly/3fJ6svK',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
     # convert -resize 50% app/assets/images/globals/events/2022-07-16/wide_1@2x.jpg app/assets/images/globals/events/2022-07-16/wide_1.jpg
-    images: [{
-      src: '/assets/globals/events/2022-09-14/wide_1.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_1@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner1&utm_campaign=14_09'
-    }, {
-      src: '/assets/globals/events/2022-09-14/wide_2.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_2@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner2&utm_campaign=14_09'
-    }, {
-      src: '/assets/globals/events/2022-09-14/wide_3.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_3@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner3&utm_campaign=14_09'
-    }, {
-      src: '/assets/globals/events/2022-09-14/wide_4.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_4@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner4&utm_campaign=14_09'
-    }, {
-      src: '/assets/globals/events/2022-09-14/wide_5.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_5@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner5&utm_campaign=14_09'
-    }, {
-      src: '/assets/globals/events/2022-09-14/wide_6.jpg',
-      src_2x: '/assets/globals/events/2022-09-14/wide_6@2x.jpg',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner6&utm_campaign=14_09'
-    }],
-    # images: (1..1).map do |i|
-    #   {
-    #     src: "/assets/globals/events/2022-07-16/wide_#{i}.jpg?v=4",
-    #     src_2x: "/assets/globals/events/2022-07-16/wide_#{i}@2x.jpg?v=4"
-    #   }
-    # end,
+    # images: [{
+    #   src: '/assets/globals/events/2022-10-05/wide.jpg',
+    #   src_2x: '/assets/globals/events/2022-10-05/wide@2x.jpg'
+    # }],
+    images: (1..1).map do |i|
+      {
+        src: "/assets/globals/events/2022-10-05/wide_#{i}.jpg?v=4",
+        src_2x: "/assets/globals/events/2022-10-05/wide_#{i}@2x.jpg?v=4"
+      }
+    end,
     # AD END
     # html: (
     #   <<~HTML
@@ -422,12 +401,12 @@ private
         Types::Ad::Type[:mt_300x250]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        # Types::Ad::Type[:special_x300], # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
         Types::Ad::Type[:yd_240x600],
         Types::Ad::Type[:mt_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        # Types::Ad::Type[:special_x300], # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
         Types::Ad::Type[:yd_300x600],
         Types::Ad::Type[:mt_300x600]
       ],
@@ -443,7 +422,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170], # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x1170], # HZ - remove 2022-10-19 23:59
         Types::Ad::Type[:yd_970x250],
         Types::Ad::Type[:mt_970x250]
       ]
@@ -455,13 +434,13 @@ private
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        # Types::Ad::Type[:special_x300], # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
         # Types::Ad::Type[:mt_240x400],
         # Types::Ad::Type[:yd_240x500],
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        # Types::Ad::Type[:special_x300], # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
         # Types::Ad::Type[:mt_300x600],
         # Types::Ad::Type[:yd_300x600],
         # Types::Ad::Type[:advrtr_240x400],
@@ -478,7 +457,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170] # IMBA - remove 2022-08-29 23:59
+        Types::Ad::Type[:special_x1170] # HZ - remove 2022-10-19 23:59
       ]
     }
   }
