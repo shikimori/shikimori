98e11c3cd3abf512b1a7c2cd74d211173a6c2be6
Andrey Sidorov <takandar@gmail.com>
2022-10-13 21:43:24 +0300

    fix clbus images issue #2564

--- app/packs/javascripts/pages/p-clubs/images.js
+++ app/packs/javascripts/pages/p-clubs/images.js
@@ -1,6 +1,15 @@
+let gallery = null;
+
+pageUnload('clubs_images', async () => {
+  if (gallery) {
+    gallery.destroy();
+    gallery = null;
+  }
+});
+
 pageLoad('clubs_images', async () => {
   const { PreloadedGallery } =
     await import(/* webpackChunkName: "galleries" */ '@/views/images/preloaded_gallery');
 
-  new PreloadedGallery('.b-gallery');
+  gallery = new PreloadedGallery('.b-gallery');
 });
--- app/packs/javascripts/pages/p-clubs/show.js
+++ app/packs/javascripts/pages/p-clubs/show.js
@@ -1,8 +1,17 @@
+let gallery = null;
+
+pageUnload('clubs_show', async () => {
+  if (gallery) {
+    gallery.destroy();
+    gallery = null;
+  }
+});
+
 pageLoad('clubs_show', async () => {
   if ($('.b-gallery').exists()) {
     const { PreloadedGallery } =
       await import(/* webpackChunkName: "galleries" */ '@/views/images/preloaded_gallery');
 
-    new PreloadedGallery('.b-gallery');
+    gallery = new PreloadedGallery('.b-gallery');
   }
 });
