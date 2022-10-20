fc5907d7962bc09b83f4dfcfe553afa763f8317a
Andrey Sidorov <takandar@gmail.com>
2022-10-20 20:59:33 +0300

    more fixes of broken code and specs

--- spec/services/list_imports/import_spec.rb
+++ spec/services/list_imports/import_spec.rb
@@ -40,8 +40,7 @@ describe ListImports::Import do
       expect(user.history).to have(1).item
       expect(user.history.first).to have_attributes(
         action: UserHistoryAction::ANIME_IMPORT,
-        value: '1',
-        target: list_import
+        value: '1'
       )
 
       expect(Achievements::Track)
