e15edf002603e34f4385cd2c9d1b53055303edaf
Andrey Sidorov <takandar@gmail.com>
2022-10-21 22:00:20 +0300

    create activestorage tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_173618) do
+ActiveRecord::Schema.define(version: 2022_10_21_185737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -44,6 +44,34 @@ ActiveRecord::Schema.define(version: 2022_10_19_173618) do
     t.datetime "updated_at", null: false
     t.index ["neko_id", "level"], name: "index_achievements_on_neko_id_and_level"
     t.index ["user_id", "neko_id", "level"], name: "index_achievements_on_user_id_and_neko_id_and_level", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "anime_calendars", id: :serial, force: :cascade do |t|
@@ -1276,6 +1304,8 @@ ActiveRecord::Schema.define(version: 2022_10_19_173618) do
   add_foreign_key "abuse_requests", "topics"
   add_foreign_key "abuse_requests", "users"
   add_foreign_key "abuse_requests", "users", column: "approver_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "anime_stat_histories", "animes"
   add_foreign_key "anime_stat_histories", "mangas"
   add_foreign_key "bans", "abuse_requests"
