63e8e928b395b1b36cbb6a448139d1dbe36a98f0
Andrey Sidorov <takandar@gmail.com>
2022-11-01 11:51:33 +0300

    add new ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -203,14 +203,14 @@ private
 
   SPECIAL_X300 = {
     provider: Types::Ad::Provider[:special],
-    # AD START: HZ - remove 2022-10-19 23:59
-    url: 'https://bit.ly/3fJ6svK',
+    # AD START: HZ - remove 2022-11-14 23:59
+    url: 'https://bit.ly/3DK6zjl',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
     # convert -resize 50% app/assets/images/globals/events/2022-06-18/menu_1@2x.jpg app/assets/images/globals/events/2022-06-18/menu_1.jpg
-    images: (1..2).map do |i|
+    images: (1..1).map do |i|
       {
-        src: "/assets/globals/events/2022-10-05/menu_#{i}.jpg?v2",
-        src_2x: "/assets/globals/events/2022-10-05/menu_#{i}@2x.jpg?v2"
+        src: "/assets/globals/events/2022-11-01/menu_#{i}.png",
+        # src_2x: "/assets/globals/events/2022-11-01/menu_#{i}@2x.jpg?v2"
       }
     end,
     # images: [{
@@ -228,20 +228,26 @@ private
   }
   SPECIAL_X1170 = {
     provider: Types::Ad::Provider[:special],
-    # AD START: HZ - remove 2022-10-19 23:59
-    url: 'https://bit.ly/3fFvZWL',
+    # AD START: HZ - remove 2022-11-14 23:59
+    url: 'https://bit.ly/3Wlvpy7',
     # pixel: 'https://ads.adfox.ru/211055/getCode?p1=coadb&p2=frfe&pfc=eevvx&pfb=lozyl&pr=[RANDOM]&pe=b',
     # convert -resize 50% app/assets/images/globals/events/2022-07-16/wide_1@2x.jpg app/assets/images/globals/events/2022-07-16/wide_1.jpg
+    images: [
+      { src: '/assets/globals/events/2022-11-01/wide_1.png' },
+      { src: '/assets/globals/events/2022-11-01/wide_1.png' },
+      { src: '/assets/globals/events/2022-11-01/wide_2.png' },
+      { src: '/assets/globals/events/2022-11-01/wide_2.png' }
+    ],
     # images: [{
     #   src: '/assets/globals/events/2022-10-05/wide.jpg',
     #   src_2x: '/assets/globals/events/2022-10-05/wide@2x.jpg'
     # }],
-    images: (1..1).map do |i|
-      {
-        src: "/assets/globals/events/2022-10-05/wide_#{i}.jpg?v2",
-        src_2x: "/assets/globals/events/2022-10-05/wide_#{i}@2x.jpg?v2"
-      }
-    end,
+    # images: (1..4).map do |i|
+    #   {
+    #     src: "/assets/globals/events/2022-11-01/wide_#{i}.jpg",
+    #     src_2x: "/assets/globals/events/2022-11-01/wide_#{i}@2x.jpg"
+    #   }
+    # end,
     # AD END
     # html: (
     #   <<~HTML
@@ -423,12 +429,12 @@ private
         Types::Ad::Type[:mt_300x250]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        # Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-11-14 23:59
         Types::Ad::Type[:yd_240x600],
         Types::Ad::Type[:mt_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        # Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-11-14 23:59
         Types::Ad::Type[:yd_300x600],
         Types::Ad::Type[:mt_300x600]
       ],
@@ -445,7 +451,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        # Types::Ad::Type[:special_x1170], # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x1170], # HZ - remove 2022-11-14 23:59
         Types::Ad::Type[:yd_970x250],
         Types::Ad::Type[:mt_970x250]
       ]
@@ -457,13 +463,13 @@ private
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        # Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-11-14 23:59
         # Types::Ad::Type[:mt_240x400],
         # Types::Ad::Type[:yd_240x500],
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        # Types::Ad::Type[:special_x300], # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x300], # HZ - remove 2022-11-14 23:59
         # Types::Ad::Type[:mt_300x600],
         # Types::Ad::Type[:yd_300x600],
         # Types::Ad::Type[:advrtr_240x400],
@@ -481,7 +487,7 @@ private
         Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        # Types::Ad::Type[:special_x1170] # HZ - remove 2022-10-19 23:59
+        Types::Ad::Type[:special_x1170] # HZ - remove 2022-11-14 23:59
       ]
     }
   }
