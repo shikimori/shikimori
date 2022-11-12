f0453ddccf425b0b3c6ffe5d3aba35dd4fda31e1
Andrey Sidorov <takandar@gmail.com>
2022-11-12 19:28:05 +0300

    updates to poster_field

--- app/packs/javascripts/vue/components/poster_field.vue
+++ app/packs/javascripts/vue/components/poster_field.vue
@@ -1,33 +1,66 @@
 <template lang='pug'>
-label.b-dropzone(
+label.b-dropzone.block(
   ref='uploaderRef'
-  data-hint='Перетаскивай сюда картинку, размер от 450x700 до 900x1400 пикселей;'
+  data-hint='Перетаскивай сюда картинку размером от 450x700 до 900x1400 пикселей;'
 )
   input.hidden(
     type='file'
   )
-.cropper-container
+
+.sizes.block
+  p(
+    v-if='sizes.naturalWidth'
+  ) Размер: {{ sizes.naturalWidth }}x{{ sizes.naturalHeight }}
+  p(
+    v-if='sizes.naturalWidth !== sizes.width'
+  ) Кроп: {{ sizes.width }}x{{ sizes.height }}
+
+.cropper-container(
+  v-show='currentSrc'
+)
   VueCropper(
-    v-if='currentSrc'
     ref='vueCropperRef'
     :src='currentSrc'
     :aspect-ratio='225/350'
     :auto-crop-area='1.0'
+    @crop='onCrop'
   )
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, reactive, watch, onMounted } from 'vue';
 import VueCropper from '@ballcat/vue-cropper';
 import 'cropperjs/dist/cropper.css';
 
 const props = defineProps({
-  src: { type: String, required: false, default: null }
+  src: { type: String, required: false, default: '' }
 });
 
 const currentSrc = ref(props.src);
 const vueCropperRef = ref(null);
 const uploaderRef = ref(null);
+
+const sizes = reactive({
+  naturalWidth: 0,
+  naturalHeight: 0,
+  width: 0,
+  height: 0
+});
+
+const onCrop = e => {
+  const canvasData = vueCropperRef.value.getCanvasData();
+
+  sizes.naturalWidth = Math.ceil(canvasData.naturalWidth);
+  sizes.naturalHeight = Math.ceil(canvasData.naturalHeight);
+  sizes.width = Math.ceil(e.detail.width);
+  sizes.height = Math.ceil(e.detail.height);
+};
+
+defineExpose({
+  toDataURI() {
+    return vueCropperRef.value?.getCroppedCanvas()?.toDataURL();
+  }
+});
 
 onMounted(async () => {
   const { FileUploader } = await import('@/views/file_uploader');
@@ -41,12 +74,6 @@ onMounted(async () => {
     .on('upload:file:added', ({ target }, file) => onFileAdded(target, file));
 });
 
-defineExpose({
-  toDataURI() {
-    return vueCropperRef.value?.getCroppedCanvas()?.toDataURL();
-  }
-});
-
 function onFileAdded(uploader, uppyFile) {
   currentSrc.value = URL.createObjectURL(uppyFile.data);
   uploader.uppy.reset();
@@ -54,7 +81,9 @@ function onFileAdded(uploader, uppyFile) {
 </script>
 
 <style scoped lang='sass'>
+.sizes
+  font-size: 14px
+
 .cropper-container
-  margin-top: 30px
   width: 450px
 </style>
