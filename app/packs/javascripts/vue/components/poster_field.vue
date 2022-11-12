bb7e4b84b3887608fad505e720aa61a0ea0a93b5
Andrey Sidorov <takandar@gmail.com>
2022-11-12 18:09:43 +0300

    logic of uploading new images via FileUploader

--- app/packs/javascripts/vue/components/poster_field.vue
+++ app/packs/javascripts/vue/components/poster_field.vue
@@ -1,10 +1,19 @@
 <template lang='pug'>
-VueCropper(
-  ref='vueCropperRef'
-  :src='src'
-  :aspect-ratio='225/350'
-  :auto-crop-area='1.0'
+label.b-dropzone(
+  ref='uploaderRef'
+  data-hint='Перетаскивай сюда картинку, размер от 450x700 до 900x1400 пикселей;'
 )
+  input.hidden(
+    type='file'
+  )
+.cropper-container
+  VueCropper(
+    v-if='currentSrc'
+    ref='vueCropperRef'
+    :src='currentSrc'
+    :aspect-ratio='225/350'
+    :auto-crop-area='1.0'
+  )
 </template>
 
 <script setup>
@@ -15,17 +24,37 @@ import 'cropperjs/dist/cropper.css';
 const props = defineProps({
   src: { type: String, required: false, default: null }
 });
-const vueCropperRef = ref(null);
 
-// vueCropperRef.value.getCroppedCanvas().toDataURL()
-// vueCropperRef.value.getCroppedCanvas().toBlob()
+const currentSrc = ref(props.src);
+const vueCropperRef = ref(null);
+const uploaderRef = ref(null);
+
+onMounted(async () => {
+  const { FileUploader } = await import('@/views/file_uploader');
+
+  new FileUploader(uploaderRef.value, {
+    autoProceed: false,
+    isResetAfterUpload: false,
+    maxNumberOfFiles: 1,
+    maxFileSize: 1024 * 1024 * 15
+  })
+    .on('upload:file:added', ({ target }, file) => onFileAdded(target, file));
+});
 
 defineExpose({
   toDataURI() {
-    return vueCropperRef.value.getCroppedCanvas().toDataURL();
+    return vueCropperRef.value?.getCroppedCanvas()?.toDataURL();
   }
 });
+
+function onFileAdded(uploader, uppyFile) {
+  currentSrc.value = URL.createObjectURL(uppyFile.data);
+  uploader.uppy.reset();
+}
 </script>
 
 <style scoped lang='sass'>
+.cropper-container
+  margin-top: 30px
+  width: 450px
 </style>
