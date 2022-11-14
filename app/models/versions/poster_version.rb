34d3d01b3cd11e007eb92042404342289e77f238
Andrey Sidorov <takandar@gmail.com>
2022-11-14 21:32:45 +0300

    basic version#upload_poster logic

--- app/models/versions/poster_version.rb
+++ app/models/versions/poster_version.rb
@@ -1,11 +1,39 @@
 class Versions::PosterVersion < Version
-  # FIELD = 'poster'
-
   Actions = Types::Strict::Symbol
     .constructor(&:to_sym)
     .enum(:upload, :delete)
 
   def action
     Actions[item_diff['action']]
+  end
+
+  def apply_changes
+    case action
+      when Actions[:upload] then upload_poster
+      when Actions[:delete] then delete_poster
+    end
+  end
+
+  def rollback_changes
+    case action
+      when Actions[:upload] then delete_poster
+      when Actions[:delete] then upload_poster
+    end
+  end
+
+  def sweep_deleted **_args
+    raise NotImplementedError
+  end
+
+private
+
+  def upload_poster
+    ApplicationRecord.transition do
+      item.update! is_approved: true
+    end
+  end
+
+  def delete_poster
+    raise NotImplementedError
   end
 end
---
+++ spec/models/versions/poster_version_spec.rb
@@ -0,0 +1,62 @@
+describe Versions::PosterVersion do
+  describe '#action' do
+    let(:version) { build :poster_version, item_diff: { action: 'upload' } }
+    it { expect(version.action).to eq Versions::PosterVersion::Actions[:upload] }
+  end
+
+  describe '#apply_changes' do
+    include_context :timecop
+
+    let(:poster) { create :poster, is_approved: false, anime: anime }
+    let(:active_poster) { nil }
+    let(:anime) { create :anime }
+
+    subject! { version.apply_changes }
+
+    context 'upload' do
+      let(:version) do
+        create :poster_version,
+          item: poster,
+          item_diff: { 'action' => 'upload' },
+          associated: anime
+      end
+
+      context 'no active poster' do
+        it { expect(poster.reload).to be_is_approved }
+      end
+
+      context 'has active poster' do
+        let!(:active_poster) { create :poster, is_approved: true, anime: anime }
+        it do
+          expect(poster.reload).to be_is_approved
+          expect(active_poster.reload.deleted_at).to be_within(0.1).of Time.zone.now
+          expect(version.reload.item_diff).to eq(
+            'action' => 'upload',
+            'replaced_poster_id' => active_poster.id
+          )
+        end
+      end
+    end
+
+    context 'delete' do
+      pending
+    end
+  end
+
+  describe '#rollback_changes' do
+    subject! { version.rollback_changes }
+
+    context 'upload' do
+      pending
+    end
+
+    context 'delete' do
+      pending
+    end
+  end
+
+  describe '#sweep_deleted' do
+    subject! { version.sweep_deleted }
+    pending
+  end
+end
