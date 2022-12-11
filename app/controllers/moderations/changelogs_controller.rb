af8bb6f77fefc623ce2cd4fc4e2d91e541048d45
Andrey Sidorov <takandar@gmail.com>
2022-12-11 16:45:34 +0300

    updates to changelog viewer

--- app/controllers/moderations/changelogs_controller.rb
+++ app/controllers/moderations/changelogs_controller.rb
@@ -14,17 +14,34 @@ class Moderations::ChangelogsController < ModerationsController
     log_name = Shellwords.shellescape(params[:id]).gsub(/[^\w_]/, '')
     log_file = Rails.root.join "log/changelog_#{log_name}.log"
 
-    @collection = `tail -n 10 #{log_file}`.strip.each_line.map(&:strip).map do |log_entry|
+    command =
+      if params[:search].present?
+        "grep \"#{Shellwords.shellescape params[:search]}\" #{log_file} | tail -n 250"
+      else
+        "tail -n 10 #{log_file}"
+      end
+
+    @collection = `#{command}`.strip.each_line.map(&:strip).map do |log_entry|
       split = log_entry.split(/(?<=\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\]) /)
       changes = split[1]
-        .gsub(/ ?:([\w_]+)=>/, '"\1":')
-        .gsub(/"([\w_]+)"=>/, '"\1":')
+        .gsub(/ ?:([a-z_]+)=>/, '"\1":')
+        .gsub(/"([a-z_]+)"=>/, '"\1":')
         .gsub(/"action"::(update|destroy)/, '"action":"\1"')
-      log = JSON.parse(changes, symbolize_names: true)
+        .gsub(/(?<=":)#<\w+(?<model>[\s\S]+)>(?=}\Z)/) do
+          '{' +
+            $LAST_MATCH_INFO[:model]
+              .gsub(/ ([a-z_]+): /, '"\1":')
+              .gsub(':nil', ':null') +
+            '}'
+        end
+      json = JSON.parse(changes, symbolize_names: true)
+      if json[:model].is_a? String
+        json[:model] = JSON.parse(json[:model], symbolize_names: true)
+      end
 
       {
         date: Time.zone.parse(split[0].gsub(/[\[\]]/, '')),
-        log: log,
+        log: json,
         raw: log_entry
       }
     end
