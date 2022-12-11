b0ddfc368dbf60849bb3e51c9e41ad9559665529
Andrey Sidorov <takandar@gmail.com>
2022-12-11 14:10:28 +0300

    read basic changelogs logic

--- app/controllers/moderations/changelogs_controller.rb
+++ app/controllers/moderations/changelogs_controller.rb
@@ -1,5 +1,22 @@
 class Moderations::ChangelogsController < ModerationsController
   def index
     og page_title: i18n_t('page_title')
+    @collection = `ls #{Rails.root.join 'log'} | grep changelog`
+      .strip
+      .split("\n")
+      .map { |v| v.gsub(/changelog_|\.log/, '') }
+  end
+
+  def show
+    og page_title: params[:id]
+    breadcrumb i18n_t('page_title'), moderations_changelogs_url
+
+    log_name = Shellwords.shellescape(params[:id]).gsub(/[^\w_]/, '')
+    log_file = Rails.root.join "log/changelog_#{log_name}.log"
+
+    @collection = `tail -n 10 #{log_file}`
+      .strip
+      .each_line
+      .map(&:strip)
   end
 end
