7bbdd73a6b7736d5f7c78d60a8e49ca6d71a1508
Andrey Sidorov <takandar@gmail.com>
2022-12-31 13:51:55 +0300

    set desynecd poster to animes having desynced image

--- db/migrate/20221231095411_cleanup_test_posters.rb
+++ db/migrate/20221231095411_cleanup_test_posters.rb
@@ -1,14 +1,21 @@
 class CleanupTestPosters < ActiveRecord::Migration[6.1]
   def up
     [Anime, Manga, Character, Person].each do |klass|
-      klass.where("desynced && '{poster}'").includes(:posters).find_each do |entry|
-        entry.desynced -= %w[poster]
-        entry.save!
-        entry.posters.each(&:destroy!)
-      end
+      klass
+        .where("desynced && '{poster}'")
+        .includes(:posters)
+        .find_each do |entry|
+          entry.desynced -= %w[poster]
+          entry.save!
+          entry.posters.each(&:destroy!)
+        end
 
-      klass.where("desynced && '{image}'").includes(:posters).find_each do |entry|
-      end
+      klass
+        .where("desynced && '{image}'")
+        .find_each do |entry|
+          entry.desynced += %w[poster]
+          entry.save!
+        end
     end
     Versions::PosterVersion.destroy_all
     Poster.where.not(deleted_at: nil).destroy_all
