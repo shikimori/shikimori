89bec5c60d86410a5c194480f2e7679f54467ab7
Andrey Sidorov <takandar@gmail.com>
2023-01-05 10:52:07 +0300

    migrate images to posters migration

--- db/migrate/20221231173400_migrate_images_to_posters.rb
+++ db/migrate/20221231173400_migrate_images_to_posters.rb
@@ -1,12 +1,19 @@
 class MigrateImagesToPosters < ActiveRecord::Migration[6.1]
-  def change
+  def up
+    return if Rails.env.test?
+
     [Anime, Manga, Character, Person].each do |klass|
       klass
         .where.not("desynced && '{poster}'")
-        .includes(:poster) do |entry|
+        .includes(:poster)
+        .find_each do |entry|
           next if entry.poster.present?
+          next unless entry.image.exists?
 
+          puts "#{klass} #{entry.id}"
 
+          entry.create_poster image: File.open(entry.image.path)
         end
+    end
   end
 end
