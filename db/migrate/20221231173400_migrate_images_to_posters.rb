128fd6456185729fa2756280d834933a92590b3d
Andrey Sidorov <takandar@gmail.com>
2023-01-06 17:55:40 +0300

    disable posters migration cause it failed. will execute it in console manually

--- db/migrate/20221231173400_migrate_images_to_posters.rb
+++ db/migrate/20221231173400_migrate_images_to_posters.rb
@@ -1,19 +1,19 @@
 class MigrateImagesToPosters < ActiveRecord::Migration[6.1]
   def up
-    return if Rails.env.test?
-
-    [Anime, Manga, Character, Person].each do |klass|
-      klass
-        .where.not("desynced && '{poster}'")
-        .includes(:poster)
-        .find_each do |entry|
-          next if entry.poster.present?
-          next unless entry.image.exists?
-
-          puts "#{klass} #{entry.id}"
-
-          entry.create_poster image: File.open(entry.image.path)
-        end
-    end
+    # return if Rails.env.test?
+    # 
+    # [Anime, Manga, Character, Person].each do |klass|
+    #   klass
+    #     .where.not("desynced && '{poster}'")
+    #     .includes(:poster)
+    #     .find_each do |entry|
+    #       next if entry.poster.present?
+    #       next unless entry.image.exists?
+    # 
+    #       puts "#{klass} #{entry.id}"
+    # 
+    #       entry.create_poster image: File.open(entry.image.path)
+    #     end
+    # end
   end
 end
