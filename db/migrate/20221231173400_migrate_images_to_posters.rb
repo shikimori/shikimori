6c8a6a5cfec32ff0126d988811059f728d530632
Andrey Sidorov <takandar@gmail.com>
2023-01-14 19:30:12 +0300

    add couple more sandboxes to migration

--- db/migrate/20221231173400_migrate_images_to_posters.rb
+++ db/migrate/20221231173400_migrate_images_to_posters.rb
@@ -29,7 +29,7 @@ end
 #       v.save!
 #     end;
 # end;
-# 
+
 # [Anime, Manga, Character, Person].each do |klass|
 #   puts klass.name
 #   puts klass.
@@ -37,7 +37,7 @@ end
 #     where.not("desynced && '{poster}'").
 #     count;
 # end;
-# 
+
 # [Character].each do |klass|
 #   klass.
 #     where(id: [2454, 42346, 46559, 157397, 170695, 173072, 178559, 179439, 184131, 184134, 184136, 185698, 186233, 187369, 187370, 187372, 187521, 193013, 200606, 205653, 208230, 214151, 219928, 223136, 223344, 223369, 223440, 223447]).
@@ -45,6 +45,29 @@ end
 #     find_each do |entry|
 #       puts "#{klass} #{entry.id}"
 #       entry.poster&.destroy!
+#       entry.create_poster image: File.open(entry.image.path)
+#     end
+# end
+
+# [Anime, Manga, Character, Person].each do |klass|
+#   puts klass.name
+#   puts klass.
+#     where("desynced && '{poster}'").
+#     includes(:poster).
+#     map { |entry| entry.poster.present? || !entry.image.exists? ? 0 : 1 }.
+#     sum
+# end;
+
+# [Anime, Manga, Character, Person].each do |klass|
+#   klass.
+#   where("desynced && '{poster}'").
+#     includes(:poster).
+#     find_each do |entry|
+#       next if entry.poster.present?
+#       next unless entry.image.exists?
+# 
+#       puts "#{klass} #{entry.id}"
+# 
 #       entry.create_poster image: File.open(entry.image.path)
 #     end
 # end
