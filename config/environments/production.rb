b7e22c7a9a28d2de39f0f7395e6ff57974a2d478
Andrey Sidorov <takandar@gmail.com>
2023-01-14 17:51:53 +0300

    new cache key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   config.cache_store = :mem_cache_store, 'localhost', {
-    namespace: 'shiki',
+    namespace: 'shiki_v2',
     compress: true,
     value_max_bytes: 1024 * 1024 * 32
   }
