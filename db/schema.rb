913d45ade6d7e95bb9f02dfbba19c195fa7eac86
Andrey Sidorov <takandar@gmail.com>
2023-01-15 21:06:40 +0300

    fix baki franchise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_31_173400) do
+ActiveRecord::Schema.define(version: 2023_01_15_180541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
