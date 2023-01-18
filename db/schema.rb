aebaea533dc3afeafbe02d0b51dcc10adc040615
Andrey Sidorov <takandar@gmail.com>
2023-01-18 20:33:25 +0300

    rename hitori_no_shita franchise to yi_ren_zhi_xia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_180541) do
+ActiveRecord::Schema.define(version: 2023_01_18_172907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
