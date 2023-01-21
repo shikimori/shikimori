b2d0e228486830488a9a7275ceb5ba5e7f24dcc5
Andrey Sidorov <takandar@gmail.com>
2023-01-21 14:09:18 +0300

    inherit Critique::Update from UserContent::UpdateBase

--- spec/operations/critique/update_spec.rb
+++ spec/operations/critique/update_spec.rb
@@ -2,40 +2,25 @@
 
 describe Critique::Update do
   include_context :timecop, 'Wed, 16 Sep 2020 16:23:41 MSK +03:00'
-  subject { Critique::Update.call critique, params, user }
+  subject { described_class.call model, params, user }
 
-  let(:critique) { create :critique }
+  let(:model) { create :critique }
   let(:anime) { create :anime }
 
-  context 'valid params' do
-    let(:params) do
-      {
-        user_id: user.id,
-        target_type: anime.class.name,
-        target_id: anime.id,
-        text: 'x' * Critique::MIN_BODY_SIZE
-      }
-    end
-
-    it do
-      is_expected.to eq true
-      expect(critique.errors).to be_empty
-      expect(critique.changed_at).to be_within(0.1).of Time.zone.now
-      expect(critique.reload).to have_attributes params
-    end
+  let(:params) do
+    {
+      user_id: user.id,
+      target_type: anime.class.name,
+      target_id: anime.id,
+      text: 'x' * Critique::MIN_BODY_SIZE
+    }
   end
 
-  context 'invalid params' do
-    let(:params) do
-      {
-        text: 'too short text'
-      }
-    end
-
-    it do
-      is_expected.to eq false
-      expect(critique.errors).to be_present
-      expect(critique.reload).not_to have_attributes params
-    end
+  it do
+    is_expected.to eq true
+    expect(model).to_not be_changed
+    expect(model.errors).to be_empty
+    expect(model.changed_at).to be_within(0.1).of Time.zone.now
+    expect(model.reload).to have_attributes params
   end
 end
