71672c3821dce9b0b7b62411d834e630b24b2e37
Andrey Sidorov <takandar@gmail.com>
2023-01-26 14:26:30 +0300

    change ad image

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -304,7 +304,7 @@ private
       url: 'https://imba.shop/product/energetik-imba-energy-zemlyanika-by-sweetie-fox?utm_source=shikimori&utm_medium=banner16&utm_campaign=22_01&marker=2VtzqwWWixs'
     }, {
       src: '/assets/globals/events/2023-01-21/inner_9.png',
-      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner17&utm_campaign=23_01&marker=2Vtzqw6sJ6X'
+      url: 'https://imba.shop/collection/anime?utm_source=shikimori&utm_medium=banner18&utm_campaign=25_01&marker=2Vtzqw6sJ6X'
     }],
     # AD END
     placement: Types::Ad::Placement[:content],
