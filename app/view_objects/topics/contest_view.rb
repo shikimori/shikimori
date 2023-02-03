676c096b551d254caadcf7ae1bf2aa071b9079b7
Andrey Sidorov <takandar@gmail.com>
2023-02-03 23:31:57 +0300

    fix broken topic image url for Topics::ContestView, Topics::ContestStatusView and Topics::ClubPageView

--- app/view_objects/topics/contest_view.rb
+++ app/view_objects/topics/contest_view.rb
@@ -1,6 +1,6 @@
 class Topics::ContestView < Topics::View
-  def poster is_2x
-    topic.user.avatar_url(is_2x ? 80 : 48)
+  def poster_image_url is_2x
+    topic.user.avatar_url is_2x ? 80 : 48
   end
 
   def show_inner?
