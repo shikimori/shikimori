9201d5e4c66e1433c3c02563f530f480c3756c30
Andrey Sidorov <takandar@gmail.com>
2023-02-15 00:09:40 +0300

    in hot topics for collection topics track only published collections #2637

--- spec/query_objects/topics/hot_topics_query_spec.rb
+++ spec/query_objects/topics/hot_topics_query_spec.rb
@@ -1,42 +1,40 @@
 describe Topics::HotTopicsQuery do
-  subject { Topics::HotTopicsQuery.call limit: 8 }
+  subject { described_class.call limit: 8 }
 
-  describe '#call' do
-    describe 'order by comments count' do
-      let!(:comment_1) do
-        create :comment,
-          commentable: site_problems_topic,
-          created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
-      end
-      let!(:comment_2) do
-        create :comment,
-          commentable: site_problems_topic,
-          created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
-      end
-      let!(:comment_3) do
-        create :comment,
-          commentable: site_rules_topic,
-          created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
-      end
-
-      it { is_expected.to eq [site_problems_topic, site_rules_topic] }
+  describe 'order by comments count' do
+    let!(:comment_1) do
+      create :comment,
+        commentable: site_problems_topic,
+        created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
+    end
+    let!(:comment_2) do
+      create :comment,
+        commentable: site_problems_topic,
+        created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
+    end
+    let!(:comment_3) do
+      create :comment,
+        commentable: site_rules_topic,
+        created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
     end
 
-    describe 'except offtopic & club topics' do
-      let!(:club) { create :club, :with_topics }
+    it { is_expected.to eq [site_problems_topic, site_rules_topic] }
+  end
 
-      let!(:comment_1) do
-        create :comment,
-          commentable: club.topic,
-          created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
-      end
-      let!(:comment_2) do
-        create :comment,
-          commentable: offtopic_topic,
-          created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
-      end
+  describe 'except offtopic & club topics' do
+    let!(:club) { create :club, :with_topics }
 
-      it { is_expected.to be_empty }
+    let!(:comment_1) do
+      create :comment,
+        commentable: club.topic,
+        created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
     end
+    let!(:comment_2) do
+      create :comment,
+        commentable: offtopic_topic,
+        created_at: (Topics::HotTopicsQuery::INTERVAL - 1.minute).ago
+    end
+
+    it { is_expected.to be_empty }
   end
 end
