6e43798b3c51ef93e05c9180395ba7592d2c97cc
Andrey Sidorov <takandar@gmail.com>
2023-02-26 11:59:39 +0300

    remvoe image from descyned

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_16_144649) do
+ActiveRecord::Schema.define(version: 2023_02_26_085233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
