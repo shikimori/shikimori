ff2187259108faa3fbfdb31c4a94c81399b161aa
Andrey Sidorov <takandar@gmail.com>
2023-04-06 17:51:16 +0300

    remove ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -431,12 +431,12 @@ private
         Types::Ad::Type[:mt_300x250]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
+        # Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
         Types::Ad::Type[:yd_240x600],
         Types::Ad::Type[:mt_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
+        # Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
         Types::Ad::Type[:yd_300x600],
         Types::Ad::Type[:mt_300x600]
       ],
@@ -465,13 +465,13 @@ private
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
+        # Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
         # Types::Ad::Type[:mt_240x400],
         # Types::Ad::Type[:yd_240x500],
         Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
+        # Types::Ad::Type[:special_x300], # ВЗАХЛЁБ
         # Types::Ad::Type[:mt_300x600],
         # Types::Ad::Type[:yd_300x600],
         # Types::Ad::Type[:advrtr_240x400],
