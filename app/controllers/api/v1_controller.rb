86dd963553460f276a8794594af35957e4cecd8e
Andrey Sidorov <takandar@gmail.com>
2023-04-13 21:47:49 +0300

    skip domain actions for api

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -5,6 +5,8 @@ class Api::V1Controller < ShikimoriController
   skip_before_action :touch_last_online
   skip_before_action :verify_authenticity_token,
     if: -> { doorkeeper_token.present? }
+  skip_before_action :ensure_proper_domain
+  skip_before_action :force_301_redirect
 
   serialization_scope :view_context
 
