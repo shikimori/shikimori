2569e898185d7b5c52fdaa0cd7f0d4bbba15c321
Andrey Sidorov <takandar@gmail.com>
2023-04-24 19:21:42 +0300

    exclude EN pages from indexing

--- app/controllers/concerns/locale_concern.rb
+++ app/controllers/concerns/locale_concern.rb
@@ -3,9 +3,20 @@ module LocaleConcern
 
   included do
     before_action :set_locale
+    before_action :ensure_only_russian_locale_indexed
   end
 
   def set_locale
     I18n.locale = params[:locale]&.to_sym || current_user&.locale&.to_sym
+  end
+
+  def ensure_only_russian_locale_indexed
+    return if I18n.russian?
+
+    og(
+      noindex: true,
+      nofollow: true,
+      canonical_url: current_url(locale: nil)
+    )
   end
 end
