8af9292d0ed57f7d30476a010f2bb6a1962ce16b
Andrey Sidorov <takandar@gmail.com>
2023-05-07 21:39:23 +0300

    list of ignored contest suggestions #2682

--- app/operations/contest/cleanup_suggestions.rb
+++ app/operations/contest/cleanup_suggestions.rb
@@ -1,12 +1,29 @@
 class Contest::CleanupSuggestions
   method_object :contest
 
+  CONFIG_PATH = 'config/app/cleanup_suggestions.yml'
+
+  delegate :suggestions, to: :@contest
+
   def call
     Rails.logger.info "Contest::CleanupSuggestions #{@contest.id}"
 
-    @contest.suggestions
+    suggestions
       .joins(:user)
       .merge(User.suspicious)
       .destroy_all
+
+    if @contest.character?
+      suggestions
+        .joins('left join characters on contest_suggestions.item_id = characters.id')
+        .where(characters: { name: config[:names] })
+        .destroy_all
+    end
+  end
+
+private
+
+  def config
+    @config ||= YAML.load_file(Rails.root.join(CONFIG_PATH))
   end
 end
