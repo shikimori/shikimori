28be9333642b9002e57646e9e824d6cf6f25f565
Andrey Sidorov <takandar@gmail.com>
2023-05-21 19:07:46 +0300

    shinkalion franchise

--- db/migrate/20230521144358_fix_broken_franchises.rb
+++ db/migrate/20230521144358_fix_broken_franchises.rb
@@ -2,5 +2,6 @@ class FixBrokenFranchises < ActiveRecord::Migration[6.1]
   def change
     Animes::UpdateFranchises.new.call Manga.where(franchise: %w[young_shima_kousaku])
     Animes::UpdateFranchises.new.call Anime.where(franchise: %w[anisama])
+    Animes::UpdateFranchises.new.call Anime.where(franchise: %w[daisuki])
   end
 end
