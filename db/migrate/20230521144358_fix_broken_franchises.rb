6f6ad158b8b33d056783fd31aea23631cbe5c17a
Andrey Sidorov <takandar@gmail.com>
2023-05-21 18:11:40 +0300

    fix anisama franchise

--- db/migrate/20230521144358_fix_broken_franchises.rb
+++ db/migrate/20230521144358_fix_broken_franchises.rb
@@ -1,5 +1,6 @@
 class FixBrokenFranchises < ActiveRecord::Migration[6.1]
   def change
     Animes::UpdateFranchises.new.call Manga.where(franchise: %w[young_shima_kousaku])
+    Animes::UpdateFranchises.new.call Anime.where(franchise: %w[anisama])
   end
 end
