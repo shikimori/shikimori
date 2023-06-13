8ef6a0dd7f042b47fa732aebf761d9dbe26293fb
Andrey Sidorov <takandar@gmail.com>
2023-06-13 14:29:09 +0300

    disable starrail ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -430,12 +430,12 @@ private
         # Types::Ad::Type[:mt_300x250]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300], # STARRAIL
+        # Types::Ad::Type[:special_x300], # STARRAIL
         Types::Ad::Type[:yd_240x600]
         # Types::Ad::Type[:mt_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300], # STARRAIL
+        # Types::Ad::Type[:special_x300], # STARRAIL
         Types::Ad::Type[:yd_300x600]
         # Types::Ad::Type[:mt_300x600]
       ],
@@ -452,7 +452,7 @@ private
         # Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170], # STARRAIL
+        # Types::Ad::Type[:special_x1170], # STARRAIL
         Types::Ad::Type[:yd_970x250]
         # Types::Ad::Type[:mt_970x250]
       ]
@@ -464,13 +464,13 @@ private
         # Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_240x400] => [
-        Types::Ad::Type[:special_x300] # STARRAIL
+        # Types::Ad::Type[:special_x300] # STARRAIL
         # Types::Ad::Type[:mt_240x400],
         # Types::Ad::Type[:yd_240x500],
         # Types::Ad::Type[:advrtr_240x400]
       ],
       Types::Ad::Meta[:menu_300x600] => [
-        Types::Ad::Type[:special_x300] # STARRAIL
+        # Types::Ad::Type[:special_x300] # STARRAIL
         # Types::Ad::Type[:mt_300x600],
         # Types::Ad::Type[:yd_300x600],
         # Types::Ad::Type[:advrtr_240x400],
@@ -488,7 +488,7 @@ private
         # Types::Ad::Type[:mt_footer_300x250]
       ],
       Types::Ad::Meta[:special_x1170] => [
-        Types::Ad::Type[:special_x1170] # STARRAIL
+        # Types::Ad::Type[:special_x1170] # STARRAIL
       ]
     }
   }
