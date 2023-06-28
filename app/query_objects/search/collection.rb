d5e3dd1a1a6ebd1c8fc8b7f862c22f5baf625775
Andrey Sidorov <takandar@gmail.com>
2023-06-28 22:03:15 +0300

    collections search by tags

--- app/query_objects/search/collection.rb
+++ app/query_objects/search/collection.rb
@@ -1,10 +1,30 @@
 class Search::Collection < Search::SearchBase
   method_object %i[scope! phrase! ids_limit!]
 
+  TAGS_PARSE_REGEXP = /,?\s*(?=#)/
+
+  def call
+    return super unless tags_search?
+
+    @scope
+      .where('tags @> array[?]', phrase_tags)
+      .limit(@ids_limit)
+  end
+
   def elastic_results
     search_klass.call(
       phrase: @phrase,
       limit: @ids_limit
     )
+  end
+
+  def phrase_tags
+    @phrase
+      .split(TAGS_PARSE_REGEXP)
+      .pluck(1..)
+  end
+
+  def tags_search?
+    @phrase.starts_with?('#')
   end
 end
--- spec/query_objects/search/collection_spec.rb
+++ spec/query_objects/search/collection_spec.rb
@@ -1,10 +1,4 @@
 describe Search::Collection do
-  before do
-    allow(Elasticsearch::Query::Collection)
-      .to receive(:call)
-      .with(phrase: phrase, limit: ids_limit)
-      .and_return results
-  end
   subject do
     described_class.call(
       scope: scope,
@@ -15,14 +9,44 @@ describe Search::Collection do
 
   describe '#call' do
     let(:scope) { Collection.all }
-    let(:phrase) { 'zxct' }
     let(:ids_limit) { 2 }
 
-    let(:results) { { collection_1.id => 0.123123 } }
+    context 'elastic search' do
+      let(:phrase) { 'zxct' }
+      let!(:collection_1) { create :collection }
+      let!(:collection_2) { create :collection }
 
-    let!(:collection_1) { create :collection }
-    let!(:collection_2) { create :collection }
+      before do
+        allow(Elasticsearch::Query::Collection)
+          .to receive(:call)
+          .with(phrase: phrase, limit: ids_limit)
+          .and_return results
+      end
+      let(:results) { { collection_1.id => 0.123123 } }
 
-    it { is_expected.to eq [collection_1] }
+      it { is_expected.to eq [collection_1] }
+    end
+
+    context 'tags' do
+      let!(:collection_1) { create :collection, tags: %w[anime test] }
+      let!(:collection_2) { create :collection, tags: %w[test] }
+
+      context 'single tag' do
+        context do
+          let(:phrase) { '#anime' }
+          it { is_expected.to eq [collection_1] }
+        end
+
+        context do
+          let(:phrase) { '#test' }
+          it { is_expected.to eq [collection_2, collection_1] }
+        end
+      end
+
+      context 'multiple tag' do
+        let(:phrase) { ['#anime #test', '#anime, #test'].sample }
+        it { is_expected.to eq [collection_1] }
+      end
+    end
   end
 end
