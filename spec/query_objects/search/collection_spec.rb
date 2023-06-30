8f2dc281eb7e2af75391bc637147356228102a7b
Andrey Sidorov <takandar@gmail.com>
2023-06-30 20:43:20 +0300

    cleanup collection tags migration

--- spec/query_objects/search/collection_spec.rb
+++ spec/query_objects/search/collection_spec.rb
@@ -7,56 +7,54 @@ describe Search::Collection do
     )
   end
 
-  describe '#call' do
-    let(:scope) { Collection.all }
-    let(:ids_limit) { 2 }
+  let(:scope) { Collection.all }
+  let(:ids_limit) { 2 }
 
-    context 'elastic search' do
-      let(:phrase) { 'zxct' }
-      let!(:collection_1) { create :collection }
-      let!(:collection_2) { create :collection }
+  context 'elastic search' do
+    let(:phrase) { 'zxct' }
+    let!(:collection_1) { create :collection }
+    let!(:collection_2) { create :collection }
 
-      before do
-        allow(Elasticsearch::Query::Collection)
-          .to receive(:call)
-          .with(phrase: phrase, limit: ids_limit)
-          .and_return results
-      end
-      let(:results) { { collection_1.id => 0.123123 } }
+    before do
+      allow(Elasticsearch::Query::Collection)
+        .to receive(:call)
+        .with(phrase: phrase, limit: ids_limit)
+        .and_return results
+    end
+    let(:results) { { collection_1.id => 0.123123 } }
 
-      it { is_expected.to eq [collection_1] }
+    it { is_expected.to eq [collection_1] }
+  end
+
+  context 'tags search' do
+    let!(:collection_1) do
+      create :collection,
+        tags: %w[аниме test],
+        cached_votes_up: 99,
+        cached_votes_down: 90
+    end
+    let!(:collection_2) do
+      create :collection,
+        tags: %w[test],
+        cached_votes_up: 10,
+        cached_votes_down: 0
     end
 
-    context 'tags' do
-      let!(:collection_1) do
-        create :collection,
-          tags: %w[аниме test],
-          cached_votes_up: 99,
-          cached_votes_down: 90
-      end
-      let!(:collection_2) do
-        create :collection,
-          tags: %w[test],
-          cached_votes_up: 10,
-          cached_votes_down: 0
-      end
-
-      context 'single tag' do
-        context do
-          let(:phrase) { ['#anime', '#аниме', '#Аниме'].sample }
-          it { is_expected.to eq [collection_1] }
-        end
-
-        context do
-          let(:phrase) { '#test' }
-          it { is_expected.to eq [collection_2, collection_1] }
-        end
-      end
-
-      context 'multiple tag' do
-        let(:phrase) { ['#anime #test', '#anime, #test'].sample }
+    context 'single tag' do
+      context do
+        let(:phrase) { ['#anime', '#аниме', '#Аниме'].sample }
         it { is_expected.to eq [collection_1] }
       end
+
+      context do
+        let(:phrase) { '#test' }
+        it { is_expected.to eq [collection_2, collection_1] }
+      end
+    end
+
+    context 'multiple tag' do
+      let(:phrase) { ['#anime #test', '#anime, #test'].sample }
+      it { is_expected.to eq [collection_1] }
     end
   end
 end
