c53cd9846554f95ec17ede97d92ff342d1969f09
Andrey Sidorov <takandar@gmail.com>
2023-07-02 17:34:19 +0300

    attempt to filter cloudflare ips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -146,6 +146,11 @@ private
   protected
 
   def extract_ip_from request
-    request.remote_ip&.split(',')&.first
+    (
+      request.env['HTTP_X_FORWARDED_FOR'].presence ||
+        request.env['HTTP_X_REAL_IP'].presence ||
+        request.env['REMOTE_ADDR'].presence ||
+        request.remote_ip
+    )&.split(',')&.first
   end
 end
