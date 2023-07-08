79ebd1881ca0a57771bf5cb540f1a041d443b308
Andrey Sidorov <takandar@gmail.com>
2023-07-08 22:38:09 +0300

    add specs for topic in premoderation forum

--- spec/policies/topic/access_policy_spec.rb
+++ spec/policies/topic/access_policy_spec.rb
@@ -1,10 +1,37 @@
 describe Topic::AccessPolicy do
   subject { described_class.allowed? topic, decorated_user }
-  let(:decorated_user) { [user.decorate, nil].sample }
+  let(:decorated_user) do
+    is_moderator ?
+      user.decorate :
+      [user.decorate, nil].sample
+  end
+  let(:is_moderator) { false }
+  before do
+    if is_moderator
+      allow(decorated_user).to receive(:moderation_staff?).and_return true
+    end
+  end
 
   context 'common topic' do
-    let(:topic) { build_stubbed :topic }
+    let(:topic) { build :topic }
     it { is_expected.to eq true }
+  end
+
+  context 'topic in premoderation forum' do
+    let(:topic) { build :topic, forum_id: Forum::PREMODERATION_ID, user: topic_user }
+    let(:topic_user) { user_2 }
+    it { is_expected.to eq false }
+
+    context 'topic author' do
+      let(:decorated_user) { user.decorate }
+      let(:topic_user) { user }
+      it { is_expected.to eq true }
+    end
+
+    context 'moderator' do
+      let(:is_moderator) { true }
+      it { is_expected.to eq true }
+    end
   end
 
   context 'club topic' do
@@ -16,24 +43,21 @@ describe Topic::AccessPolicy do
     end
     let(:is_allowed) { [true, false].sample }
 
-    let(:club) { build_stubbed :club }
-    let(:club_page) { build_stubbed :club_page, club: club }
+    let(:club) { build :club }
+    let(:club_page) { build :club_page, club: club }
 
     let(:topic) do
       [
-        build_stubbed(:club_topic, linked: club),
-        build_stubbed(:club_page_topic, linked: club_page),
-        build_stubbed(:club_user_topic, linked: club)
+        build(:club_topic, linked: club),
+        build(:club_page_topic, linked: club_page),
+        build(:club_user_topic, linked: club)
       ].sample
     end
 
     it { is_expected.to eq is_allowed }
 
     context 'moderator' do
-      let(:comment_user) { user }
-      let(:decorated_user) { user.decorate }
-      before { allow(decorated_user).to receive(:moderation_staff?).and_return true }
-
+      let(:is_moderator) { true }
       it { is_expected.to eq true }
     end
   end
