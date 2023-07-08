78371ff379ef0e7c75201aa9fd4c72ebd49c2cf0
Andrey Sidorov <takandar@gmail.com>
2023-07-08 21:30:11 +0300

    apply restrictions for new negative roles #2713

--- spec/policies/versions_policy_spec.rb
+++ spec/policies/versions_policy_spec.rb
@@ -124,6 +124,97 @@ describe VersionsPolicy do
     end
   end
 
+  context 'not_trusted_videos_changer' do
+    before { user.roles = %i[not_trusted_videos_changer] }
+
+    context 'not videos field' do
+      let(:field) { 'russian' }
+      it { expect(version_allowed).to eq true }
+      it { expect(change_allowed).to eq true }
+    end
+
+    context 'videos field' do
+      let(:field) { Abilities::VersionVideosModerator::MANAGED_FIELDS.sample }
+
+      context 'not DbEntry model' do
+        let(:item) { build_stubbed :poster }
+        it { expect(version_allowed).to eq true }
+        it { expect(change_allowed).to eq true }
+      end
+
+      context 'DbEntry model' do
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
+      end
+
+      context 'video model' do
+        let(:item) { build_stubbed :video }
+        let(:field) { 'zxc' }
+
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
+      end
+    end
+  end
+
+  context 'not_trusted_images_changer' do
+    before { user.roles = %i[not_trusted_images_changer] }
+
+    context 'not images field' do
+      let(:field) { 'russian' }
+      it { expect(version_allowed).to eq true }
+      it { expect(change_allowed).to eq true }
+    end
+
+    context 'videos field' do
+      let(:field) { Abilities::VersionImagesModerator::MANAGED_FIELDS.sample }
+
+      context 'not DbEntry model' do
+        let(:item) { build_stubbed :video }
+        it { expect(version_allowed).to eq true }
+        it { expect(change_allowed).to eq true }
+      end
+
+      context 'DbEntry model' do
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
+      end
+
+      context 'poster model' do
+        let(:item) { build_stubbed :poster }
+        let(:field) { 'zxc' }
+
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
+      end
+    end
+  end
+
+  context 'not_trusted_links_changer' do
+    before { user.roles = %i[not_trusted_links_changer] }
+
+    context 'not links field' do
+      let(:field) { 'russian' }
+      it { expect(version_allowed).to eq true }
+      it { expect(change_allowed).to eq true }
+    end
+
+    context 'links field' do
+      let(:field) { Abilities::VersionLinksModerator::MANAGED_FIELDS.sample }
+
+      context 'not DbEntry model' do
+        let(:item) { build_stubbed :video }
+        it { expect(version_allowed).to eq true }
+        it { expect(change_allowed).to eq true }
+      end
+
+      context 'DbEntry model' do
+        it { expect(version_allowed).to eq false }
+        it { expect(change_allowed).to eq false }
+      end
+    end
+  end
+
   context 'not matched author' do
     let(:author) { user_2 }
     it { expect(version_allowed).to eq false }
