998c7e17fb3cf74ac49a530afc53d282fb54b399
Andrey Sidorov <takandar@gmail.com>
2023-07-08 22:33:13 +0300

    refactor Topic::AccessPolicy specs

--- spec/policies/topic/access_policy_spec.rb
+++ spec/policies/topic/access_policy_spec.rb
@@ -1,25 +1,33 @@
 describe Topic::AccessPolicy do
   subject { described_class.allowed? topic, decorated_user }
-
   let(:decorated_user) { [user.decorate, nil].sample }
-  before do
-    allow(Club::AccessPolicy)
-      .to receive(:allowed?)
-      .with(club, decorated_user)
-      .and_return is_allowed
+
+  context 'common topic' do
+    let(:topic) { build_stubbed :topic }
+    it { is_expected.to eq true }
   end
-  let(:club) { build_stubbed :club }
-  let(:club_page) { build_stubbed :club_page, club: club }
 
-  let(:is_allowed) { [true, false].sample }
-
-  describe 'user' do
-    let(:topic) { build_stubbed :club_topic, linked: club }
-
-    context 'guest' do
-      let(:decorated_user) { nil }
-      it { is_expected.to eq is_allowed }
+  context 'club topic' do
+    before do
+      allow(Club::AccessPolicy)
+        .to receive(:allowed?)
+        .with(club, decorated_user)
+        .and_return is_allowed
     end
+    let(:is_allowed) { [true, false].sample }
+
+    let(:club) { build_stubbed :club }
+    let(:club_page) { build_stubbed :club_page, club: club }
+
+    let(:topic) do
+      [
+        build_stubbed(:club_topic, linked: club),
+        build_stubbed(:club_page_topic, linked: club_page),
+        build_stubbed(:club_user_topic, linked: club)
+      ].sample
+    end
+
+    it { is_expected.to eq is_allowed }
 
     context 'moderator' do
       let(:comment_user) { user }
@@ -27,30 +35,6 @@ describe Topic::AccessPolicy do
       before { allow(decorated_user).to receive(:moderation_staff?).and_return true }
 
       it { is_expected.to eq true }
-    end
-
-    context 'user' do
-      context 'no linked club' do
-        let(:topic) { build_stubbed :topic }
-        it { is_expected.to eq true }
-      end
-
-      context 'linked club' do
-        context 'Topics::EntryTopics::ClubTopic' do
-          let(:topic) { build_stubbed :club_topic, linked: club }
-          it { is_expected.to eq is_allowed }
-        end
-
-        context 'Topics::EntryTopics::ClubPageTopic' do
-          let(:topic) { build_stubbed :club_page_topic, linked: club_page }
-          it { is_expected.to eq is_allowed }
-        end
-
-        context 'Topics::ClubUserTopic' do
-          let(:topic) { build_stubbed :club_user_topic, linked: club }
-          it { is_expected.to eq is_allowed }
-        end
-      end
     end
   end
 end
