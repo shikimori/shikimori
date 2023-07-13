a8e02b8ba93c126d049d3d1d978e7cb850a0be04
VelKod <65678373+VelKod@users.noreply.github.com>
2023-07-13 22:22:25 +0700

    Update version.rb

--- app/models/version.rb
+++ app/models/version.rb
@@ -12,7 +12,7 @@ class Version < ApplicationRecord # rubocop:disable ClassLength
     user_id_key: :user_id
   )
   antispam(
-    per_day: 10,
+    per_day: 3,
     disable_if: -> {
       item_diff['description_ru'].blank? || user.staff? || user.trusted_version_changer?
     },
