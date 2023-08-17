0f1cd74dc88f29b095099a0f25d4e718eec206b3
Andrey Sidorov <takandar@gmail.com>
2023-08-17 21:19:49 +0300

    add GUEST_KEYS with polls to JsExports::Supervisor

--- app/view_objects/js_exports/supervisor.rb
+++ app/view_objects/js_exports/supervisor.rb
@@ -1,27 +1,36 @@
 class JsExports::Supervisor
   include Singleton
 
-  KEYS = %i[user_rates topics comments polls]
+  USER_KEYS = %i[user_rates topics comments polls]
+  GUEST_KEYS = %i[polls]
 
   def export user
-    return unless user
-    ability = Ability.new user
-
-    KEYS.each_with_object({}) do |key, memo|
-      memo[key] = instance(key).export(user, ability)
-    end
+    user_export user, self.class.user_keys(user)
   end
 
-  def sweep html
+  def sweep user, html
     return html if html.blank?
 
-    KEYS.each do |key|
+    self.class.user_keys(user).each do |key|
       instance(key).sweep html
     end
+
     html
   end
 
+  def self.user_keys user
+    user ? USER_KEYS : GUEST_KEYS
+  end
+
 private
+
+  def user_export user, keys
+    ability = Ability.new user if user
+
+    keys.index_with do |key|
+      instance(key).export(user, ability)
+    end
+  end
 
   def instance key
     "JsExports::#{key.to_s.classify.pluralize}Export".constantize.instance
--- spec/view_objects/js_exports/supervisor_spec.rb
+++ spec/view_objects/js_exports/supervisor_spec.rb
@@ -9,9 +9,21 @@ describe JsExports::Supervisor do
 
   describe '#export' do
     subject(:export) { view.export user }
-    it do
-      expect(export).to be_kind_of Hash
-      expect(export.keys).to eq described_class::KEYS
+
+    context 'user' do
+      it do
+        expect(export).to be_kind_of Hash
+        expect(export.keys).to eq described_class::USER_KEYS
+      end
+    end
+
+    context 'guest' do
+      let(:user) { nil }
+
+      it do
+        expect(export).to be_kind_of Hash
+        expect(export.keys).to eq described_class::GUEST_KEYS
+      end
     end
   end
 
@@ -22,21 +34,37 @@ describe JsExports::Supervisor do
       allow(comments_export).to receive :sweep
       allow(polls_export).to receive :sweep
     end
-    subject! { view.sweep html }
+    subject! { view.sweep user, html }
 
     context 'with html' do
       let(:html) { 'test' }
-      it do
-        is_expected.to eq html
-        expect(user_rates_export).to have_received(:sweep).with html
-        expect(topics_export).to have_received(:sweep).with html
-        expect(comments_export).to have_received(:sweep).with html
-        expect(polls_export).to have_received(:sweep).with html
+
+      context 'user' do
+        it do
+          is_expected.to eq html
+          expect(user_rates_export).to have_received(:sweep).with html
+          expect(topics_export).to have_received(:sweep).with html
+          expect(comments_export).to have_received(:sweep).with html
+          expect(polls_export).to have_received(:sweep).with html
+        end
+      end
+
+      context 'guest' do
+        let(:user) { nil }
+
+        it do
+          is_expected.to eq html
+          expect(user_rates_export).to_not have_received :sweep
+          expect(topics_export).to_not have_received :sweep
+          expect(comments_export).to_not have_received :sweep
+          expect(polls_export).to have_received(:sweep).with html
+        end
       end
     end
 
     context 'without html' do
       let(:html) { nil }
+
       it do
         is_expected.to eq html
         expect(user_rates_export).to_not have_received :sweep
