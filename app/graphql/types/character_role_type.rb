0211ec80b8c6442484e7f81d172488eb66e9c030
Andrey Sidorov <takandar@gmail.com>
2023-08-20 00:12:34 +0300

    add rolesRussian field

--- app/graphql/types/character_role_type.rb
+++ app/graphql/types/character_role_type.rb
@@ -1,5 +1,10 @@
 class Types::CharacterRoleType < Types::BaseObject
   field :id, GraphQL::Types::BigInt
-  field :roles, [String]
   field :character, Types::CharacterType
+
+  field :roles, [String]
+  field :roles_russian, [String]
+  def roles_russian
+    object.roles.map { |role| I18n.t "role.#{role}", default: role }
+  end
 end
--- app/graphql/types/person_role_type.rb
+++ app/graphql/types/person_role_type.rb
@@ -1,5 +1,10 @@
 class Types::PersonRoleType < Types::BaseObject
   field :id, GraphQL::Types::BigInt
-  field :roles, [String]
   field :person, Types::PersonType
+
+  field :roles, [String]
+  field :roles_russian, [String]
+  def roles_russian
+    object.roles.map { |role| I18n.t "role.#{role}", default: role }
+  end
 end
