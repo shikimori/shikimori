019f1f6e0902d44106c766efe70ff20b331aec80
Andrey Sidorov <takandar@gmail.com>
2023-08-20 00:20:57 +0300

    extract Types::Concerns::AniMangaFields

--- app/graphql/types/manga_type.rb
+++ app/graphql/types/manga_type.rb
@@ -1,43 +1,14 @@
 class Types::MangaType < Types::BaseObject
   include Types::Concerns::DbEntryFields
+  include Types::Concerns::AniMangaFields
   include Types::Concerns::DescriptionFields
 
-  field :license_name_ru, String
-  field :english, String
-  field :franchise, String, description: 'Franchise name'
   field :kind, Types::Enums::Manga::KindEnum
-  field :score, Float
-  field :status, Types::Enums::StatusEnum
+
   field :volumes, Integer
   field :chapters, Integer
 
-  field :aired_on, Types::Scalars::IncompleteDate
-  field :released_on, Types::Scalars::IncompleteDate
-
-  field :licensors, [String]
-
-  field :is_censored, Boolean
-  def is_censored # rubocop:disable Naming/PredicateName
-    object.censored?
-  end
-
-  field :genres, [Types::GenreType]
   field :publishers, [Types::PublisherType]
-
-  field :external_links, [Types::ExternalLinkType], complexity: 10
-  def external_links
-    decorated_object.menu_external_links
-  end
-
-  field :character_roles, [Types::CharacterRoleType], complexity: 10
-  def character_roles
-    object.person_roles.select(&:character_id)
-  end
-
-  field :person_roles, [Types::PersonRoleType], complexity: 10
-  def person_roles
-    object.person_roles.select(&:person_id)
-  end
 
   field :user_rate, Types::UserRateType, complexity: 50
   def user_rate
