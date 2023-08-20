b44fda6dfeccfd187c6ee3cdb6fbc7c733cb7ea3
Andrey Sidorov <takandar@gmail.com>
2023-08-20 17:23:37 +0300

    rename roles fields, add new examples to graphql.html

--- app/graphql/types/character_role_type.rb
+++ app/graphql/types/character_role_type.rb
@@ -2,9 +2,13 @@ class Types::CharacterRoleType < Types::BaseObject
   field :id, GraphQL::Types::BigInt
   field :character, Types::CharacterType
 
-  field :roles, [String]
-  field :roles_russian, [String]
-  def roles_russian
+  field :roles_ru, [String]
+  def roles_ru
     object.roles.map { |role| I18n.t "role.#{role}", default: role }
+  end
+
+  field :roles_en, [String]
+  def roles_en
+    object.roles
   end
 end
