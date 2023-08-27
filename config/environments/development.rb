7b07198805f93ef35d0e76e39cdcd32f565186e0
Andrey Sidorov <takandar@gmail.com>
2023-08-27 13:14:33 +0300

    upgrading configs to rails 7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
@@ -34,15 +37,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # RAILS 6.0 UPGRADE
-  # if Shikimori::PROTOCOL == 'https'
-  #   config.force_ssl = true
-  #   config.ssl_options = {
-  #     hsts: { preload: true, subdomains: true, expires: 3.years }
-  #   }
-  # end
 
   config.hosts << 'shikimori.local'
   config.hosts << 'shiki.local'
@@ -77,8 +71,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  # config.assets.debug = true
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
