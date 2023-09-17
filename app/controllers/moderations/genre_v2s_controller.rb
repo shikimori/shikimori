624d0e7b83b68d6031bd3db026aec0a3aa8232dc
Andrey Sidorov <takandar@gmail.com>
2023-09-17 18:14:10 +0300

    move updated sorting options to genresv2

--- app/controllers/moderations/genre_v2s_controller.rb
+++ app/controllers/moderations/genre_v2s_controller.rb
@@ -8,4 +8,12 @@ class Moderations::GenreV2sController < Moderations::GenresController
   def index_url
     moderations_genre_v2s_url
   end
+
+  def sorting_options type = GenreV2
+    [
+      build_sort(sorting_field(type), sorting_order(type)),
+      build_sort(genres_sort_key, :asc),
+      build_sort(:entry_type, :asc)
+    ]
+  end
 end
