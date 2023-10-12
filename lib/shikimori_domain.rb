db954394944c861828e6c1c1bf86eb4d26cddd07
Andrey Sidorov <takandar@gmail.com>
2023-10-12 10:34:50 +0300

    misc ShikimoriDomain changes

--- lib/shikimori_domain.rb
+++ lib/shikimori_domain.rb
@@ -1,10 +1,13 @@
 module ShikimoriDomain
-  HOSTS = %w[shikimori.me shikimori.one shikimori.org] + (
-    Rails.env.development? ? %w[shikimori.local shiki.local localhost] : []
-  )
-  BANNED_HOSTS = %w[shikimori.org shikimori.me]
+  FOREVER_BANNED_HOST = 'shikimori.org'
   OLD_HOST = 'shikimori.me'
   NEW_HOST = 'shikimori.one'
+
+  HOSTS = [NEW_HOST, OLD_HOST, FOREVER_BANNED_HOST] + (
+    Rails.env.development? ? %w[shikimori.local shiki.local localhost] : []
+  )
+
+  BANNED_HOSTS = [FOREVER_BANNED_HOST, OLD_HOST]
 
   PROPER_HOST = Rails.env.production? ?
     NEW_HOST :
