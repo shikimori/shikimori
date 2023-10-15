c568a59835ccc0f87a2a78ee5aba86ac44ae1dc4
Andrey Sidorov <takandar@gmail.com>
2023-10-15 20:30:54 +0300

    spcify more ids of genres2, add censored?, banned?, probably_banned?

--- app/models/genre_v2.rb
+++ app/models/genre_v2.rb
@@ -10,15 +10,18 @@ class GenreV2 < ApplicationRecord
   boolean_attribute :active
   boolean_attribute :censored
 
+  EROTICA_IDS = [539, 540]
+  HENTAI_IDS = [12, 59]
+  YAOI_IDS = [195, 197]
+  YURI_IDS = [196, 198]
+
   SHOUNEN_AI_IDS = [133, 165]
   SHOUJO_AI_IDS = [129, 170]
 
+  BANNED_IDS = YAOI_IDS + YURI_IDS
   PROBABLY_BANNED_IDS = SHOUJO_AI_IDS + SHOUNEN_AI_IDS
 
-  EROTICA_IDS = [539, 540]
-  HENTAI_IDS = [12, 59]
-
-  CENSORED_IDS = EROTICA_IDS + HENTAI_IDS + PROBABLY_BANNED_IDS
+  CENSORED_IDS = EROTICA_IDS + HENTAI_IDS + BANNED_IDS + PROBABLY_BANNED_IDS
 
   def to_param
     "#{id}-#{name.tr ' ', '-'}"
@@ -30,5 +33,17 @@ class GenreV2 < ApplicationRecord
 
   def manga?
     entry_type == Types::GenreV2::EntryType['Manga']
+  end
+
+  def censored?
+    CENSORED_IDS.include? id
+  end
+
+  def banned?
+    id.in? BANNED_IDS
+  end
+
+  def probably_banned?
+    id.in? PROBABLY_BANNED_IDS
   end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,4 +1,3 @@
-describe Genre do
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
