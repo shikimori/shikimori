40667f591ffe4a48e8cc89b463587dd578f0977e
Andrey Sidorov <takandar@gmail.com>
2023-10-15 16:51:07 +0300

    fix 7 franchises

---
+++ db/migrate/20231015133455_fix_franchises.rb
@@ -0,0 +1,17 @@
+class FixFranchises < ActiveRecord::Migration[7.0]
+  def change
+    Animes::UpdateFranchises.new.call(
+      Anime.where(
+        franchise: %w[
+          yakusoku_no_neverland
+          fire_force
+          temple
+          choco_kyouju_no_oheya
+          gamera
+          ookami_kodomo_no_ame_to_yuki
+          mirai
+        ]
+      )
+    )
+  end
+end
