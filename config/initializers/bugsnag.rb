427cf08afaeeed2bcf23e6491c7de8bc5017dc37
Andrey Sidorov <takandar@gmail.com>
2023-11-17 20:16:19 +0300

    use class name instead of class object in bugsnag config

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,11 +2,8 @@ if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = '13facc6d8be8f234e594695f9a61b9a9'
 
-    Shikimori::IGNORED_EXCEPTIONS
-      .map { |v| v.constantize rescue NameError }
-      .reject { |v| v == NameError }
-      .each do |klass|
-        config.ignore_classes << klass
-      end
+    Shikimori::IGNORED_EXCEPTIONS.each do |klass|
+      config.ignore_classes << klass
+    end
   end
 end
