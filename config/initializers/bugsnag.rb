be9f353103e885d3edbbc857015544e1e7030cda
Andrey Sidorov <takandar@gmail.com>
2023-11-17 20:30:34 +0300

    attempt to fix bugsnag ignore_classes

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,9 +1,6 @@
 if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = 'b2baf38009494b2a6acd61a8c940d9ea'
-
-    Shikimori::IGNORED_EXCEPTIONS.each do |klass|
-      config.ignore_classes << klass
-    end
+    config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
   end
 end
