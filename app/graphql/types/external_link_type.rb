be63e7a44502c23aabec08216c51dbd9286791fc
Andrey Sidorov <takandar@gmail.com>
2023-12-04 19:12:05 +0300

    nullable fields in Types::ExternalLinkType

--- app/graphql/types/external_link_type.rb
+++ app/graphql/types/external_link_type.rb
@@ -1,9 +1,9 @@
 class Types::ExternalLinkType < Types::BaseObject
-  field :id, ID, null: false
+  field :id, ID, null: true # MAL external link has null field
 
   field :kind, Types::Enums::ExternalLink::KindEnum, null: false
   field :url, String, null: false
 
-  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-  field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: true # MAL external link has null field
+  field :updated_at, GraphQL::Types::ISO8601DateTime, null: true # MAL external link has null field
 end
