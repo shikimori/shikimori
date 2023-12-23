439beaec0f8f854d46702624cc019ae73264e217
Andrey Sidorov <takandar@gmail.com>
2023-12-23 21:55:43 +0300

    add player_url and image_url fields to graphql Video type #2745

--- app/graphql/types/video_type.rb
+++ app/graphql/types/video_type.rb
@@ -3,4 +3,6 @@ class Types::VideoType < Types::BaseObject
   field :name, String
   field :url, String, null: false
   field :kind, Types::Enums::Video::KindEnum, null: false
+  field :player_url, String, null: false
+  field :image_url, String, null: false
 end
