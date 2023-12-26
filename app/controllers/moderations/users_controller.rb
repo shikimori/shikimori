143850eac096fcbb2472bd893d566286c845f8e7
Andrey Sidorov <takandar@gmail.com>
2023-12-27 01:27:00 +0300

    log mass bans

--- app/controllers/moderations/users_controller.rb
+++ app/controllers/moderations/users_controller.rb
@@ -3,18 +3,24 @@ class Moderations::UsersController < ModerationsController
   MAX_BAN_USERS_LIMIT = 200
   MASS_BAN_ERROR_ALERT =
     "Массовый бан можно выдавать только выборкам размером < #{MAX_BAN_USERS_LIMIT} пользователей"
+  MASS_BAN_NOTICE = 'Пользователи забанены на 10 лет. Запись о банах внесена в логи.'
 
-  def index
+  def index # rubocop:disable Metrics/AbcSize
     og noindex: true, nofollow: true
     og page_title: i18n_t('page_title')
 
     if params[:mass_ban]
+      authorize! :mass_ban, User
+
       if users_scope.size > MAX_BAN_USERS_LIMIT
         return redirect_to current_url(mass_ban: nil),
           alert: MASS_BAN_ERROR_ALERT
       end
       users_scope.update_all read_only_at: 10.years.from_now
-      return redirect_to current_url(mass_ban: nil)
+      NamedLogger.mass_ban.info(
+        "User=#{current_user.id} IDS=#{users_scope.pluck(:id).join(',')}"
+      )
+      return redirect_to current_url(mass_ban: nil), notice: MASS_BAN_NOTICE
     end
 
     @collection = users_scope.paginate(@page, PER_PAGE)
