9d6517c6fce5bf212ed751048d20c5167390a1b1
Andrey Sidorov <takandar@gmail.com>
2023-12-27 01:49:21 +0300

    list mass registrations

--- app/controllers/moderations/users_controller.rb
+++ app/controllers/moderations/users_controller.rb
@@ -5,7 +5,10 @@ class Moderations::UsersController < ModerationsController
     "Массовый бан можно выдавать только выборкам размером < #{MAX_BAN_USERS_LIMIT} пользователей"
   MASS_BAN_NOTICE = 'Пользователи забанены на 10 лет. Запись о банах внесена в логи.'
 
-  def index # rubocop:disable Metrics/AbcSize
+  MASS_REGISTRATION_INTERVAL = 1.month
+  MASS_REGISTRATION_THRESHOLD = 5
+
+  def index # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     og noindex: true, nofollow: true
     og page_title: i18n_t('page_title')
 
@@ -24,6 +27,7 @@ class Moderations::UsersController < ModerationsController
     end
 
     @collection = users_scope.paginate(@page, PER_PAGE)
+    @mass_ips = fetch_mass_ips
   end
 
 private
@@ -45,5 +49,16 @@ private
     end
 
     scope
+  end
+
+  def fetch_mass_ips
+    Users::Query
+      .fetch
+      .created_on(MASS_REGISTRATION_INTERVAL.ago.to_date.to_s, Users::Query::ConditionType[:gte])
+      .group_by(&:current_sign_in_ip)
+      .sort_by { |_ip, users| users.size }
+      .map { |ip, users| [ip, users.size, users] }
+      .reverse.select { |(_ip, size, _users)| size > MASS_REGISTRATION_THRESHOLD }
+      .map(&:first)
   end
 end
