478b27d9c91dfded4ae7f61ea57cbd12c1d640a5
Andrey Sidorov <takandar@gmail.com>
2024-01-06 13:22:56 +0300

    new bugsnag account

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 if defined? Bugsnag
   Bugsnag.configure do |config|
-    config.api_key = 'bd16dd05d285d815afb841c7dd13338b'
+    config.api_key = 'ffad25c27fa0e8e7c5d5a9a3ba925f5f'
     config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
   end
 end
