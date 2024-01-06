88ca2710ded5d16a549bf29d65d4ca20c046a47f
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-01-06 09:41:12 +0300

    fix: correct status string name for Anime

--- app/graphql/types/scalars/anime/status_string.rb
+++ app/graphql/types/scalars/anime/status_string.rb
@@ -1,4 +1,4 @@
 class Types::Scalars::Anime::StatusString < Types::StringType
-  graphql_name 'MangaStatusString'
+  graphql_name 'AnimeStatusString'
   i18n_comma_separeted_description 'enumerize.anime.status'
 end
