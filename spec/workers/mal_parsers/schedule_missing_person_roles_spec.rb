bc9625aec07a81f78419835c2327fe37e0388416
Andrey Sidorov <takandar@gmail.com>
2024-01-06 13:43:05 +0300

    attempt to fix unstable spec

--- spec/workers/mal_parsers/schedule_missing_person_roles_spec.rb
+++ spec/workers/mal_parsers/schedule_missing_person_roles_spec.rb
@@ -3,23 +3,23 @@ describe MalParsers::ScheduleMissingPersonRoles do
 
   let!(:character) { create :character }
   let!(:person_role_1) { create :person_role, character_id: character.id }
-  let!(:person_role_2) { create :person_role, character_id: 123 }
-  let!(:person_role_3) { create :person_role, character_id: 123 }
-  let!(:person_role_4) { create :person_role, character_id: 456 }
-  let!(:person_role_5) { create :person_role, person_id: 789 }
+  let!(:person_role_2) { create :person_role, character_id: 123456 }
+  let!(:person_role_3) { create :person_role, character_id: 123456 }
+  let!(:person_role_4) { create :person_role, character_id: 456789 }
+  let!(:person_role_5) { create :person_role, person_id: 789012 }
 
   before { allow(MalParsers::FetchEntry).to receive :perform_async }
   subject! { worker.perform 'character' }
 
   it do
-    is_expected.to eq [123, 456]
+    is_expected.to eq [123456, 456789]
     expect(MalParsers::FetchEntry)
       .to have_received(:perform_async)
-      .with(123, 'character')
+      .with(123456, 'character')
       .ordered
     expect(MalParsers::FetchEntry)
       .to have_received(:perform_async)
-      .with(456, 'character')
+      .with(456789, 'character')
       .ordered
   end
 end
