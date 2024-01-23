188e75fbe25984632ee173a720d19a70a1e199b2
Andrey Sidorov <takandar@gmail.com>
2024-01-23 14:08:18 +0300

    fix broken nicknames

---
+++ db/migrate/20240123110027_fix_broken_nicknames.rb
@@ -0,0 +1,8 @@
+class FixBrokenNicknames < ActiveRecord::Migration[7.0]
+  def up
+    User.where(nickname: NameValidator::PREDEFINED_PATHS).find_each do |user|
+      user.update! nickname: user.nickname + '_'
+      puts user.nickname
+    end
+  end
+end
