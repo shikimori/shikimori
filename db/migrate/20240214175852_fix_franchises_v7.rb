bd83c930ff1b8ed8f403e429fa390ecb3eea5c7b
Andrey Sidorov <takandar@gmail.com>
2024-02-14 21:07:07 +0300

    fix umisea franchise

--- db/migrate/20240214175852_fix_franchises_v7.rb
+++ db/migrate/20240214175852_fix_franchises_v7.rb
@@ -1,5 +1,11 @@
 class FixFranchisesV7 < ActiveRecord::Migration[7.0]
   def up
-    Animes::UpdateFranchises.new.call Anime.where(franchise: %w[planetarian kaginado])
+    Animes::UpdateFranchises.new.call(
+      Anime.where(
+        franchise: %w[
+          planetarian kaginado umisea
+        ]
+      )
+    )
   end
 end
