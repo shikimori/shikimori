2ab12cd885400ee29df7a2cda2d0d29af85f25be
Andrey Sidorov <takandar@gmail.com>
2024-02-18 13:47:25 +0300

    another attempt to discard some exceptions

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,11 @@
 if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = 'ecf287b64e83e9e0314344a8b58ada64'
+
     config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
+
+    Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
+      config.discard_classes << class_name
+    end
   end
 end
