ffa3ae130c25341f0feca8cec238b5c729d777f0
Andrey Sidorov <takandar@gmail.com>
2024-04-06 19:37:06 +0300

    display censored posters approved by moderators

--- app/decorators/ani_manga_decorator/poster_helpers.rb
+++ app/decorators/ani_manga_decorator/poster_helpers.rb
@@ -1,7 +1,10 @@
 module AniMangaDecorator::PosterHelpers
   def poster
     return if rkn_banned? || rkn_banned_poster?
-    return if respond_to?(:genres_v2) && genres_v2.any?(&:temporarily_posters_disabled?)
+
+    if respond_to?(:genres_v2) && genres_v2.any?(&:temporarily_posters_disabled?)
+      return super&.moderation_accepted? ? super : nil
+    end
 
     super
   end
