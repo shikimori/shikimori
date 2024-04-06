6e0d5e95d49942c9a58b9caaa8956dd0d2b84a1c
Andrey Sidorov <takandar@gmail.com>
2024-04-06 15:48:49 +0300

    add antispam for Ignore and TopicIgnore

--- app/models/ignore.rb
+++ app/models/ignore.rb
@@ -1,5 +1,12 @@
 # TODO: rename to UserIgnore
 class Ignore < ApplicationRecord
+  include AntispamConcern
+
+  antispam(
+    per_day: 300,
+    user_id_key: :user_id
+  )
+
   belongs_to :user
   belongs_to :target, class_name: 'User'
 
--- app/models/topic_ignore.rb
+++ app/models/topic_ignore.rb
@@ -1,4 +1,11 @@
 class TopicIgnore < ApplicationRecord
+  include AntispamConcern
+
+  antispam(
+    per_day: 300,
+    user_id_key: :user_id
+  )
+
   belongs_to :user
   belongs_to :topic
 end
