ef951fc09f29c8ca43448dd56abc503f6c081620
Andrey Sidorov <takandar@gmail.com>
2024-04-07 13:24:45 +0300

    temporarily allow all execption in bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,10 +2,10 @@ if defined? Bugsnag
   Bugsnag.configure do |config|
     config.api_key = '021939c1780de638daee18bb65cb36cc'
 
-    config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
-
-    Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
-      config.discard_classes << class_name
-    end
+    # config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
+    #
+    # Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
+    #   config.discard_classes << class_name
+    # end
   end
 end
