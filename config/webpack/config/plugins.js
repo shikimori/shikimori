4be87471bf5d73af5168b5cc506f825916a1ad1a
Andrey Sidorov <takandar@gmail.com>
2024-04-19 03:18:44 +0300

    setup __VUE_PROD_HYDRATION_MISMATCH_DETAILS__

--- config/webpack/config/plugins.js
+++ config/webpack/config/plugins.js
@@ -18,7 +18,8 @@ module.exports = {
       IS_FAYE_LOGGING: process.env.NODE_ENV === 'production',
       // IS_FAYE_LOGGING: true, // process.env.NODE_ENV === 'production'
       __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
     })
   ]
 };
