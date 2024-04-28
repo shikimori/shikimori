3fdb88031e112945c9e502f215a3a2f9401c35cb
Andrey Sidorov <takandar@gmail.com>
2024-04-28 12:36:50 +0300

    new bugsnag account, ignore standard errors in bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,11 +1,11 @@
 if defined? Bugsnag
   Bugsnag.configure do |config|
-    config.api_key = '0c9ff54ce7f7b59d174cf722ae5c5d61'
+    config.api_key = '9e44228b59c1b3c3a6fdabb3466066da'
 
-    # config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
-    #
-    # Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
-    #   config.discard_classes << class_name
-    # end
+    config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
+    
+    Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
+      config.discard_classes << class_name
+    end
   end
 end
