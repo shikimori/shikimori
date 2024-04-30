cf9ab9ff6fde65965ee672b940153659f28626ec
Andrey Sidorov <takandar@gmail.com>
2024-04-30 15:56:39 +0300

    fix zeitwerk error

--- app/controllers/spnsrs_controller.rb
+++ app/controllers/spnsrs_controller.rb
@@ -1,7 +1,8 @@
-# class SpnsrsController < ShikimoriController
-#   layout false
-#   before_action { og noindex: true, nofollow: true }
-#    # contains xss https://shikimori.one/spnsrs/alert(%60xss1%60)?container_class=asd&width=alert(`xss2`)&height=alert(`xss2`)
-#   def show
-#   end
-# end
+class SpnsrsController < ShikimoriController
+  # layout false
+  # before_action { og noindex: true, nofollow: true }
+  #
+  # # contains xss https://shikimori.one/spnsrs/alert(%60xss1%60)?container_class=asd&width=alert(`xss2`)&height=alert(`xss2`)
+  # def show
+  # end
+end
