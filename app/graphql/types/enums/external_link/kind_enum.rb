87b4fbdcfff20cc62282572103968f67c368e067
Andrey Sidorov <takandar@gmail.com>
2024-05-14 19:52:34 +0300

    fix missing external link kinds

--- app/graphql/types/enums/external_link/kind_enum.rb
+++ app/graphql/types/enums/external_link/kind_enum.rb
@@ -1,9 +1,8 @@
 class Types::Enums::ExternalLink::KindEnum < GraphQL::Schema::Enum
   graphql_name 'ExternalLinkKindEnum'
 
-  (
-    Types::ExternalLink::Kind.values - Types::ExternalLink::WATCH_ONLINE_KINDS
-  ).each do |key|
+  # Types::ExternalLink::Kind.values - Types::ExternalLink::WATCH_ONLINE_KINDS
+  Types::ExternalLink::Kind.values.each do |key|
     value key, I18n.t("enumerize.external_link.kind.#{key}", locale: :en)
   end
 end
