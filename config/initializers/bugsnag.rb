9ebff5f9ba90968b1b3b975119ad4f0b740defb5
Andrey Sidorov <takandar@gmail.com>
2024-05-15 17:53:18 +0300

    use backend bugsnag api key constant on frontend

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,9 +1,11 @@
+BUGSNAG_API_KEY = '6af83d8b2e903c62bd6c559994cbbedf'
+
 if defined? Bugsnag
   Bugsnag.configure do |config|
-    config.api_key = '6af83d8b2e903c62bd6c559994cbbedf'
+    config.api_key = BUGSNAG_API_KEY
 
     config.discard_classes << lambda { |ex| ex.class.name.in? Shikimori::IGNORED_EXCEPTIONS }
-    
+
     Shikimori::IGNORED_EXCEPTIONS.each do |class_name|
       config.discard_classes << class_name
     end
