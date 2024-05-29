9ab4e4e665d0e183142c089d5659ec49547c4819
Andrey Sidorov <takandar@gmail.com>
2024-05-29 10:03:46 +0300

    new dirs to symlink during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,9 @@ set :linked_dirs, %w[
   log
   tmp/pids
   tmp/cache
+  tmp/cache/assets
+  tmp/cache/bootsnap-compile-cache
+  tmp/cache/shakapacker
   tmp/sockets
   public/assets
   public/system
