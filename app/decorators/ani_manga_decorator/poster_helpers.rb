778e26ade27d727390f1f344a68b7b81b1058250
Andrey Sidorov <takandar@gmail.com>
2024-06-08 12:51:40 +0300

    fix display of blurred posters

--- app/decorators/ani_manga_decorator/poster_helpers.rb
+++ app/decorators/ani_manga_decorator/poster_helpers.rb
@@ -1,10 +1,12 @@
 module AniMangaDecorator::PosterHelpers
-  def poster
+  def poster # rubocop:disable Metrics/CyclomaticComplexity
     return if rkn_banned? || rkn_banned_poster?
     # return if poster_disabled_by_genres?
 
     if poster_disabled_by_genres?
-      return super&.moderation_accepted? ? super : nil
+      return (super&.moderation_accepted? || super&.moderation_censored?) ?
+        super :
+        nil
     end
 
     super
