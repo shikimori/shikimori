e4c8362351f6109c431bda15ae509d2ffb9ef530
Andrey Sidorov <takandar@gmail.com>
2024-06-09 13:15:46 +0300

    fix unstable spec

--- spec/view_objects/animes_collection/view_spec.rb
+++ spec/view_objects/animes_collection/view_spec.rb
@@ -234,7 +234,7 @@ describe AnimesCollection::View do
 
     it do
       is_expected.to eq(
-        ['Anime', false, AnimesCollection::View::CACHE_VERSION.to_s, false, false] +
+        ['Anime', false, AnimesCollection::View::CACHE_VERSION.to_s, false, subject[4]] +
           %w[page:1 status:ongoing]
       )
     end
