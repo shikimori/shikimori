3d3ff3ac4de4f0e7c468268723b3f06b7f60e469
Andrey Sidorov <takandar@gmail.com>
2024-06-16 13:06:07 +0300

    fix broken migration

--- db/migrate/20240616095419_remove_mystery_and_police_achievements.rb
+++ db/migrate/20240616095419_remove_mystery_and_police_achievements.rb
@@ -1,9 +1,9 @@
 class RemoveMysteryAndPoliceAchievements < ActiveRecord::Migration[7.0]
   def up
-    Achievement.where(neko_id: %[mystery police]).delete_all
+    Achievement.where(neko_id: %i[mystery police]).delete_all
   end
 
   def down
-    Achievement.where(neko_id: %[detektiv]).delete_all
+    Achievement.where(neko_id: %i[detektiv]).delete_all
   end
 end
