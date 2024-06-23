2a2c5930347ed32651b0d3334899c57925d9fbe6
Andrey Sidorov <takandar@gmail.com>
2024-06-23 20:21:16 +0300

    updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # config.active_support.deprecation = :log
+  config.active_support.deprecation = :raise
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
