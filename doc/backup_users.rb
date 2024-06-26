83eb630d7c31da26e9beb58b92789d4a1c099f80
Andrey Sidorov <takandar@gmail.com>
2024-06-26 20:16:31 +0300

    scripts to backup/restore users

---
+++ doc/backup_users.rb
@@ -0,0 +1,70 @@
+# rubocop:disable Layout/LineLength
+ids = [8, 419948, 1028805, 1105696, 1118835, 1172315, 1227283, 1383110, 1421200, 1422272, 1448249, 1469066, 1469728] # rubocop:disable Style/NumericLiterals
+
+File.open('/tmp/z.json', 'w') do |f|
+  data = User.where(id: ids).each_with_object([]) do |user, memo|
+    raise 'user.critiques' if user.critiques.any?
+    raise 'user.reviews' if user.reviews.any?
+    raise 'user.articles' if user.articles.any?
+
+    # raise 'user.collections' if user.collections.any?
+
+    memo.push({
+      user: user.attributes,
+      style: user.style,
+      user_preferences: user.preferences,
+      oauth_applications: user.oauth_applications,
+      access_grants: user.access_grants,
+      abuse_requests: user.abuse_requests,
+      user_tokens: user.user_tokens,
+      achievements: user.achievements,
+      anime_rates: user.anime_rates,
+      manga_rates: user.manga_rates,
+      user_rate_logs: user.user_rate_logs,
+      history: user.history,
+      topic_viewings: user.topic_viewings,
+      comment_viewings: user.comment_viewings,
+      friend_links: user.friend_links,
+      favourites: user.favourites,
+      messages: user.messages,
+      ignores: user.ignores,
+      collections: user.collections.map do |collection|
+        {
+          collection:,
+          links: collection.links,
+          topics: [collection.topic],
+          topics_comments: collection.topic.comments
+        }
+      end,
+      collection_roles: user.collection_roles,
+      versions: user.versions,
+      topic_ignores: user.topic_ignores,
+      nickname_changes: user.nickname_changes,
+      bans: user.bans,
+      polls: user.polls,
+      'acts_as_votable/vote': (user.polls.flat_map(&:votes_for) + ActsAsVotable::Vote.where(voter: user.id)).uniq,
+      profile_comments: Comment.where(commentable: user),
+      user_images: user.user_images,
+      clubs_owned: user.clubs_owned.map do |club|
+        {
+          club:,
+          style: club.style,
+          member_roles: user.clubs_owned.flat_map(&:member_roles),
+          pages: user.clubs_owned.flat_map(&:pages),
+          pages_topics: user.clubs_owned.flat_map(&:pages).map(&:topic),
+          pages_topics_comments: user.clubs_owned.flat_map(&:pages).map(&:topic).flat_map(&:comments),
+          links: user.clubs_owned.flat_map(&:links),
+          bans: user.clubs_owned.flat_map(&:bans),
+          topics: user.clubs_owned.flat_map(&:all_topics),
+          topics_comments: user.clubs_owned.flat_map(&:all_topics).flat_map(&:comments)
+        }
+      end,
+      topics: user.topics,
+      comments: user.comments_all
+    })
+  end
+
+  f.write(data.to_json)
+end
+# rubocop:enable Layout/LineLength
+`scp /tmp/z.json shiki:/tmp/`
---
+++ doc/restore_users.rb
@@ -0,0 +1,125 @@
+json = JSON.parse(File.open('/tmp/z.json', 'r').read, symbolize_names: true);
+
+User.transaction do
+  ap "Users"
+  json.each do |data|
+    User.import([User.new(data[:user])], validate: false)
+
+    user = User.find data[:user][:id]
+    puts "User##{user.id}"
+    user.style = Style.new(data[:style])
+    user.preferences = UserPreferences.new(data[:user_preferences])
+    user.save!
+  end
+
+  ap "Clubs"
+  json.each do |data|
+    data[:clubs_owned].each do |club_data|
+      Club.import([Club.new(club_data[:club])], validate: false)
+      club = Club.find club_data[:club][:id]
+      puts "Club##{club.id}"
+      club.style = Style.new(club_data[:style])
+      club.save!
+
+      {
+        member_roles: ClubRole,
+        pages: ClubPage,
+        pages_topics: Topic,
+        pages_topics_comments: Comment,
+        links: ClubLink,
+        bans: ClubBan,
+        topics: Topic,
+        topics_comments: Comment
+      }.each do |key, klass|
+        ap key
+        result = klass.import(club_data[key].map { |v| klass.new v }, on_duplicate_key_ignore: true, validate: false)
+        if result.failed_instances.any?
+          ap result
+          1/0
+        end
+      end
+    end
+  end
+
+  ap "Collections"
+  json.each do |data|
+    data[:collections].each do |collection_data|
+      Collection.import([Collection.new(collection_data[:collection])], validate: false)
+      collection = Collection.find collection_data[:collection][:id]
+      puts "Collection##{collection.id}"
+
+      {
+        links: CollectionLink,
+        topics: Topic,
+        topics_comments: Comment
+      }.each do |key, klass|
+        ap key
+        result = klass.import(collection_data[key].map { |v| klass.new v }, on_duplicate_key_ignore: true, validate: false)
+        if result.failed_instances.any?
+          ap result
+          1/0
+        end
+      end
+    end
+  end
+
+  json.each do |data|
+    {
+      topics: Topic,
+      comments: Comment
+    }.each do |key, klass|
+      result = klass.import(data[key].map { |v| klass.new v }, on_duplicate_key_ignore: true, validate: false)
+      if result.failed_instances.any?
+        ap result
+        1/0
+      end
+    end
+  end
+
+  ap "Relations"
+  json.each do |data|
+    {
+      oauth_applications: OauthApplication,
+      access_grants: Doorkeeper::AccessGrant,
+      user_tokens: UserToken,
+      achievements: Achievement,
+      anime_rates: UserRate,
+      manga_rates: UserRate,
+      user_rate_logs: UserRateLog,
+      history: UserHistory,
+      favourites: Favourite,
+      ignores: Ignore,
+      topic_ignores: TopicIgnore,
+      nickname_changes: UserNicknameChange,
+      polls: Poll,
+      profile_comments: Comment,
+      user_images: UserImage,
+      messages: Message,
+      versions: Version,
+      collection_roles: CollectionRole,
+      topic_viewings: TopicViewing,
+      comment_viewings: CommentViewing,
+      abuse_requests: AbuseRequest,
+      friend_links: FriendLink
+    }.each do |key, klass|
+      ap key
+      result = klass.import(data[key].map { |v| klass.new v }, on_duplicate_key_ignore: true, validate: false)
+      if result.failed_instances.any?
+        ap result
+        1/0
+      end
+    end
+
+    ActsAsVotable::Vote.import(data[:'acts_as_votable/vote'].map { |v| ActsAsVotable::Vote.new v }, on_duplicate_key_ignore: true)
+  end
+
+  json.each do |data|
+    data[:bans].each do |ban_data|
+      ban = Ban.new({ **ban_data, duration: ban_data[:duration][:value] })
+      ban.abuse_request_id = nil if ban.abuse_request.nil?
+      ban.save!
+    end
+  end
+
+  ap "Done"
+end;
