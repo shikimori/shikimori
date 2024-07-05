ca46564c3378f6054100fb19b633c740d5ad9ccf
Andrey Sidorov <takandar@gmail.com>
2024-07-05 11:02:05 +0300

    turbolinks history fix

--- app/packs/javascripts/application.js
+++ app/packs/javascripts/application.js
@@ -110,6 +110,8 @@ $(document).on(Object.keys(bindings).join(' '), e => {
       group.callback();
     }
   });
+
+  turbolinksHistoryFix();
 });
 
 $(document).on('selectionchange', _ => {
@@ -128,3 +130,34 @@ $(document).on('selectionchange', _ => {
       ?.setSelection();
   }
 });
+
+function turbolinksHistoryFix() {
+  window.addEventListener('popstate', function(event) {
+    this.turbolinks_location = Turbolinks.Location.wrap(window.location);
+    if (
+      Turbolinks.controller.location.requestURL ===
+      this.turbolinks_location.requestURL
+    ) {
+      return;
+    }
+    if (event.state != null ? event.state.turbolinks : undefined) {
+      return;
+    }
+    if (
+      (this.window_turbolinks =
+        window.history.state != null ?
+          window.history.state.turbolinks :
+          undefined)
+    ) {
+      return Turbolinks.controller.historyPoppedToLocationWithRestorationIdentifier(
+        this.turbolinks_location,
+        this.window_turbolinks.restorationIdentifier
+      );
+    } else {
+      return Turbolinks.controller.historyPoppedToLocationWithRestorationIdentifier(
+        this.turbolinks_location,
+        Turbolinks.uuid()
+      );
+    }
+  });
+}
