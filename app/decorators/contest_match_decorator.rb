dd4bc9b0a95d4d29feaf0879ecf3168f0d23fdcb
Andrey Sidorov <takandar@gmail.com>
2024-08-18 16:54:42 +0300

    updates to match-day display for freezed contest_match

--- app/decorators/contest_match_decorator.rb
+++ app/decorators/contest_match_decorator.rb
@@ -36,6 +36,8 @@ class ContestMatchDecorator < BaseDecorator
       :created
     elsif started?
       :started
+    elsif freezed?
+      :freezed
     elsif winner_id == member_id
       :winner
     else
