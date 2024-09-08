015e78c26ad81cee5cecd472cb2759f63c9eacf7
Andrey Sidorov <takandar@gmail.com>
2024-09-08 21:41:42 +0300

    Revert "nyaa.shikimori.one"
    This reverts commit ea7db8d359b4d17e570ec3eb2091ecd3d5d0a8aa.

--- config/application.rb
+++ config/application.rb
@@ -53,8 +53,7 @@ module Shikimori
   NAME_RU = 'Шикимори'
   NAME_EN = 'Shikimori'
 
-  # STATIC_SUBDOMAINS = %w[desu]
-  STATIC_SUBDOMAINS = %w[nyaa]
+  STATIC_SUBDOMAINS = %w[desu]
   # STATIC_SUBDOMAINS = %w[nyaa kawai moe desu dere]
   EMAIL = 'admin@shikimori.me'
   # EMAIL_DATA_DELETION = 'mail+data_deletion@shikimori.org'
