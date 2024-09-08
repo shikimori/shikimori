e7da4d8713223146dd4147b6b7c62268b69ae7bd
Andrey Sidorov <takandar@gmail.com>
2024-09-08 09:23:13 +0300

    updates to env production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,13 @@ Rails.application.configure do
     }
   end
 
-  config.log_level = :fatal
-
   # Log to STDOUT by default
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     config.logger = ActiveSupport::Logger.new(STDOUT)
       .tap  { |logger| logger.formatter = Logger::Formatter.new }
       .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
+    ActiveRecord::Base.logger = Rails.logger
   end
 
   # Prepend all log lines with the following tags.
@@ -78,7 +78,7 @@ Rails.application.configure do
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
-  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'fatal')
 
   # Use a different cache store in production.
   config.cache_store = :mem_cache_store, 'localhost', {
