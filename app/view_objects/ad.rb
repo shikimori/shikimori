b48cf3d61593b032c99393bf043371856c3fcc2b
Andrey Sidorov <takandar@gmail.com>
2024-10-09 15:30:28 +0300

    remove funpay ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -436,13 +436,13 @@ private
     #   # Types::Ad::Type[:advrtr_240x400]
     # ],
     Types::Ad::Meta[:menu_240x400] => [
-      Types::Ad::Type[:special_x300], # funpay
+      # Types::Ad::Type[:special_x300], # funpay
       # # Types::Ad::Type[:mt_240x400],
       Types::Ad::Type[:yd_240x400]
       # # Types::Ad::Type[:advrtr_240x400]
     ],
     Types::Ad::Meta[:menu_300x600] => [
-      Types::Ad::Type[:special_x300], # funpay
+      # Types::Ad::Type[:special_x300], # funpay
       # # Types::Ad::Type[:mt_300x600],
       Types::Ad::Type[:yd_300x600]
       # # Types::Ad::Type[:advrtr_240x400],
@@ -462,7 +462,7 @@ private
       # Types::Ad::Type[:mt_footer_300x250]
     ],
     Types::Ad::Meta[:special_x1170] => [
-      Types::Ad::Type[:special_x1170] # funpay
+      # Types::Ad::Type[:special_x1170] # funpay
       # # Types::Ad::Type[:yd_1170x200]
     ]
   }
