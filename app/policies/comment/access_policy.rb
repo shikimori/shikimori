9e60fb92bcf8781ccd57be4ef6becb216b39c40e
Andrey Sidorov <takandar@gmail.com>
2024-11-07 20:25:29 +0300

    use Commentable::AccessPolicy in Comment::AccessPolicy

--- app/policies/comment/access_policy.rb
+++ app/policies/comment/access_policy.rb
@@ -1,34 +1,15 @@
 class Comment::AccessPolicy
   static_facade :allowed?, :comment, :current_user
 
-  # CENSORED_CLUB_COMMENT_EXPIRATION_INTERVAL = 6.months
-
   def allowed?
-    commentable = @comment.commentable
-    return true if own_comment? || moderator?
+    return true if own_comment?
 
-    case commentable
-      when Topic
-        Topic::AccessPolicy.allowed? commentable, @current_user
-
-      when User
-        profile_access? commentable
-    end
+    Commentable::AccessPolicy.allowed? @comment.commentable, @current_user
   end
 
 private
 
   def own_comment?
-    @current_user && @comment.user_id == @current_user.id
-  end
-
-  def profile_access? user
-    !!(
-      user&.preferences&.comments_in_profile? && !user.censored_profile?
-    )
-  end
-
-  def moderator?
-    @current_user&.moderation_staff?
+    @comment.user_id == @current_user&.id
   end
 end
--- spec/policies/comment/access_policy_spec.rb
+++ spec/policies/comment/access_policy_spec.rb
@@ -1,76 +1,26 @@
 describe Comment::AccessPolicy do
-  subject { described_class.allowed? comment, decorated_user }
+  subject { described_class.allowed? comment, current_user }
 
   let(:comment) do
     build_stubbed :comment,
       user: comment_user,
       commentable: commentable
   end
-  let(:decorated_user) { [user.decorate, nil].sample }
+  let(:current_user) { [nil, user, user.decorate].sample }
   let(:comment_user) { user_2 }
   before do
-    allow(Topic::AccessPolicy)
+    allow(Commentable::AccessPolicy)
       .to receive(:allowed?)
-      .with(commentable, decorated_user)
+      .with(commentable, current_user)
       .and_return is_allowed
   end
   let(:commentable) { build_stubbed :topic }
   let(:is_allowed) { [true, false].sample }
 
-  describe 'user' do
-    context 'guest' do
-      let(:decorated_user) { nil }
-      it { is_expected.to eq is_allowed }
-    end
+  it { is_expected.to eq is_allowed }
 
-    context 'moderator' do
-      let(:comment_user) { user }
-      let(:decorated_user) { user.decorate }
-      before { allow(decorated_user).to receive(:moderation_staff?).and_return true }
-
-      it { is_expected.to eq true }
-    end
-
-    context 'user' do
-      let(:decorated_user) { nil }
-
-      context "user's own comment" do
-        let(:comment_user) { user }
-        let(:decorated_user) { user.decorate }
-        let(:is_allowed) { false }
-
-        it { is_expected.to eq true }
-      end
-
-      context "other user's comment" do
-        describe 'commentable' do
-          context 'user' do
-            let(:commentable) { user_2 }
-
-            context 'common user' do
-              it { is_expected.to eq true }
-            end
-
-            context 'user with disabled profile comments' do
-              let(:commentable) do
-                build_stubbed :user,
-                  preferences: build_stubbed(:user_preferences, comments_in_profile: false)
-              end
-              it { is_expected.to eq false }
-            end
-
-            context 'user with censored_profile' do
-              let(:commentable) { build_stubbed :user, roles: %i[censored_profile] }
-              it { is_expected.to eq false }
-            end
-          end
-
-          context 'topic' do
-            let(:commentable) { build_stubbed :topic }
-            it { is_expected.to eq is_allowed }
-          end
-        end
-      end
-    end
+  context 'own comment' do
+    let(:current_user) { comment_user }
+    it { is_expected.to eq true }
   end
 end
