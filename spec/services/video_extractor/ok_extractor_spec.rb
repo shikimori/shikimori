1ccc3ee0126a80f3e8f0442aa92f2379d9e820ad
Andrey Sidorov <takandar@gmail.com>
2024-11-12 19:54:21 +0300

    fix broken specs

--- spec/services/video_extractor/ok_extractor_spec.rb
+++ spec/services/video_extractor/ok_extractor_spec.rb
@@ -8,24 +8,24 @@ describe VideoExtractor::OkExtractor, :vcr do
       let(:url) { 'https://ok.ru/live/815923404420' }
 
       its(:hosting) { is_expected.to eq :ok }
-      its(:image_url) { is_expected.to eq '//pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
-      its(:player_url) { is_expected.to eq '//ok.ru/videoembed/815923404420' }
+      its(:image_url) { is_expected.to eq 'http://pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
+      its(:player_url) { is_expected.to eq 'http://ok.ru/videoembed/815923404420' }
     end
 
     context '/videoembed/' do
       let(:url) { 'https://ok.ru/videoembed/815923404420' }
 
       its(:hosting) { is_expected.to eq :ok }
-      its(:image_url) { is_expected.to eq '//pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
-      its(:player_url) { is_expected.to eq '//ok.ru/videoembed/815923404420' }
+      its(:image_url) { is_expected.to eq 'http://pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
+      its(:player_url) { is_expected.to eq 'http://ok.ru/videoembed/815923404420' }
     end
 
     context '/video/' do
       let(:url) { 'https://ok.ru/video/815923404420' }
 
       its(:hosting) { is_expected.to eq :ok }
-      its(:image_url) { is_expected.to eq '//pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
-      its(:player_url) { is_expected.to eq '//ok.ru/videoembed/815923404420' }
+      its(:image_url) { is_expected.to eq 'http://pimg.mycdn.me/getImage?disableStub=true&type=VIDEO_S_720&url=http%3A%2F%2Fvdp.mycdn.me%2FgetImage%3Fid%3D381639461508%26idx%3D0%26thumbType%3D37%26f%3D1&signatureToken=TZAM-cHk_kQis5JdW_-ncg' }
+      its(:player_url) { is_expected.to eq 'http://ok.ru/videoembed/815923404420' }
     end
   end
 end
