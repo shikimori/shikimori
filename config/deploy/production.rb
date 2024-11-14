8763c4ff419a61c1968df974571004366a6acffe
Andrey Sidorov <takandar@gmail.com>
2024-11-14 12:19:10 +0300

    use ssh host instead of ip address in capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :deploy_to, "/home/apps/#{fetch :application}/#{fetch :stage}"
 
-server '135.181.210.175', user: fetch(:user), roles: %w{app web db}
+server 'shiki', user: fetch(:user), roles: %w{app web db}
