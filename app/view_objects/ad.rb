bc53d94eef257cc02e47863d93b75599f644a5a2
Andrey Sidorov <takandar@gmail.com>
2024-11-19 17:56:50 +0300

    stop saga ad

--- app/view_objects/ad.rb
+++ app/view_objects/ad.rb
@@ -425,13 +425,13 @@ private
     #   # Types::Ad::Type[:advrtr_240x400]
     # ],
     Types::Ad::Meta[:menu_240x400] => [
-      Types::Ad::Type[:special_x300], # saga
+      # Types::Ad::Type[:special_x300], # saga
       # # Types::Ad::Type[:mt_240x400],
       Types::Ad::Type[:yd_240x400]
       # # Types::Ad::Type[:advrtr_240x400]
     ],
     Types::Ad::Meta[:menu_300x600] => [
-      Types::Ad::Type[:special_x300], # saga
+      # Types::Ad::Type[:special_x300], # saga
       # # Types::Ad::Type[:mt_300x600],
       Types::Ad::Type[:yd_300x600]
       # # Types::Ad::Type[:advrtr_240x400],
@@ -451,7 +451,7 @@ private
       # Types::Ad::Type[:mt_footer_300x250]
     ],
     Types::Ad::Meta[:special_x1170] => [
-      Types::Ad::Type[:special_x1170] # saga
+      # Types::Ad::Type[:special_x1170] # saga
       # # Types::Ad::Type[:yd_1170x200]
     ]
   }
