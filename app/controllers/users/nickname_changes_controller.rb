d7e1375d55864fe5c04aa07b4c4542e20d4d68a2
Andrey Sidorov <takandar@gmail.com>
2025-01-05 18:56:16 +0300

    add limit to query

--- app/controllers/users/nickname_changes_controller.rb
+++ app/controllers/users/nickname_changes_controller.rb
@@ -1,11 +1,16 @@
 class Users::NicknameChangesController < ProfilesController
+  PER_PAGE = 100
+
   before_action do
     @back_url = profile_url @user
   end
 
   def index
     og noindex: true, nofollow: true
-    @collection = Users::NicknameChangesQuery.call(@user, can?(:manage, Ban))
+    @collection = Users::NicknameChangesQuery
+      .call(@user, can?(:manage, Ban))
+      .take(PER_PAGE)
+
     render formats: :html
   end
 end
